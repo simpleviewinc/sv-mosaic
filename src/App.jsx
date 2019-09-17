88f9d265da56642c49928c2b9eee49e9b61b12ee
Owen Allen <owenallenaz@gmail.com>
2019-09-16 18:08:43 -0700

    Working state

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { hot } from "react-hot-loader/root";
+import Grid from "./components/Grid.jsx";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Components</h1>
+			<div>Grid</div>
+		</div>
+	);
+}
+
+export default hot(App);
---
+++ src/index.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "@hot-loader/react-dom";
+import App from "./App.jsx";
+ReactDOM.render(<App />, document.getElementById("root"));
