6b8178ff9d2ed061e4ae31ae0cf80c858998b2d8
Owen Allen <owenallenaz@gmail.com>
2019-10-29 10:14:23 -0700

    Refactors the gridColumnDrawer with the new drawer semantics

--- src/components/internal/GridColumnDrawer.jsx
+++ src/components/internal/GridColumnDrawer.jsx
@@ -1,163 +1,20 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React from "react";
 
-import CloseIcon from '@material-ui/icons/Close';
-import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
-import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
-
-import ButtonRow from "../ButtonRow.jsx";
-import Button from "../Button.jsx";
 import Drawer from "../Drawer.jsx";
-import CheckboxList from "../CheckboxList.jsx";
-import theme from "../../utils/theme.js";
-
-const DrawerContent = styled.div`
-	display: flex;
-	width: 700px;
-	
-	& > div > h2 {
-		${theme.h2}
-		padding-left: 15px;
-		margin: 0 0 1rem 0;
-	}
-	
-	& > .left {
-		flex: 1;
-		padding-right: 20px;
-		width: 200px;
-	}
-	
-	& > .left .listItem {
-		margin: 3px 0px;
-		background: white;
-	}
-	
-	& > .right {
-		flex: 1;
-		width: 200px;
-	}
-	
-	& > .right > .item {
-		background: white;
-		line-height: 42px;
-		margin: 3px 0px;
-		display: flex;
-		justify-content: space-between;
-		padding-left: 15px;
-	}
-	
-	& > .right > .item > .buttons {
-		text-align: right;
-	}
-`
+import GridColumnDrawerContent from "./GridColumnDrawerContent.jsx";
 
 function GridColumnDrawer(props) {
-	const [state, setState] = useState({
-		activeColumns : props.columns.map(val => val.name)
-	});
-	
-	const saveColumns = function() {
-		props.onClose();
-		props.onChange(state.activeColumns);
-	}
-	
-	const onColumnsChange = function(data) {
-		setState({
-			...state,
-			activeColumns : data
-		});
-	}
-	
-	const orderClick = (name, num) => () => {
-		// changing the order is actually just swapping one element for another
-		// so we find the elements current position and it's new position and swap the two names
-		const index = state.activeColumns.indexOf(name);
-		const newIndex = index + num;
-		const swapValue = state.activeColumns[newIndex];
-		
-		state.activeColumns[index] = swapValue;
-		state.activeColumns[newIndex] = name;
-		
-		setState({
-			...state,
-			activeColumns : [...state.activeColumns]
-		});
-	}
-	
-	const columnOptions = props.allColumns.map(column => {
-		return {
-			label : column.label,
-			value : column.name
-		}
-	}).sort((a, b) => {
-		return a.label.localeCompare(b.label)
-	});
-	
 	return (
 		<Drawer
-			title="Table Settings"
 			open={props.open}
 			onClose={props.onClose}
-			background="gray"
-			buttons={
-				<ButtonRow>
-					<Button
-						label="cancel"
-						color="gray"
-						variant="text"
-						onClick={props.onClose}
-					/>
-					<Button
-						label="Apply"
-						color="blue"
-						variant="contained"
-						onClick={saveColumns}
-					/>
-				</ButtonRow>
-			}
 		>
-			<DrawerContent>
-				<div className="left">
-					<h2>Columns</h2>
-					<CheckboxList
-						options={columnOptions}
-						checked={state.activeColumns}
-						onChange={onColumnsChange}
-					/>
-				</div>
-				<div className="right">
-					<h2>Column Order</h2>
-					{
-						state.activeColumns.map((name, i) => {
-							const column = props.allColumns.find(val => val.name === name);
-							
-							return (
-								<div className="item" key={column.name}>
-									<span>
-										{column.label}
-									</span>
-									<span className="buttons">
-										<Button
-											mIcon={ArrowDownwardIcon}
-											variant="icon"
-											color="black"
-											disabled={i === state.activeColumns.length - 1}
-											onClick={orderClick(column.name, 1)}
-										/>
-										<Button
-											mIcon={ArrowUpwardIcon}
-											variant="icon"
-											color="black"
-											disabled={i === 0}
-											onClick={orderClick(column.name, -1)}
-										/>
-									</span>
-								</div>
-							)
-						})
-					}
-				</div>
-			</DrawerContent>
+			<GridColumnDrawerContent
+				columns={props.columns}
+				allColumns={props.allColumns}
+				onChange={props.onChange}
+				onClose={props.onClose}
+			/>
 		</Drawer>
 	)
 }
