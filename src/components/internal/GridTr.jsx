0a2a4eecc4ca9c1ed341d23d4b69f20b85f828e1
Owen Allen <owenallenaz@gmail.com>
2019-11-01 16:46:13 -0700

    Splits TBody and Tr into separate components for better caching capability.

---
+++ src/components/internal/GridTr.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+
+import Checkbox from "../Checkbox.jsx";
+import GridTd from "./GridTd.jsx";
+import ButtonRow from "../ButtonRow.jsx";
+import Button from "../Button.jsx";
+import { transformColumn } from "../../utils/gridTools.js";
+
+function GridTr(props) {
+	const onActionClick = (action) => () => {
+		props.onActionClick(action, props.row);
+	}
+	
+	return (
+		<tr>
+			{
+				props.bulkActions &&
+				<GridTd key="_bulk">
+					<Checkbox
+						checked={props.checked === true}
+						onClick={props.onCheckboxClick}
+					/>
+				</GridTd>
+			}
+			{
+				props.columns.map(column => {
+					const data = transformColumn(props.row, column);
+					
+					return (
+						<GridTd
+							key={column.name}
+							className={column.style === "bold" ? "bold" : undefined}
+						>
+							{data}
+						</GridTd>
+					);
+				})
+			}
+			<GridTd>
+				<ButtonRow>
+					{
+						props.primaryActions && 
+						props.primaryActions.map((action, i) => {
+							return (
+								<Button
+									key={`primary_${i}`}
+									{ ...action }
+									onClick={onActionClick(action)}
+								/>
+							)
+						})
+					}
+					{
+						props.additionalActions &&
+						<Button
+							key="additional"
+							color="blue"
+							variant="icon"
+							mIcon={MoreHorizIcon}
+							menuItems={props.additionalActions.map(action => {
+								return {
+									...action,
+									onClick : onActionClick(action)
+								}
+							})}
+						/>
+					}
+				</ButtonRow>
+			</GridTd>
+		</tr>
+	);
+}
+
+export default GridTr;
