799bf1c48132ce6c3b7234101dd10e629b636d1e
Owen Allen <owenallenaz@gmail.com>
2019-11-11 16:19:37 -0700

    Removes dead import.

--- src/components/CheckboxList.jsx
+++ src/components/CheckboxList.jsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
 import { xor } from "lodash";
 import jsvalidator from "jsvalidator";
 
