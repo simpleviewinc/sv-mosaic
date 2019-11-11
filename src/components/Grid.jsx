68087ead30ea5bc3e78df6d1204cd99d7daece76
Owen Allen <owenallenaz@gmail.com>
2019-11-11 16:35:59 -0700

    Unused callback.

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useMemo } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import jsvalidator from "jsvalidator";
 import styled from "styled-components";
 
