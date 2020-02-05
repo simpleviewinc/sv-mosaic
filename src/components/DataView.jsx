bd3786cf9d500c80e29ecd020665c4c7a320d239
Owen Allen <owenallenaz@gmail.com>
2020-02-05 13:11:31 -0700

    Removes unneeded changes.

--- src/components/DataView.jsx
+++ src/components/DataView.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo, useRef } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import jsvalidator from "jsvalidator";
 import styled from "styled-components";
 
