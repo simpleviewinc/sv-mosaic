5d8690d15064d416a7f7b687b2aa7e34b5736fe2
Owen Allen <owenallenaz@gmail.com>
2020-02-20 15:52:51 -0700

    FilterSingleSelect - Adds storybook entries.

---
+++ src/components/FilterSingleSelect/FilterSingleSelect.stories.tsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+import { useState } from "react";
+import FilterSingleSelect from "./FilterSingleSelect";
+
+export default {
+	title : "Components|FilterSingleSelect"
+}
+
+const options = [
+	{ label : "Option A", value : "a" },
+	{ label : "Option B", value : "b" },
+	{ label : "Option C", value : "c" },
+	{ label : "Option D", value : "d" }
+]
+
+export const example = () => {
+	const [state, setState] = useState({
+		value : undefined
+	});
+
+	const onChange = function(data) {
+		setState(data);
+	}
+
+	const onRemove = function() {
+
+	}
+
+	const getOptions = function() {
+		return {
+			docs : options,
+			hasMore : false
+		}
+	}
+
+	const getSelected = function(id) {
+		return options.filter(val => val.value === id)[0];
+	}
+	
+	return (
+		<FilterSingleSelect
+			label="Testing"
+			type="primary"
+			data={state}
+			args={{ getOptions, getSelected }}
+			onRemove={onRemove}
+			onChange={onChange}
+		/>
+	)
+}
