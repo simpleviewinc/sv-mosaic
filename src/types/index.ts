13e2c0ba3009467186e19dfb0f864f9f917e0afa
Owen Allen <owenallenaz@gmail.com>
2020-02-28 16:17:03 -0700

    Better transform documentation and interfaces.

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,8 @@ export interface MosaicLabelValue {
 	label: string
 	value: string
 }
+
+/** Javascript object than can have any keys and any data */
+export interface MosaicObject {
+	[key: string]: any
+}
