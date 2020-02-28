d6d74e6a5f854b3f4c885888af55b08733e2b66b
Owen Allen <owenallenaz@gmail.com>
2020-02-28 15:25:01 -0700

    Breaking - Converts the column transforms to get function({ data, row }) instead of function(data) to allow the ability to utilize multi-column transformations.

--- src/transforms/column_transforms.ts
+++ src/transforms/column_transforms.ts
@@ -5,8 +5,8 @@ import { createElement } from "react";
 import Image from "../components/internal/Image";
 
 export function transform_boolean() {
-	return function(bool: boolean): string {
-		if (bool === true) {
+	return function({ data }: { data: boolean }): string {
+		if (data === true) {
 			return "Yes";
 		} else {
 			return "No";
@@ -15,19 +15,19 @@ export function transform_boolean() {
 }
 
 export function transform_dateFormat() {
-	return function(dateStr: Date): string {
-		return format(dateStr, "M/d/yyyy");
+	return function({ data }: { data: Date }): string {
+		return format(data, "M/d/yyyy");
 	}
 }
 
 export function transform_get(path) {
-	return function(data) {
+	return function({ data }) {
 		return get(data, path);
 	}
 }
 
 export function transform_mapGet(path) {
-	return (data: object[]) => {
+	return ({ data }: { data: object[] }) => {
 		const results = map(data, (obj) => {
 			return get(obj, path);
 		});
@@ -38,7 +38,7 @@ export function transform_mapGet(path) {
 }
 
 export function transform_join() {
-	return (data: string[]) => {
+	return ({ data }: { data: string[] }) => {
 		return data.join(", ");
 	}
 }
@@ -49,8 +49,8 @@ interface TransformThumbnailProps {
 }
 
 export function transform_thumbnail({ width, height }: TransformThumbnailProps) {
-	return function(url: string) {
-		const newUrl = url.replace(/\/upload\//, `/upload/c_fill,h_${height},w_${width}/`);
+	return function({ data }: { data: string }) {
+		const newUrl = data.replace(/\/upload\//, `/upload/c_fill,h_${height},w_${width}/`);
 		
 		const element = createElement(Image, {
 			src : newUrl,
