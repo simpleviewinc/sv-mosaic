7be74895d5d76486275fbcca3a2846197a65bec0
Owen Allen <owenallenaz@gmail.com>
2020-03-06 12:50:41 -0700

    New build.

---
+++ dist/types/theme/theme.d.ts
@@ -0,0 +1,36 @@
+declare const _default: {
+    h1: string;
+    h2: string;
+    iconFontSize: string;
+    fontWeight: {
+        normal: number;
+        semiBold: number;
+        bold: number;
+    };
+    colors: {
+        black: string;
+        blue: string;
+        blueHover: string;
+        blueActive: string;
+        red: string;
+        redHover: string;
+        redActive: string;
+        gray100: string;
+        gray200: string;
+        gray300: string;
+        gray400: string;
+        gray500: string;
+        gray600: string;
+        gray700: string;
+        gray800: string;
+    };
+    borders: {
+        lightGray: string;
+        gray: string;
+    };
+    fontFamily: string;
+    animations: {
+        backgroundMs: string;
+    };
+};
+export default _default;
