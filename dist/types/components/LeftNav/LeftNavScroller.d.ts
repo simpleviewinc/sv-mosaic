9bf55fa0af1f2c8124291e422b2d37b69076c90c
Owen Allen <owenallenaz@gmail.com>
2020-06-02 14:48:12 -0700

    New build

--- dist/types/components/LeftNav/LeftNavScroller.d.ts
+++ dist/types/components/LeftNav/LeftNavScroller.d.ts
@@ -1,7 +1,9 @@
 import * as React from "react";
+import { MosaicObject } from "../../types";
 interface Props {
     className?: string;
     children?: React.ReactNode;
+    attrs?: MosaicObject;
 }
 declare function LeftNavScroller(props: Props): JSX.Element;
 export default LeftNavScroller;
