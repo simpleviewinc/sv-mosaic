8df99017af0b4bbacb13853d1ae93e779f7dc866
Owen Allen <owenallenaz@gmail.com>
2020-06-23 10:18:57 -0700

    Adds Typography.P element.

--- src/components/Typography/Typography.stories.tsx
+++ src/components/Typography/Typography.stories.tsx
@@ -3,7 +3,8 @@ import * as React from "react";
 import {
 	H1 as H1Elm,
 	H3 as H3Elm,
-	BodyText as BodyTextElm
+	BodyText as BodyTextElm,
+	P as PElm
 } from "./";
 
 export default {
@@ -14,7 +15,7 @@ export function H1() {
 	return (
 		<React.Fragment>
 			<H1Elm>H1 - lorem ipsum dolor</H1Elm>
-			<BodyTextElm as="p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</BodyTextElm>
+			<PElm>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</PElm>
 		</React.Fragment>
 	)
 }
@@ -23,7 +24,7 @@ export function H3() {
 	return (
 		<React.Fragment>
 			<H3Elm>H3 - lorem ipsum dolor</H3Elm>
-			<BodyTextElm as="p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</BodyTextElm>
+			<PElm>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</PElm>
 		</React.Fragment>
 	)
 }
@@ -31,7 +32,15 @@ export function H3() {
 export function BodyText() {
 	return (
 		<React.Fragment>
-			<BodyTextElm as="p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</BodyTextElm>
+			<div>
+				<BodyTextElm>Lorem</BodyTextElm> <BodyTextElm>ipsum</BodyTextElm>
+			</div>
 		</React.Fragment>
+	)
+}
+
+export function P() {
+	return (
+		<PElm>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</PElm>
 	)
 }
