52744448fdbd62524cccd75d20724d7b3e148637
Owen Allen <owenallenaz@gmail.com>
2020-07-03 11:58:55 -0700

    Exposes addResourceBundle to make it easier for developers to split i18n into components. Adds resource bundling info to the docs.

--- src/i18n/internal.ts
+++ src/i18n/internal.ts
@@ -3,20 +3,27 @@ import { default as i18next } from "i18next";
 import { MosaicObject } from "../types";
 import * as common from "./common.json";
 
-interface AddCoreResourceBundleProps {
+interface AddResourceBundleProps {
+	namespace: string
 	prefix: string
 	bundle: {
 		[lang: string]: MosaicObject
 	}
 }
 
+type AddCoreResourceBundle = Omit<AddResourceBundleProps, "namespace">;
+
+export function addResourceBundle({ prefix, bundle, namespace }: AddResourceBundleProps) {
+	for(let [lang, data] of Object.entries(bundle)) {
+		defaulti18n.addResourceBundle(lang, namespace, { [prefix] : data }, true, false);
+	}
+}
+
 /**
  * Adds a core resource bundle exposed when using the default i18n or when using the useMosaicSettings() context variant.
  */
-export function addCoreResourceBundle({ prefix, bundle }: AddCoreResourceBundleProps) {
-	for(let [lang, data] of Object.entries(bundle)) {
-		defaulti18n.addResourceBundle(lang, "mosaic", { [prefix] : data }, true, false);
-	}
+export function addCoreResourceBundle({ prefix, bundle }: AddCoreResourceBundle) {
+	addResourceBundle({ prefix, bundle, namespace : "mosaic" });
 }
 
 export const defaulti18n = i18next.createInstance();
