8c11c3a58f97dc4d3f1b8754481109ee76781c8e
Owen Allen <owenallenaz@gmail.com>
2020-07-03 11:40:31 -0700

    LeftNav - Makes LeftNav i18n compliant.

--- src/components/LeftNav/index.ts
+++ src/components/LeftNav/index.ts
@@ -1,2 +1,10 @@
 export { default } from "./LeftNav";
 export * from "./LeftNavTypes";
+
+import * as localeData from "./LeftNavLocales.json";
+import { addCoreResourceBundle } from "@root/i18n";
+
+addCoreResourceBundle({
+	prefix : "LeftNav",
+	bundle : localeData
+});
