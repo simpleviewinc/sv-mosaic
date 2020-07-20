e324035113a1e54603e9fc8dbd3e7f3d47dad9e3
Owen Allen <owenallenaz@gmail.com>
2020-07-20 15:57:10 -0700

    new build.

--- dist/types/components/Button/ButtonTypes.d.ts
+++ dist/types/components/Button/ButtonTypes.d.ts
@@ -4,6 +4,7 @@ import { MenuItemProps } from "../MenuItem";
 export interface ButtonProps {
     label?: string | JSX.Element;
     className?: string;
+    href?: string;
     color: "black" | "blue" | "lightBlue" | "red";
     mIcon?: MosaicMIcon;
     variant: "icon" | "outlined" | "contained" | "text";
@@ -20,7 +21,10 @@ export interface ButtonProps {
     menuContent?: JSX.Element;
     mIconColor?: string;
     onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+    /** Attrs for the root span that wraps the button */
     attrs?: MosaicObject;
+    /** Attrs for the nested Material UI IconButton or Button */
+    muiAttrs?: MosaicObject;
 }
 export interface ButtonPopoverContextProps {
     onClose(): void;
