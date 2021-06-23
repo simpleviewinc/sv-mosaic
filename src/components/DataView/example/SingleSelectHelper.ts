f8726a3467881ae1dd0901facb48c0379200a7df
Owen Allen <owenallenaz@gmail.com>
2021-06-23 14:57:45 -0700

    Linting.

--- src/components/DataView/example/SingleSelectHelper.ts
+++ src/components/DataView/example/SingleSelectHelper.ts
@@ -1,20 +1,21 @@
-import MultiSelectHelper from "./MultiSelectHelper";
+import { default as MultiSelectHelper, MultiSelectHelperArgs, MultiSelectHelperGetOptionsReturn } from "./MultiSelectHelper";
+import { MosaicObject, MosaicLabelValue } from "../../../types";
 
 class SingleSelectHelper {
 	multiHelper: MultiSelectHelper;
-	constructor(args) {
+	constructor(args: MultiSelectHelperArgs) {
 		this.multiHelper = new MultiSelectHelper(args);
 	}
-	mapOptions(args) {
+	mapOptions(args: Record<string, string>): MosaicLabelValue {
 		return this.multiHelper.mapOptions(args);
 	}
-	async getOptions(filter) {
+	async getOptions(): Promise<MultiSelectHelperGetOptionsReturn> {
 		return this.multiHelper.getOptions({
 			limit : Infinity,
 			skip : 0
 		})
 	}
-	async getSelected(id: string) {
+	async getSelected(id: string): Promise<MosaicLabelValue> {
 		const items = await this.multiHelper.getSelected([id]);
 		return items[0];
 	}
