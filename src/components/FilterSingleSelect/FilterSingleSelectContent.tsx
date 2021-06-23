4e976c0c0467a5ab3e09b94c8d4c24566a632631
Owen Allen <owenallenaz@gmail.com>
2021-06-23 14:44:22 -0700

    Linting.

--- src/components/FilterSingleSelect/FilterSingleSelectContent.tsx
+++ src/components/FilterSingleSelect/FilterSingleSelectContent.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
+import { ReactElement } from "react";
 
 import MenuBase from "../MenuBase";
 
 import { FilterSingleSelectContentProps } from "./FilterSingleSelectTypes";
 
-export default function FilterSingleSelectContent(props: FilterSingleSelectContentProps) {
+export default function FilterSingleSelectContent(props: FilterSingleSelectContentProps): ReactElement {
 	return (
 		<MenuBase anchorEl={props.anchorEl} open={Boolean(props.anchorEl)} onClose={props.onClose}>
 			{props.children}
