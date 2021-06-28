1211fb426ae59979774b1d52e2b40a586e2898b4
Owen Allen <owenallenaz@gmail.com>
2021-06-28 16:54:22 -0700

    Adds test for linter.

---
+++ testing/linter.test.js
@@ -0,0 +1,7 @@
+const { execSync } = require("child_process");
+
+describe(__filename, function() {
+	test("Run Linter", async () => {
+		execSync(`npm run style`,  {stdio : "inherit" });
+	});
+});
