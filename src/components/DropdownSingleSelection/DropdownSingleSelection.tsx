ac6ee2cae42cab697454a045d5bd9479f3de5ad3
Mauricio De Leon <mauricio.deleon.c13@gmail.com>
2021-08-19 20:04:31 -0500

    Dropdown rendering as the design specs, missing unit tests

--- src/components/DropdownSingleSelection/DropdownSingleSelection.tsx
+++ src/components/DropdownSingleSelection/DropdownSingleSelection.tsx
@@ -1,20 +1,93 @@
 import * as React from 'react';
-import { memo } from "react";
-import styled from "styled-components";
+import { useState, memo } from "react";
+import {
+	StyledLabel,
+	StyledAutocomplete,
+	StyledHelperText,
+	StyledInstructionText,
+	StyledInputWrapper,
+	StyledDisabledDropdownText,
+	StyledErrorText,
+	StyledErrorWrapper,
+	StyledErrorIcon,
+	StyledErrorMessage,
+} from "./DropdownSingleSelection.styled";
 
 import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
 import { DropdownSingleSelectionProps } from "./DropdownSingleSelectionTypes";
 
 
-const DropdownSingleSelection = (props: DropdownSingleSelectionProps) => (
-	<Autocomplete
-	id="combo-box-demo"
-	options={props.options}
-	getOptionLabel={(option) => option.title}
-	style={{ width: 300 }}
-	renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
-	/>
-);
+const DropdownSingleSelection = (props: DropdownSingleSelectionProps) => {
+	const [selectedOption, setSelectedOption] = useState('');
+	const [error, setError] = useState(false);
+
+	const onBlur = (e) => {
+		if(props.required && (!e.target.value || e.target.value?.trim().length === 0)) {
+			setError(true);
+		} else {
+			setError(false);
+		}
+	}
+
+	return(
+		<StyledErrorWrapper error={error && props.required}>	
+			<StyledLabel 
+				required={props.required}
+			>
+				{props.label}
+			</StyledLabel>
+			{!props.disabled ? 
+				<>
+					<StyledAutocomplete
+						id="combo-box-demo"
+						options={props.options}
+						size={props.size}
+						getOptionLabel={(option) => {
+								setSelectedOption(option.title);
+								return option.title;
+							}
+						}
+						error={props.required && error}
+						errorText={props.errorText}
+						renderInput={
+							(params) => 
+								<StyledInputWrapper>
+									<TextField
+										{...params}
+										variant="outlined"
+										placeholder={props.placeholder}
+										onBlur={(e) => onBlur(e)}
+									/>
+									<StyledInstructionText>
+										{props.instructionText}
+									</StyledInstructionText>
+								</StyledInputWrapper>
+						}
+					/>
+					{(!error && props.helperText?.trim().length > 0) &&
+						<StyledHelperText>
+							{props.helperText}
+						</StyledHelperText>
+					}
+
+					{(error && props.required && props.errorText?.trim().length > 0) &&
+						<StyledErrorMessage>
+							<StyledErrorIcon />
+							<StyledErrorText>
+								{props.errorText}
+							</StyledErrorText>
+						</StyledErrorMessage>
+					}
+				</>
+			:
+				<StyledDisabledDropdownText>
+					{(!selectedOption || selectedOption.trim() === '') ? 
+						"No option selected" : selectedOption
+					}
+				</StyledDisabledDropdownText>
+			}
+		</StyledErrorWrapper>
+	);
+};
 
 export default memo(DropdownSingleSelection);
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -28,11 +28,18 @@ export default {
 		gray500 : "#848791",
 		gray600 : "#6B6F7C",
 		gray700 : "#3B424E",
-		gray800 : "#0A1323"
+		gray800 : "#0A1323",
+		simplyGold: "#FDB924",
+		assistiveText: "#6B6F7C", //Same as gray600
+		label: "#3B424E", //Same as gray700
+		almostBlack: "1A1A1A",
+		errorBackground: "#B100000D",
 	},
 	borders : {
 		lightGray : "1px solid rgba(0, 0, 0, .05)",
-		gray : "1px solid rgba(0, 0, 0, .15)"
+		gray : "1px solid rgba(0, 0, 0, .15)",
+		fieldGray : "1px solid #CCCCCC",
+		error : "1px solid #B10000",
 	},
 	fontFamily : "-apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Ubuntu, Arial, sans-serif",
 	animations : {
