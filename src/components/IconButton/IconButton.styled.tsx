b07727131e976c6fa541c54929873e2c45decdeb
Andrés Hernández <jorge.hj@globant.com>
2021-09-24 18:23:06 -0500

    MOS-365: Icon button

---
+++ src/components/IconButton/IconButton.styled.tsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+// Material UI
+import IconButton from '@material-ui/core/IconButton';
+
+// Utils
+import theme from '@root/theme';
+
+export const StyledIconButton = styled(IconButton)`
+  &.MuiIconButton-root {
+    padding: 0;
+
+    &.Mui-disabled {
+      color: ${theme.colors.almostBlack};
+      opacity: 0.5;
+    }
+  }
+
+  svg {
+    font-size: 24px;
+  }
+
+  color: ${theme.colors.gray600};
+
+  &:hover {
+    color: ${theme.colors.almostBlack};
+  }
+
+  &:active {
+    color: ${theme.colors.blueTeal};
+  }
+`;
