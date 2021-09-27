d2faabf2a3c617eff5b95579ba1519cfed376073
Mauricio De Leon <mauricio.deleon.c13@gmail.com>
2021-09-27 13:46:30 -0500

    Advanced tooltip now allows multiple lines (deleted max height)

--- src/components/Tooltip/Tooltip.styled.tsx
+++ src/components/Tooltip/Tooltip.styled.tsx
@@ -31,25 +31,17 @@ export const StyledAdvancedTooltip = styled(props => (
 	& .tooltip {
 		background-color: white;
 		color: ${theme.colors.gray600};
-		padding: 0px;
+		padding: 12px;
 		margin-bottom: 8px;
 		font-size: 12px;
 		max-width: 280px;
-		max-height: 46px;
 		box-shadow: 0px 2px 6px #00000029;
 		border: 1px solid ${theme.colors.gray200};
 		left: -30px !important;
-		border: 12px solid transparent;
-		display: -webkit-box;
-		-webkit-line-clamp: 3;
-		-webkit-box-orient: vertical;
-		overflow: hidden;
-		text-overflow: ellipsis;
 	}
 
 	& .arrow {
 		color: white;
-		left: 21px !important;
-		bottom: -12px !important;
+		left: 32px !important;
 	}
 `;
