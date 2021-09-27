97340b3c2dcb1c3b3f2266c6c76eda551a5bac63
Mauricio De Leon <mauricio.deleon.c13@gmail.com>
2021-09-27 13:30:21 -0500

    Tooltip testing finished, also added comments to tooltipTypes

--- src/components/Tooltip/Tooltip.stories.tsx
+++ src/components/Tooltip/Tooltip.stories.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import { ReactElement } from 'react';
-import { boolean, withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs, text } from '@storybook/addon-knobs';
 import { Meta } from '@storybook/addon-docs/blocks';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 
 // Components
 import Tooltip from './Tooltip';
@@ -11,8 +13,14 @@ export default {
 	decorators: [withKnobs],
 } as Meta;
 
-export const Example = (): ReactElement => (
-	<Tooltip text='test' description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec iaculis quam adipiscing elit. Quisque'}>
-		<p style={{marginTop: '400px'}}>HOLA</p>
+export const AdvancedTooltip = (): ReactElement => (
+	<Tooltip type='advanced' text={text('Tooltip text','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec iaculis quam adipiscing elit. Quisque Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec iaculis quam adipiscing elit. Quisque')}>
+		<InfoOutlinedIcon style={{margin: '95px 30px'}} />
+	</Tooltip>
+);
+
+export const IconTooltip = (): ReactElement => (
+	<Tooltip text={text('Tooltip text', 'More')}>
+		<MoreVertIcon style={{marginLeft: '10px'}} />
 	</Tooltip>
 );
