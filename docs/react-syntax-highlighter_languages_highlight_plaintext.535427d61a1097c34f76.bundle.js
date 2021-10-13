d356eb1d8a8bee3fbc30285c4fd5dda08b5503ad
Mauricio De Leon <mauricio.deleon.c13@gmail.com>
2021-10-13 11:30:02 -0500

    Build for snackbar

--- docs/react-syntax-highlighter_languages_highlight_plaintext.535427d61a1097c34f76.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[121],{2378:function(module,exports){module.exports=function(hljs){return{disableAutodetect:!0}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_plaintext.535427d61a1097c34f76.bundle.js.map
---
+++ docs/react-syntax-highlighter_languages_highlight_tap.3d0f26c7e19cb1868ed7.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[160],{2428:function(module,exports){module.exports=function(hljs){return{case_insensitive:!0,contains:[hljs.HASH_COMMENT_MODE,{className:"meta",variants:[{begin:"^TAP version (\\d+)$"},{begin:"^1\\.\\.(\\d+)$"}]},{begin:"(s+)?---$",end:"\\.\\.\\.$",subLanguage:"yaml",relevance:0},{className:"number",begin:" (\\d+) "},{className:"symbol",variants:[{begin:"^ok"},{begin:"^not ok"}]}]}}}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_tap.3d0f26c7e19cb1868ed7.bundle.js.map
