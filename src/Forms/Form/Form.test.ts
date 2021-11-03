3498549810adc5469f7e0773f39d788ab4a5a625
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2021-11-03 17:54:44 -0600

    Playing around with testing reducer logic

---
+++ src/Forms/Form/Form.test.ts
@@ -0,0 +1,43 @@
+import testArray from "../../utils/testArray";
+import * as assert from "assert";
+import { useForm, actions } from "./formUtils";
+
+describe('Reducer logic', () => {
+	const { state, dispatch, events, registerFields } = useForm();
+
+	const fields = [
+		{
+			name: "text1",
+			label: "Simple Text",
+			type: "text",
+			inputSettings: {
+				maxCharacters: 20,
+			},
+			instructionText: 'testing',
+		},
+	];
+
+	registerFields(fields);
+
+	const tests = [
+		{
+			name: 'Set field value',
+			args: {
+				data: state.data.text1,
+				result: 'abc'
+			}
+		},
+	];
+
+	testArray(tests, async (test) => {
+		await dispatch(
+			actions.setFieldValue({
+				name: 'text1',
+				value: 'abc'
+			})
+		);
+		const result = state.data.text1;
+		assert.strictEqual(result, test.result);
+	});
+
+});
