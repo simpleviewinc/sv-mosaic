84adb6fbf9201b31f4760ee503b48e554208918f
Andrés Hernández <jorge.hj@globant.com>
2021-11-03 16:47:01 -0600

    MOS-447: TopComponent. Unit tests

--- src/Forms/TopComponent/TopComponent.test.tsx
+++ src/Forms/TopComponent/TopComponent.test.tsx
@@ -14,14 +14,6 @@ const sections = [
 		name: 'Account Profile',
 		id: 'section1',
 	},
-	{
-		name: 'Account Information',
-		id: 'section2',
-	},
-	{
-		name: 'Contact Information',
-		id: 'section3',
-	},
 ];
 
 const TopComponentExample = () => {
@@ -44,31 +36,25 @@ const TopComponentExample = () => {
             while scrolling!
 					</p>
 				</div>
-				<div id='section2'>
-					<h1>Account Information</h1>
-					<p>
-            Try to scroll this section and look at the navigation bar while
-            scrolling! Try to scroll this section and look at the navigation bar
-            while scrolling!
-					</p>
-				</div>
-				<div id='section3'>
-					<h1>Contact Information</h1>
-					<p>
-            Try to scroll this section and look at the navigation bar while
-            scrolling! Try to scroll this section and look at the navigation bar
-            while scrolling!
-					</p>
-				</div>
 			</div>
 		</TopComponent>
 	);
 };
 
-describe('TopComponent component', () => {
-	it('should trigger cancel and save onClick callback', () => {
+describe('TopComponent', () => {
+	beforeEach(() => {
 		render(<TopComponentExample />);
+	});
 
+	it('should display TopComponent content', () => {
+		expect(screen.getByText('Description')).toBeTruthy();
+		expect(screen.getByText('Form title')).toBeTruthy();
+		expect(screen.getAllByText('Account Profile')).toBeTruthy();
+		expect(screen.getByTestId('tooltip-test-id')).toBeTruthy();
+		expect(screen.getByTestId('checkbox-test-id')).toBeTruthy();
+	});
+
+	it('should trigger cancel and save onClick callback', () => {
 		const saveButton = screen.getByText('Save');
 		const cancelButton = screen.getByText('Cancel');
 
@@ -77,5 +63,45 @@ describe('TopComponent component', () => {
 
 		expect(cancelCallback).toHaveBeenCalled();
 		expect(saveCallback).toHaveBeenCalled();
+	});
+});
+
+describe('TopComponent elements that are conditionally rendered', () => {
+	it('should not display help icon when tooltip info is not provided', () => {
+		render(
+			<TopComponent
+				description='Description'
+				formTitle='Form title'
+				handleCancelButton={cancelCallback}
+				handleSaveButton={saveCallback}
+				sections={sections}
+				showActive={true}
+			>
+				<></>
+			</TopComponent>
+		);
+
+		const helpIcon = screen.queryByTestId('tooltip-test-id');
+
+		expect(helpIcon).toBe(null);
+	});
+
+	it('should not display checkbox if show active is false', () => {
+		render(
+			<TopComponent
+				description='Description'
+				formTitle='Form title'
+				handleCancelButton={cancelCallback}
+				handleSaveButton={saveCallback}
+				sections={sections}
+				showActive={false}
+			>
+				<></>
+			</TopComponent>
+		);
+
+		const activeCheckbox = screen.queryByTestId('checkbox-test-id');
+
+		expect(activeCheckbox).toBe(null);
 	});
 });
