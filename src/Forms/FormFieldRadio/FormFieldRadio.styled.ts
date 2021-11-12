6a9c9d173722754821d994eb9cfda83a3bab4c55
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2021-11-12 17:06:54 -0600

    Finished integrating current form fields into form builder

--- src/Forms/FormFieldRadio/FormFieldRadio.styled.ts
+++ src/Forms/FormFieldRadio/FormFieldRadio.styled.ts
@@ -3,11 +3,6 @@ import styled from 'styled-components';
 // Material UI
 import { default as MUIRadioGroup } from '@material-ui/core/RadioGroup';
 
-
 export const StyledRadioGroup = styled(MUIRadioGroup)`
   margin-left: -12px;
-`;
-
-export const RadioGroupWrapper = styled.div`
-  margin-top: ${pr => pr.instructionText ? '6px' : '-2px'};
 `;
