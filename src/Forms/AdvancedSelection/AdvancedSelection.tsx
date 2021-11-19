c7ed3e4fccd58c4f4a6d6c2ca1a6ddcc0565471c
Andrés Hernández <jorge.hj@globant.com>
2021-11-19 12:09:31 -0600

    MOS-507: AdvanceSelection adds and deletes Chips

--- src/Forms/AdvancedSelection/AdvancedSelection.tsx
+++ src/Forms/AdvancedSelection/AdvancedSelection.tsx
@@ -4,15 +4,20 @@ import { AdvancedSelectionProps } from './AdvancedSelectionTypes';
 
 // Components
 import Button from '@root/forms/Button';
+import Chip from '@root/components/Chip';
 import CheckboxList from '@root/components/CheckboxList';
 import Modal from '@root/components/Modal';
+
+// Styles
+import { StyledInput } from './AdvancedSelection.styled';
+import { ChipsWrapper } from './AdvancedSelection.styled';
 
 const AdvancedSelection = (props: AdvancedSelectionProps): ReactElement => {
 	const { options, inputTitle } = props;
 
 	// State variables
 	const [isModalOpen, setIsModalOpen] = useState(false);
-	const [checked, setChecked] = useState([]);
+	const [optionsChecked, setOptionsChecked] = useState([]);
 
 	const handleOpenModal = () => {
 		setIsModalOpen(true);
@@ -23,19 +28,35 @@ const AdvancedSelection = (props: AdvancedSelectionProps): ReactElement => {
 	};
 
 	const onChange = (checked) => {
-		setChecked(checked);
+		setOptionsChecked(checked);
 	};
 
 	const handleSaveSelection = () => {
 		console.log('Save selection');
 	};
 
+	const onChipDelete = (label) => {
+		setOptionsChecked((options) =>
+			options.filter((option) => option !== label)
+		);
+	};
+
+	const listOfChips = optionsChecked.map((option, idx) => {
+		return (
+			<Chip
+				key={`${option}-${idx}`}
+				label={option}
+				onDelete={() => onChipDelete(option)}
+			/>
+		);
+	});
+
 	const listOfOptions = options.map((option, idx) => {
 		return (
 			<div key={`${option.category}-${idx}`}>
 				<span>{option.category}</span>
 				<CheckboxList
-					checked={checked}
+					checked={optionsChecked}
 					options={option.options}
 					onChange={onChange}
 				/>
@@ -57,6 +78,10 @@ const AdvancedSelection = (props: AdvancedSelectionProps): ReactElement => {
 				secondaryAction={handleClose}
 				secondaryBtnLabel='Cancel'
 			>
+				{optionsChecked.length > 0 && (
+					<ChipsWrapper>{listOfChips}</ChipsWrapper>
+				)}
+				<StyledInput type='text' placeholder='Search...' />
 				{listOfOptions}
 			</Modal>
 		</>
