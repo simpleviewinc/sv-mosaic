26d85da5f029212f47fe9f42cfe6fbdbed4bdc00
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2021-11-24 20:59:59 -0600

    Continued standardizing formField stories

--- src/Forms/FormFieldTextArea/FormFieldTextArea.stories.tsx
+++ src/Forms/FormFieldTextArea/FormFieldTextArea.stories.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ReactElement, useState } from 'react';
+import { ReactElement, useCallback, useMemo, useState } from 'react';
 import {
 	boolean,
 	withKnobs,
@@ -14,6 +14,9 @@ import TextArea from '.';
 
 // Helpers
 import { Sizes } from '../../theme/sizes';
+import Field, { FieldDefProps } from '@root/components/Field';
+import { useForm } from '../Form/formUtils';
+import Form from '../Form/Form';
 
 export default {
 	title: 'Forms|FormFieldTextArea',
@@ -27,21 +30,97 @@ export const Default = () : ReactElement => {
 	};
 
 	return (
-		<TextArea
+		<Field
 			label={text('Label', 'Label')}
-			inputSettings={{
-				htmlFor: 'text-area',
-				size: select('Size', [Sizes.xs, Sizes.sm, Sizes.md, Sizes.lg], Sizes.sm),
-				placeholder: text('Placeholder', 'placeholder'),
-				maxCharacters: number('Max characters', 200),
-				value
-			}}
+			required={boolean('Required', false)}
+			disabled={boolean('Disabled', false)}
 			helperText={text('Helper text', '')}
 			instructionText={text('Instructional text', '')}
 			error={text('Error text', '')}
-			disabled={boolean('Disabled', false)}
-			onChange={onHandleChange}
-			required={boolean('Required', true)}
-		/>
+			size={select('Size', [Sizes.xs, Sizes.sm, Sizes.md, Sizes.lg], Sizes.sm)}
+			maxCharacters={number('Max characters', 20)}
+			htmlFor={'text-input'}
+			value={value}
+		>
+			<TextArea
+				label={text('Label', 'Label')}
+				disabled={boolean('Disabled', false)}
+				inputSettings={{
+					htmlFor: 'text-area',
+					size: select('Size', [Sizes.xs, Sizes.sm, Sizes.md, Sizes.lg], Sizes.sm),
+					placeholder: text('Placeholder', 'placeholder'),
+					maxCharacters: number('Max characters', 200),
+					value
+				}}
+				error={text('Error text', '')}
+				onChange={onHandleChange}
+			/>
+		</Field>
+	);
+};
+
+export const FormExample = (): ReactElement => {
+	const { state, dispatch, events, registerFields, registerOnSubmit } = useForm();
+
+	const size = select('Size', [Sizes.xs, Sizes.sm, Sizes.md, Sizes.lg], Sizes.sm);
+	const placeholder = text('Placeholder', 'placeholder');
+	const maxCharacters = number('Max characters', 20);
+	const disabled = boolean('Disabled', false);
+	const required = boolean('Required', false);
+
+	const fields = useMemo(
+		() =>
+			[
+				{
+					name: "regular",
+					label: "Regular example",
+					type: "textArea",
+					required,
+					disabled,
+					maxCharacters,
+					size,
+					inputSettings: {
+						disabled,
+						maxCharacters,
+						size,
+						placeholder,
+					},
+					helperText: 'Helper text',
+					instructionText: 'Instruction text',
+				},
+			] as unknown as FieldDefProps[],
+		[required, disabled, maxCharacters, size, placeholder]
+	);
+
+	useMemo(() => {
+		registerFields(fields);
+	}, [fields, registerFields]);
+
+	const onSubmit = useCallback((data) => {
+		alert('Form submitted with the following data: ' + JSON.stringify(data, null, " "));
+	}, [state.validForm]);
+
+	useMemo(() => {
+		registerOnSubmit(onSubmit);
+	}, [onSubmit, registerOnSubmit]);
+
+	const onCancel = () => {
+		alert('Cancelling form, going back to previous site');
+	};
+
+	return (
+		<>
+			<pre>{JSON.stringify(state, null, "  ")}</pre>
+			<Form
+				title={text('Title', 'Form Title')}
+				description={text('Description', 'This is a description example')}
+				state={state}
+				fields={fields}
+				dispatch={dispatch}
+				events={events}
+				onCancel={onCancel}
+				onSubmit={onSubmit}
+			/>
+		</>
 	);
 };
