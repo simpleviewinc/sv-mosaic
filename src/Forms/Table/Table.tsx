e611ab973e755f5ab6ab99afc70980cbdc43157e
Andrés Hernández <jorge.hj@globant.com>
2021-11-30 12:22:13 -0600

    MOS-530: Table delete and drag and drop functionality

--- src/Forms/Table/Table.tsx
+++ src/Forms/Table/Table.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { memo, ReactElement } from 'react';
+import { memo, ReactElement, useState } from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import { TableProps } from './TableTypes';
 
 // Components
@@ -10,60 +11,95 @@ import EditIcon from '@material-ui/icons/Edit';
 
 // Styles
 import {
-  AddButton,
-  StyledTable,
-  TableContainer,
-  Td,
-  TdDrag,
-  Th,
+	AddButton,
+	StyledTable,
+	TableContainer,
+	Td,
+	TdDrag,
+	Th,
 } from './Table.styled';
 import IconButton from '@root/components/IconButton';
 
 const Table = (props: TableProps): ReactElement => {
-  const { handleAddElement, handleDelete, handleEdit, headers, rows } = props;
+	const { handleAddElement, handleDelete, handleEdit, headers, rows } = props;
 
-  return (
-    <TableContainer>
-      <AddButton
-        buttonType='blueText'
-        icon={AddIcon}
-        onClick={handleAddElement}
-      >
+	// State variables
+	const [rowData, setRowData] = useState(rows);
+
+	const handleDragEnd = (e) => {
+		if (!e.destination) return;
+		const rowDataCopy = Array.from(rowData);
+		const [source_data] = rowDataCopy.splice(e.source.index, 1);
+		rowDataCopy.splice(e.destination.index, 0, source_data);
+		setRowData(rowDataCopy);
+	};
+
+	/**
+   * Executes external delete function and removes a row.
+   * @param rowIndex of the row that is going to be deleted
+   */
+	const deleteRow = (rowIndex: number) => {
+		handleDelete(rowIndex);
+		const rowDataCopy = [...rowData];
+		rowDataCopy.splice(rowIndex, 1);
+		setRowData(rowDataCopy);
+	}
+
+	return (
+		<TableContainer>
+			<AddButton
+				buttonType='blueText'
+				icon={AddIcon}
+				onClick={handleAddElement}
+			>
         Add Element
-      </AddButton>
-      <StyledTable>
-        <thead>
-          <tr>
-            <Th></Th>
-            <Th>Actions</Th>
-            {headers.map((header) => (
-              <Th>{header}</Th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {rows.map((row, index) => (
-            <tr>
-              <TdDrag>
-                <IconButton icon={DragIndicatorIcon} />
-              </TdDrag>
-              <Td>
-                <IconButton icon={EditIcon} onClick={() => handleEdit(index)} />
-                <IconButton
-                  icon={DeleteIcon}
-                  onClick={() => handleDelete(index)}
-                />
-              </Td>
-              <Td>{row.fullName}</Td>
-              <Td>{row.address}</Td>
-              <Td>{row.phoneNumber}</Td>
-              <Td>{row.email}</Td>
-            </tr>
-          ))}
-        </tbody>
-      </StyledTable>
-    </TableContainer>
-  );
+			</AddButton>
+			<DragDropContext onDragEnd={handleDragEnd}>
+				<StyledTable>
+					<thead>
+						<tr>
+							<Th></Th>
+							<Th>Actions</Th>
+							{headers.map((header, index) => (
+								<Th key={`${header}-${index}`}>{header}</Th>
+							))}
+						</tr>
+					</thead>
+					<Droppable droppableId='droppable-rows'>
+						{(provider) => (
+							<tbody ref={provider.innerRef} {...provider.droppableProps}>
+								{rowData?.map((row, index) => (
+									<Draggable key={row.id} draggableId={row.id} index={index}>
+										{(provider) => (
+											<tr {...provider.draggableProps} ref={provider.innerRef}>
+												<TdDrag {...provider.dragHandleProps}>
+													<IconButton icon={DragIndicatorIcon} />
+												</TdDrag>
+												<Td>
+													<IconButton
+														icon={EditIcon}
+														onClick={() => handleEdit(index)}
+													/>
+													<IconButton
+														icon={DeleteIcon}
+														onClick={() => deleteRow(index)}
+													/>
+												</Td>
+												{row.items.map((item) => {
+													return <Td key={item}>{item}</Td>;
+												})}
+											</tr>
+										)}
+									</Draggable>
+								))}
+								{provider.placeholder}
+							</tbody>
+						)}
+					</Droppable>
+				</StyledTable>
+			</DragDropContext>
+		</TableContainer>
+	);
 };
 
 export default memo(Table);
--- src/Forms/Table/TableTypes.ts
+++ src/Forms/Table/TableTypes.ts
@@ -1,7 +1,36 @@
+export type TableRow = {
+  /**
+   * Unique identifier. It is used as a value for 
+   * the react key prop and as an identifier for the 
+   * draggable item.  
+   */
+  id: string;
+  /**
+   * Data that is shown in the table.
+   */
+  items: string[];
+}
+
 export interface TableProps {
+  /**
+   * Used to create a new row.
+   */
   handleAddElement: () => void;
+  /**
+   * Used to define how that edition should happen.
+   */
   handleEdit: (rowIndex: number) => void;
-  handleDelete: (rowIndex: number) => void;
+  /**
+   * It can be used to show some kind of confirmation
+   * before removing the row.
+   */
+  handleDelete?: (rowIndex: number) => void;
+  /**
+   * Table headers.
+   */
   headers: string[];
-  rows: any;
+  /**
+   * Row data
+   */
+  rows: TableRow[];
 }
