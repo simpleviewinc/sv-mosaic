110999e702dbe6a3e9e29b4361b79bef107cfc5d
Andrés Hernández <jorge.hj@globant.com>
2021-12-15 17:32:53 -0600

    MOS-431: Image upload drag and drop functionality

--- src/Forms/FormFieldImageUpload/FormFieldImageUploadTypes.ts
+++ src/Forms/FormFieldImageUpload/FormFieldImageUploadTypes.ts
@@ -1,6 +1,16 @@
+import { Options } from '@root/forms/ImageVideoDocumentSetUp/ImageVideoDocumentSetUpTypes';
+
 export interface ImageUploadProps {
+  /**
+   * 
+   */
+   updateFilesCb: (files: any) => void;
   /**
    * Disable all the buttons.
    */
   disabled?: boolean;
+  /**
+   * List of menu options that can be executed by the component.
+   */
+   options?: Options[];
 }
