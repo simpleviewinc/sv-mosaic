9e28181022de434f9d41e5b6e99c7a2653416d04
Andrés Hernández <jorge.hj@globant.com>
2022-01-11 12:51:25 -0600

    MOS-552: [FB] DropdownSingleSelection linter applyed

--- src/Forms/FormFieldDropdownSingleSelection/FormFieldDropdownSingleSelection.test.tsx
+++ src/Forms/FormFieldDropdownSingleSelection/FormFieldDropdownSingleSelection.test.tsx
@@ -1,11 +1,7 @@
-import * as React from "react";
-import {
-	render,
-	screen,
-	cleanup,
-} from "@testing-library/react";
+import * as React from 'react';
+import { render, screen, cleanup } from '@testing-library/react';
 
-import DropdownSingleSelection from "./FormFieldDropdownSingleSelection";
+import DropdownSingleSelection from './FormFieldDropdownSingleSelection';
 import Field from '@root/components/Field';
 
 const topFilms = [
@@ -13,46 +9,42 @@ const topFilms = [
 	{ title: 'The Godfather', year: 1972 },
 	{ title: 'The Godfather: Part II', year: 1974 },
 	{ title: 'The Dark Knight', year: 2008 },
-]
+];
 
 const { getByText, getByTestId, getByRole } = screen;
 
 afterEach(cleanup);
 
 describe('DropdownSingleSelection component', () => {
-	it("should render on the screen", () => {
+	it('should render on the screen', () => {
 		render(
 			<DropdownSingleSelection
-				label="Example label"
-				inputSettings={{ 
-					options: topFilms	
+				label='Example label'
+				inputSettings={{
+					options: topFilms,
 				}}
 				value={topFilms[0]}
 			/>
 		);
 
 		/**
-		 * Since the textfield is the inner-most component, looking if this
-		 * is getting rendered will tell us by default if the dropdown is
-		 * getting rendered too.
-		 */
+     * Since the textfield is the inner-most component, looking if this
+     * is getting rendered will tell us by default if the dropdown is
+     * getting rendered too.
+     */
 
-		const textfield = getByTestId("textfield-test-id");
+		const textfield = getByTestId('textfield-test-id');
 		expect(textfield).toBeTruthy();
 	});
 
 	it('should show as error when clicking outside of it without anything selected and only when being required', () => {
 		render(
-			<Field
-				label='Label'
-				required={true}
-				error='Error text'
-		  >
+			<Field label='Label' required={true} error='Error text'>
 				<DropdownSingleSelection
-					label="Example label"
-					placeholder="Placeholder"
-					inputSettings={{ 
-						options: topFilms	
+					label='Example label'
+					placeholder='Placeholder'
+					inputSettings={{
+						options: topFilms,
 					}}
 					required={true}
 					value={null}
@@ -61,12 +53,12 @@ describe('DropdownSingleSelection component', () => {
 			</Field>
 		);
 
-		const autocomplete = getByTestId("autocomplete-test-id");
+		const autocomplete = getByTestId('autocomplete-test-id');
 
-		getByRole("textbox").focus();
+		getByRole('textbox').focus();
 		expect(autocomplete.classList.contains('Mui-focused')).toBe(true);
 
-		getByRole("textbox").blur();
+		getByRole('textbox').blur();
 		expect(autocomplete.classList.contains('Mui-focused')).toBe(false);
 
 		expect(getByText('Error text')).toBeDefined();
@@ -74,14 +66,14 @@ describe('DropdownSingleSelection component', () => {
 });
 
 describe('DropdownSingleSelection disabled state', () => {
-	it("should render a dropdown disabled with no option selected by default", () => {
+	it('should render a dropdown disabled with no option selected by default', () => {
 		render(
 			<DropdownSingleSelection
-				label="Example label"
+				label='Example label'
 				disabled
-				inputSettings={{ 
+				inputSettings={{
 					options: topFilms,
-					placeholder: 'placeholder'
+					placeholder: 'placeholder',
 				}}
 				value={null}
 			/>
@@ -89,43 +81,43 @@ describe('DropdownSingleSelection disabled state', () => {
 
 		expect(getByText('placeholder')).toBeDefined();
 	});
-})
+});
 
 describe('DropdownSingleSelection component as a form field', () => {
 	beforeEach(() => {
 		render(
 			<Field
-				label='Label test'        
+				label='Label test'
 				helperText='Helper text test'
 				instructionText='Instruction text test'
 			>
 				<DropdownSingleSelection
 					label='Label test'
-					inputSettings={{ 
+					inputSettings={{
 						options: topFilms,
 						placeholder: 'Placeholder test',
 					}}
 					value={null}
 				/>
 			</Field>
-		)
+		);
 	});
 
-	it("should render a dropdown with a label", () => {
-		expect(getByText("Label test")).toBeDefined();
+	it('should render a dropdown with a label', () => {
+		expect(getByText('Label test')).toBeDefined();
 	});
 
-	it("should render a dropdown with a helper text", () => {
-		expect(getByText("Helper text test")).toBeDefined();
+	it('should render a dropdown with a helper text', () => {
+		expect(getByText('Helper text test')).toBeDefined();
 	});
 
-	it("should render a dropdown with an instructional text", () => {
-		expect(getByText("Instruction text test")).toBeDefined();
+	it('should render a dropdown with an instructional text', () => {
+		expect(getByText('Instruction text test')).toBeDefined();
 	});
 
-	it("should render a dropdown with a placeholder", () => {
-		const input = getByRole("textbox") as HTMLInputElement;
+	it('should render a dropdown with a placeholder', () => {
+		const input = getByRole('textbox') as HTMLInputElement;
 
-		expect(input.placeholder).toEqual("Placeholder test");
+		expect(input.placeholder).toEqual('Placeholder test');
 	});
 });
