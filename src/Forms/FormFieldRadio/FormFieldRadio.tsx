372888cf4ee494ebb99bdca06cd5a33887fcd0ae
Andrés Hernández <jorge.hj@globant.com>
2022-01-14 15:18:23 -0600

    ImageVideoDocumentSetUp interfaces adjusted

--- src/Forms/FormFieldRadio/FormFieldRadio.tsx
+++ src/Forms/FormFieldRadio/FormFieldRadio.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { HTMLAttributes, ReactElement } from 'react';
+import { ReactElement } from 'react';
 
 // Components
 import RadioButton from '@root/components/RadioButton';
