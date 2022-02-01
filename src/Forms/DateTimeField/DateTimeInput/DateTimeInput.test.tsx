1dfb037f91be294045394bfd68e7c0bec4a5b9c7
Cristian Villota <cristian.villota@globant.com>
2022-02-01 11:53:44 -0500

    updated forms name folder

--- src/Forms/DateTimeField/DateTimeInput/DateTimeInput.test.tsx
+++ src/Forms/DateTimeField/DateTimeInput/DateTimeInput.test.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import * as React from 'react';
-import DateTimeInput from '../DateTimeInput';
+import DateTimeInput from '.';
 
 describe('DateTimeInput component', () => {
 	it('should should display the date value', () => {
