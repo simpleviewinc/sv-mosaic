d840e20d5763acb3e4fdc6ea9a86f1db2bb0cfc5
Andrés Hernández <jorge.hj@globant.com>
2022-02-03 16:35:31 -0600

    MOS-569: [FB] Review Image Upload

--- src/Forms/FormFieldImageUpload/FormFieldImageUploadTypes.ts
+++ src/Forms/FormFieldImageUpload/FormFieldImageUploadTypes.ts
@@ -1,27 +1,31 @@
-import { Options } from '@root/forms/ImageVideoDocumentSetUp/ImageVideoDocumentSetUpTypes';
+import { Options } from '@root/forms/MenuFormFieldCard/MenuFormFieldCardTypes';
 import { ImageUploadCanvasProps } from './ImageUploadCanvas/ImageUploadCanvas';
 
 export type ImageUploadDef = {
-	/**
-		 * Callback used to pass the mouse coordinates to the parent component
-		 * when the user clicks at some (x, y) point of the uploaded image.
-		 */
-	handleImageCoordinates?: (mouseCoordinates: {
-		x: number | null;
-		y: number | null;
-	}) => void;
-	/**
-	 * Callback executed when the set focus button is clicked.
-	 */
-	handleSetFocus?: () => void;
-	/**
-	 * List of menu options that can be executed by
-	 * the component.
-	 */
-	options?: Options[];
-	/**
-	 * Callback passed from parent component to
-	 * set the uploaded image file.
-	 */
-	uploadImage?: (files) => void;
+  /**
+   * Callback executed when the set focus button is clicked.
+   */
+  handleSetFocus?: () => void;
+  /**
+   * List of menu options that can be executed by
+   * the component.
+   */
+  options?: Options[];
 } & ImageUploadCanvasProps;
+
+
+/**
+ * Describes the structure of the value argument received by 
+ * the onChange function used by the form.
+ */
+export type ImageUploadValue = {
+  imgName?: string;
+  size?: number;
+  type?: string;
+  height?: number;
+  width?: number;
+  imgCoords?: {
+    x: number;
+    y: number;
+  };
+};
