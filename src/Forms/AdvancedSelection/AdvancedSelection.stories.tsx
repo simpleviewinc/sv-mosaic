1af6b5b58ceacf96dd38a8b6517851d92d3db00c
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2022-02-03 18:20:11 -0600

    Implemented async getOptions to advancved selection

--- src/Forms/AdvancedSelection/AdvancedSelection.stories.tsx
+++ src/Forms/AdvancedSelection/AdvancedSelection.stories.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 import { ReactElement, useCallback, useMemo, useState } from 'react';
-import { boolean, text, withKnobs } from '@storybook/addon-knobs';
+import { boolean, number, text, withKnobs } from '@storybook/addon-knobs';
 
 // Components
-import AdvancedSelection, { AdvancedSelectionDef } from '.';
-import { optionsWithCategory } from './AdvancedSelectionTypes';
+import { AdvancedSelectionDef } from '.';
 import Form from '../Form/Form';
 import { FieldDef } from '@root/components/Field';
 import { useForm } from '../Form/formUtils';
@@ -20,60 +19,6 @@ const externalOptions = [
 		label: 'Option 1',
 		value: 'option_1-cat_1',
 	},
-	{
-		category: 'Category 1',
-		label: 'Option 2',
-		value: 'option_2-cat_1',
-	},
-	{
-		category: 'Category 1',
-		label: 'Option 3',
-		value: 'option_3-cat_1',
-	},
-	{
-		category: 'Category 1',
-		label: 'Option 4',
-		value: 'option_4-cat_1',
-	},
-	{
-		category: 'Category 2',
-		label: 'Option 1 category 2',
-		value: 'option_1-cat_2',
-	},
-	{
-		category: 'Category 2',
-		label: 'Test option category 2',
-		value: 'option_2-cat_2',
-	},
-	{
-		category: 'Category 2',
-		label: 'Another option of catergory 2',
-		value: 'option_3-cat_2',
-	},
-	{
-		category: 'Category 2',
-		label: 'Option 4 category 2',
-		value: 'option_4-cat_2',
-	},
-	{
-		category: 'Test Category',
-		label: 'You can filter by category',
-		value: 'option_1-test_category',
-	},
-	{
-		category: 'Test Category',
-		label: 'Very long label that does not fit',
-		value: 'option_2-test_category',
-	},
-	{
-		category: 'Category 4',
-		label: 'Option 1 category 4',
-		value: 'option_1-cat_4',
-	},
-	{
-		label: 'Option without category',
-		value: 'option_without_category',
-	},
 ];
 
 export const Playground = (): ReactElement => {
@@ -87,10 +32,80 @@ export const Playground = (): ReactElement => {
 	const disabled = boolean('Disabled', false);
 	const instructionText = text('Instruction text', 'Instruction text');
 	const helperText = text('Helper text', 'Helper text');
+	const getOptionsLimit = number('Get options limit', 5);
 
-	const updateOptionsCb = (newOption) => {
-		const newOptions = [...options, newOption];
-		setOptions(newOptions);
+	// const updateOptionsCb = (newOption) => {
+	// 	const newOptions = [...options, newOption];
+	// 	setOptions(newOptions);
+	// };
+
+	const getOptions = async ({ currentPage, limit, filter }) => {
+		await new Promise((res) => setTimeout(res, 2000));
+
+		const additionalOptions = [
+			{
+				category: 'Category 1',
+				label: 'Option 2',
+				value: 'option_2-cat_1',
+			},
+			{
+				category: 'Category 1',
+				label: 'Option 3',
+				value: 'option_3-cat_1',
+			},
+			{
+				category: 'Category 1',
+				label: 'Option 4',
+				value: 'option_4-cat_1',
+			},
+			{
+				category: 'Category 2',
+				label: 'Option 1 category 2',
+				value: 'option_1-cat_2',
+			},
+			{
+				category: 'Category 2',
+				label: 'Test option category 2',
+				value: 'option_2-cat_2',
+			},
+			{
+				category: 'Category 2',
+				label: 'Another option of catergory 2',
+				value: 'option_3-cat_2',
+			},
+			{
+				category: 'Category 2',
+				label: 'Option 4 category 2',
+				value: 'option_4-cat_2',
+			},
+			{
+				category: 'Test Category',
+				label: 'You can filter by category',
+				value: 'option_1-test_category',
+			},
+			{
+				category: 'Test Category',
+				label: 'Very long label that does not fit',
+				value: 'option_2-test_category',
+			},
+			{
+				category: 'Category 4',
+				label: 'Option 1 category 4',
+				value: 'option_1-cat_4',
+			},
+			{
+				label: 'Option without category',
+				value: 'option_without_category',
+			},
+		];
+
+		const optionsToReturn = [];
+		for (let i = currentPage * limit; i < (currentPage * limit) + limit; i++) {
+			if (i < additionalOptions.length)
+				optionsToReturn.push(additionalOptions[i]);
+		}
+
+		return optionsToReturn;
 	};
 
 	const fields = useMemo(
@@ -108,7 +123,9 @@ export const Playground = (): ReactElement => {
 						modalTitle,
 						checkboxOptions: options,
 						groupByCategory,
-						updateOptionsCb,
+						getOptions,
+						getOptionsLimit,
+						// updateOptionsCb,
 					}
 				},
 			] as FieldDef<AdvancedSelectionDef>[]
@@ -166,10 +183,10 @@ export const KitchenSink = (): ReactElement => {
 	const modalTitle = text('Modal title', 'Modal title');
 	const groupByCategory = boolean('Group by category', false);
 
-	const updateOptionsCb = (newOption) => {
-		const newOptions = [...options, newOption];
-		setOptions(newOptions);
-	};
+	// const updateOptionsCb = (newOption) => {
+	// 	const newOptions = [...options, newOption];
+	// 	setOptions(newOptions);
+	// };
 
 	const fields = useMemo(
 		() => (
@@ -182,7 +199,7 @@ export const KitchenSink = (): ReactElement => {
 						modalTitle,
 						checkboxOptions: options,
 						groupByCategory,
-						updateOptionsCb,
+						// updateOptionsCb,
 					}
 				},
 			] as FieldDef<AdvancedSelectionDef>[]
