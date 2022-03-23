3297c5547f716666d2a6bb03ae1e1b48578223b0
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2022-03-23 10:24:36 -0600

    Deleted unused styling on button row

--- src/components/ButtonRow/ButtonRow.tsx
+++ src/components/ButtonRow/ButtonRow.tsx
@@ -9,7 +9,6 @@ const StyledWrapper = styled.span`
 	${/* ensures that the NEXT button spaces itself, if it isn't an icon button */""}
 	& > .button + .normalButton {
 		margin-left: 10px;
-		// margin-bottom: 5px;
 	}
 `;
 
