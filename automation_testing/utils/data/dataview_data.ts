bc85eb69c82b686d1ff0f3250eda1be7f11d2369
carol-bohorquez <carol.bohorquez@globant.com>
2022-05-26 11:46:33 -0500

    fixing eslint rules

--- automation_testing/utils/data/dataview_data.ts
+++ automation_testing/utils/data/dataview_data.ts
@@ -1,11 +1,11 @@
 const dataview = {
-    defaultView: 'Default View',
-    saveAsView: 'mosaic_automation',
-    saveAsViewEdit: 'new_mosaic_automation',
-    saveAsViewShared: 'mosaic_automation_shared',
-    defaultType: 'default',
-    viewNotSharedType: 'mine',
-    viewSharedType: 'shared'
+	defaultView: "Default View",
+	saveAsView: "mosaic_automation",
+	saveAsViewEdit: "new_mosaic_automation",
+	saveAsViewShared: "mosaic_automation_shared",
+	defaultType: "default",
+	viewNotSharedType: "mine",
+	viewSharedType: "shared"
 }
 
 export { dataview };
