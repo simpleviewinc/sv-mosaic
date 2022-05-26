4230d3fe8762dd9715719ade688b97c282597267
carol-bohorquez <carol.bohorquez@globant.com>
2022-05-26 11:46:33 -0500

    fixing eslint rules

--- automation_testing/pages/DataViewPage.ts
+++ automation_testing/pages/DataViewPage.ts
@@ -1,45 +1,45 @@
-import { expect, Locator, Page } from '@playwright/test';
-import { url } from '../utils/formUrls';
-import { SaveAsComponent } from './SaveAsComponent';
+import { expect, Locator, Page } from "@playwright/test";
+import { url } from "../utils/formUrls";
+import { SaveAsComponent } from "./SaveAsComponent";
 
 export class DataviewPage {
 
-    readonly page: Page;
-    readonly saveAsComponent: SaveAsComponent;
-    readonly createNewBtn: Locator;
-    readonly dialog: Page;
-    readonly editIcon: Locator;
-    readonly moreOptions: Locator;
-    readonly viewChildren: Locator;
-    readonly history: Locator;
-    readonly title: Locator;
-    
+	readonly page: Page;
+	readonly saveAsComponent: SaveAsComponent;
+	readonly createNewBtn: Locator;
+	readonly dialog: Page;
+	readonly editIcon: Locator;
+	readonly moreOptions: Locator;
+	readonly viewChildren: Locator;
+	readonly history: Locator;
+	readonly title: Locator;
 
-    constructor(page: Page) {
-        this.page = page;
-        this.saveAsComponent = new SaveAsComponent(page);
-        this.createNewBtn = page.locator('[data-mosaic-id=button_create]');
-        this.editIcon = page.locator('[data-mosaic-id=action_primary_edit]').first();
-        this.moreOptions = page.locator('[data-mosaic-id="additional_actions_dropdown"]').first();
-        this.viewChildren = page.locator('[data-mosaic-id=action_additional_view_children]').first();
-        this.history = page.locator('[data-mosaic-id=action_additional_history]').first();;
-        this.title = page.locator('text=Your Uploads');
-    }
 
-    async visit() {
-        await this.page.goto(url('dataview'), { timeout: 900000 });
-        await this.title.waitFor();
-    }
+	constructor(page: Page) {
+		this.page = page;
+		this.saveAsComponent = new SaveAsComponent(page);
+		this.createNewBtn = page.locator("[data-mosaic-id=button_create]");
+		this.editIcon = page.locator("[data-mosaic-id=action_primary_edit]").first();
+		this.moreOptions = page.locator("[data-mosaic-id=\"additional_actions_dropdown\"]").first();
+		this.viewChildren = page.locator("[data-mosaic-id=action_additional_view_children]").first();
+		this.history = page.locator("[data-mosaic-id=action_additional_history]").first();
+		this.title = page.locator("text=Your Uploads");
+	}
 
-    async validateSnatshot(component: Locator, name: string) {
-        await component.waitFor();
-        expect(await component.screenshot()).toMatchSnapshot("dataview-" + name + ".png")
-    }
+	async visit(): Promise<void> {
+		await this.page.goto(url("dataview"), { timeout: 900000 });
+		await this.title.waitFor();
+	}
 
-    async validateDialogMessage(message: string) {
-        await this.page.on('dialog', async dialog => {
-            await expect(dialog.message()).toContain(message);
-            dialog.accept();
-        });
-    }
+	async validateSnatshot(component: Locator, name: string): Promise<void> {
+		await component.waitFor();
+		expect(await component.screenshot()).toMatchSnapshot("dataview-" + name + ".png")
+	}
+
+	async validateDialogMessage(message: string): Promise<void> {
+		await this.page.on("dialog", async dialog => {
+			await expect(dialog.message()).toContain(message);
+			dialog.accept();
+		});
+	}
 }
