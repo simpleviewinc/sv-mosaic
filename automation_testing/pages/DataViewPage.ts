173a54f8e94c6fb90469d308a459c67bec223e9f
carol-bohorquez <carol.bohorquez@globant.com>
2022-05-30 12:55:41 -0500

    Adding dataview table actions tests

--- automation_testing/pages/DataViewPage.ts
+++ automation_testing/pages/DataViewPage.ts
@@ -13,17 +13,28 @@ export class DataviewPage {
 	readonly viewChildren: Locator;
 	readonly history: Locator;
 	readonly title: Locator;
+	readonly checkboxRow: Locator;
+	readonly columnsBtn: Locator;
+	readonly downloadBtn: Locator;
+	readonly deleteBtn: Locator;
+	readonly allSelectedLabel: Locator;
 
 
 	constructor(page: Page) {
 		this.page = page;
 		this.saveAsComponent = new SaveAsComponent(page);
 		this.createNewBtn = page.locator("[data-mosaic-id=button_create]");
-		this.editIcon = page.locator("[data-mosaic-id=action_primary_edit]").first();
-		this.moreOptions = page.locator("[data-mosaic-id=\"additional_actions_dropdown\"]").first();
-		this.viewChildren = page.locator("[data-mosaic-id=action_additional_view_children]").first();
-		this.history = page.locator("[data-mosaic-id=action_additional_history]").first();
+		this.editIcon = page.locator("[data-mosaic-id=action_primary_edit] button");
+		this.moreOptions = page.locator("[data-mosaic-id='additional_actions_dropdown'] button");
+		this.viewChildren = page.locator("[data-mosaic-id=action_additional_view_children]");
+		this.history = page.locator("[data-mosaic-id=action_additional_history]");
 		this.title = page.locator("text=Your Uploads");
+		this.checkboxRow = page.locator("input[data-indeterminate='false']");
+		this.columnsBtn = page.locator("button:has-text('Columns')");
+		this.downloadBtn = page.locator("[data-mosaic-id='action_bulk_download'] button");
+		this.deleteBtn = page.locator("//*[@id='root']/div/div/div[3]/table/thead/tr[1]/th[2]/span/span[2]/button");
+		this.allSelectedLabel = page.locator(".bulkText");
+
 	}
 
 	async visit(): Promise<void> {
@@ -38,8 +49,30 @@ export class DataviewPage {
 
 	async validateDialogMessage(message: string): Promise<void> {
 		await this.page.on("dialog", async dialog => {
-			await expect(dialog.message()).toContain(message);
+			expect(await dialog.message()).toContain(message);
 			dialog.accept();
 		});
+	}
+
+	async validateRecordsNumberInDialogMessage(number: number): Promise<void> {
+		await this.page.on("dialog", async dialog => {
+			expect(await dialog.message().toString().split(",").length).toBe(number);
+		});
+	}
+
+	async getFirstRowEditIcon(): Promise<Locator> {
+		return this.editIcon.first();
+	}
+
+	async getFirstRowMoreOptions(): Promise<Locator> {
+		return this.moreOptions.first();
+	}
+
+	async getFirstRowCheckbox(): Promise<Locator> {
+		return this.checkboxRow.nth(1);
+	}
+
+	async getAllRowCheckbox(): Promise<Locator> {
+		return this.checkboxRow.nth(0);
 	}
 }
