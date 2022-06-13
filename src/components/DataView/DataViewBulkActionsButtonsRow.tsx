cfea58cee8bf86ccddce958d138ab39b567b3531
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2022-06-13 18:30:10 -0500

    Isolated filterAction function and added show functionality to bulkActions

--- src/components/DataView/DataViewBulkActionsButtonsRow.tsx
+++ src/components/DataView/DataViewBulkActionsButtonsRow.tsx
@@ -3,13 +3,18 @@ import { memo } from "react";
 
 import ButtonRow from "../ButtonRow";
 import { DataViewBulkActionsButtonsRowProps } from "./DataViewTypes";
+import { filterAction } from "./utils/bulkActionsUtils";
 
 function DataViewBulkActionsButtonsRow(props: DataViewBulkActionsButtonsRowProps) {
 	const validActions = props.bulkActions.filter(action => {
-		return (
-			(props.checkedAllPages === false && action.onClick) ||
-			(props.checkedAllPages === true && action.onAllClick)
-		);
+		if (props.checkedAllPages)
+			return filterAction(action, { checkedAllPages: true })
+		else
+			return filterAction(action, { checkedAllPages: false, data: props.data.filter((val, i) => props.checked[i] === true)})
+		// return (
+		// 	(props.checkedAllPages === false && action.onClick) ||
+		// 	(props.checkedAllPages === true && action.onAllClick)
+		// );
 	});
 
 	const buttons = validActions.map(action => {
