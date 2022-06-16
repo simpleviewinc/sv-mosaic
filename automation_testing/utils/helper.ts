87cd2819637900ba4c244482eb30aabd0d43140f
carol-bohorquez <carol.bohorquez@globant.com>
2022-06-16 10:11:31 -0500

    Basic Filter tests Adding Pages class to include some common methods

--- automation_testing/utils/helper.ts
+++ automation_testing/utils/helper.ts
@@ -8,5 +8,22 @@ const sortDatesDesc = (arr: string[]): string[] => {
 		(a, b): number => new Date(b).getTime() - new Date(a).getTime());
 }
 
+const addQuotes = (word: string): string => {
+	return "\"" + word + "\"";
+}
 
-export { sortDatesAsc, sortDatesDesc }
+const addComma = (words: string[]): string => {
+	let result = ""
+	const itemNumber = words.length;
+	for (let i = 0; i < itemNumber; i++) {
+		if (i == itemNumber - 1) {
+			result = result + words[i];
+		} else {
+			result = result + words[i] + ", ";
+		}
+	}
+	return result;
+
+}
+
+export { sortDatesAsc, sortDatesDesc, addQuotes, addComma }
