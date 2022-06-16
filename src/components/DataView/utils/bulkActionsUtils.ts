c2a68b6ae52e709b20b34a5b91316e823478ebba
Andrés Hernández <jorge.hj@globant.com>
2022-06-16 15:26:05 -0500

    Replace for of iteration with every to return true only if all elements are truthy

--- src/components/DataView/utils/bulkActionsUtils.ts
+++ src/components/DataView/utils/bulkActionsUtils.ts
@@ -12,17 +12,21 @@ type FilterActionArgs = RowArgs | CheckedArgs;
 type Action = DataViewAction | DataViewAdditionalAction | DataViewBulkAction;
 
 export const filterAction = (action: Action, args: FilterActionArgs) => {
+	if (action.show === undefined) {
+		return true;
+	} 
+
 	const shows = isArray(action.show) ? action.show : [action.show];
 
-	for (const show of shows) {
-		if (show === undefined) {
-			return true;
-		} else if (typeof show === "boolean") {
+	const isValid = shows.every(show => {
+		if (typeof show === "boolean") {
 			return show;
 		} else if (typeof show === "function") {
 			return show(args);
 		} else {
 			throw new Error(`Action ${action.name}.show must be boolean or a function`);
 		}
-	}
+	});
+
+	return isValid;
 };
