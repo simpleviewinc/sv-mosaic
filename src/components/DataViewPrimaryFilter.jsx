42a637ebdcba0902a55c5ee27bf5cddbddae4e93
Andrés Hernández <jorge.hj@globant.com>
2022-06-22 14:54:43 -0500

    MOS-700: Value of filters should not be capitalized

--- src/components/DataViewPrimaryFilter.jsx
+++ src/components/DataViewPrimaryFilter.jsx
@@ -33,10 +33,15 @@ const LabelWrapper = styled.div`
 		background-color: ${theme.colors.blue}45;
 	}
 
-	& > .filter {
+	& > .filter-label {
 		font-weight: ${theme.fontWeight.normal};
 		margin-right: 8px;
 		text-transform: capitalize;
+	}
+
+	& > .filter-value {
+		font-weight: ${theme.fontWeight.normal};
+		margin-right: 8px;
 	}
 
 	&.type_optional > * {
@@ -75,8 +80,8 @@ function DataViewPrimaryFilter(props) {
 				type_${props.type}
 			`}
 		>
-			<BodyText className="filter">{props.label}:</BodyText>
-			<BodyText className="filter"><b>{props.value || "Any"}</b></BodyText>
+			<BodyText className="filter-label">{props.label}:</BodyText>
+			<BodyText className="filter-value"><b>{props.value || "Any"}</b></BodyText>
 			{
 				props.type === "optional" &&
 				<CloseIcon
