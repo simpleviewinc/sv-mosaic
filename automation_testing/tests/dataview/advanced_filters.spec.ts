dff1de1dad627eafda808666adb0424a4c0f806c
torres-nahuel <nahuel.torres@globant.com>
2022-06-24 10:46:24 -0300

    Added helper

--- automation_testing/tests/dataview/advanced_filters.spec.ts
+++ automation_testing/tests/dataview/advanced_filters.spec.ts
@@ -14,36 +14,28 @@ test.describe("DataView - Advanced Filters", () => {
 		await dataviewPage.visit();
 	});
 
-	// test("Validate that all advanced filters have default value equal to Any", async() => {
-	// 	await advancedFilters.moreBtn.click();
-	// 	await advancedFilters.selectAllAdvancedFilters();
-	// 	const allSelectedFilters = await advancedFilters.getAllSelectedValuesForAdvancedFilters();
-	// 	console.log(allSelectedFilters);
-	// 	for(let i=0;i<allSelectedFilters.length; i++){
-	// 		expect(allSelectedFilters.toString()).toBe("Any");
-	// 	}
-	// });
-
-    // test("Validate Single select category", async() => { 
-	// 	await advancedFilters.moreBtn.click();
-	// 	await advancedFilters.singleSelectCategoryOption.click();
-	// 	//This clicks the title of the page so the More menu closes.
-	// 	await dataviewPage.title.click({force: true});
-	// 	await advancedFilters.optionalFilters.click();
-	// 	const categorySelected = (await advancedFilters.selectARandomCategoryForSingleSelectCategoryOption());
-	// 	expect((await advancedFilters.getSelectedValueForSingleSelectCategoryOption())).toBe(categorySelected);
-	// 	const allCategoriesOfRows = await dataviewPage.getCategoriesFromRow();
-	// 	for(let i=0;i<allCategoriesOfRows.length; i++){
-	// 		expect(allCategoriesOfRows.toString()).toContain(categorySelected);
-	// 	}
-	// });
-
-	test("Categories with Comparisons", async() => {
+	test("Validate that all advanced filters have default value equal to Any", async() => {
 		await advancedFilters.moreBtn.click();
-		await advancedFilters.categoryWithComparisonOption.click();
+		await advancedFilters.selectAllAdvancedFilters();
+		const allSelectedFilters = await advancedFilters.getAllSelectedValuesForAdvancedFilters();
+		console.log(allSelectedFilters);
+		for(let i=0;i<allSelectedFilters.length; i++){
+			expect(allSelectedFilters.toString()).toBe("Any");
+		}
+	});
+
+    test("Validate Single select category", async() => { 
+		await advancedFilters.moreBtn.click();
+		await advancedFilters.singleSelectCategoryOption.click();
+		//This clicks the title of the page so the More menu closes.
 		await dataviewPage.title.click({force: true});
 		await advancedFilters.optionalFilters.click();
-		await advancedFilters.selectFIrstCategoriesForCategoryWithComparisonOption();
+		const categorySelected = (await advancedFilters.selectARandomCategoryForSingleSelectCategoryOption());
+		expect((await advancedFilters.getSelectedValueForSingleSelectCategoryOption())).toBe(categorySelected);
+		const allCategoriesOfRows = await dataviewPage.getCategoriesFromRow();
+		for(let i=0;i<allCategoriesOfRows.length; i++){
+			expect(allCategoriesOfRows.toString()).toContain(categorySelected);
+		}
 	});
 
 
