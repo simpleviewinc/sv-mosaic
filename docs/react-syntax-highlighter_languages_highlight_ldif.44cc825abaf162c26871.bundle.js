affcab46e722bed975ad74312ebfbd139dc104ad
Owen Allen <owenallenaz@gmail.com>
2022-06-28 12:40:34 -0700

    Removes docs folder now that we're dynamically building it in CI.

--- docs/react-syntax-highlighter_languages_highlight_ldif.44cc825abaf162c26871.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[88],{2693:function(module,exports){module.exports=function(hljs){return{contains:[{className:"attribute",begin:"^dn",end:": ",excludeEnd:!0,starts:{end:"$",relevance:0},relevance:10},{className:"attribute",begin:"^\\w",end:": ",excludeEnd:!0,starts:{end:"$",relevance:0}},{className:"literal",begin:"^-",end:"$"},hljs.HASH_COMMENT_MODE]}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_ldif.44cc825abaf162c26871.bundle.js.map
--- docs/react-syntax-highlighter_languages_highlight_vbscriptHtml.44cc825abaf162c26871.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[170],{2777:function(module,exports){module.exports=function(hljs){return{subLanguage:"xml",contains:[{begin:"<%",end:"%>",subLanguage:"vbscript"}]}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_vbscriptHtml.44cc825abaf162c26871.bundle.js.map
