50d84fa3392b8958f2b61e29b885829b22faf845
Owen Allen <owenallenaz@gmail.com>
2022-06-28 11:26:25 -0700

    Updates jest to run with jest-fail-on-console. Removes webpack-pnp since we're not using it.

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,12 @@ module.exports = {
 		"@root/(.*)": "<rootDir>/src/$1",
 		"\\.(css|less)$": "identity-obj-proxy"
 	},
-	setupFiles: ["jest-canvas-mock"],
+	setupFiles: [
+		"jest-canvas-mock"
+	],
+	setupFilesAfterEnv: [
+		"./src/setup-jest.ts"
+	],
 	testEnvironment: "jsdom",
 	collectCoverage: true,
 }
