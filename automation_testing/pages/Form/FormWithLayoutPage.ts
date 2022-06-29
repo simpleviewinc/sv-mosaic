a42a690f149220cb51f90157caef8cdc3ef07b2c
torres-nahuel <nahuel.torres@globant.com>
2022-06-29 16:35:13 -0300

    Fixed formatting issues.

--- automation_testing/pages/Form/FormWithLayoutPage.ts
+++ automation_testing/pages/Form/FormWithLayoutPage.ts
@@ -3,89 +3,89 @@ import { BasePage } from "../BasePage";
 
 export class FormWithLayout extends BasePage {
 
-    readonly page: Page;
-    readonly topComponentContainer: Locator;
-    readonly topComponentContainerSections: Locator;
-    readonly sectionContainer: Locator;
-    readonly sectionRows: Locator;
+	readonly page: Page;
+	readonly topComponentContainer: Locator;
+	readonly topComponentContainerSections: Locator;
+	readonly sectionContainer: Locator;
+	readonly sectionRows: Locator;
 
-    constructor(page: Page) {
-        super(page);
+	constructor(page: Page) {
+		super(page);
 		this.page = page;
-        this.topComponentContainer = page.locator("div.ehYpWB.sc-cqpYsc");
-        this.topComponentContainerSections = page.locator("div.sc-exkUMo");
-        this.sectionContainer = page.locator(".section");
-        this.sectionRows = page.locator("div.sc-gPzReC div.bOGurL.sc-eXNvrr");
-    }
+		this.topComponentContainer = page.locator("div.ehYpWB.sc-cqpYsc");
+		this.topComponentContainerSections = page.locator("div.sc-exkUMo");
+		this.sectionContainer = page.locator(".section");
+		this.sectionRows = page.locator("div.sc-gPzReC div.bOGurL.sc-eXNvrr");
+	}
 
-    async getNumberOfSectionsFromTopComponent():Promise<number> {
-        await this.topComponentContainer.waitFor();
+	async getNumberOfSectionsFromTopComponent():Promise<number> {
+		await this.topComponentContainer.waitFor();
 		return await this.topComponentContainerSections.count();
 	}   
 
-    async getNumberOfSections():Promise<number> {
+	async getNumberOfSections():Promise<number> {
 		return await this.sectionContainer.count();
 	} 
 
-    async getSectionsTitlesFromTopComponent():Promise<String[]>{
-        const numberOfSections = await this.getNumberOfSectionsFromTopComponent();
-        const sectionTitles = [];
-        for (let i = 0; i <  numberOfSections; i++) {
+	async getSectionsTitlesFromTopComponent():Promise<string[]> {
+		const numberOfSections = await this.getNumberOfSectionsFromTopComponent();
+		const sectionTitles = [];
+		for (let i = 0; i <  numberOfSections; i++) {
 			sectionTitles.push(await this.topComponentContainer.locator("a").nth(i).textContent());
 		}
-        return sectionTitles;
-    }
-    
-    async getPositionOfSection(section:string):Promise<number> {
-        const sections = this.getSectionsTitlesFromTopComponent();
-        return (await sections).indexOf(section);
+		return sectionTitles;
+	}
+	
+	async getPositionOfSection(section:string):Promise<number> {
+		const sections = this.getSectionsTitlesFromTopComponent();
+		return (await sections).indexOf(section);
 	} 
 
-    async getSectionsTitles():Promise<String[]>{
-        const numberOfSections = await this.getNumberOfSectionsFromTopComponent();
-        const sectionTitles = [];
-        for (let i = 0; i <  numberOfSections; i++) {
+	async getSectionsTitles():Promise<string[]> {
+		const numberOfSections = await this.getNumberOfSectionsFromTopComponent();
+		const sectionTitles = [];
+		for (let i = 0; i <  numberOfSections; i++) {
 			sectionTitles.push(await this.sectionContainer.locator("h1").nth(i).textContent());
 		}
-        return sectionTitles;
-    }
+		return sectionTitles;
+	}
 
-    async getSectionsDescriptions():Promise<String[]>{
-        const numberOfSections = await this.getNumberOfSectionsFromTopComponent();
-        const sectionsDescriptions = [];
-        for (let i = 0; i <  numberOfSections; i++) {
+	async getSectionsDescriptions():Promise<string[]> {
+		const numberOfSections = await this.getNumberOfSectionsFromTopComponent();
+		const sectionsDescriptions = [];
+		for (let i = 0; i <  numberOfSections; i++) {
 			sectionsDescriptions.push(await this.sectionContainer.locator("p").nth(i).textContent());
 		}
-        return sectionsDescriptions;
-    }
+		return sectionsDescriptions;
+	}
 
-    async getNumberOfColumsInEachRowFromSections():Promise<number[]>{
-        const numberOfRows = await this.sectionRows.count();
-        const numberOfColumsPerRow = [];
-        for (let i = 0; i <  numberOfRows; i++) {
+	async getNumberOfColumsInEachRowFromSections():Promise<number[]> {
+		const numberOfRows = await this.sectionRows.count();
+		const numberOfColumsPerRow = [];
+		for (let i = 0; i <  numberOfRows; i++) {
 			numberOfColumsPerRow.push(await this.sectionRows.nth(i).locator(".sc-gVyKpa").count());
 		}
-        return numberOfColumsPerRow;
-    }
+		return numberOfColumsPerRow;
+	}
 
-    async getSelectedSectionFromTopComponent():Promise<string>{
-        await this.topComponentContainer.waitFor();
-        await this.wait();
-        return await this.topComponentContainer.locator(".czFTET").textContent();
-    }
+	async getSelectedSectionFromTopComponent():Promise<string> {
+		await this.topComponentContainer.waitFor();
+		await this.wait();
+		return await this.topComponentContainer.locator(".czFTET").textContent();
+	}
 
-    async isSectionVisible(section:string):Promise<boolean>{
-        return await this.sectionContainer.locator("h1", {hasText: section}).isVisible();
-    }
-    
-    async scrollToSection(section:string):Promise<void>{
-        await this.topComponentContainer.waitFor({ state: "visible" });
-        await this.sectionContainer.locator("h1", {hasText: section}).scrollIntoViewIfNeeded();
-    }
+	async isSectionVisible(section:string):Promise<boolean> {
+		return await this.sectionContainer.locator("h1", {hasText: section}).isVisible();
+	}
+	
+	async scrollToSection(section:string):Promise<void> {
+		await this.topComponentContainer.waitFor({ state: "visible" });
+		await this.sectionContainer.locator("h1", {hasText: section}).scrollIntoViewIfNeeded();
+	}
 
-    async validateSectionTopComponentElementIsSelected(section:string):Promise<void> {
-        await this.topComponentContainer.waitFor({ state: "visible" });
-        const indexOfSection = await this.getPositionOfSection(section);
-        await expect(this.topComponentContainerSections.nth(indexOfSection)).toHaveClass(/czFTET/);
-    }
+	async validateSectionTopComponentElementIsSelected(section:string):Promise<void> {
+		await this.topComponentContainer.waitFor({ state: "visible" });
+		const indexOfSection = await this.getPositionOfSection(section);
+		await expect(this.topComponentContainerSections.nth(indexOfSection)).toHaveClass(/czFTET/);
+	}
 }
