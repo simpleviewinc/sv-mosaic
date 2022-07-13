7e9bc5382cc1e0dde1b5fe59b5ea203f15a3050a
Mauricio A. De León Cárdenas <mauricio.deleon.c13@gmail.com>
2022-07-13 13:31:54 -0500

    Adjusted spacing in styled component

--- src/components/Button/Button.styled.ts
+++ src/components/Button/Button.styled.ts
@@ -12,13 +12,13 @@ export const ButtonWrapper = styled.span`
   }
 
   & > button {
-    font-family: ${theme.fontFamily};
-    text-transform: none;
-    font-size: 14px;
-    font-weight: ${theme.fontWeight.medium};
-    min-width: auto;
-    line-height: 1.715;
-	  letter-spacing: 1px;
+	font-family: ${theme.fontFamily};
+	text-transform: none;
+	font-size: 14px;
+	font-weight: ${theme.fontWeight.medium};
+	min-width: auto;
+	line-height: 1.715;
+	letter-spacing: 1px;
   }
 
   &.normalButton > button {
