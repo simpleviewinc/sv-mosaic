cb89445e229a526894486e4eb7239b7c17d26aca
torres-nahuel <nahuel.torres@globant.com>
2022-07-21 13:00:51 -0300

    Added Radio button tests

---
+++ automation_testing/pages/FormFields/FormFieldRadioPage.ts
@@ -0,0 +1,30 @@
+import { Locator, Page } from "@playwright/test";
+import { BasePage } from "../BasePage";
+import { randomIntFromInterval } from "../../utils/helpers/helper";
+
+export class FormFieldRadioPage extends BasePage {
+
+	readonly page_path = "formfields-formfieldradio--kitchen-sink";
+
+	readonly page: Page;
+	readonly regularRadioButton: Locator;
+	readonly disabledRadioButton: Locator;
+	
+	constructor(page: Page) {
+		super(page);
+		this.page = page;
+		this.regularRadioButton = page.locator("[role='radiogroup']").nth(0);
+		this.disabledRadioButton = page.locator("[role='radiogroup']").nth(1);
+	}
+
+	async visitPage(): Promise<void> {
+		await this.visit(this.page_path, this.title);
+	}
+
+	async selectRandomRadioButtonOption(): Promise<([number , string])> {
+		const randomOptionNumber = randomIntFromInterval(1, await this.regularRadioButton.locator("input").count()) - 1;
+		const optionSelected = await this.regularRadioButton.locator("input").nth(randomOptionNumber).inputValue();
+		await this.regularRadioButton.locator("input").nth(randomOptionNumber).check();
+		return [randomOptionNumber, optionSelected];
+	}
+}
