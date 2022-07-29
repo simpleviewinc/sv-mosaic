0584a26cc497e7b8c4adb7900e93f54da6a3598f
Cristian Villota <cristian.villota@globant.com>
2022-07-29 12:14:13 -0500

    Added validations for contains and not contains conditions

--- src/utils/JSONDB.js
+++ src/utils/JSONDB.js
@@ -90,6 +90,14 @@ function filterData(data, filter) {
 			newData = newData.filter(row => row[key] !== val.$ne);
 		}
 
+		if (val.$contains !== undefined) {
+			newData = newData.filter(row => row[key].toLowerCase().indexOf(val.$contains.toLowerCase()) >= 0);
+		}
+
+		if (val.$not_contains !== undefined) {
+			newData = newData.filter(row => row[key].toLowerCase().indexOf(val.$not_contains.toLowerCase()) < 0);
+		}
+
 		if (val.$exists === true) {
 			newData = newData.filter(row => row.categories_ids.length > 0);
 		}
@@ -103,7 +111,7 @@ function filterData(data, filter) {
 		}
 
 		if (typeof val === "string") {
-			newData = newData.filter(row => row[key].toLowerCase().indexOf(val.toLowerCase()) >= 0);
+			newData = newData.filter(row => row[key] === val);
 		}
 	}
 	
