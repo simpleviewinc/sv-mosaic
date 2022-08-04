c2a4f41857b6d9ca86caa648a6cefe2fb37c15ae
torres-nahuel <nahuel.torres@globant.com>
2022-08-04 16:54:58 -0300

    Continued working with the tests

--- automation_testing/tests/FormFields/formFieldChipSingleSelect.spec.ts
+++ automation_testing/tests/FormFields/formFieldChipSingleSelect.spec.ts
@@ -10,60 +10,38 @@ test.describe("FormFields - FormFieldImageUpload - Kitchen Sink", () => {
 	});
 
 	test("Validate the selection Regular Chip Single Select ", async () => {
-		/*
-		Hago click en una option
-		el background color deberia ser #FDB924
-		si hago click de vuelta en la misma opcion, se deberia deseleccionar >> #F0F2F5
-		*/
 		const numberOfOptionSelected = await ffChipSingleSelectPage.selectRandomChipOption(ffChipSingleSelectPage.regularChipSingleSelectDiv);
 		const bgColor = await ffChipSingleSelectPage.getBackgroundColorFromOption(numberOfOptionSelected, false);
-		expect(bgColor).toBe("rgb(252, 188, 47)")
+		expect(bgColor).toBe("rgb(252, 188, 47)");
+		await ffChipSingleSelectPage.selectSpecificChipOption(numberOfOptionSelected, false);
+		expect(bgColor).toBe("rgb(240, 241, 240)");
 	});
 
 	test("Validate the Disabled Regular Chip Single Select", async () => {
-		expect(await ffColorPickerPage.disabledColorPickerLabel.textContent()).toBe("Disabled Example");
-		expect(await ffColorPickerPage.disabledColorPicker.getAttribute("disabled")).toBe("");
+		const numberOfOptions = await ffChipSingleSelectPage.disabledChipSingleSelectDiv.locator(ffChipSingleSelectPage.optionButton).count();
+		for (let i = 0; i < numberOfOptions; i++) {
+			expect(await ffChipSingleSelectPage.disabledChipSingleSelectDiv.locator(ffChipSingleSelectPage.optionButton).nth(i).getAttribute("aria-disabled")).toBe("true");
+		}
 	});
 
-	test("Validate the selection Required Chip Single Select", async ({ page }) => {
+	test("Validate the selection Required Chip Single Select ", async () => {
+		const numberOfOptionSelected = await ffChipSingleSelectPage.selectRandomChipOption(ffChipSingleSelectPage.requiredChipSingleSelectDiv);
+		const bgColor = await ffChipSingleSelectPage.getBackgroundColorFromOption(numberOfOptionSelected, true);
+		expect(bgColor).toBe("rgb(252, 188, 47)");
+		await ffChipSingleSelectPage.selectSpecificChipOption(numberOfOptionSelected, false);
+		expect(bgColor).toBe("rgb(240, 241, 240)");
+	});
+
+	test("Validate saving the selection for Chip Single Select", async ({ page }) => {
 		page.on("dialog", async dialog => {
-			expect(dialog.message()).toContain('"color": "#' + hexCode + '"');
+			expect(dialog.message()).toContain('"chipRegular": "' + regularOptionLabel + '"');
+			expect(dialog.message()).toContain('"chipRequired": "' + requiredOptionLabel + '"');
 			await dialog.dismiss();
 		});
-		const hexCode = getRandomHexCode(6);
-		await ffColorPickerPage.regularColorPicker.click();
-		await ffColorPickerPage.hexColorInput.selectText();
-		await ffColorPickerPage.clearAllValuesFromField();
-		await ffColorPickerPage.hexColorInput.type(hexCode);
-		await ffColorPickerPage.saveBtn.click();
-
-		 /*
-		Hago click en una option
-		el background color deberia ser #FDB924
-		si hago click de vuelta en la misma opcion, NO se deberia deseleccionar 
-
-		 */
+		const regularOptionSelected = await ffChipSingleSelectPage.selectRandomChipOption(ffChipSingleSelectPage.regularChipSingleSelectDiv);
+		const requiredOptionSelected = await ffChipSingleSelectPage.selectRandomChipOption(ffChipSingleSelectPage.requiredChipSingleSelectDiv);
+		const regularOptionLabel = await ffChipSingleSelectPage.regularChipSingleSelectDiv.locator(ffChipSingleSelectPage.optionButton).nth(regularOptionSelected - 1).textContent();
+		const requiredOptionLabel = await ffChipSingleSelectPage.requiredChipSingleSelectDiv.locator(ffChipSingleSelectPage.optionButton).nth(requiredOptionSelected - 1).textContent();
+		await ffChipSingleSelectPage.saveBtn.click();
 	});
-
-
-	test("Validate the selection Regular Chip Single Select", async ({ page }) => {
-		page.on("dialog", async dialog => {
-			expect(dialog.message()).toContain('"color": "#' + hexCode + '"');
-			await dialog.dismiss();
-		});
-		const hexCode = getRandomHexCode(6);
-		await ffColorPickerPage.regularColorPicker.click();
-		await ffColorPickerPage.hexColorInput.selectText();
-		await ffColorPickerPage.clearAllValuesFromField();
-		await ffColorPickerPage.hexColorInput.type(hexCode);
-		await ffColorPickerPage.saveBtn.click();
-
-		 /*
-		Hago click en una option
-		el background color deberia ser #FDB924
-		si hago click de vuelta en la misma opcion, NO se deberia deseleccionar 
-
-		 */
-	});
-	
 });
