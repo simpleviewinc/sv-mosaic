6394b3f0d58d688b2468f0002207f39fe06689e0
Andrés Hernández <jorge.hj@globant.com>
2022-08-09 17:24:20 -0500

    MOS-740: Oddities with the coordinates field

--- src/forms/FormFieldMapCoordinates/MapCoordinatesDrawer/ResetButton.tsx
+++ src/forms/FormFieldMapCoordinates/MapCoordinatesDrawer/ResetButton.tsx
@@ -1,21 +1,22 @@
 import * as React from "react";
 import { memo, ReactElement } from "react";
 import Button from "@root/components/Button";
+import { ResetButtonContainer } from "../MapCoordinates.styled";
 
-const ResetButton = (props: { value: any; fieldDef: any; }): ReactElement => {
+const ResetButton = (props: { value: any; fieldDef: any }): ReactElement => {
 	const { value, fieldDef } = props;
 
 	return (
-		value && (
+		<ResetButtonContainer hasValue={value}>
 			<Button
+				className="reset-button"
 				color="teal"
+				disabled={!value}
 				variant="text"
 				label="Reset"
 				onClick={fieldDef?.inputSettings?.resetLocation}
-				muiAttrs={{ disableRipple: true }}
-				attrs={{ style: { margin: "auto auto 35px auto" } }}
-			></Button>
-		)
+			/>
+		</ResetButtonContainer>
 	);
 };
 
