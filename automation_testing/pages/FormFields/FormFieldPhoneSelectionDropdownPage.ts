5de30097f3c99ddc7a67361a0f761ae7099662f5
torres-nahuel <nahuel.torres@globant.com>
2022-08-18 11:13:19 -0300

    Added phone selection tests

---
+++ automation_testing/pages/FormFields/FormFieldPhoneSelectionDropdownPage.ts
@@ -0,0 +1,38 @@
+import { Locator, Page } from "@playwright/test";
+import { BasePage } from "../BasePage";
+
+export class FormFieldPhoneSelectionDropdownPage extends BasePage {
+
+	readonly page_path = "formfields-formfieldphoneselectiondropdown--kitchen-sink";
+
+	readonly page: Page;
+	readonly regularPhoneField: Locator;
+	readonly regularPhoneFieldDropdown: Locator;
+	readonly countryCodeProvidedPhoneField: Locator;
+	readonly countryCodeProvidedPhoneFieldDropdown: Locator;
+	readonly autoformatPhoneField: Locator;
+	readonly autoformatPhoneFieldDropdown: Locator;
+	readonly customPlaceholderPhoneField: Locator;
+	readonly customPlaceholderPhoneFieldDropdown: Locator;
+	
+	constructor(page: Page) {
+		super(page);
+		this.page = page;
+		this.regularPhoneField = page.locator("input.form-control").nth(0);
+		this.regularPhoneFieldDropdown = page.locator(".flag-dropdown").nth(0);
+		
+		this.countryCodeProvidedPhoneField = page.locator("input.form-control").nth(1);
+		this.countryCodeProvidedPhoneFieldDropdown = page.locator(".flag-dropdown").nth(1);
+
+		this.autoformatPhoneField = page.locator("input.form-control").nth(2);
+		this.autoformatPhoneFieldDropdown = page.locator(".flag-dropdown").nth(2);
+
+		this.customPlaceholderPhoneField = page.locator("input.form-control").nth(3);
+		this.customPlaceholderPhoneFieldDropdown = page.locator(".flag-dropdown").nth(3);
+	}
+
+	async visitPage(): Promise<void> {
+		await this.visit(this.page_path, this.title);
+	}
+
+}
