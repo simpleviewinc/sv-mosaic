5d61ab021d1be3d9c23a64d97d23adc33f8bb58f
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2022-08-31 16:42:48 -0500

    Updated styles for date and time pickers

--- src/forms/FormFieldDate/DatePicker/DatePicker.styled.tsx
+++ src/forms/FormFieldDate/DatePicker/DatePicker.styled.tsx
@@ -8,6 +8,18 @@ export const popperSx = {
 		boxShadow: "0px 1px 5px rgba(26, 26, 26, 0.161)",
 	},
 
+	"& .MuiPickersDay-today": {
+		color: `${theme.colors.blueTeal} !important`,
+		border: "none !important",
+		fontFamily: theme.fontFamily,
+		backgroundColor: "transparent !important",
+
+		"&:hover": {
+			color: theme.colors.blueTeal,
+			backgroundColor: `${theme.colors.blueTealOpacity} !important`,
+		},
+	},
+
 	"& .MuiPickersDay-dayWithMargin": {
 		color: theme.colors.almostBlack,
 		fontFamily: theme.fontFamily,
@@ -24,46 +36,54 @@ export const popperSx = {
 
 	"& .MuiPickersDay-root.Mui-selected" : {
 		backgroundColor: `${theme.colors.blueTeal} !important`,
+		color: "#FFFF !important",
 
 		"&:hover": {
-			color: "#FFFF",
+			color: "#FFFF !important",
 			backgroundColor: `${theme.colors.blueTeal} !important`,
 		},
+	},
+
+	"& .MuiCalendarPicker-root > div:first-child" : {
+		marginBottom: "16px",
+		paddingLeft: "16px",
+		paddingRight: "16px",
 	}
 };
 
 export const DisabledDateTimeValue = styled.span`
-  color: ${theme.colors.almostBlack};
-  font-family: ${theme.fontFamily};
-  font-size: 16px;
+	color: ${theme.colors.almostBlack};
+	font-family: ${theme.fontFamily};
+	font-size: 16px;
 `;
 
 export const DatePickerWrapper = styled.div`
-  .MuiOutlinedInput-root {
+  	.MuiOutlinedInput-root {
 		background-color: ${theme.colors.gray100}
 		height: 51px;
 		width: ${Sizes.sm};
+		padding-right: 16px;
 
-    &:hover {
-      background-color: ${theme.colors.gray200};
-    }
-
-    & fieldset {
-      border-radius: 0;
-      border: ${pr => pr.isPickerOpen ? `1px solid ${theme.colors.almostBlack}` : `1px solid ${theme.colors.simplyGray}`};
-    }
-
-		.MuiOutlinedInput-input {
-			padding-left: 16px;
+		&:hover {
+			background-color: ${theme.colors.gray200};
 		}
 
-    &.Mui-focused fieldset {
-      border-color: ${theme.colors.almostBlack};
-      border-width: 2px;
-    }
-  }
+		& fieldset {
+			border-radius: 0;
+			border: ${pr => pr.isPickerOpen ? `1px solid ${theme.colors.almostBlack}` : `1px solid ${theme.colors.simplyGray}`};
+		}
 
-  .MuiIconButton-root {
-    color: ${pr => pr.isPickerOpen ? theme.colors.almostBlack : theme.colors.gray600};
-  }
+			.MuiOutlinedInput-input {
+				padding-left: 16px;
+			}
+
+		&.Mui-focused fieldset {
+			border-color: ${theme.colors.almostBlack};
+			border-width: 2px;
+		}
+  	}
+
+	.MuiIconButton-root {
+		color: ${pr => pr.isPickerOpen ? theme.colors.almostBlack : theme.colors.gray600};
+	}
 `;
