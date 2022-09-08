7756b62d6467802fdf450e045c67ece574cd78c4
torres-nahuel <nahuel.torres@globant.com>
2022-09-08 13:08:15 -0300

    Fixed FormFieldImageUpload tests

--- automation_testing/pages/FormFields/FormFieldImageUploadPage.ts
+++ automation_testing/pages/FormFields/FormFieldImageUploadPage.ts
@@ -16,12 +16,12 @@ export class FormFieldImageUploadPage extends BasePage {
 	constructor(page: Page) {
 		super(page);
 		this.page = page;
-		this.imageUploadWithoutSetFocusHandlerDiv = page.locator("//*[@id='root']/div/form/div[2]/div[1]/div/div/div/div/div/div[2]/div");
+		this.imageUploadWithoutSetFocusHandlerDiv = page.locator("//*[@id='0']/div/div/div/div/div/div[2]/div");
 		this.imageUploadWithoutSetFocusHandlerInput = page.locator("input[data-testid='input-file-test']").nth(0);
-		this.imageUploadWithSetFocusHandlerDiv = page.locator("//*[@id='root']/div/form/div[2]/div[2]/div/div/div/div/div/div[2]/div");
+		this.imageUploadWithSetFocusHandlerDiv = page.locator("//*[@id='1']/div/div/div/div/div/div[2]/div");
 		this.imageUploadWithSetFocusHandlerInput = page.locator("input[data-testid='input-file-test']").nth(1);
 		this.disabledImageUpload = page.locator("div[disabled='']");
-		this.disabledImageUploadDiv = page.locator("//*[@id='root']/div/form/div[2]/div[3]/div/div/div/div/div/div[2]")
+		this.disabledImageUploadDiv = page.locator("//*[@id='2']/div/div/div/div/div")
 	}
 
 	async visitPage(): Promise<void> {
