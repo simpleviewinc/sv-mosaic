a9da52f5adf41cb5e0b932fef44d7b8bea2f5a29
torres-nahuel <nahuel.torres@globant.com>
2022-09-15 11:09:41 -0300

    Modified locator def

--- automation_testing/pages/FormFields/FormFieldColorPickerPage.ts
+++ automation_testing/pages/FormFields/FormFieldColorPickerPage.ts
@@ -20,7 +20,7 @@ export class FormFieldColorPickerPage extends BasePage {
 		this.disabledColorPicker = page.locator("[data-testid='colordiv-test']").nth(1);
 		this.disabledColorPickerLabel = page.locator("label[for='colorDisabled']")
 		this.hexColorInput = page.locator(".flexbox-fix input").nth(0);
-		this.colorSketchPicker = page.locator("//*[@id='0']/div/div/div/div/div/div[3]/div/div/div[1]");
+		this.colorSketchPicker = page.locator(".saturation-black");
 	}
 
 	async visitPage(): Promise<void> {
