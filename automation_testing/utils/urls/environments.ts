a70bd6263c724aca8ef2292bf7c44b8c1d8b7064
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2022-09-21 12:02:58 -0500

    Updated automation env to use develop storybook when online

--- automation_testing/utils/urls/environments.ts
+++ automation_testing/utils/urls/environments.ts
@@ -1,6 +1,6 @@
 const env = {
 	local: "http://kube.simpleview.io:10000/iframe.html",
-	develop: "https://simpleviewinc.github.io/sv-mosaic/iframe.html",
+	develop: "https://simpleviewinc.github.io/sv-mosaic/develop/iframe.html",
 }
 
 export { env };
