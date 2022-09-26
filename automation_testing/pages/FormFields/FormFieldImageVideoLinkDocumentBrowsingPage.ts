2d1a6518ec6cb824e2c30892c204ab020254a008
torres-nahuel <nahuel.torres@globant.com>
2022-09-26 14:35:20 -0300

    Modified tests and added before each condition

--- automation_testing/pages/FormFields/FormFieldImageVideoLinkDocumentBrowsingPage.ts
+++ automation_testing/pages/FormFields/FormFieldImageVideoLinkDocumentBrowsingPage.ts
@@ -29,7 +29,7 @@ export class FormFieldImageVideoLinkDocumentBrowsingPage extends BasePage {
 		this.imageWithoutSrcButton = page.locator("//*[@id='1']/div/div/div/div/div/div[2]/div/div/div[1]/div[1]");
 		this.videoWithoutSrcButton = page.locator("[data-testid='browse-video-test']").nth(1);
 		this.browsingImageWithSrcCard = page.locator("//*[@id='2']");
-		this.imageWithSrcButton = page.locator("//*[@id='2']/div/div/div/div/div/div[2]/div/div/div/div[1]");
+		this.imageWithSrcButton = page.locator("[data-testid='browse-image-test']").nth(2);
 		this.browsingVideoWithSrcCard = page.locator("//*[@id='3']");
 		this.videoWithSrcButton = page.locator("//*[@id='3']/div/div/div/div/div/div[2]/div/div/div/div[1]");
 		this.browsingDocumentCard = page.locator("//*[@id='4']");
@@ -67,5 +67,12 @@ export class FormFieldImageVideoLinkDocumentBrowsingPage extends BasePage {
 			options.push(await this.page.locator("li[role='menuitem']").nth(i).textContent());
 		}
 		return options;
+	}
+
+	async removeAllVisibleCards(): Promise<void> {
+		const numberOfCards = await this.page.locator("button", { hasText: "Remove" }).count();
+		for (let i = 0; i < numberOfCards; i++) {
+			await this.page.locator("text=Remove").nth(0).click();
+		}
 	}
 }
