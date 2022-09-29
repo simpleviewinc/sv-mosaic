1e98aaeed39b0325eabdfa4b184c4d6f172adfc1
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2022-09-28 20:05:15 -0500

    Updated config.yaml to load build image on all jobs

--- automation_testing/utils/urls/environments.ts
+++ automation_testing/utils/urls/environments.ts
@@ -1,5 +1,6 @@
 const env = {
-	local: "http://kube.simpleview.io:10000/iframe.html",
+	// local: "http://kube.simpleview.io:10000/iframe.html",
+	local: "node:10000/iframe.html",
 	develop: "https://simpleviewinc.github.io/sv-mosaic/develop/iframe.html",
 }
 
