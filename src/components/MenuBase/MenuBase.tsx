ef7dcfc33abdf39340b5aa4cea3f0f69d009d190
Cristian Villota <cristian.villota@globant.com>
2022-10-04 08:56:34 -0500

    Updated DataViewSingleSelect design

--- src/components/MenuBase/MenuBase.tsx
+++ src/components/MenuBase/MenuBase.tsx
@@ -3,15 +3,38 @@ import { createContext, ReactElement } from "react";
 import MUIMenu from "@mui/material/Menu";
 
 import { MenuBaseProps, MenuBaseContextProps } from "./MenuBaseTypes";
+import styled from "@emotion/styled";
+import theme from "@root/theme";
 
 export const MenuBaseContext = createContext<MenuBaseContextProps>(null);
 
+const StyledMUIMenu = styled(MUIMenu)`
+	& > div {
+		box-shadow: 0px 2px 6px rgba(26, 26, 26, 0.16);
+		margin-top: 4px;
+
+		& > ul {
+			& .MuiTouchRipple-child {
+				background-color: ${theme.colors.grayHover};
+			}
+			& > li {
+				padding: 8px 16px;
+				&:active {
+					span {
+						color: #1a1a1a;
+					}
+				}
+			}
+		}
+	}
+`
+
 export default function MenuBase(props: MenuBaseProps): ReactElement {
 	return (
-		<MUIMenu anchorEl={props.anchorEl} open={props.open} onClose={props.onClose} disablePortal={true}>
+		<StyledMUIMenu anchorEl={props.anchorEl} open={props.open} onClose={props.onClose} disablePortal={true}>
 			<MenuBaseContext.Provider value={{ onClose : props.onClose }}>
 				{props.children}
 			</MenuBaseContext.Provider>
-		</MUIMenu>
+		</StyledMUIMenu>
 	)
 }
