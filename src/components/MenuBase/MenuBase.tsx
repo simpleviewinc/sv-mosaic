abf25aa8b1301742c079705f0663a5ed51bb4e54
Cristian Villota <cristian.villota@globant.com>
2022-10-06 10:56:41 -0500

    Updated styles for DataViewFilterSingleSelect

--- src/components/MenuBase/MenuBase.tsx
+++ src/components/MenuBase/MenuBase.tsx
@@ -3,29 +3,15 @@ import { createContext, ReactElement } from "react";
 import MUIMenu from "@mui/material/Menu";
 
 import { MenuBaseProps, MenuBaseContextProps } from "./MenuBaseTypes";
-import styled from "@emotion/styled";
+import styled from "styled-components";
 import theme from "@root/theme";
 
 export const MenuBaseContext = createContext<MenuBaseContextProps>(null);
 
 const StyledMUIMenu = styled(MUIMenu)`
 	& > div {
-		box-shadow: 0px 2px 6px rgba(26, 26, 26, 0.16);
+		box-shadow: 0px 2px 6px ${theme.colors.almostBlack}29;
 		margin-top: 4px;
-
-		& > ul {
-			& .MuiTouchRipple-child {
-				background-color: ${theme.colors.grayHover};
-			}
-			& > li {
-				padding: 8px 16px;
-				&:active {
-					span {
-						color: #1a1a1a;
-					}
-				}
-			}
-		}
 	}
 `
 
