b9314c85af1e4ab0b49d1ed5eb589550b2b64190
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2022-10-06 10:59:26 -0500

    Removed environments from automation_testing

--- automation_testing/utils/formUrls.ts
+++ automation_testing/utils/formUrls.ts
@@ -1,7 +1,11 @@
 import { env } from "../utils/urls/environments";
 
+// const url = (page_path: string): string => {
+// 	return `${env[process.env.ENV]}?id=${page_path}`;
+// }
+
 const url = (page_path: string): string => {
-	return `${env[process.env.ENV]}?id=${page_path}`;
+	return `${env.local}?id=${page_path}`;
 }
 
 export { url }
