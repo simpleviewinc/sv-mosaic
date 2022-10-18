f0ae5f922ac5911c026ccbda7f9a991c23dcb698
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2022-10-18 14:35:46 -0500

    Fixed issue with getOptions on advancedSelection

--- src/forms/FormFieldCheckbox/FormFieldCheckbox.tsx
+++ src/forms/FormFieldCheckbox/FormFieldCheckbox.tsx
@@ -46,12 +46,12 @@ const FormFieldCheckbox = (
 			})
 		}
 
-		setChecked(value?.map(selectedOption => selectedOption.value));
+		setChecked(value?.map(selectedOption => selectedOption?.value));
 
 	}, [internalOptions, value, origin]);
 
 	const internalOnChange = (checkedOptions: string[], cb:(val:MosaicLabelValue[])=>void) => {
-		const newCheckedOptions = checkedOptions?.map(checkedOption => internalOptions.find(option => option.value === checkedOption));
+		const newCheckedOptions = checkedOptions?.map(checkedOption => internalOptions.find(option => option?.value === checkedOption));
 		if (cb) {
 			cb(newCheckedOptions)
 		}
