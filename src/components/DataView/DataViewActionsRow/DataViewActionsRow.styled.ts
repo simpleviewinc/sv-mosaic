893781465fcd56949873890908fa3c80c66d64fb
Andrés Hernández <jorge.hj@globant.com>
2022-11-15 15:44:58 -0600

    MOS-869: Update create table actions row

---
+++ src/components/DataView/DataViewActionsRow/DataViewActionsRow.styled.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const DataViewActionsRowWrapper = styled.div`
+	align-items: center;
+	display: flex;
+	justify-content: space-between;
+	margin-bottom: 20px;
+`;
