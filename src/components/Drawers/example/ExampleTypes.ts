32b7202e252eb3dc381f778b96ca9e3042df4463
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2022-11-15 16:51:15 -0600

    Updated ExampleTypes file for Drawers

--- src/components/Drawers/example/ExampleTypes.ts
+++ src/components/Drawers/example/ExampleTypes.ts
@@ -1,35 +1,3 @@
-// import { Callbacks } from "../DrawersTypes";
-
-// export interface PageDef<T> {
-// 	name: string;
-// 	component: string;
-// 	args: T;
-// }
-
-// export interface GridDef {
-// 	title: string;
-// }
-
-
-// interface NavigateBack {
-// 	context: "back";
-// }
-
-// interface NavigateForward {
-// 	context: "main" | "drawer";
-// 	name: string;
-// 	callbacks?: Callbacks;
-// }
-
-// export type NavigateOptions = NavigateBack | NavigateForward;
-
-// export interface NavigateFn {
-// 	(options: NavigateOptions): void;
-// }
-
-
-//NEW
-
 export type PageConfig = FormConfig | GridConfig;
 
 export type AnyFunc = (...args: any) => any;
