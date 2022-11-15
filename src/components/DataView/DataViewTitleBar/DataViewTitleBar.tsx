8fa6d11d50e1234d0fbc3087b05a26cbd623b4f9
Andrés Hernández <jorge.hj@globant.com>
2022-11-15 16:46:44 -0600

    Moves filters to the TitleBar component

---
+++ src/components/DataView/DataViewTitleBar/DataViewTitleBar.tsx
@@ -0,0 +1,56 @@
+import React, { useMemo } from "react";
+import ButtonRow from "../../ButtonRow";
+import DataViewViewControls from "../DataViewViewControls";
+import { H1 } from "../../Typography";
+import DataViewFilters from "../DataViewFilters";
+import { DataViewTitleBarProps } from "./DataViewTitleBarTypes";
+import { TitleBarWrapper, StyledWrapper } from "./DataViewTitleBar.styled";
+
+
+function DataViewTitleBar(props: DataViewTitleBarProps) {
+	const buttons = useMemo(() => {
+		if (props.buttons === undefined) {
+			return;
+		}
+
+		return props.buttons.map((button) => {
+			const { name, ...buttonArgs } = button;
+			buttonArgs.attrs = { "data-mosaic-id": `button_${name}` };
+			return buttonArgs;
+		});
+	}, [props.buttons]);
+
+	return (
+		<TitleBarWrapper>
+			<StyledWrapper>
+				<div className="left">
+					{props.title && <H1>{props.title}</H1>}
+					{props.savedViewEnabled && (
+						<DataViewViewControls
+							savedView={props.savedView}
+							savedViewState={props.savedViewState}
+							savedViewCallbacks={props.savedViewCallbacks}
+							savedViewAllowSharedViewSave={props.savedViewAllowSharedViewSave}
+						/>
+					)}
+				</div>
+				{props.buttons && <ButtonRow buttons={buttons} />}
+			</StyledWrapper>
+			<div>
+				{
+					//loading isn't being used in DataViewFilters, should it be propped down?
+					props.filters &&
+						<DataViewFilters
+							loading={props.loading}
+							filter={props.filter}
+							filters={props.filters}
+							activeFilters={props.activeFilters}
+							onActiveFiltersChange={props.onActiveFiltersChange}
+						/>
+				}
+			</div>
+		</TitleBarWrapper>
+	);
+}
+
+export default DataViewTitleBar;
