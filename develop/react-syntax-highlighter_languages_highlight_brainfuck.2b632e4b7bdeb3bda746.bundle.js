3a9f4945565884cfc2bd2e9212987e892b7fa11b
Owen Allen <owenallenaz@gmail.com>
2022-11-15 23:27:54 +0000

    Updates

---
+++ develop/react-syntax-highlighter_languages_highlight_brainfuck.2b632e4b7bdeb3bda746.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{2438:function(module,exports){module.exports=function(hljs){var LITERAL={className:"literal",begin:"[\\+\\-]",relevance:0};return{aliases:["bf"],contains:[hljs.COMMENT("[^\\[\\]\\.,\\+\\-<> \r\n]","[\\[\\]\\.,\\+\\-<> \r\n]",{returnEnd:!0,relevance:0}),{className:"title",begin:"[\\[\\]]",relevance:0},{className:"string",begin:"[\\.,]",relevance:0},{begin:/(?:\+\+|\-\-)/,contains:[LITERAL]},LITERAL]}}}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_brainfuck.2b632e4b7bdeb3bda746.bundle.js.map
