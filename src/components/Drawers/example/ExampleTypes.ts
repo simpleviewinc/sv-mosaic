d9cbe636ed437bfdebd9281ce70aaa2548ca821c
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2022-11-15 10:51:10 -0600

    Updated Drawers files with newest from codesandbox

--- src/components/Drawers/example/ExampleTypes.ts
+++ src/components/Drawers/example/ExampleTypes.ts
@@ -1,28 +1,54 @@
-import { Callbacks } from "../DrawersTypes";
+// import { Callbacks } from "../DrawersTypes";
 
-export interface PageDef<T> {
-	name: string;
-	component: string;
-	args: T;
+// export interface PageDef<T> {
+// 	name: string;
+// 	component: string;
+// 	args: T;
+// }
+
+// export interface GridDef {
+// 	title: string;
+// }
+
+
+// interface NavigateBack {
+// 	context: "back";
+// }
+
+// interface NavigateForward {
+// 	context: "main" | "drawer";
+// 	name: string;
+// 	callbacks?: Callbacks;
+// }
+
+// export type NavigateOptions = NavigateBack | NavigateForward;
+
+// export interface NavigateFn {
+// 	(options: NavigateOptions): void;
+// }
+
+
+//NEW
+
+export type PageConfig = FormConfig | GridConfig;
+
+export type AnyFunc = (...args: any) => any;
+
+export interface FormConfig {
+  type: "form";
+  title: string;
+  fields: string[];
 }
 
-export interface GridDef {
-	title: string;
+export interface GridConfig {
+  type: "grid";
+  title: string;
+  data: Record<string, string>[];
 }
 
-  
-interface NavigateBack {
-	context: "back";
+export interface DrawerDef {
+  config: PageConfig;
+  callbacks?: PageCallbacks;
 }
 
-interface NavigateForward {
-	context: "main" | "drawer";
-	name: string;
-	callbacks?: Callbacks;
-}
-  
-export type NavigateOptions = NavigateBack | NavigateForward;
-
-export interface NavigateFn {
-	(options: NavigateOptions): void;
-}
+export type PageCallbacks = Record<string, AnyFunc>;
