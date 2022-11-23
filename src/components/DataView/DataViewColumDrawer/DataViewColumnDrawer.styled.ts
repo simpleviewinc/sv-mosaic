fbb4f62d662809b93c6e461e2f79bdd86f5cf9a2
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2022-11-23 12:45:32 -0600

    Updated spacing in column drawer component

--- src/components/DataView/DataViewColumDrawer/DataViewColumnDrawer.styled.ts
+++ src/components/DataView/DataViewColumDrawer/DataViewColumnDrawer.styled.ts
@@ -6,7 +6,7 @@ export const ColumnTitle = styled.h2`
   font-size: 20px;
   font-weight: ${theme.fontWeight.normal};
   line-height: 24px;
-  margin: 0 0 24px 0;
+  margin: 0px;
 `;
 
 export const StyledWrapper = styled.div`
@@ -16,12 +16,12 @@ export const StyledWrapper = styled.div`
   & > .left {
     border-right: 2px solid ${theme.newColors.grey2["100"]};
     flex: 1;
-    padding: 0 32px 0 40px;
+    margin: 0 32px 0 40px;
     width: 265px;
   }
 
   & > .left .listItem {
-    margin: 3px 0px;
+    margin: 14px 0px 0px -10px;
     background: white;
   }
 
@@ -30,19 +30,23 @@ export const StyledWrapper = styled.div`
     margin: 0 44px 0 32px;
     width: 280px;
   }
+
+  & > .right .droppable-columns {
+	margin-top: 4px;
+}
 `;
 
 export const ColumnItem = styled.div`
-  align-items: center;
+  	align-items: center;
 	border-bottom: 1px solid ${theme.newColors.grey2["100"]};
-  display: flex;
-  padding: 20px 0 20px 0;
+  	display: flex;
+  	padding: 20px 0 20px 0;
 
-  span {
-    margin-left: 23px;
-  }
+	span {
+		margin-left: 16px;
+	}
 
-  .MuiSvgIcon-root {
-    color: ${theme.newColors.grey3["100"]};
-  }
+	.MuiSvgIcon-root {
+		color: ${theme.newColors.grey3["100"]};
+	}
 `;
