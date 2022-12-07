866105b93b16e6a40004defa3ff51e88f48a0d01
torres-nahuel <nahuel.torres@globant.com>
2022-12-07 10:43:49 -0300

    Added almostBlack test for Content

---
+++ automation_testing/tests/Components/Content/content.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect, Page } from "@playwright/test";
+import { ContentPage } from "../../../pages/Components/Content/ContentPage";
+import theme from "../../../../src/theme";
+
+test.describe("Components - ContentPage - Playground", () => {
+	let page: Page;
+	let contentPage: ContentPage;
+
+	test.beforeAll(async ({ browser }) => {
+		page = await browser.newPage();
+		contentPage = new ContentPage(page);
+		await contentPage.visitPage();
+	});
+
+	test.afterAll(async ({ browser }) => {
+		await browser.close();
+	});
+
+	test("Validate Content title has almostBlack color.", async () => {
+		const expectColor = theme.newColors.almostBlack["100"];
+		expect(await contentPage.getColorFromElement(contentPage.mainSectionTitle)).toBe(expectColor);
+	});
+});
