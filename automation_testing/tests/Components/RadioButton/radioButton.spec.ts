1af77044334325a0005aed8344d34a8b62b4d7e7
torres-nahuel <nahuel.torres@globant.com>
2022-12-07 11:34:33 -0300

    Added almostBlack test for RadioButton

--- automation_testing/tests/Components/RadioButton/radioButton.spec.ts
+++ automation_testing/tests/Components/RadioButton/radioButton.spec.ts
@@ -2,7 +2,7 @@ import { test, expect, Page } from "@playwright/test";
 import { RadioButtonPage } from "../../../pages/Components/RadioButton/RadioButtonPage";
 import theme from "../../../../src/theme";
 
-test.describe("Components - RadioButton - Group", () => {
+test.describe.parallel("Components - RadioButton - Group", () => {
 	let page: Page;
 	let radioButtonPage: RadioButtonPage;
 
@@ -16,9 +16,17 @@ test.describe("Components - RadioButton - Group", () => {
 		await browser.close();
 	});
 
-	test("Validate Chip has simplyGold color.", async () => {
+	test("Validate selected radio button has simplyGold color.", async () => {
 		const expectColor = (theme.newColors.simplyGold["100"]);
 		await radioButtonPage.radioButtonInput.first().check();
+		expect(await radioButtonPage.getColorFromElement(radioButtonPage.radioButtonSpan.first())).toBe(expectColor);
+	});
+
+	test("Validate unselected radio button has almostBlack color.", async () => {
+		const expectColor = theme.newColors.almostBlack["100"];
+		if (await radioButtonPage.radioButtonSpan.first().isChecked()) {
+			await radioButtonPage.radioButtonSpan.first().uncheck();
+		}
 		expect(await radioButtonPage.getColorFromElement(radioButtonPage.radioButtonSpan.first())).toBe(expectColor);
 	});
 });
