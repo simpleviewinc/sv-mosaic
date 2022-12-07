b7df41bb637124390674e3d36cd7d84fb688ee78
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2022-12-06 18:29:58 -0600

    Deleted commented code from formUrls

--- automation_testing/utils/formUrls.ts
+++ automation_testing/utils/formUrls.ts
@@ -4,8 +4,4 @@ const url = (page_path: string): string => {
 	return `${env[process.env.ENV]}?id=${page_path}`;
 }
 
-// const url = (page_path: string): string => {
-// 	return `${env.local}?id=${page_path}`;
-// }
-
 export { url }
