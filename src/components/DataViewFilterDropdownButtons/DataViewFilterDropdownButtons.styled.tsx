b48f2295e0abe68d1c6d33bc29f18a1ee313d73d
Cristian Villota <cristian.villota@globant.com>
2022-12-15 10:42:19 -0500

    Removed StyledButtonRow styled component

--- src/components/DataViewFilterDropdownButtons/DataViewFilterDropdownButtons.styled.tsx
+++ src/components/DataViewFilterDropdownButtons/DataViewFilterDropdownButtons.styled.tsx
@@ -1,16 +1,9 @@
 import styled from "styled-components";
-import ButtonRow from "../ButtonRow";
 
 export const StyledWrapper = styled.div`
   & {
     display: flex;
     justify-content: space-between;
     margin-top: 16px;
-  }
-`;
-
-export const StyledButtonRow = styled(ButtonRow)`
-  .button + .normalButton {
-    margin: 0;
   }
 `;
