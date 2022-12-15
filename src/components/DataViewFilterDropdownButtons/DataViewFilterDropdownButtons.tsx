752e446a1589c28b099387e99e4a42ccbb5fc8dc
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2022-12-15 10:37:39 -0600

    Removed commented code and updated types

--- src/components/DataViewFilterDropdownButtons/DataViewFilterDropdownButtons.tsx
+++ src/components/DataViewFilterDropdownButtons/DataViewFilterDropdownButtons.tsx
@@ -1,56 +1,21 @@
 import React from "react";
-// import jsvalidator from "jsvalidator";
 
 import Button from "../Button";
-import ButtonRow from "../ButtonRow";
 import { useMosaicTranslation } from "@root/i18n";
 import { StyledWrapper } from "./DataViewFilterDropdownButtons.styled";
 
 interface DataViewFilterDropdownButtonsProps {
-	onClear?: any;
-	disableApply?: any;
-	onApply?: any;
+	onClear: () => void;
+	onApply: () => void;
+	disableApply?: boolean;
 }
-// interface DataViewFilterDropdownButtons {
-// 	onClear: () => void;
-// 	onCancel: () => void;
-// 	disableApply?: boolean;
-// 	onApply: () => void
-// }
 
-//TODO PROPS
 function DataViewFilterDropdownButtons(props: DataViewFilterDropdownButtonsProps) {
-	// jsvalidator.validate(props, {
-	// 	type : "object",
-	// 	schema : [
-	// 		{
-	// 			name : "onClear",
-	// 			type : "function"
-	// 		},
-	// 		{
-	// 			name : "onCancel",
-	// 			type : "function"
-	// 		},
-	// 		{
-	// 			name : "onApply",
-	// 			type : "function"
-	// 		},
-	// 		{
-	// 			name : "disableApply",
-	// 			type : "boolean"
-	// 		}
-	// 	],
-	// 	allowExtraKeys : false,
-	// 	throwOnInvalid : true
-	// });
-
 	const { t } = useMosaicTranslation();
 
 	return (
 		<StyledWrapper>
-			<ButtonRow>
-				<Button label={t("mosaic:common.clear")} color="black" variant="text" onClick={props.onClear}/>
-			</ButtonRow>
+			<Button label={t("mosaic:common.clear")} color="black" variant="text" onClick={props.onClear}/>
 			<Button label={t("mosaic:common.apply")} color="yellow" variant="contained" onClick={props.onApply} disabled={ (props.disableApply !== undefined) ? props.disableApply : false }/>
 		</StyledWrapper>
 	)
