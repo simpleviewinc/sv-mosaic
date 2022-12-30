aa575d26852a07f7350d5592be4c8b794af675d3
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2022-12-30 00:57:18 -0600

    Started adding magic values to date field

--- src/components/DataViewFilterDate/DataViewFilterDate.stories.tsx
+++ src/components/DataViewFilterDate/DataViewFilterDate.stories.tsx
@@ -1,13 +1,28 @@
 import * as React from "react";
 import { useState, ReactElement } from "react";
+import { boolean, withKnobs } from "@storybook/addon-knobs";
 import DataViewFilterDate from "./DataViewFilterDate";
+import { MosaicLabelValue } from "@root/types";
 
 export default {
-	title : "Components/DataViewFilterDate"
+	title : "Components/DataViewFilterDate",
+	decorators: [withKnobs],
 }
 
 export const example = (): ReactElement => {
 	const [state, setState] = useState({});
+
+	const showOptions = boolean("Show options", false);
+	const options: MosaicLabelValue[] = [
+		{
+			label: "Today",
+			value: "today",
+		},
+		{
+			label: "Last 3 years",
+			value: "last_3_years",
+		}
+	];
 
 	const onChange = function(data) {
 		setState(data);
@@ -19,7 +34,7 @@ export const example = (): ReactElement => {
 		<DataViewFilterDate
 			label="Testing"
 			data={state}
-			args={{}}
+			args={{ options: showOptions ? options : undefined}}
 			onRemove={onRemove}
 			onChange={onChange}
 		/>
