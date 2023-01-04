86251ddac226648126c2e4df4f02ab6de3c8c9c1
torres-nahuel <nahuel.torres@globant.com>
2023-01-04 12:56:13 -0300

    Added grey3 test for Popover

--- automation_testing/tests/Components/Popover/popoverTextAndLink.spec.ts
+++ automation_testing/tests/Components/Popover/popoverTextAndLink.spec.ts
@@ -30,4 +30,10 @@ test.describe.parallel("Components - PopoverPage - Text And Links", () => {
 			expect(await popoverPage.getColorFromElement(popoverPage.labelLocator.nth(i))).toBe(expectColor);
 		}
 	});
+
+	test("Validate Subtitle has grey3 color.", async () => {
+		const expectColor = theme.newColors.grey3["100"];
+		await popoverPage.openPopoverButton.click();
+		expect(await popoverPage.getColorFromElement(popoverPage.subtitleLocator)).toBe(expectColor);
+	});
 });
