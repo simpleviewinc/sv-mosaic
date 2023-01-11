ac9a03e9c238e1dd631288cd6b6c2b52106ab3fa
torres-nahuel <nahuel.torres@globant.com>
2023-01-11 16:28:57 -0300

    Moved chip locator to BasePage

--- automation_testing/tests/Components/Chip/chip.spec.ts
+++ automation_testing/tests/Components/Chip/chip.spec.ts
@@ -19,22 +19,22 @@ test.describe("Components - Chip - Kitchen Sink", () => {
 	test("Validate Chip has simplyGold background.", async () => {
 		const expectedColor = (theme.newColors.simplyGold["100"]);
 		const expectedDisabledBgColor = (theme.newColors.simplyGold["60"]);
-		expect(await chipPage.getBackgroundColorFromElement(chipPage.chip.nth(1))).toBe(expectedColor);
-		expect(await chipPage.getBackgroundColorFromElement(chipPage.chip.nth(3))).toBe(expectedDisabledBgColor);
+		expect(await chipPage.getBackgroundColorFromElement(chipPage.chipTestIDLocator.nth(1))).toBe(expectedColor);
+		expect(await chipPage.getBackgroundColorFromElement(chipPage.chipTestIDLocator.nth(3))).toBe(expectedDisabledBgColor);
 		expect(await chipPage.getBackgroundColorFromElement(chipPage.deletableChip)).toBe(expectedColor);
 	});
 
 	test("Validate Chip has almostBlack color as text.", async () => {
 		const expectedColor = theme.newColors.almostBlack["100"];
-		for (let i = 0; i < await chipPage.chip.count(); i++) {
-			expect(await chipPage.getColorFromElement(chipPage.chip.nth(i))).toBe(expectedColor);
+		for (let i = 0; i < await chipPage.chipTestIDLocator.count(); i++) {
+			expect(await chipPage.getColorFromElement(chipPage.chipTestIDLocator.nth(i))).toBe(expectedColor);
 		}
 		expect(await chipPage.getColorFromElement(chipPage.deletableChip)).toBe(expectedColor);
 	});
 
 	test("Validate Chip has grey2 background.", async () => {
 		const expectedColor = theme.newColors.grey2["100"];
-		expect(await chipPage.getBackgroundColorFromElement(chipPage.chip.nth(0))).toBe(expectedColor);
-		expect(await chipPage.getBackgroundColorFromElement(chipPage.chip.nth(2))).toBe(expectedColor);
+		expect(await chipPage.getBackgroundColorFromElement(chipPage.chipTestIDLocator.nth(0))).toBe(expectedColor);
+		expect(await chipPage.getBackgroundColorFromElement(chipPage.chipTestIDLocator.nth(2))).toBe(expectedColor);
 	});
 });
