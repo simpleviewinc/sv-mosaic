5a94752daf26511a36b3ab41a28be829e141930a
torres-nahuel <nahuel.torres@globant.com>
2023-01-11 16:32:45 -0300

    Added tests for Content.

--- automation_testing/tests/Components/Content/content.spec.ts
+++ automation_testing/tests/Components/Content/content.spec.ts
@@ -17,12 +17,24 @@ test.describe("Components - ContentPage - Playground", () => {
 	});
 
 	test("Validate Content title has almostBlack color.", async () => {
-		const expectColor = theme.newColors.almostBlack["100"];
-		expect(await contentPage.getColorFromElement(contentPage.mainSectionTitle)).toBe(expectColor);
+		const expectedColor = theme.newColors.almostBlack["100"];
+		expect(await contentPage.getColorFromElement(contentPage.mainSectionTitle)).toBe(expectedColor);
 	});
 
 	test("Validate Edit button has grey3 as color.", async () => {
-		const expectColor = theme.newColors.grey3["100"];
-		expect(await contentPage.getColorFromElement(contentPage.editButton)).toBe(expectColor);
+		const expectedColor = theme.newColors.grey3["100"];
+		expect(await contentPage.getColorFromElement(contentPage.editButton)).toBe(expectedColor);
+	});
+
+	test("Validate Main wrapper has grey2 as border bottom.", async () => {
+		const expectedColor = theme.newColors.grey2["100"];
+		expect(await contentPage.getSpecificBorderFromElement(contentPage.mainWrapperLocator, "bottom")).toContain(expectedColor);
+	});
+
+	test("Validate Chios has grey2 as background color.", async () => {
+		const expectedColor = theme.newColors.grey2["100"];
+		for (let i = 0; i < await contentPage.chipTestIDLocator.count(); i++) {
+			expect(await contentPage.getBackgroundColorFromElement(contentPage.chipTestIDLocator.nth(i))).toBe(expectedColor);
+		}
 	});
 });
