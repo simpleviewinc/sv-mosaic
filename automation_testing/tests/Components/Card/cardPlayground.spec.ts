0c0db7c9ea9f82a592a5eaade7d8413c4afea891
torres-nahuel <nahuel.torres@globant.com>
2023-01-12 15:49:31 -0300

    Added tests for multiple buttons for Card

---
+++ automation_testing/tests/Components/Card/cardPlayground.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect, Page } from "@playwright/test";
+import { CardPage } from "../../../pages/Components/Card/CardPlaygroundPage";
+import { cardKnobs } from "../../../utils/data/knobs";
+
+test.describe.parallel("Components - Card - Playground", () => {
+	let page: Page;
+	let cardPage: CardPage;
+
+	test.beforeAll(async ({ browser }) => {
+		page = await browser.newPage();
+		cardPage = new CardPage(page);
+		await cardPage.visitPage();
+	});
+
+	test.afterAll(async ({ browser }) => {
+		await browser.close();
+	});
+
+	async function validateNumberOfButtons(knob: string) {
+		const buttonsCount = 3;
+		for (let i = 1; i <= buttonsCount ;i++) {
+			await cardPage.visitPage([knob + i.toString()]);
+			expect(await cardPage.sectionTitleLocator.locator("button").count()).toBe(i);
+		}
+	}
+
+	test("Validate card component shows multiple top actions.", async () => {
+		await validateNumberOfButtons(cardKnobs.knobTopActions);
+	});
+
+	test("Validate card component shows multiple bottom actions.", async () => {
+		await validateNumberOfButtons(cardKnobs.knobTopActions);
+	});
+});
