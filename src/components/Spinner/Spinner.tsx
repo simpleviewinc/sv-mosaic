c0937e41ea198ccebd7ae1082c6af1316051a8d0
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2023-01-18 15:11:49 -0600

    Started adding FormFieldUpload

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -5,11 +5,12 @@ import theme from "@root/theme";
 
 interface SpinnerProps {
 	className?: string;
+	progress?: number;
 }
 
 function Spinner(props: SpinnerProps) {
 	return (
-		<CircularProgress style={{ color : theme.colors.blue }} size={30} className={props.className}/>
+		<CircularProgress style={{ color : theme.colors.blue }} size={30} className={props.className} value={props.progress ?? undefined} />
 	)
 }
 
