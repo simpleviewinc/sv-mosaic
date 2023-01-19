5f2f9612dd72d9acfdff97fbd760cd9cf49c3f35
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2023-01-19 01:23:10 -0600

    Updated styles for Upload component

--- src/forms/FormFieldUpload/FormFieldUpload.styled.ts
+++ src/forms/FormFieldUpload/FormFieldUpload.styled.ts
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 export const StyledFileGrid = styled.div`
 	display: grid;
-	grid-template-columns: repeat(auto-fill, minmax(max(200px, 400px), 1fr));
+	grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
 	grid-column-gap: 60px;
 	grid-row-gap: 30px;
+	margin-top: 16px;
 `;
