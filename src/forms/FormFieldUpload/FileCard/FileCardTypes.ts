181aa36e7039efe586527d719a49651d749fd0cc
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2023-01-24 18:01:33 -0600

    Updated types on FileCard

--- src/forms/FormFieldUpload/FileCard/FileCardTypes.ts
+++ src/forms/FormFieldUpload/FileCard/FileCardTypes.ts
@@ -1,7 +1,7 @@
 import { UploadData, UploadDef } from "../FormFieldUploadTypes";
 
 export type FileCardProps = {
-	onFileDelete: UploadDef["onFileDelete"];
+	onFileDelete?: UploadDef["onFileDelete"];
 	percent?: number,
 	error?: string,
 } & UploadData;
