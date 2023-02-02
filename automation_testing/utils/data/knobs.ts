03ecac0624c9ecc315fdaadd28485474fa54115a
torres-nahuel <nahuel.torres@globant.com>
2023-02-02 18:14:28 -0300

    Added playground Knobs

--- automation_testing/utils/data/knobs.ts
+++ automation_testing/utils/data/knobs.ts
@@ -9,3 +9,13 @@ export const cardKnobs = {
 	knobBottomActions: "knob-Bottom%20actions=",
 	knobTopActions: "knob-Top%20actions="
 }
+
+export const playgroundKnobs = {
+	knobDefaultValues: "knob-Default%20Values=",
+	knobGetFormValues: "knob-GetFormValues=",
+	knobPrepopulate: "knob-Prepopulate=",
+	optionHasDefaultValues: "Has%20Defaults",
+	optionNone: "None",
+	optionReturnData: "Returns%20Data",
+	optionReturnUndefined: "Returns%20Undefined"
+}
