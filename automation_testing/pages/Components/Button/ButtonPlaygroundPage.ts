2ea168c8e5d858d3b3257afd9bd58e939edf24d9
torres-nahuel <nahuel.torres@globant.com>
2023-02-14 10:00:34 -0300

    Fixed typo. Added more methods for knobs.

--- automation_testing/pages/Components/Button/ButtonPlaygroundPage.ts
+++ automation_testing/pages/Components/Button/ButtonPlaygroundPage.ts
@@ -1,6 +1,7 @@
 import { Locator, Page } from "@playwright/test";
 import { BasePage } from "../../BasePage";
 import { buttonKnobs as knob } from "../../../utils/data/knobs";
+import { ButtonProps, ColorTypes } from "../../../../src/components/Button/ButtonTypes";
 
 export class ButtonPage extends BasePage {
 
@@ -8,26 +9,44 @@ export class ButtonPage extends BasePage {
 
 	readonly page: Page;
 	readonly button: Locator;
+	readonly listIconLocator: Locator;
+	readonly hrefLocator: Locator;
 
 	constructor(page: Page) {
 		super(page);
 		this.page = page;
 		this.button = page.locator("button");
+		this.listIconLocator = page.locator("[data-testid='FormatListBulletedOutlinedIcon']");
+		this.hrefLocator = page.locator("a");
 	}
 
-	async getKbobForVariant(variant: "text"|"contained"|"outlined"|"icon"): Promise<string> {
+	async getKnobForVariant(variant: ButtonProps["variant"]): Promise<string> {
 		return (knob.knobVariant + variant).toString();
 	}
 
-	async getKbobForColor(color: "black"|"blue"|"lightBlue"|"red"|"yellow"|"teal"|"gray"|"white"): Promise<string> {
+	async getKnobForColor(color: ColorTypes): Promise<string> {
 		return (knob.knobColor + color).toString();
 	}
 
-	async getKbobForPopoverEvent(event: "onClick"|"onHover"): Promise<string> {
+	async getKnobForPopoverEvent(event: ButtonProps["popoverEvent"]): Promise<string> {
 		return (knob.knobPopoverEvent + event).toString();
 	}
 
-	async getKbobForSize(size: "medium"|"small"): Promise<string> {
+	async getKnobForSize(size: ButtonProps["size"]): Promise<string> {
 		return (knob.knobPopoverEvent + size).toString();
+	}
+
+	async getKnobForShow(show: "Undefined"|"True"|"False"|"Function that returns true"|"Function that returns false"|"Array of true values"|
+	"Array with one falsy value"|"Array of functions that return true"|"Array of functions, one returns false"): Promise<string> {
+		const showWithoutWhitespace = show.split(" ").join("%20");
+		return (knob.knobShow + showWithoutWhitespace).toString();
+	}
+
+	async getKnobForTypeOfLabel(type: "String"|"JSX"): Promise<string> {
+		return (knob.knobTypeOfLabel + type).toString();
+	}
+
+	async getKnobForIconColor(color: ColorTypes): Promise<string> {
+		return (knob.knobmIconColor + color).toString();
 	}
 }
