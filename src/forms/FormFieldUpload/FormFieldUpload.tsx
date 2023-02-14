0d688bd9aea3312ea9aa6eda944e96a12aacd852
Andrés Hernández <jorge.hj@globant.com>
2023-02-14 09:55:00 -0600

    MOS-961: Updated to upload component

--- src/forms/FormFieldUpload/FormFieldUpload.tsx
+++ src/forms/FormFieldUpload/FormFieldUpload.tsx
@@ -170,7 +170,7 @@ const FormFieldUpload = (props: MosaicFieldProps<"upload", UploadFieldInputSetti
 		for (const [key, file] of Object.entries(transformedFiles) as [string, TransformedFile][]) {
 			onFileAdd({
 				file: file?.rawData,
-				onChunkComplete: ({percent}) => onChunkComplete({uuid: key, percent}),
+				onChunkComplete: ({ percent }) => onChunkComplete({uuid: key, percent}),
 				onUploadComplete: (data) => onUploadComplete({uuid: key, data}),
 				onError: (message) => onError({uuid: key, message}),
 			})
@@ -200,8 +200,14 @@ const FormFieldUpload = (props: MosaicFieldProps<"upload", UploadFieldInputSetti
 	};
 
 	const shouldDisableField = useMemo(() => {
-		return fieldDef.disabled || (limit !== undefined && value?.length >= limit)
-	}, [fieldDef.disabled, limit, value]);
+		const numOfPendingFiles = Object.keys(pendingFiles).length;
+
+		return (
+			fieldDef.disabled ||
+			(limit !== undefined && value?.length >= limit ||
+			(value?.length + numOfPendingFiles) >= limit ||
+			numOfPendingFiles >= limit)
+		)}, [fieldDef.disabled, limit, value, pendingFiles]);
 
 	return (
 		<>
