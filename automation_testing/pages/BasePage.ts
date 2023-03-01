bc201943ce4400c00d28f38e5667aeab778da724
torres-nahuel <nahuel.torres@globant.com>
2023-03-01 12:44:01 -0300

    Added test for card in Content

--- automation_testing/pages/BasePage.ts
+++ automation_testing/pages/BasePage.ts
@@ -36,6 +36,7 @@ export class BasePage {
 	readonly rolePresentationLocator: Locator;
 	readonly deleteIconSelectedOptionChip: Locator;
 	readonly formLocator: Locator;
+	readonly button: Locator;
 
 	constructor(page: Page) {
 		this.page = page;
@@ -69,6 +70,7 @@ export class BasePage {
 		this.rolePresentationLocator = page.locator("[role='presentation']");
 		this.deleteIconSelectedOptionChip = page.locator("[data-testid='delete-icon-test-id']");
 		this.formLocator = page.locator("form");
+		this.button = page.locator("button");
 	}
 
 	async visit(page_path: string, knobs?: string[]): Promise<void> {
