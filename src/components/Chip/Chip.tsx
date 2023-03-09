90a3dde3427c6d450d05095c318d2f2c94f01217
Cristian Villota <cristian.villota@globant.com>
2023-03-09 09:52:05 -0500

    Added tooltip to deletable chip component

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { ReactElement, HTMLAttributes } from "react";
+import { ReactElement, HTMLAttributes, useRef, useState, useEffect } from "react";
 
 // Material UI
 import ClearIcon from "@mui/icons-material/Clear";
@@ -7,21 +7,34 @@ import ClearIcon from "@mui/icons-material/Clear";
 // Types and styles
 import { ChipsProps } from "./ChipTypes";
 import { StyledChip, StyledDeletableChip, StyledLabel } from "./Chip.styled";
+import Tooltip from "../Tooltip";
+
 
 const Chip = (props: ChipsProps & HTMLAttributes<HTMLDivElement>): ReactElement => {
 	const { label, required, disabled, selected, onDelete, onClick } = props;
 
+	const [deletableChipWidth, setDeletableChipWidth] = useState(0);
+
+	const deletableChipRef = useRef(null);
+
+	useEffect(() => {
+		setDeletableChipWidth(deletableChipRef?.current?.offsetWidth);
+	}, [onDelete, label])
+
 	return onDelete ? (
-		<StyledDeletableChip
-			label={<StyledLabel>{label}</StyledLabel>}
-			{...props}
-			required={required}
-			disabled={disabled}
-			selected={selected}
-			deleteIcon={<ClearIcon data-testid='delete-icon-test-id' />}
-			onDelete={onDelete}
-			data-testid="delete-chip-testid"
-		/>
+		<Tooltip text={deletableChipWidth >= 186 ? label : ""}>
+			<StyledDeletableChip
+				ref={deletableChipRef}
+				label={<StyledLabel>{label}</StyledLabel>}
+				{...props}
+				required={required}
+				disabled={disabled}
+				selected={selected}
+				deleteIcon={<ClearIcon data-testid='delete-icon-test-id' />}
+				onDelete={onDelete}
+				data-testid="delete-chip-testid"
+			/>
+		</Tooltip>
 	) : (
 		<StyledChip
 			label={<StyledLabel>{label}</StyledLabel>}
