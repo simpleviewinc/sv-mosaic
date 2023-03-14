c4c28fef26f54b4645575a6c1a038ceb80803b0a
Owen Allen <owenallenaz@gmail.com>
2023-03-14 16:26:42 +0000

    Updates

--- develop/react-syntax-highlighter_languages_highlight_brainfuck.b32f64508c029e52a93b.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{2500:function(module,exports){module.exports=function(hljs){var LITERAL={className:"literal",begin:"[\\+\\-]",relevance:0};return{aliases:["bf"],contains:[hljs.COMMENT("[^\\[\\]\\.,\\+\\-<> \r\n]","[\\[\\]\\.,\\+\\-<> \r\n]",{returnEnd:!0,relevance:0}),{className:"title",begin:"[\\[\\]]",relevance:0},{className:"string",begin:"[\\.,]",relevance:0},{begin:/(?:\+\+|\-\-)/,contains:[LITERAL]},LITERAL]}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_brainfuck.b32f64508c029e52a93b.bundle.js.map
---
+++ develop/react-syntax-highlighter_languages_highlight_plaintext.7f47296f9949258d55c0.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[121],{2604:function(module,exports){module.exports=function(hljs){return{disableAutodetect:!0}}}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_plaintext.7f47296f9949258d55c0.bundle.js.map
