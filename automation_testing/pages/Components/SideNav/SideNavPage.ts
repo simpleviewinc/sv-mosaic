8f79ca6da85c6ba9dea65840bc2fca97748e837d
torres-nahuel <nahuel.torres@globant.com>
2023-03-14 15:46:04 -0300

    Modified tests. Removed unused methods.

--- automation_testing/pages/Components/SideNav/SideNavPage.ts
+++ automation_testing/pages/Components/SideNav/SideNavPage.ts
@@ -17,23 +17,4 @@ export class SideNavPage extends BasePage {
 		this.sections = page.locator("[data-testid='section-wrapper']");
 		this.navLocator = page.locator("nav");
 	}
-
-	async getLocatorOfSelectedSection(sectionTitle: string): Promise<Locator> {
-		const sectionText =  this.sections.locator("div");
-		for (let i = 0; i < await sectionText.count(); i++) {
-			if (await sectionText.nth(i).textContent() == sectionTitle) {
-				return sectionText.nth(i);
-			}
-		}
-	}
-
-	async selectSpecificSection(sectionToSelect: string): Promise<void> {
-		const sections =  this.sections.locator("div");
-		for (let i = 0; i < await sections.count(); i++) {
-			if (await sections.nth(i).textContent() == sectionToSelect) {
-				await sections.nth(i).click({force: true});
-				break;
-			}
-		}
-	}
 }
