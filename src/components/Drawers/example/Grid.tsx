817026156956f5bc766ac30a12f8922e2946c615
Andrés Hernández <jorge.hj@globant.com>
2023-03-15 10:21:08 -0600

    MOS-990: Adds onBack to form fields

--- src/components/Drawers/example/Grid.tsx
+++ src/components/Drawers/example/Grid.tsx
@@ -1,8 +1,18 @@
+import PageHeader from "@root/components/PageHeader";
 import * as React from "react";
+import { useContext } from "react";
+import AppContext from "./AppContext";
 import { GridConfig } from "./ExampleTypes";
 
 function Grid({ config }: { config: GridConfig }) {
-	return <p>Grid</p>;
+	const appContext = useContext(AppContext);
+
+	return (
+		<>
+			<PageHeader title="Grid" onBack={appContext.removeDrawer} />
+			<p>Grid</p>
+		</>
+	);
 }
 
 export default Grid;
