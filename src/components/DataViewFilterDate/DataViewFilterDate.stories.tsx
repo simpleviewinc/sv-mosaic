12a6ae72c7949bba6556d6da24180853f2dfae1f
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2023-03-16 16:59:58 -0600

    Added Playground and KitchenSink for DVFilterDate

--- src/components/DataViewFilterDate/DataViewFilterDate.stories.tsx
+++ src/components/DataViewFilterDate/DataViewFilterDate.stories.tsx
@@ -9,7 +9,7 @@ export default {
 	decorators: [withKnobs],
 }
 
-export const example = (): ReactElement => {
+export const Playground = (): ReactElement => {
 	const [state, setState] = useState({});
 
 	const showOptions = boolean("Show options", false);
@@ -54,5 +54,68 @@ export const example = (): ReactElement => {
 			onRemove={onRemove}
 			onChange={onChange}
 		/>
+	)
+}
+
+export const KitchenSink = (): ReactElement => {
+	const [state, setState] = useState({
+		regular: {},
+		options: {},
+	});
+
+	const options: MosaicLabelValue[] = [
+		{
+			label: "Today",
+			value: "today",
+		},
+		{
+			label: "Yesterday",
+			value: "yesterday",
+		},
+		{
+			label: "Tomorrow",
+			value: "tomorrow",
+		},
+		{
+			label: "Last year",
+			value: "last_year",
+		},
+		{
+			label: "Last 2 years",
+			value: "last_2_years",
+		},
+		{
+			label: "Last 3 years",
+			value: "last_3_years",
+		}
+	];
+
+	const onChange = function(type, data) {
+		const newState = {
+			...state,
+			[type]: data,
+		};
+		setState(newState);
+	}
+
+	const onRemove = () => undefined;
+
+	return (
+		<div style={{display: "flex", flexDirection: "column", gap: "16px"}}>
+			<DataViewFilterDate
+				label="Regular Date filter"
+				data={state.regular}
+				onRemove={onRemove}
+				onChange={(data) => onChange("regular", data)}
+				args={{ options: undefined }}
+			/>
+			<DataViewFilterDate
+				label="Date filter with options"
+				data={state.options}
+				args={{ options }}
+				onRemove={onRemove}
+				onChange={(data) => onChange("options", data)}
+			/>
+		</div>
 	)
 }
