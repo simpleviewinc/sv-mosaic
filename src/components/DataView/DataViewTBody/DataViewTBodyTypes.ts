2de0c65d95d937ff74eb2a919e0449957f495979
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2023-04-20 12:20:24 -0600

    Created folder for DVTBody and all its sub-files

---
+++ src/components/DataView/DataViewTBody/DataViewTBodyTypes.ts
@@ -0,0 +1,15 @@
+import { MosaicObject } from "@root/types";
+import { DataViewProps } from "../DataViewTypes";
+
+export interface DataViewTBodyProps {
+	onReorder?: DataViewProps["onReorder"];
+	onCheckboxClick?: DataViewProps["onCheckChange"];
+	transformedData: MosaicObject[];
+	data: DataViewProps["data"];
+	bulkActions?: DataViewProps["bulkActions"];
+	primaryActions?: DataViewProps["primaryActions"];
+	additionalActions?: DataViewProps["additionalActions"];
+	checked?: DataViewProps["checked"];
+	columns: DataViewProps["columns"];
+	hasActions: boolean;
+}
