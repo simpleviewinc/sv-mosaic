0018c795b733861ccc72c54d9059f7217bbf3edc
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2023-04-20 10:34:33 -0600

    Created folder for DVBulkAllBar and all its sub-files

---
+++ src/components/DataView/DataViewBulkAllBar/DataViewBulkAllBarTypes.ts
@@ -0,0 +1,12 @@
+export interface DataViewBulkAllBarProps {
+	count?: any;
+	rowCount?: any;
+	checkedAllPages?: any;
+	onCheckAllPagesClick?: any;
+}
+// interface Props {
+// 	count: number
+// 	rowCount: number
+// 	checkedAllPages: boolean
+// 	onCheckAllPagesClick: DataViewBulkAction["onAllClick"]
+// }
