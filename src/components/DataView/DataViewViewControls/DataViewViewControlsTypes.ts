748dbf310563ebf160f8059dcbe808cb02c022a5
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2023-04-20 12:23:07 -0600

    Created folder for DVViewControls and all its sub-files

---
+++ src/components/DataView/DataViewViewControls/DataViewViewControlsTypes.ts
@@ -0,0 +1,20 @@
+export interface DataViewViewControlsProps {
+	savedView?: any;
+	savedViewAllowSharedViewSave?: any;
+	savedViewCallbacks?: any;
+	savedViewState?: any;
+}
+// interface DataViewViewControlsProps {
+// 	savedView: {
+// 		label: string |number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal;
+// 		type: string;
+// 	};
+// 	savedViewAllowSharedViewSave: any;
+// 	savedViewCallbacks: {
+// 		onSave: (arg0: any) => any;
+// 		onRemove: any;
+// 		onChange: any;
+// 		onGetOptions: any;
+// 	};
+// 	savedViewState: any;
+// }
