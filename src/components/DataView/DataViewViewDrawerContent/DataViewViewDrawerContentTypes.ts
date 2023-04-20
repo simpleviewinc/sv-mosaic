5a34de19f00472e8f043d8948beaad480bba4e5a
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2023-04-20 12:25:56 -0600

    Created folder for DVViewDrawerContent and all its sub-files

---
+++ src/components/DataView/DataViewViewDrawerContent/DataViewViewDrawerContentTypes.ts
@@ -0,0 +1,16 @@
+export interface DataViewViewDrawerContentProps {
+	onGetOptions?: any;
+	allowSharedViewSave?: any;
+	onChange?: any;
+	onClose?: any;
+	onRemove?: any;
+	onSave?: any;
+}
+// interface DataViewViewDrawerContentProps {
+// 	onGetOptions: () => any;
+// 	allowSharedViewSave: any;
+// 	onChange: (arg0: MosaicObject) => any;
+// 	onClose: () => void;
+// 	onRemove: (arg0: MosaicObject) => any;
+// 	onSave: any;
+// }
