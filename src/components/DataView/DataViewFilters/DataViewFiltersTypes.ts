9dbc33ef7fc35cf24ce238252e10281a49fde78c
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2023-04-20 11:07:31 -0600

    Created folder for DVFilters and all its sub-files

---
+++ src/components/DataView/DataViewFilters/DataViewFiltersTypes.ts
@@ -0,0 +1,9 @@
+import { DataViewProps } from "../DataViewTypes";
+
+export interface DataViewFiltersProps {
+	activeFilters?: DataViewProps["activeFilters"];
+	filters?: DataViewProps["filters"];
+	filter?: DataViewProps["filter"];
+	onActiveFiltersChange?: DataViewProps["onActiveFiltersChange"];
+	loading?: DataViewProps["loading"];
+}
