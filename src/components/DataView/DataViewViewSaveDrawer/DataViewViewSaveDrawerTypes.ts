dac185d6401f97c702afba40ee39bbb224a5f155
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2023-04-20 12:25:14 -0600

    Created folder for DVViewSaveDrawer and all its sub-files

---
+++ src/components/DataView/DataViewViewSaveDrawer/DataViewViewSaveDrawerTypes.ts
@@ -0,0 +1,14 @@
+export interface DataViewViewSaveDrawerProps {
+	open?: any;
+	onClose?: any;
+	data?: any;
+	onSave?: any;
+	allowSharedViewSave?: any;
+}
+// interface DataViewViewSaveDrawerProps {
+// 	open: boolean;
+// 	onClose: () => void;
+// 	data: any;
+// 	onSave: any;
+// 	allowSharedViewSave: any;
+// }
