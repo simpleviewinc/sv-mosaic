39e1c887a7b36c19e3a65e5e1260c221131fa1ce
Mauricio A. De León Cárdenas <mauricio.deleon@globant.com>
2023-04-21 14:57:11 -0600

    Updated types for DVTr

--- src/components/DataView/DataViewTr/DataViewTrTypes.ts
+++ src/components/DataView/DataViewTr/DataViewTrTypes.ts
@@ -4,14 +4,14 @@ import { DataViewProps } from "../DataViewTypes";
 
 export interface DataViewTrProps {
 	bulkActions?: DataViewProps["bulkActions"];
-	checked?: boolean;
+	checked: boolean;
 	hasActions: boolean;
 	onReorder?: DataViewProps["onReorder"];
 	onCheckboxClick?: React.MouseEventHandler<HTMLButtonElement>;
 	primaryActions?: DataViewProps["primaryActions"];
 	additionalActions?: DataViewProps["additionalActions"];
-	originalRowData?: MosaicObject;
-	columns?: DataViewProps["columns"];
+	originalRowData: MosaicObject;
+	columns: DataViewProps["columns"];
 	row?: {[x: string]: any};
 	rowIdx: number;
 }
