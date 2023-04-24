04c30a116cd078065ac6051673b2d5005b776f43
Owen Allen <owenallenaz@gmail.com>
2023-04-24 17:43:45 +0000

    Updates

--- develop/react-syntax-highlighter_languages_highlight_juliaRepl.a3c7dea0cad6ee2c407b.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[85],{2577:function(module,exports){module.exports=function(hljs){return{contains:[{className:"meta",begin:/^julia>/,relevance:10,starts:{end:/^(?![ ]{6})/,subLanguage:"julia"},aliases:["jldoctest"]}]}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_juliaRepl.a3c7dea0cad6ee2c407b.bundle.js.map
--- develop/react-syntax-highlighter_languages_highlight_vbscriptHtml.a3c7dea0cad6ee2c407b.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[170],{2665:function(module,exports){module.exports=function(hljs){return{subLanguage:"xml",contains:[{begin:"<%",end:"%>",subLanguage:"vbscript"}]}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_vbscriptHtml.a3c7dea0cad6ee2c407b.bundle.js.map
