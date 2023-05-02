e2a910ebc2c9456df801ac986f886d688d0ec575
Andrés Hernández <jorge.hj@globant.com>
2023-05-02 16:05:25 -0600

    MOS-1047: Fix flawed SummaryPageTop component's padding

--- src/components/SummaryPageTopComponent/SummaryPageTopComponent.styled.tsx
+++ src/components/SummaryPageTopComponent/SummaryPageTopComponent.styled.tsx
@@ -2,30 +2,30 @@ import styled from "styled-components";
 import theme from "../../theme";
 
 export const StyledSummaryPageTopComponent = styled.div`
-    background: white;
-    border-bottom: 2px solid ${theme.newColors.grey2["100"]};
-    color: ${theme.newColors.almostBlack["100"]};
-    font-family: ${theme.fontFamily};
-    padding: 24px 24px 16px 24px;
-    display: flex;
-    gap: 24px;
-    align-items: center;
+  background: white;
+  border-bottom: 2px solid ${theme.newColors.grey2["100"]};
+  color: ${theme.newColors.almostBlack["100"]};
+  font-family: ${theme.fontFamily};
+  padding: 24px 24px 16px 24px;
+  display: flex;
+  gap: 24px;
+  align-items: center;
 
-    .img-rounded {
-        width: 71px;
-        height: 71px;
-        border: 2px solid ${theme.newColors.grey2["100"]};
-        border-radius: 50%;
-        opacity: 1;
-    }
+  .img-rounded {
+    width: 71px;
+    height: 71px;
+    border: 2px solid ${theme.newColors.grey2["100"]};
+    border-radius: 50%;
+    opacity: 1;
+  }
 `;
 
 export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 16px;
-    width: 100%;
-    flex-wrap: wrap;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  width: 100%;
+  flex-wrap: wrap;
 `;
 
 export const ContainerTitle = styled.div`
@@ -44,43 +44,40 @@ export const ContainerTitle = styled.div`
 `;
 
 export const Row = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: 100%;
-    flex-wrap: wrap;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  flex-wrap: wrap;
 `;
 
 export const ContainerItems = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-	align-items: center;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
 
-    div:first-child {
-        padding-left: 0px;
-    };
-
-    div:last-child {
-        border: none;
-    };
+  div:last-child {
+    border: none;
+  }
 `;
 
 export const Item = styled.div`
-    padding: 0 16px;
-    border-right: 2px solid ${theme.newColors.grey2["100"]};
+  padding: 0 16px 0 0;
+  margin-right: 16px;
+  border-right: 2px solid ${theme.newColors.grey2["100"]};
 
-	& p,
-	& h1,
-	& h2,
-	& h3,
-	& h4,
-	& h5,
-	& h6 {
-		margin: 0px;
-		font-size: 14px;
-		max-width: 280px;
-		overflow: hidden;
-		text-overflow: ellipsis;
-		white-space: nowrap;
-	}
+  & p,
+  & h1,
+  & h2,
+  & h3,
+  & h4,
+  & h5,
+  & h6 {
+    margin: 0px;
+    font-size: 14px;
+    max-width: 280px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
 `;
