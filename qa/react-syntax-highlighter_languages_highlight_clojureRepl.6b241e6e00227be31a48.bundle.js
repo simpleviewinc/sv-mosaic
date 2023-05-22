c0ea898940fd4a660bdfa8c3f8de146b4ea9e226
Owen Allen <owenallenaz@gmail.com>
2023-05-22 19:31:16 +0000

    Updates

--- qa/react-syntax-highlighter_languages_highlight_clojureRepl.6b241e6e00227be31a48.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{2566:function(module,exports){module.exports=function(hljs){return{contains:[{className:"meta",begin:/^([\w.-]+|\s*#_)?=>/,starts:{end:/$/,subLanguage:"clojure"}}]}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_clojureRepl.6b241e6e00227be31a48.bundle.js.map
--- qa/react-syntax-highlighter_languages_highlight_erb.6b241e6e00227be31a48.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[52],{2592:function(module,exports){module.exports=function(hljs){return{subLanguage:"xml",contains:[hljs.COMMENT("<%#","%>"),{begin:"<%[%=-]?",end:"[%-]?%>",subLanguage:"ruby",excludeBegin:!0,excludeEnd:!0}]}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_erb.6b241e6e00227be31a48.bundle.js.map
