ce94523f1487008c7321d4e4c2ad5014a3b5a059
Owen Allen <owenallenaz@gmail.com>
2023-07-19 09:40:29 +0000

    Updates

--- develop/react-syntax-highlighter_languages_highlight_erb.fb8d7c5635be62a3b7f5.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[52],{2596:function(module,exports){module.exports=function(hljs){return{subLanguage:"xml",contains:[hljs.COMMENT("<%#","%>"),{begin:"<%[%=-]?",end:"[%-]?%>",subLanguage:"ruby",excludeBegin:!0,excludeEnd:!0}]}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_erb.fb8d7c5635be62a3b7f5.bundle.js.map
---
+++ develop/react-syntax-highlighter_languages_highlight_vbscriptHtml.65be0f22363ada6c7cc6.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[170],{2766:function(module,exports){module.exports=function(hljs){return{subLanguage:"xml",contains:[{begin:"<%",end:"%>",subLanguage:"vbscript"}]}}}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_vbscriptHtml.65be0f22363ada6c7cc6.bundle.js.map
