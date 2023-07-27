2c9495d6ef81b8fc2458ddc09124f4e4171506e6
George Batt <george.batt@simpleviewinc.com>
2023-07-27 15:13:14 +0100

    Updates

---
+++ qa/react-syntax-highlighter_languages_highlight_juliaRepl.c2cc09d3e6217967248b.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[85],{2660:function(module,exports){module.exports=function(hljs){return{contains:[{className:"meta",begin:/^julia>/,relevance:10,starts:{end:/^(?![ ]{6})/,subLanguage:"julia"},aliases:["jldoctest"]}]}}}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_juliaRepl.c2cc09d3e6217967248b.bundle.js.map
---
+++ qa/react-syntax-highlighter_languages_highlight_vbscriptHtml.c2cc09d3e6217967248b.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[170],{2748:function(module,exports){module.exports=function(hljs){return{subLanguage:"xml",contains:[{begin:"<%",end:"%>",subLanguage:"vbscript"}]}}}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_vbscriptHtml.c2cc09d3e6217967248b.bundle.js.map
