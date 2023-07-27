9cdd7b419e4a773947b359f9cba8663a54a3260e
George Batt <george.batt@simpleviewinc.com>
2023-07-27 15:05:12 +0100

    Updates

---
+++ develop/react-syntax-highlighter_languages_highlight_clojureRepl.c2cc09d3e6217967248b.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{2600:function(module,exports){module.exports=function(hljs){return{contains:[{className:"meta",begin:/^([\w.-]+|\s*#_)?=>/,starts:{end:/$/,subLanguage:"clojure"}}]}}}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_clojureRepl.c2cc09d3e6217967248b.bundle.js.map
---
+++ develop/react-syntax-highlighter_languages_highlight_ldif.c2cc09d3e6217967248b.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[88],{2664:function(module,exports){module.exports=function(hljs){return{contains:[{className:"attribute",begin:"^dn",end:": ",excludeEnd:!0,starts:{end:"$",relevance:0},relevance:10},{className:"attribute",begin:"^\\w",end:": ",excludeEnd:!0,starts:{end:"$",relevance:0}},{className:"literal",begin:"^-",end:"$"},hljs.HASH_COMMENT_MODE]}}}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_ldif.c2cc09d3e6217967248b.bundle.js.map
