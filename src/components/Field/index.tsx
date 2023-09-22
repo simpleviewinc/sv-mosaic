189fa0d0f1a935966a8d7b0beb6c3e1f576242bf
George Batt <george.batt@simpleviewinc.com>
2023-09-22 14:30:11 +0100

    Barrel field exports

--- src/components/Field/index.tsx
+++ src/components/Field/index.tsx
@@ -1,3 +1,65 @@
 export { default } from "./Field";
 export * from "./Field";
 export * from "./FieldTypes";
+
+export { default as FormFieldAddress } from "./FormFieldAddress";
+export * from "./FormFieldAddress";
+
+export { default as FormFieldAdvancedSelection } from "./FormFieldAdvancedSelection";
+export * from "./FormFieldAdvancedSelection";
+
+export { default as FormFieldCheckbox } from "./FormFieldCheckbox";
+export * from "./FormFieldCheckbox";
+
+export { default as FormFieldChipSingleSelect } from "./FormFieldChipSingleSelect";
+export * from "./FormFieldChipSingleSelect";
+
+export { default as FormFieldColorPicker } from "./FormFieldColorPicker";
+export * from "./FormFieldColorPicker";
+
+export { default as FormFieldDate } from "./FormFieldDate/DateField";
+export * from "./FormFieldDate/DateField";
+export * from "./FormFieldDate/DatePicker";
+export * from "./FormFieldDate/TimePicker";
+
+export { default as FormFieldDropdownSingleSelection } from "./FormFieldDropdownSingleSelection";
+export * from "./FormFieldDropdownSingleSelection";
+
+export { default as FormFieldImageUpload } from "./FormFieldImageUpload";
+export * from "./FormFieldImageUpload";
+
+export { default as FormFieldImageVideoLinkDocumentBrowsing } from "./FormFieldImageVideoLinkDocumentBrowsing";
+export * from "./FormFieldImageVideoLinkDocumentBrowsing";
+
+export { default as FormFieldMapCoordinates } from "./FormFieldMapCoordinates";
+export * from "./FormFieldMapCoordinates";
+
+export { default as FormFieldMatrix } from "./FormFieldMatrix";
+export * from "./FormFieldMatrix";
+
+export { default as FormFieldNumberTable } from "./FormFieldNumberTable";
+export * from "./FormFieldNumberTable";
+
+export { default as FormFieldPhoneSelectionDropdown } from "./FormFieldPhoneSelectionDropdown";
+export * from "./FormFieldPhoneSelectionDropdown";
+
+export { default as FormFieldRadio } from "./FormFieldRadio";
+export * from "./FormFieldRadio";
+
+export { default as FormFieldRaw } from "./FormFieldRaw";
+export * from "./FormFieldRaw";
+
+export { default as FormFieldTable } from "./FormFieldTable";
+export * from "./FormFieldTable";
+
+export { default as FormFieldText } from "./FormFieldText";
+export * from "./FormFieldText";
+
+export { default as FormFieldTextEditor } from "./FormFieldTextEditor";
+export * from "./FormFieldTextEditor";
+
+export { default as FormFieldToggleSwitch } from "./FormFieldToggleSwitch";
+export * from "./FormFieldToggleSwitch";
+
+export { default as FormFieldUpload } from "./FormFieldUpload";
+export * from "./FormFieldUpload";
