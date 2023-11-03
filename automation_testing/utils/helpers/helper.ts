f26b4ff9e9b52bbba40f62ca32e45b83f961b69a
George Batt <george.batt@simpleviewinc.com>
2023-11-03 14:53:39 +0000

    (Button) Updates tests

--- automation_testing/utils/helpers/helper.ts
+++ automation_testing/utils/helpers/helper.ts
@@ -73,3 +73,19 @@ export const getRandomHexCode = (size: number): string => {
 export const rgbToHex = (r: number, g: number, b: number): string => {
 	return ("#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)).toLocaleUpperCase();
 }
+
+export const hexToRgb = (hex: string) => {
+	const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+	if (!result) {
+		return "";
+	}
+
+	const { r, g, b } = {
+		r: parseInt(result[1], 16),
+		g: parseInt(result[2], 16),
+		b: parseInt(result[3], 16)
+	};
+
+	return `rgb(${r}, ${g}, ${b})`;
+}
