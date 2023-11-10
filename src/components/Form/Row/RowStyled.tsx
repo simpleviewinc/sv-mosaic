a0a9433c4e452ced980aaaf5bf6e7fd2c6f8b06d
George Batt <george.batt@simpleviewinc.com>
2023-11-10 15:34:57 +0000

    (Form) Adds support for custom min width section grid collapse

--- src/components/Form/Row/RowStyled.tsx
+++ src/components/Form/Row/RowStyled.tsx
@@ -3,17 +3,23 @@ import styled from "styled-components";
 // Components
 import { containerQuery } from "@root/utils/css";
 
-export const StyledRow = styled.div<{$columns?: number}>`
-	${({$columns}) => $columns && `
+export const StyledRow = styled.div<{$columns?: number, $gridMinWidth?: string}>`
+	${({$columns, $gridMinWidth}) => $columns && `
 		display: grid;
 		grid-template-columns: repeat(1,minmax(0,1fr));
-		gap: 24px 40px;
+		gap: 24px;
 
-		${containerQuery("md")} {
-			grid-template-columns: repeat(${$columns > 1 ? 2 : 1},minmax(0,1fr));
-		}
-		${containerQuery("lg")} {
-			grid-template-columns: repeat(${$columns},minmax(0,1fr));
-		}
+		${$gridMinWidth ? `
+			@container form (min-width: ${$gridMinWidth}) {
+				grid-template-columns: repeat(${$columns},minmax(0,1fr));
+			}
+		` : `
+			${containerQuery("md", "FORM")} {
+				grid-template-columns: repeat(${$columns > 1 ? 2 : 1},minmax(0,1fr));
+			}
+			${containerQuery("lg", "FORM")} {
+				grid-template-columns: repeat(${$columns},minmax(0,1fr));
+			}
+		`}
 	`}
 `;
