a40ee108518ae6cee392fcbcbb44e3a0a37a9b53
George Batt <george.batt@simpleviewinc.com>
2023-12-04 15:21:25 +0000

    (SideNav) Adds meta key to list of event exceptions

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -22,7 +22,7 @@ const SideNav = (props: SideNavProps): ReactElement => {
 	 */
 	const onLinkClicked = (args: { item: Item, event?: MouseEvent }) => {
 		const { item, event } = args;
-		const hasModifier = event.ctrlKey || event.shiftKey || event.altKey;
+		const hasModifier = event.ctrlKey || event.metaKey || event.shiftKey || event.altKey;
 
 		if (hasModifier) {
 			return;
