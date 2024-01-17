f50d3dd31432cbc41f38672381ad7b89be464cfc
George Batt <george.batt@simpleviewinc.com>
2024-01-17 13:02:13 +0000

    (array) Updates arrayDifference for wider usage

--- src/utils/array/arrayDifference.ts
+++ src/utils/array/arrayDifference.ts
@@ -1,20 +1,20 @@
 /**
- * Constructs an array containing all of the items that exist in the left array but not in the right array.
+ * Constructs an array containing the difference between the given arrays
  *
- * @param left The source array
- * @param right The comparison array
- * @returns An array containg the items that exist in the left array but not in the right array
+ * @param array1 The source array
+ * @param array2 The comparison array
+ * @returns An array containg the items that exist in either array1 but not array2 and vise versa
  */
-function arrayDifference<T>(left: T[], right: T[]): T[] {
-	const difference: T[] = [];
+function arrayDifference<T>(array1: T[], array2: T[]): T[] {
+	const set1 = new Set(array1);
+	const set2 = new Set(array2);
 
-	for (const item of left) {
-		if (!right.includes(item)) {
-			difference.push(item);
-		}
-	}
+	const difference1 = [...array1].filter(item => !set2.has(item));
+	const difference2 = [...array2].filter(item => !set1.has(item));
 
-	return difference;
+	const result = [...difference1, ...difference2];
+
+	return result;
 }
 
 export default arrayDifference;
