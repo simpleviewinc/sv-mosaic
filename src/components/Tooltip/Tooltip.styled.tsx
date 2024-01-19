73e9f8f8230d176215a8d9f689f33ca59b7306ce
George Batt <george.batt@simpleviewinc.com>
2024-01-19 15:33:53 +0000

    (Tooltip) TypeScript fix

--- src/components/Tooltip/Tooltip.styled.tsx
+++ src/components/Tooltip/Tooltip.styled.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import theme from "@root/theme";
 import Popper from "@mui/material/Popper";
 
-export const TooltipPopper = styled(Popper)`
+export const TooltipPopper: any = styled(Popper)`
 	z-index: 1500;
 	background: ${theme.newColors.almostBlack["100"]};
 	color: white;
