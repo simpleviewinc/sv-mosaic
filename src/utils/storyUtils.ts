f375c49c07a86930a07a9af542e46c2a5fb36ac4
George Batt <george.batt@simpleviewinc.com>
2024-02-09 16:25:24 +0000

    (Form) Updates common submit button handleSubmit usage

--- src/utils/storyUtils.ts
+++ src/utils/storyUtils.ts
@@ -15,7 +15,7 @@ export const renderButtons = (handleSubmit: FormHandleSubmit, show = { showCance
 	},
 	{
 		label: "Save",
-		onClick: () => handleSubmit((data) => alert("Form submitted with the following data: " + JSON.stringify(data, null, " "))),
+		onClick: handleSubmit((data) => alert("Form submitted with the following data: " + JSON.stringify(data, null, " "))),
 		color: "yellow",
 		variant: "contained",
 		show: show.showSave,
