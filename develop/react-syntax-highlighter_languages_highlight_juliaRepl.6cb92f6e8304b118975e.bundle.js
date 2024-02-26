f603d3f5e0b2b74241cab347ade00f8441bf9dd3
Owen Allen <owenallenaz@gmail.com>
2024-02-26 17:48:34 +0000

    Updates

--- develop/react-syntax-highlighter_languages_highlight_juliaRepl.6cb92f6e8304b118975e.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[85],{2595:function(module,exports){module.exports=function(hljs){return{contains:[{className:"meta",begin:/^julia>/,relevance:10,starts:{end:/^(?![ ]{6})/,subLanguage:"julia"},aliases:["jldoctest"]}]}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_juliaRepl.6cb92f6e8304b118975e.bundle.js.map
---
+++ develop/react-syntax-highlighter_languages_highlight_vbscriptHtml.a0d39b8ec51d3dd839ec.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[170],{2683:function(module,exports){module.exports=function(hljs){return{subLanguage:"xml",contains:[{begin:"<%",end:"%>",subLanguage:"vbscript"}]}}}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_vbscriptHtml.a0d39b8ec51d3dd839ec.bundle.js.map
