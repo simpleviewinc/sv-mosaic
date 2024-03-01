5297a0e61179a873d25a6004731f33e211680615
Owen Allen <owenallenaz@gmail.com>
2024-03-01 14:34:55 +0000

    Updates

---
+++ qa/react-syntax-highlighter_languages_highlight_taggerscript.75bcf300de6761e3ac73.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[159],{2673:function(module,exports){module.exports=function(hljs){return{contains:[{className:"comment",begin:/\$noop\(/,end:/\)/,contains:[{begin:/\(/,end:/\)/,contains:["self",{begin:/\\./}]}],relevance:10},{className:"keyword",begin:/\$(?!noop)[a-zA-Z][_a-zA-Z0-9]*/,end:/\(/,excludeEnd:!0},{className:"variable",begin:/%[_a-zA-Z0-9:]*/,end:"%"},{className:"symbol",begin:/\\./}]}}}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_taggerscript.75bcf300de6761e3ac73.bundle.js.map
