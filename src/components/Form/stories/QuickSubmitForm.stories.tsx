c5b453e783e15c74da6ba77d3d2ccf6073225844
George Batt <george.batt@simpleviewinc.com>
2024-03-02 00:02:23 +0000

    (Form) Create story to demonstrate native onSubmit

---
+++ src/components/Form/stories/QuickSubmitForm.stories.tsx
@@ -0,0 +1,76 @@
+import * as React from "react";
+import { ReactElement, useEffect, useMemo } from "react";
+
+// Utils
+import { formActions, useForm } from "@root/components/Form";
+
+// Components
+import Form from "../Form";
+
+// Types
+import { FieldDef } from "@root/components/Field";
+
+import { ORIGINAL_BODY_MARGIN } from "./utils";
+import { ButtonProps } from "@root/components/Button";
+
+export default {
+	title: "Components/Form",
+};
+
+export const QuickSubmit = (): ReactElement => {
+	const { state, dispatch } = useForm();
+
+	useEffect(() => {
+		document.body.style.margin = "0px";
+
+		return () => {
+			document.body.style.margin = ORIGINAL_BODY_MARGIN;
+		};
+	}, []);
+
+	const fields = useMemo(
+		(): FieldDef[] =>
+			[
+				{
+					name: "firstName",
+					label: "First Name",
+					type: "text",
+				},
+				{
+					name: "lastName",
+					label: "Last Name",
+					type: "text",
+				},
+			],
+		[],
+	);
+
+	const buttons = useMemo<ButtonProps[]>(() => [
+		{
+			label: "Save",
+			color: "yellow",
+			variant: "contained",
+			type: "submit",
+		},
+	], []);
+
+	const onSubmit = async () => {
+		const { valid, data } = await dispatch(formActions.submitForm());
+		if (!valid) return;
+
+		alert("Form submitted with the following data: " + JSON.stringify(data, null, " "));
+	};
+
+	return (
+		<div style={{ height: "100vh" }}>
+			<Form
+				buttons={buttons}
+				title="Quick Submit"
+				state={state}
+				fields={fields}
+				dispatch={dispatch}
+				onSubmit={onSubmit}
+			/>
+		</div>
+	);
+};
