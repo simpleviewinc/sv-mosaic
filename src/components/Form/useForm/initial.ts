bf47f3e676dfdfa90e988aa783be49353ce3cd21
George Batt <george.batt@simpleviewinc.com>
2024-03-13 11:42:25 +0000

    (Form) Correct instantiation of initial form state and stable

--- src/components/Form/useForm/initial.ts
+++ src/components/Form/useForm/initial.ts
@@ -1,23 +1,27 @@
 import { FormStable, FormState } from "./types";
 
-export const initialState: FormState = {
-	internalData: {},
-	data: {},
-	errors: {},
-	disabled: true,
-	touched: {},
-	submitWarning: { open: false, lead: "", reasons: [] },
-	waits: [],
-	loadingInitial: true,
-};
+export function getInitialState(): FormState {
+	return {
+		internalData: {},
+		data: {},
+		errors: {},
+		disabled: true,
+		touched: {},
+		submitWarning: { open: false, lead: "", reasons: [] },
+		waits: [],
+		loadingInitial: true,
+	};
+}
 
-export const initialStable: FormStable = {
-	...initialState,
-	initialData: {},
-	fields: {},
-	mounted: {},
-	internalValidators: {},
-	hasBlurred: {},
-	moveToError: false,
-	loadingInitial: true,
-};
+export function getInitialStable(): FormStable {
+	return {
+		...getInitialState(),
+		initialData: {},
+		fields: {},
+		mounted: {},
+		internalValidators: {},
+		hasBlurred: {},
+		moveToError: false,
+		loadingInitial: true,
+	};
+}
