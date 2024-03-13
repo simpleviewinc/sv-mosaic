5254edde0c8b6b64a38ba689f83995eb0ce4b21d
George Batt <george.batt@simpleviewinc.com>
2024-03-13 11:42:25 +0000

    (Form) Correct instantiation of initial form state and stable

--- src/components/Form/useForm/useForm.ts
+++ src/components/Form/useForm/useForm.ts
@@ -26,14 +26,14 @@ import { getToggle, wrapToggle } from "@root/utils/toggle";
 import { MosaicObject } from "@root/types";
 import { FieldDefSanitized } from "../../Field";
 import { getFieldConfig } from "../Col/fieldConfigMap";
-import { initialState, initialStable } from "./initial";
+import { getInitialState, getInitialStable } from "./initial";
 import { reducer } from "./reducers";
 import { cleanValue, mapsValidators, runValidators, stateFromStable } from "./utils";
 
 export function useForm(): UseFormReturn {
-	const stable = useRef<FormStable>(structuredClone(initialStable));
+	const stable = useRef<FormStable>(getInitialStable());
 
-	const [state, dispatch] = useReducer(reducer, initialState);
+	const [state, dispatch] = useReducer(reducer, getInitialState());
 
 	const getFieldFromExtra = useCallback((name: string) => {
 		if (!stable.current.fields[name]) {
@@ -217,7 +217,7 @@ export function useForm(): UseFormReturn {
 		}), {});
 
 		stable.current = {
-			...initialState,
+			...getInitialState(),
 			...stable.current,
 			hasBlurred: {},
 			data: values,
