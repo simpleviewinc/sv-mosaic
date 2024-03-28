0a83658122121c2e83623c617358fc0fe1448d11
George Batt <george.batt@simpleviewinc.com>
2024-03-28 12:07:48 +0000

    (storybook) Print environment and version above interface

---
+++ .storybook/env-banner/environments.js
@@ -0,0 +1,26 @@
+export const environments = [
+    {
+        location: "localhost",
+        name: "local",
+    },
+    {
+        location: "kube.simpleview.io:10000",
+        name: "local",
+    },
+    {
+        location: "simpleviewinc.github.io/sv-mosaic/develop",
+        name: "develop",
+    },
+    {
+        location: "simpleviewinc.github.io/sv-mosaic/qa",
+        name: "qa",
+    },
+    {
+        location: "simpleviewinc.github.io/sv-mosaic/staging",
+        name: "staging",
+    },
+    {
+        location: "simpleviewinc.github.io/sv-mosaic/master",
+        name: "master",
+    },
+];
