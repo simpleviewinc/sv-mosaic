378f5029dfb5c3c9fc5b77d6c6d8691861038382
George Batt <george.batt@simpleviewinc.com>
2024-04-24 10:14:50 +0100

    (Checkbox) Use form field name as ID if available, fallback to a React generated ID if not

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,33 +1,40 @@
 import * as React from "react";
-import { memo } from "react";
+import { useId, memo } from "react";
 import { CheckboxProps } from "./CheckboxTypes";
 
 import { StyledCheckbox, StyledFormControlLabel } from "./Checkbox.styled";
 
-const Checkbox = (props: CheckboxProps) => (
-	<>
-		<StyledFormControlLabel
-			label={props.label}
-			labelPlacement="end"
-			data-testid="label-test-id"
-			value={props.value}
-			disabled={props.disabled}
-			control={(
-				<StyledCheckbox
-					data-testid="checkbox-test-id"
-					className={`
-						custom-checkbox
-						${props.checked ? "checked" : ""}
-						${props.className ? props.className : ""}
-					`}
-					edge={props.edge}
-					checked={props.checked}
-					onClick={props.onClick}
-					indeterminate={props.indeterminate}
-				/>
-			)}
-		/>
-	</>
-);
+const Checkbox = (props: CheckboxProps) => {
+	const fallbackId = useId();
+	const id = props.id || fallbackId;
+
+	return (
+		<>
+			<StyledFormControlLabel
+				label={props.label}
+				labelPlacement="end"
+				data-testid="label-test-id"
+				value={props.value}
+				disabled={props.disabled}
+				htmlFor={id}
+				control={(
+					<StyledCheckbox
+						data-testid="checkbox-test-id"
+						className={`
+							custom-checkbox
+							${props.checked ? "checked" : ""}
+							${props.className ? props.className : ""}
+						`}
+						edge={props.edge}
+						checked={props.checked}
+						onClick={props.onClick}
+						indeterminate={props.indeterminate}
+						id={id}
+					/>
+				)}
+			/>
+		</>
+	);
+};
 
 export default memo(Checkbox);
