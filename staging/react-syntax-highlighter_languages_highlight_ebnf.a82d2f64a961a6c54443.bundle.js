a8a5c1f515acdfeb083e4734b1fc557c0f1a885c
Owen Allen <owenallenaz@gmail.com>
2024-04-29 09:06:03 +0000

    Updates

---
+++ staging/react-syntax-highlighter_languages_highlight_ebnf.a82d2f64a961a6c54443.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[49],{2654:function(module,exports){module.exports=function(hljs){var commentMode=hljs.COMMENT(/\(\*/,/\*\)/);return{illegal:/\S/,contains:[commentMode,{className:"attribute",begin:/^[ ]*[a-zA-Z][a-zA-Z-_]*([\s-_]+[a-zA-Z][a-zA-Z]*)*/},{begin:/=/,end:/[.;]/,contains:[commentMode,{className:"meta",begin:/\?.*\?/},{className:"string",variants:[hljs.APOS_STRING_MODE,hljs.QUOTE_STRING_MODE,{begin:"`",end:"`"}]}]}]}}}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_ebnf.a82d2f64a961a6c54443.bundle.js.map
