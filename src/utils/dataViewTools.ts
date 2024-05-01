a8e8aef2ea65aee76bccd44864a5fdf657b7a156
George Batt <george.batt@simpleviewinc.com>
2024-05-01 12:31:09 +0100

    (DateView) Improve on the type of data

--- src/utils/dataViewTools.ts
+++ src/utils/dataViewTools.ts
@@ -1,7 +1,6 @@
-import { DataViewColumn } from "../components/DataView";
-import { MosaicObject } from "../types";
+import { DataViewColumn, DataViewProps } from "../components/DataView";
 
-export function transformColumn(row: MosaicObject, column: DataViewColumn): unknown {
+export function transformColumn(row: DataViewProps["data"][number], column: DataViewColumn): unknown {
 	let data = row[column.column || column.name];
 	if (data !== undefined && column.transforms !== undefined) {
 		for (const transform of column.transforms) {
@@ -14,7 +13,7 @@ export function transformColumn(row: MosaicObject, column: DataViewColumn): unkn
 	return data;
 }
 
-export function transformRows(rows: MosaicObject[], columns: DataViewColumn[]): MosaicObject[] {
+export function transformRows(rows: DataViewProps["data"], columns: DataViewColumn[]): DataViewProps["data"] {
 	const newRows = rows.map((row) => {
 		const newRow = {
 			...row,
