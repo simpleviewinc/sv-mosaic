6c7bf70cb77339c83def70ac755cf8e96b073f43
Owen Allen <owenallenaz@gmail.com>
2024-05-16 09:35:49 +0000

    Updates

---
+++ qa/react-syntax-highlighter_languages_highlight_vbscriptHtml.72dfe8388b452a67d56a.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[170],{2667:function(module,exports){module.exports=function(hljs){return{subLanguage:"xml",contains:[{begin:"<%",end:"%>",subLanguage:"vbscript"}]}}}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_vbscriptHtml.72dfe8388b452a67d56a.bundle.js.map
