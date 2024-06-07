80a1438e368fb8dc21b5b6cfa22ece89124ec300
George Batt <george.batt@simpleviewinc.com>
2024-06-07 16:33:39 +0100

    Migration script prunes dependencies and creates dockerfile

--- migrate.sh
+++ migrate.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-mkdir -p containers/lib
+LIB_DIRECTORY="./containers/lib"
+
+mkdir -p $LIB_DIRECTORY
 
 find . '(' \
         '(' \
             -path './.yarn' -o \
-            -path './src/**/*.tsx' -o \
+            -path './src/**/*.css' -o \
+            -path './src/**/*.json' -o \
             -path './src/**/*.ts' -o \
+            -path './src/**/*.tsx' -o \
             -path './.yarnrc.yml' -o \
             -path './package.json' -o \
             -path './tsconfig.cjs.json' -o \
@@ -19,7 +23,69 @@ find . '(' \
             -name '*.test.tsx' \
         ')' \
     ')' \
-    -exec cp -r --parents \{\} ./containers/lib \;
+    -exec cp -r --parents \{\} $LIB_DIRECTORY \;
 
-cd ./containers/lib
+cd $LIB_DIRECTORY
+
 yarn install
+
+yarn remove \
+    @babel/cli \
+    @babel/core \
+    @babel/plugin-transform-runtime \
+    @babel/polyfill \
+    @babel/preset-env \
+    @babel/preset-react \
+    @babel/preset-typescript \
+    @babel/runtime \
+    @hot-loader/react-dom \
+    @storybook/addon-docs \
+    @storybook/addon-knobs \
+    @storybook/addon-storysource \
+    @storybook/addon-viewport \
+    @storybook/react \
+    @testing-library/jest-dom \
+    @testing-library/react \
+    @types/jest \
+    @types/testing-library__jest-dom \
+    @types/testing-library__react \
+    babel-loader \
+    babel-plugin-module-resolver \
+    css-loader \
+    gh-pages \
+    identity-obj-proxy \
+    jest \
+    jest-canvas-mock \
+    jest-fail-on-console \
+    mocha \
+    postcss \
+    react-hot-loader \
+    release-it \
+    style-loader \
+    ts-jest \
+    webpack \
+    webpack-cli \
+    webpack-dev-server \
+    @date-io/date-fns
+
+cat > Dockerfile <<- EOM
+FROM node:18.18.2
+
+RUN apt-get update
+RUN apt-get -y install gettext-base jq
+RUN corepack enable && yarn set version 3.1.1
+
+COPY package.json /app/package.json
+COPY .yarnrc.yml /app/.yarnrc.yml
+COPY .yarn /app/.yarn
+COPY yarn.lock /app/yarn.lock
+
+RUN cd /app && yarn install
+
+COPY src /app/src
+COPY tsconfig.json /app/tsconfig.json
+COPY tsconfig.cjs.json /app/tsconfig.cjs.json
+COPY tsconfig.esm.json /app/tsconfig.esm.json
+
+WORKDIR /app
+EOM
