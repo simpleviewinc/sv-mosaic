4220fd76b3a74b2b9fd0f50274ce938890ab2e16
George Batt <george.batt@simpleviewinc.com>
2024-06-10 22:57:50 +0100

    Copies test files

---
+++ containers/lib/src/__tests__/components/Field/FormFieldText/FormFieldText.test.tsx
@@ -0,0 +1,70 @@
+import { render, screen } from "@testing-library/react";
+import * as React from "react";
+
+// Components
+import TextField from "./FormFieldText";
+
+describe("TextField component", () => {
+	it("should display the placeholder", () => {
+		render(
+			<TextField
+				fieldDef={{
+					name: "textField",
+					type: "text",
+					label: "Label test",
+					instructionText: "Instruction text",
+					inputSettings: {
+						placeholder: "placeholder",
+					},
+				}}
+			/>,
+		);
+		const placeholderElement = screen.getByPlaceholderText("placeholder");
+		expect(placeholderElement).toBeDefined();
+	});
+});
+
+describe("TextField multiline behaviour", () => {
+	it("should render an input element when multiline is off", () => {
+		render(
+			<TextField
+				fieldDef={{
+					name: "textField",
+					type: "text",
+					label: "Label test",
+					helperText: "Helper text",
+					inputSettings: {
+						placeholder: "placeholder",
+						multiline: false,
+					},
+				}}
+
+			/>,
+		);
+		const textField = screen.getByPlaceholderText("placeholder");
+
+		expect(textField.nodeName).toBe("INPUT");
+	});
+
+	it("should render a text area element when the multiline prop is present", () => {
+		render(
+			<TextField
+				fieldDef={{
+					name: "textField",
+					type: "text",
+					label: "Label test",
+					helperText: "Helper text",
+					inputSettings: {
+						placeholder: "placeholder",
+						multiline: true,
+					},
+				}}
+
+			/>,
+		);
+
+		const textField = screen.getByPlaceholderText("placeholder");
+
+		expect(textField.nodeName).toBe("TEXTAREA");
+	});
+});
