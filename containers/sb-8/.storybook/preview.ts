c2a7357e6943f509fe53ceaf2984c05f2990f188
George Batt <george.batt@simpleviewinc.com>
2024-06-17 19:00:49 +0100

    Tidy up. ESLint migration and enforcment.

--- containers/sb-8/.storybook/preview.ts
+++ containers/sb-8/.storybook/preview.ts
@@ -8,6 +8,17 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    /**
+     * There's a problem with the react-element-to-jsx-string 15.0.0
+     * package that causes Storybook to hang indefinitely when any
+     * component props refer to a DOM element. This is a tempoary
+     * fix.
+     *
+     * @link https://github.com/storybookjs/storybook/issues/19575
+     */
+    docs: {
+      source: { type: 'code' }
+    }
   },
 };
 
--- scripts/bump-packages.ts
+++ scripts/bump-packages.ts
@@ -6,26 +6,26 @@ const containersDir = path.resolve(__dirname, "../containers");
 const containers = fs.readdirSync(containersDir);
 
 containers.forEach(container => {
-    try {
-        const packagePath = path.resolve(containersDir, container, "package.json");
-        const packageContents = JSON.parse(fs.readFileSync(packagePath).toString());
+	try {
+		const packagePath = path.resolve(containersDir, container, "package.json");
+		const packageContents = JSON.parse(fs.readFileSync(packagePath).toString());
 
-        if (!packageContents.version) {
-            throw new Error(`${container} package.json has no version.`);
-        }
+		if (!packageContents.version) {
+			throw new Error(`${container} package.json has no version.`);
+		}
 
-        if (packageContents.version === version) {
-            throw new Error(`${container} package.json is already ${version}`)
-        }
+		if (packageContents.version === version) {
+			throw new Error(`${container} package.json is already ${version}`);
+		}
 
-        packageContents.version = version;
+		packageContents.version = version;
 
-        const newPackageContents = JSON.stringify(packageContents, undefined, 4) + "\n";
+		const newPackageContents = JSON.stringify(packageContents, undefined, 4) + "\n";
 
-        fs.writeFileSync(packagePath, newPackageContents);
+		fs.writeFileSync(packagePath, newPackageContents);
 
-        console.log(`Wrote ${container} package.json with new version (${version})`);
-    } catch(err) {
-        console.error(err instanceof Error ? err.message : err);
-    }
+		console.log(`Wrote ${container} package.json with new version (${version})`);
+	} catch (err) {
+		console.error(err instanceof Error ? err.message : err);
+	}
 });
