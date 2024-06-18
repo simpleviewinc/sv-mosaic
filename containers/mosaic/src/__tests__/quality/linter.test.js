5a8450c90c69fcf768c5b07ed61bb7cca8a8f868
George Batt <george.batt@simpleviewinc.com>
2024-06-18 11:02:51 +0100

    Working library linter and unit tests

--- containers/mosaic/src/__tests__/quality/linter.test.js
+++ containers/mosaic/src/__tests__/quality/linter.test.js
@@ -2,8 +2,8 @@ const path = require("path");
 const { execSync } = require("child_process");
 
 describe(__filename, function() {
-	test("Run Linter", async () => {
-		execSync("yarn run style", {
+	test("Run linter to check code style", async () => {
+		execSync("yarn lint", {
 			stdio : "inherit",
 			cwd: path.resolve(__dirname, "../.."),
 		});
