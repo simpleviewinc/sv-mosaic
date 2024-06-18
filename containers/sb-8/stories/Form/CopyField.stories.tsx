aee7b21d16dd1c2cb41cc9eecd1465f8135a776b
George Batt <george.batt@simpleviewinc.com>
2024-06-18 17:30:23 +0100

    Migrates form stories to Storybook 8

---
+++ containers/sb-8/stories/Form/CopyField.stories.tsx
@@ -0,0 +1,84 @@
+import * as React from "react";
+import { ReactElement, useEffect, useMemo } from "react";
+
+// Utils
+import { useForm } from "@root/components/Form";
+import { renderButtons } from "@root/utils/storyUtils";
+
+// Components
+import Form from "@root/components/Form";
+
+// Types
+import { FieldDef } from "@root/components/Field";
+
+export default {
+	title: "Components/Form",
+};
+
+const ORIGINAL_BODY_MARGIN = document.body.style.margin;
+
+export const CopyField = ({ showState }: typeof CopyField.args): ReactElement => {
+	const controller = useForm();
+	const { state, methods: { setFieldValue }, handleSubmit } = controller;
+
+	useEffect(() => {
+		document.body.style.margin = "0px";
+
+		return () => {
+			document.body.style.margin = ORIGINAL_BODY_MARGIN;
+		};
+	}, []);
+
+	const fields = useMemo(
+		() : FieldDef[] =>
+			[
+				{
+					name: "name",
+					label: "Name",
+					type: "text",
+					required: true,
+				},
+				{
+					name: "slug",
+					label: "Slug",
+					type: "text",
+					required: true,
+					helperText: "The text for this field will be populated with a slugified version the name field, but only if this field hasn't been touched",
+				},
+			],
+		[],
+	);
+
+	useEffect(() => {
+		if (!state.touched.slug) {
+			const transformedLabel = state.data.name?.trim().toLowerCase().replace(/ {1,}/g, "_").replace(/[^a-z_]/g, "");
+			setFieldValue({ name: "slug", value: transformedLabel });
+		}
+	}, [setFieldValue, state.data.name, state.touched]);
+
+	return (
+		<>
+			{
+				showState && <pre>{JSON.stringify(state, null, "  ")}</pre>
+			}
+			<div style={{ height: "100vh" }}>
+				<Form
+					{...controller}
+					buttons={renderButtons(handleSubmit)}
+					title="Validators story"
+					fields={fields}
+				/>
+			</div>
+		</>
+	);
+};
+
+CopyField.args = {
+	showState: false,
+};
+
+CopyField.argTypes = {
+	showState: {
+		name: "Show state",
+	},
+};
---
+++ containers/sb-8/stories/Form/RuntimeBehaviours.stories.tsx
@@ -0,0 +1,137 @@
+import * as React from "react";
+import { ReactElement, useEffect, useMemo } from "react";
+
+// Utils
+import { useForm } from "@root/components/Form";
+import { validateEmail, validateSlow } from "@root/components/Form/validators";
+import { renderButtons } from "@root/utils/storyUtils";
+
+// Components
+import Form from "@root/components/Form";
+
+// Types
+import { FieldDef } from "@root/components/Field";
+
+import { ORIGINAL_BODY_MARGIN } from "@root/components/Form/stories/utils";
+
+export default {
+	title: "Components/Form",
+};
+
+export const RuntimeBehaviours = ({ showState }: typeof RuntimeBehaviours.args): ReactElement => {
+	const controller = useForm();
+	const { state, methods: { setFieldValue }, handleSubmit } = controller;
+
+	useEffect(() => {
+		document.body.style.margin = "0px";
+
+		return () => {
+			document.body.style.margin = ORIGINAL_BODY_MARGIN;
+		};
+	}, []);
+
+	const fields = useMemo(
+		() : FieldDef[] =>
+			[
+				{
+					name: "text1",
+					label: "Simple Text",
+					type: "text",
+					instructionText: "testing",
+					validators: [validateEmail, validateSlow],
+				},
+				{
+					name: "text2",
+					label: "Text with validators and dynamic help",
+					type: "text",
+					helperText: state.data.text2,
+					validators: [validateEmail, validateSlow],
+				},
+				{
+					name: "text3",
+					label: "Text that copies to the next input",
+					type: "text",
+				},
+				{
+					name: "text4",
+					label: "Text that receives copy",
+					type: "text",
+				},
+				{
+					name: "text5",
+					label: "Text that enables another field",
+					type: "text",
+				},
+				{
+					name: "text6",
+					label: "Text that is initially disabled",
+					type: "text",
+					instructionText: "Type \"ENABLE\" into the previous field to enable this field",
+					disabled: [
+						({ data }) => data?.text5 !== "ENABLE",
+						true,
+						() => true,
+					],
+					required: true,
+				},
+			],
+		[state.data.text2],
+	);
+
+	useEffect(() => {
+		setFieldValue({
+			name: "text4",
+			value: state.data.text3,
+		});
+	}, [setFieldValue, state.data.text3]);
+
+	const setText1Value = function () {
+		setFieldValue({
+			name: "text1",
+			value: "test@test.com",
+		});
+	};
+
+	const setText2Value = function () {
+		setFieldValue({
+			name: "text2",
+			value: "notanemail",
+			validate: true,
+		});
+	};
+
+	return (
+		<>
+			{
+				showState && <pre>{JSON.stringify(state, null, "  ")}</pre>
+			}
+			<div style={{ height: "100vh" }}>
+				<Form
+					{...controller}
+					buttons={renderButtons(handleSubmit)}
+					title="Runtime behaviors"
+					fields={fields}
+				/>
+			</div>
+			<div>
+				<p>
+					Here are some buttons that are not part of the form, but can change
+					values in the form proving communication between in/out of the form.
+					Notice that settext2 runs the validation after setting the value.
+				</p>
+				<button onClick={setText1Value}>Set Text1 Value</button>
+				<button onClick={setText2Value}>Set Text2 Value</button>
+			</div>
+		</>
+	);
+};
+
+RuntimeBehaviours.args = {
+	showState: false,
+};
+
+RuntimeBehaviours.argTypes = {
+	showState: {
+		name: "Show state",
+	},
+};
