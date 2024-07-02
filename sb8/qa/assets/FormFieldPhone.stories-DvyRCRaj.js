b41af041916c367a7823a0afaa5fe0b9980bd7b3
Owen Allen <owenallenaz@gmail.com>
2024-07-02 09:47:37 +0000

    Updates

---
+++ sb8/qa/assets/FormFieldPhone.stories-DvyRCRaj.js
@@ -0,0 +1,75 @@
+import{j as e}from"./jsx-runtime-CexXSJP5.js";import{r as y}from"./index-BP8_t0zE.js";import{u as F,F as T}from"./formUtils-C5IZy0Gf.js";import{r as g}from"./storyUtils-ByV2Bsyv.js";import"./FormFieldText.styled-C9eExh2D.js";import"./theme-CtvfJOCv.js";import"./Button-BBNnoZZP.js";import"./useThemeProps-Ds-LJhvX.js";import"./ButtonBase-D0hlh4xx.js";import"./emotion-react.browser.esm-DIgfljIv.js";import"./assertThisInitialized-CO-dlV3K.js";import"./Popper-CwZOyH_P.js";import"./index-BxmsGmlx.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useToggle-lipGlewK.js";import"./useMosaicTranslation-DzvJLSut.js";import"./formControlState-CeBwnzuK.js";import"./createSvgIcon-CQgce1KM.js";import"./createSvgIcon-COYRGr1j.js";import"./useId-BlXdlps1.js";import"./useControlled-im5M3O13.js";import"./DataViewFilterMultiselectDropdownContent-O9LV55xE.js";import"./CheckboxList-BCG88NR0.js";import"./isSymbol-CEVJpixJ.js";import"./Checkbox-DnFKua5S.js";import"./FormControlLabel-CIIRzCdb.js";import"./reactTools-D2O6dTsh.js";import"./debounce-C_xMouBr.js";import"./DataViewPrimaryFilter.styled-DecvZ7Zb.js";import"./ButtonRow-BZVmEtFA.js";import"./Spinner-BPM3b5sV.js";import"./Chip-C1CRxgK0.js";import"./Typography-v36okEvj.js";import"./SubtitleText-jYpjhjNV.js";import"./sizes-C6oSOElx.js";import"./RadioButton-DUvfdQ-A.js";import"./StyledFormControl.styled-DBTTYzHm.js";import"./Toggle-BQ1JdWW7.js";import"./testIds-B75WQI-K.js";import"./ColorSelected-BZr_74pT.js";import"./TitleWrapper-DkQZJitR.js";import"./containerQuery-C4iAQuB_.js";import"./TitleText-Dy6rc9cG.js";import"./MoreVert-DQju6pbX.js";import"./ChevronRight-JG1qL3pJ.js";import"./PageHeader-B5WzlHGP.js";import"./MenuSelect-nAevESHO.js";import"./useMediaQuery-CVp5GJAd.js";import"./formats-CDjt32hU.js";import"./Dialog-Dh5ejmnv.js";import"./SideNav-BDfV6asS.js";import"./Snackbar-BbTrOH4B.js";import"./Close-B-5TycoQ.js";import"./ClickAwayListener-DFiznkjI.js";const He={title:"FormFields/FormFieldPhone"},t=({label:r,required:n,skeleton:s,disabled:o,instructionText:l,helperText:a,autoFormat:p,country:m})=>{const u=F(),{state:S,handleSubmit:q}=u,j=y.useMemo(()=>[{name:"phone",label:r,type:"phone",required:n,disabled:o,inputSettings:{autoFormat:p,country:m},helperText:a,instructionText:l}],[o,n,p,m,r,a,l]);return e.jsxs(e.Fragment,{children:[e.jsx("pre",{children:JSON.stringify(S,null,"  ")}),e.jsx(T,{...u,buttons:g(q),title:"Phone Field",fields:j,skeleton:s})]})};t.args={label:"Label",disabled:!1,required:!1,skeleton:!1,instructionText:"Instruction text",helperText:"Helper text",autoFormat:!0,country:""};t.argTypes={label:{name:"Label"},disabled:{name:"Disabled"},required:{name:"Required"},skeleton:{name:"Skeleton"},instructionText:{name:"Instruction Text"},helperText:{name:"Helper Text"},autoFormat:{name:"Autoformat"},country:{name:"Country"}};const i=()=>{const r=F(),{state:n,handleSubmit:s}=r,o=y.useMemo(()=>[{name:"phone",label:"Regular example",type:"phone",required:!1,disabled:!1,helperText:"Helper text",instructionText:'Default contry code is "us"'},{name:"countryCode",label:"With a country code provided",type:"phone",required:!1,disabled:!1,inputSettings:{country:"ar"},helperText:"Helper text",instructionText:'The country code of "ar" was provided'},{name:"autoformatEnabled",label:"Autoformat enabled",type:"phone",required:!1,disabled:!1,inputSettings:{autoFormat:!0,country:"us"},helperText:"Helper text",instructionText:"Type a phone number to see the format"}],[]);return e.jsxs(e.Fragment,{children:[e.jsx("pre",{children:JSON.stringify(n,null,"  ")}),e.jsx(T,{...r,buttons:g(s),title:"Form Title",description:"Form description",fields:o})]})};var d,c,f;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`({
+  label,
+  required,
+  skeleton,
+  disabled,
+  instructionText,
+  helperText,
+  autoFormat,
+  country
+}: typeof Playground.args): ReactElement => {
+  const controller = useForm();
+  const {
+    state,
+    handleSubmit
+  } = controller;
+  const fields = useMemo((): FieldDef[] => [{
+    name: "phone",
+    label,
+    type: "phone",
+    required,
+    disabled,
+    inputSettings: {
+      autoFormat,
+      country
+    },
+    helperText,
+    instructionText
+  }], [disabled, required, autoFormat, country, label, helperText, instructionText]);
+  return <>
+            <pre>{JSON.stringify(state, null, "  ")}</pre>
+            <Form {...controller} buttons={renderButtons(handleSubmit)} title="Phone Field" fields={fields} skeleton={skeleton} />
+        </>;
+}`,...(f=(c=t.parameters)==null?void 0:c.docs)==null?void 0:f.source}}};var h,x,b;i.parameters={...i.parameters,docs:{...(h=i.parameters)==null?void 0:h.docs,source:{originalSource:`(): ReactElement => {
+  const controller = useForm();
+  const {
+    state,
+    handleSubmit
+  } = controller;
+  const fields: FieldDef[] = useMemo(() => [{
+    name: "phone",
+    label: "Regular example",
+    type: "phone",
+    required: false,
+    disabled: false,
+    helperText: "Helper text",
+    instructionText: 'Default contry code is "us"'
+  }, {
+    name: "countryCode",
+    label: "With a country code provided",
+    type: "phone",
+    required: false,
+    disabled: false,
+    inputSettings: {
+      country: "ar"
+    },
+    helperText: "Helper text",
+    instructionText: 'The country code of "ar" was provided'
+  }, {
+    name: "autoformatEnabled",
+    label: "Autoformat enabled",
+    type: "phone",
+    required: false,
+    disabled: false,
+    inputSettings: {
+      autoFormat: true,
+      country: "us"
+    },
+    helperText: "Helper text",
+    instructionText: "Type a phone number to see the format"
+  }], []);
+  return <>
+            <pre>{JSON.stringify(state, null, "  ")}</pre>
+            <Form {...controller} buttons={renderButtons(handleSubmit)} title="Form Title" description="Form description" fields={fields} />
+        </>;
+}`,...(b=(x=i.parameters)==null?void 0:x.docs)==null?void 0:b.source}}};const Ee=["Playground","KitchenSink"];export{i as KitchenSink,t as Playground,Ee as __namedExportsOrder,He as default};
---
+++ sb8/qa/assets/FormFieldRadio.stories-C4vDx_Ju.js
@@ -0,0 +1,42 @@
+import{j as e}from"./jsx-runtime-CexXSJP5.js";import{r as R}from"./index-BP8_t0zE.js";import{r as F}from"./storyUtils-ByV2Bsyv.js";import{u as h,F as S}from"./formUtils-C5IZy0Gf.js";import{g as T}from"./getOptions-FYxwAUJc.js";import"./FormFieldText.styled-C9eExh2D.js";import"./theme-CtvfJOCv.js";import"./Button-BBNnoZZP.js";import"./useThemeProps-Ds-LJhvX.js";import"./ButtonBase-D0hlh4xx.js";import"./emotion-react.browser.esm-DIgfljIv.js";import"./assertThisInitialized-CO-dlV3K.js";import"./Popper-CwZOyH_P.js";import"./index-BxmsGmlx.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useToggle-lipGlewK.js";import"./useMosaicTranslation-DzvJLSut.js";import"./formControlState-CeBwnzuK.js";import"./createSvgIcon-CQgce1KM.js";import"./createSvgIcon-COYRGr1j.js";import"./useId-BlXdlps1.js";import"./useControlled-im5M3O13.js";import"./DataViewFilterMultiselectDropdownContent-O9LV55xE.js";import"./CheckboxList-BCG88NR0.js";import"./isSymbol-CEVJpixJ.js";import"./Checkbox-DnFKua5S.js";import"./FormControlLabel-CIIRzCdb.js";import"./reactTools-D2O6dTsh.js";import"./debounce-C_xMouBr.js";import"./DataViewPrimaryFilter.styled-DecvZ7Zb.js";import"./ButtonRow-BZVmEtFA.js";import"./Spinner-BPM3b5sV.js";import"./Chip-C1CRxgK0.js";import"./Typography-v36okEvj.js";import"./SubtitleText-jYpjhjNV.js";import"./sizes-C6oSOElx.js";import"./RadioButton-DUvfdQ-A.js";import"./StyledFormControl.styled-DBTTYzHm.js";import"./Toggle-BQ1JdWW7.js";import"./testIds-B75WQI-K.js";import"./ColorSelected-BZr_74pT.js";import"./TitleWrapper-DkQZJitR.js";import"./containerQuery-C4iAQuB_.js";import"./TitleText-Dy6rc9cG.js";import"./MoreVert-DQju6pbX.js";import"./ChevronRight-JG1qL3pJ.js";import"./PageHeader-B5WzlHGP.js";import"./MenuSelect-nAevESHO.js";import"./useMediaQuery-CVp5GJAd.js";import"./formats-CDjt32hU.js";import"./Dialog-Dh5ejmnv.js";import"./SideNav-BDfV6asS.js";import"./Snackbar-BbTrOH4B.js";import"./Close-B-5TycoQ.js";import"./ClickAwayListener-DFiznkjI.js";import"./advancedSelectionUtils-Dc6CTSWK.js";const _e={title:"FormFields/FormFieldRadio"},l=[{label:"Label 1",value:"label_1"},{label:"Label 2",value:"label_2"},{label:"Label 3",value:"label_3"}],t=({label:r,required:i,skeleton:n,disabled:a,instructionText:p,helperText:m,optionsOrigin:s})=>{const d=h(),{state:O,handleSubmit:y}=d,k=R.useMemo(()=>[{name:"radio",label:r,type:"radio",required:i,disabled:a,inputSettings:{options:s==="Local"?l:void 0,getOptions:s==="DB"?T:void 0},helperText:m,instructionText:p}],[r,i,a,p,m,s]);return e.jsxs(e.Fragment,{children:[e.jsx("pre",{children:JSON.stringify(O,null,"  ")}),e.jsx(S,{...d,buttons:F(y),title:"Radio Field",fields:k,skeleton:n})]})};t.args={label:"Label",disabled:!1,required:!1,skeleton:!1,instructionText:"Instruction text",helperText:"Helper text",optionsOrigin:"Local"};t.argTypes={label:{name:"Label"},disabled:{name:"Disabled"},required:{name:"Required"},skeleton:{name:"Skeleton"},instructionText:{name:"Instruction Text"},helperText:{name:"Helper Text"},optionsOrigin:{name:"Options Origin"}};const j=[{name:"radio",label:"Regular example",type:"radio",required:!1,disabled:!1,inputSettings:{options:l},helperText:"Helper text",instructionText:"Instruction text"},{name:"radio-disabled",label:"Disabled example",type:"radio",required:!1,disabled:!0,inputSettings:{options:l},helperText:"Helper text",instructionText:"Instruction text"},{name:"radio-db",label:"From data base example",type:"radio",required:!1,disabled:!1,inputSettings:{getOptions:T},helperText:"Helper text",instructionText:"Instruction text"}],o=()=>{const r=h(),{state:i,handleSubmit:n}=r;return e.jsxs(e.Fragment,{children:[e.jsx("pre",{children:JSON.stringify(i,null,"  ")}),e.jsx(S,{...r,buttons:F(n),title:"Form Title",description:"This is a description example",fields:j})]})};var u,c,b;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`({
+  label,
+  required,
+  skeleton,
+  disabled,
+  instructionText,
+  helperText,
+  optionsOrigin
+}: typeof Playground.args): ReactElement => {
+  const controller = useForm();
+  const {
+    state,
+    handleSubmit
+  } = controller;
+  const fields = useMemo((): FieldDef[] => ([{
+    name: "radio",
+    label,
+    type: "radio",
+    required,
+    disabled,
+    inputSettings: {
+      options: optionsOrigin === "Local" ? options : undefined,
+      getOptions: optionsOrigin === "DB" ? getOptions : undefined
+    },
+    helperText,
+    instructionText
+  }] as FieldDef[]), [label, required, disabled, instructionText, helperText, optionsOrigin]);
+  return <>
+            <pre>{JSON.stringify(state, null, "  ")}</pre>
+            <Form {...controller} buttons={renderButtons(handleSubmit)} title="Radio Field" fields={fields} skeleton={skeleton} />
+        </>;
+}`,...(b=(c=t.parameters)==null?void 0:c.docs)==null?void 0:b.source}}};var x,g,f;o.parameters={...o.parameters,docs:{...(x=o.parameters)==null?void 0:x.docs,source:{originalSource:`(): ReactElement => {
+  const controller = useForm();
+  const {
+    state,
+    handleSubmit
+  } = controller;
+  return <>
+            <pre>{JSON.stringify(state, null, "  ")}</pre>
+            <Form {...controller} buttons={renderButtons(handleSubmit)} title="Form Title" description="This is a description example" fields={kitchenSinkFields} />
+        </>;
+}`,...(f=(g=o.parameters)==null?void 0:g.docs)==null?void 0:f.source}}};const ve=["Playground","KitchenSink"];export{o as KitchenSink,t as Playground,ve as __namedExportsOrder,_e as default};
