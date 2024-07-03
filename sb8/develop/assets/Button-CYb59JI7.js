b7042460d77c32deb0e9cb5463910031c70c8f1f
Owen Allen <owenallenaz@gmail.com>
2024-07-03 14:35:05 +0000

    Updates

---
+++ sb8/develop/assets/Button-CYb59JI7.js
@@ -0,0 +1,17 @@
+import{j as o}from"./jsx-runtime-CexXSJP5.js";import{useMDXComponents as e}from"./index-3-_tf5dF.js";import{M as i,C as p}from"./index-p5R4BIF1.js";import{s as m,P as s}from"./Button.stories-DUgD2_FM.js";import"./index-BP8_t0zE.js";import"./iframe-DV5PNIdz.js";import"../sb-preview/runtime.js";import"./index-BxmsGmlx.js";import"./extends-CF3RwP-h.js";import"./tslib.es6-CRos2fHm.js";import"./index-DXimoRZY.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-BjCnz-8e.js";import"./index-DJqTsaQd.js";import"./index-DrFu-skq.js";import"./Button-BBNnoZZP.js";import"./theme-CtvfJOCv.js";import"./useThemeProps-Ds-LJhvX.js";import"./ButtonBase-D0hlh4xx.js";import"./emotion-react.browser.esm-DIgfljIv.js";import"./assertThisInitialized-CO-dlV3K.js";import"./Popper-CwZOyH_P.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useToggle-lipGlewK.js";import"./Create-CgopsPUN.js";import"./createSvgIcon-DQitHYV2.js";import"./styled-BlYH_7wi.js";import"./emotion-unitless.esm-sScrWPmR.js";import"./Delete-DdXiJfUx.js";import"./Add-ktkGHE_h.js";import"./CloudDownload-CJZ6vFKn.js";import"./Help-BjgJhcSB.js";import"./ButtonRow-BZVmEtFA.js";import"./reactTools-D2O6dTsh.js";import"./storyUtils-ByV2Bsyv.js";function n(r){const t={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...e(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{of:m}),`
+`,o.jsx(t.h1,{id:"button",children:"Button"}),`
+`,o.jsx(t.p,{children:"Displays a button."}),`
+`,o.jsx(t.h2,{id:"props",children:"Props"}),`
+`,o.jsx(t.p,{children:"https://github.com/simpleviewinc/sv-mosaic/blob/develop/src/components/Button/ButtonTypes.tsx"}),`
+`,o.jsx(t.h2,{id:"popover",children:"Popover"}),`
+`,o.jsxs(t.p,{children:["When using ",o.jsx(t.code,{children:"popover"})," if you need to programmatically close it, you can do so by using ",o.jsx(t.code,{children:"onClose"})," provided by the React context API."]}),`
+`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-js",children:`import * as React from "react";
+import { useContext } from "react";
+import { Button, ButtonPopoverContext } from "@simpleview/sv-mosaic";
+
+function MyComponent(props) {
+	const buttonPopoverContext = useContext(ButtonPopoverContext);
+	// ...rest of component
+}
+`})}),`
+`,o.jsx(p,{of:s})]})}function K(r={}){const{wrapper:t}={...e(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(n,{...r})}):n(r)}export{K as default};
