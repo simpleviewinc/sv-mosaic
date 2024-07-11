a500b085284a6ade5bc2e71f5bf3174a16e1d80a
George Batt <george.batt@simpleviewinc.com>
2024-07-11 17:42:42 +0100

    (DataView) Updates multiselect filter e2e tests

--- containers/mosaic/src/components/DataViewFilterMultiselect/index.ts
+++ containers/mosaic/src/components/DataViewFilterMultiselect/index.ts
@@ -1,3 +1,11 @@
 export { default } from "./DataViewFilterMultiselect";
 export { default as DataViewFilterMultiselectDropdownContent } from "./DataViewFilterMultiselectDropdownContent";
 export * from "./DataViewFilterMultiselectTypes";
+
+import localeData from "../DataView/DataViewLocales.json";
+import { addCoreResourceBundle } from "../../i18n";
+
+addCoreResourceBundle({
+	prefix : "DataView",
+	bundle : localeData,
+});
