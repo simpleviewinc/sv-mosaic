f5371d19d09d48a2a5461c28643910536f3940e2
George Batt <george.batt@simpleviewinc.com>
2024-07-11 10:46:24 +0100

    (DataViewFilters) Standardises the footers of all filter dropdown contents. Performs a little cleanup.

--- containers/mosaic/src/components/DataViewFilterDropdownButtons/DataViewFilterDropdownButtons.styled.tsx
+++ containers/mosaic/src/components/DataViewFilterDropdownButtons/DataViewFilterDropdownButtons.styled.tsx
@@ -1,11 +1,10 @@
+import theme from "@root/theme";
 import styled from "styled-components";
 
 export const StyledWrapper = styled.div`
-  & {
+	border-top: 2px solid ${theme.newColors.grey2[100]};
     display: flex;
     justify-content: space-between;
-    margin-top: 16px;
-    padding: 0px 16px 16px 16px;
+    padding: 16px;
     gap: 12px;
-  }
 `;
