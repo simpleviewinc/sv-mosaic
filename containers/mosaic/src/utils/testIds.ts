cb5da09794d2eb04453f7f4669f51d1876027ea0
George Batt <george.batt@simpleviewinc.com>
2024-07-12 10:42:38 +0100

    (DataViewFilter) Fixes/updates to e2e tests

--- containers/mosaic/src/utils/testIds.ts
+++ containers/mosaic/src/utils/testIds.ts
@@ -6,8 +6,10 @@ const testIds = {
 	CARD_HEADING: "mos:Card:heading",
 	CARD_ITEM: "mos:Card:item",
 	DATA_VIEW_TITLE_BAR: "mos:DataView:titleBar",
+	DATA_VIEW_FILTER_BAR: "mos:DataView:filterBar",
 	DATA_VIEW_VIEW_APPLY: "mos:DataView:view:apply",
 	DATA_VIEW_VIEW_DELETE: "mos:DataView:view:delete",
+	DATA_VIEW_FILTER_MULTI_COUNTER: "mos:DataView:filterMultiCounter",
 	TOP_SUMMARY: "mos:TopSummary:root",
 	TOGGLE: "mos:Toggle:root",
 };
