4632ef5a2af22338827c9fbed086dcf58e8f73e6
Owen Allen <owenallenaz@gmail.com>
2024-07-12 15:23:11 +0000

    Updates

---
+++ sb8/develop/assets/FormFieldNumber.stories-DVn_lvw-.js
@@ -0,0 +1,41 @@
+import{j as r}from"./jsx-runtime-nGUw9YhZ.js";import{r as S}from"./index-BP8_t0zE.js";import{u as T,F as P}from"./formUtils-DjW1hEz-.js";import{r as k}from"./storyUtils-ByV2Bsyv.js";import"./FormFieldText.styled-D8aJbBKM.js";import"./theme-CtvfJOCv.js";import"./Button-uCo3N7IM.js";import"./useThemeProps-Ds-LJhvX.js";import"./ButtonBase-BVb7KZlV.js";import"./emotion-react.browser.esm-DIgfljIv.js";import"./assertThisInitialized-CO-dlV3K.js";import"./Popper-Cx7DIS0Z.js";import"./index-BxmsGmlx.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useToggle-lipGlewK.js";import"./useMosaicTranslation-DzvJLSut.js";import"./formControlState-CeBwnzuK.js";import"./createSvgIcon-Coberten.js";import"./createSvgIcon-Df7bw5TJ.js";import"./useId-BlXdlps1.js";import"./useControlled-im5M3O13.js";import"./DataViewFilterMultiselectDropdownContent-DlL8z0DF.js";import"./CheckboxList-D1zEVDHX.js";import"./isSymbol-CEVJpixJ.js";import"./Checkbox-BVMkdcLx.js";import"./FormControlLabel-DCWWYNLI.js";import"./reactTools-D2O6dTsh.js";import"./debounce-C_xMouBr.js";import"./DataViewPrimaryFilter.styled-B3u2V0ac.js";import"./ButtonRow-DW5Veo41.js";import"./Spinner-D_slr9_p.js";import"./Chip-DZJmUX56.js";import"./Typography-DSyzRETI.js";import"./SubtitleText-mEFPNCHN.js";import"./sizes-C6oSOElx.js";import"./RadioButton-DhL92CuH.js";import"./StyledFormControl.styled-dMQyuqfm.js";import"./Toggle-osRV5K3v.js";import"./testIds-B75WQI-K.js";import"./ColorSelected-Bv70I260.js";import"./TitleWrapper-DGXp-QZq.js";import"./containerQuery-C4iAQuB_.js";import"./TitleText-Dhe7UX1h.js";import"./MoreVert-Bjq7SBh2.js";import"./ChevronRight-3hnMDb9i.js";import"./PageHeader-BkDRAe3B.js";import"./MenuSelect-BpoY6Cxg.js";import"./useMediaQuery-CVp5GJAd.js";import"./formats-CDjt32hU.js";import"./Dialog-CaN6ZDK8.js";import"./SideNav-CAh37aY1.js";import"./Snackbar-CZels2n9.js";import"./Close-CFi9yJil.js";import"./ClickAwayListener-BOIWMbZr.js";import"./Blank-JgbFIOSE.js";const qe={title:"FormFields/FormFieldNumber"},e=({label:i,required:o,skeleton:g,disabled:n,instructionText:m,helperText:s,size:l,placeholder:t,decimalPlaces:p,prefix:a,suffix:d,sign:c})=>{const u=T(),{handleSubmit:h,state:F}=u,y=S.useMemo(()=>[{name:"numberfield",label:i,type:"number",required:o,disabled:n,size:l,inputSettings:{placeholder:t===""?void 0:t,decimalPlaces:p,prefix:a,suffix:d,sign:c},helperText:s,instructionText:m}],[i,o,n,l,t,s,m,p,a,d,c]);return r.jsxs(r.Fragment,{children:[r.jsx(P,{buttons:k(h),title:"Number Field",fields:y,...u,skeleton:g}),r.jsx("pre",{children:JSON.stringify(F,null,"  ")})]})};e.args={label:"Label",disabled:!1,required:!1,skeleton:!1,instructionText:"Instruction text",helperText:"Helper text",size:"sm",placeholder:"placeholder",decimalPlaces:-1,prefix:"",suffix:"",sign:"any"};e.argTypes={label:{name:"Label"},disabled:{name:"Disabled"},required:{name:"Required"},skeleton:{name:"Skeleton"},instructionText:{name:"Instruction Text"},helperText:{name:"Helper Text"},size:{name:"Size",control:{type:"select"},options:["xs","sm","md","lg"]},placeholder:{name:"Placeholder"},decimalPlaces:{name:"Decimal Places"},prefix:{name:"Prefix"},suffix:{name:"Suffix"},sign:{name:"Sign",control:{type:"select"},options:["any","positive","negative"]}};var f,x,b;e.parameters={...e.parameters,docs:{...(f=e.parameters)==null?void 0:f.docs,source:{originalSource:`({
+  label,
+  required,
+  skeleton,
+  disabled,
+  instructionText,
+  helperText,
+  size,
+  placeholder,
+  decimalPlaces,
+  prefix,
+  suffix,
+  sign
+}: typeof Playground.args): ReactElement => {
+  const controller = useForm();
+  const {
+    handleSubmit,
+    state
+  } = controller;
+  const fields: FieldDef[] = useMemo((): FieldDef[] => [{
+    name: "numberfield",
+    label,
+    type: "number",
+    required,
+    disabled,
+    size,
+    inputSettings: {
+      placeholder: placeholder === "" ? undefined : placeholder,
+      decimalPlaces,
+      prefix,
+      suffix,
+      sign
+    },
+    helperText,
+    instructionText
+  }], [label, required, disabled, size, placeholder, helperText, instructionText, decimalPlaces, prefix, suffix, sign]);
+  return <>
+            <Form buttons={renderButtons(handleSubmit)} title="Number Field" fields={fields} {...controller} skeleton={skeleton} />
+            <pre>{JSON.stringify(state, null, "  ")}</pre>
+        </>;
+}`,...(b=(x=e.parameters)==null?void 0:x.docs)==null?void 0:b.source}}};const ze=["Playground"];export{e as Playground,ze as __namedExportsOrder,qe as default};
