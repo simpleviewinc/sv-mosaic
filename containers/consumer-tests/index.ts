d91633b79e9898071f37b33cd710b2e28eeb4d3b
George Batt <george.batt@simpleviewinc.com>
2024-08-21 09:15:06 +0100

    WIP

---
+++ containers/consumer-tests/index.ts
@@ -0,0 +1,66 @@
+/**
+ * Components
+ */
+import Button, { Barf } from "@simpleview/sv-mosaic/components/Button";
+import Card from "@simpleview/sv-mosaic/components/Card";
+import Checkbox from "@simpleview/sv-mosaic/components/Checkbox";
+import CheckboxList from "@simpleview/sv-mosaic/components/CheckboxList";
+import Chip from "@simpleview/sv-mosaic/components/Chip";
+import Content from "@simpleview/sv-mosaic/components/Content";
+import DataView from "@simpleview/sv-mosaic/components/DataView";
+import DataViewFilterDate from "@simpleview/sv-mosaic/components/DataViewFilterDate";
+import DataViewFilterDropdown from "@simpleview/sv-mosaic/components/DataViewFilterDropdown";
+import DataViewFilterDropdownButtons from "@simpleview/sv-mosaic/components/DataViewFilterDropdownButtons";
+import DataViewFilterMultiselect from "@simpleview/sv-mosaic/components/DataViewFilterMultiselect";
+import DataViewFilterNumber from "@simpleview/sv-mosaic/components/DataViewFilterNumber";
+import DataViewFilterSingleSelect from "@simpleview/sv-mosaic/components/DataViewFilterSingleSelect";
+import DataViewFilterText from "@simpleview/sv-mosaic/components/DataViewFilterText";
+import DataViewPrimaryFilter from "@simpleview/sv-mosaic/components/DataViewPrimaryFilter";
+import Dialog from "@simpleview/sv-mosaic/components/Dialog";
+import Drawer from "@simpleview/sv-mosaic/components/Drawer";
+import DrawerContent from "@simpleview/sv-mosaic/components/DrawerContent";
+import Drawers from "@simpleview/sv-mosaic/components/Drawers";
+import Form from "@simpleview/sv-mosaic/components/Form";
+import LeftNav from "@simpleview/sv-mosaic/components/LeftNav";
+import Menu from "@simpleview/sv-mosaic/components/Menu";
+import MenuBase from "@simpleview/sv-mosaic/components/MenuBase";
+import MenuItem from "@simpleview/sv-mosaic/components/MenuItem";
+import MenuSelect from "@simpleview/sv-mosaic/components/MenuSelect";
+import MosaicContext from "@simpleview/sv-mosaic/components/MosaicContext";
+import PageHeader from "@simpleview/sv-mosaic/components/PageHeader";
+import Popover from "@simpleview/sv-mosaic/components/Popover";
+import RadioButton from "@simpleview/sv-mosaic/components/RadioButton";
+import SideNav from "@simpleview/sv-mosaic/components/SideNav";
+import Snackbar from "@simpleview/sv-mosaic/components/Snackbar";
+import Spinner from "@simpleview/sv-mosaic/components/Spinner";
+import Toggle from "@simpleview/sv-mosaic/components/Toggle";
+import TopSummary from "@simpleview/sv-mosaic/components/TopSummary";
+import Typography from "@simpleview/sv-mosaic/components/Typography";
+
+/**
+ * Theme
+ */
+import theme from "@simpleview/sv-mosaic/theme";
+import { Sizes } from "@simpleview/sv-mosaic/theme";
+import { TridentIcon } from "@simpleview/sv-mosaic/theme";
+
+/**
+ * Transforms
+ */
+import { transform_boolean } from "@simpleview/sv-mosaic/transforms";
+import { transform_dateFormat } from "@simpleview/sv-mosaic/transforms";
+import { transform_get } from "@simpleview/sv-mosaic/transforms";
+import { transform_mapGet } from "@simpleview/sv-mosaic/transforms";
+import { transform_join } from "@simpleview/sv-mosaic/transforms";
+import { transform_thumbnail } from "@simpleview/sv-mosaic/transforms";
+import { transform_chips } from "@simpleview/sv-mosaic/transforms";
+import { transform_colorPicker } from "@simpleview/sv-mosaic/transforms";
+import { transform_dataview } from "@simpleview/sv-mosaic/transforms";
+
+/**
+ * Utils
+ */
+import { wrapToggle } from "@simpleview/sv-mosaic/utils/toggle"
+import { getToggle } from "@simpleview/sv-mosaic/utils/toggle"
+import { useWrappedToggle } from "@simpleview/sv-mosaic/utils/toggle"
+import { useToggle } from "@simpleview/sv-mosaic/utils/toggle"
