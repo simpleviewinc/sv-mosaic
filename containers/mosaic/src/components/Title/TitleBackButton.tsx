b67444ab74fa315ecbe7fa06def9c629af037d70
George Batt <george.batt@simpleviewinc.com>
2024-08-21 17:36:22 +0100

    (TitleWrapper) Use label as a tooltip

--- containers/mosaic/src/components/Title/TitleBackButton.tsx
+++ containers/mosaic/src/components/Title/TitleBackButton.tsx
@@ -5,23 +5,30 @@ import { memo, ReactElement } from "react";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import { BackButton } from "./TitleWrapper.styled";
 import { TitleBackButtonProps } from "./TitleWrapperTypes";
+import Tooltip, { useTooltip } from "../Tooltip";
 
 const TitleBackButton = ({
 	collapse,
 	label = "Go back",
 	...props
 }: TitleBackButtonProps): ReactElement => {
+	const { anchorProps, tooltipProps } = useTooltip();
 
 	return (
-		<BackButton
-			$collapse={collapse}
-			{...props}
-			className="back-button"
-			color="gray"
-			variant="icon"
-			mIcon={ChevronLeftIcon}
-			muiAttrs={{ "aria-label": label }}
-		/>
+		<>
+			<BackButton
+				$collapse={collapse}
+				{...props}
+				className="back-button"
+				color="gray"
+				variant="icon"
+				mIcon={ChevronLeftIcon}
+				muiAttrs={{ "aria-label": label, ...anchorProps }}
+			/>
+			<Tooltip {...tooltipProps} >
+				{label}
+			</Tooltip>
+		</>
 	);
 };
 
