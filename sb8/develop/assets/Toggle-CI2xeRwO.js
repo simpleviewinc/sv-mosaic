eec949fa212f29651f6974a462360bc060391200
Owen Allen <owenallenaz@gmail.com>
2024-08-27 10:59:03 +0000

    Updates

---
+++ sb8/develop/assets/Toggle-CI2xeRwO.js
@@ -0,0 +1,12 @@
+import{j as o}from"./jsx-runtime-nGUw9YhZ.js";import{useMDXComponents as r}from"./index-3-_tf5dF.js";import{M as n,C as s}from"./index-Dy8UpG4S.js";import{s as l,P as m}from"./Toggle.stories-BeXa3up3.js";import"./index-BP8_t0zE.js";import"./iframe-CFjbv-Fp.js";import"../sb-preview/runtime.js";import"./index-BxmsGmlx.js";import"./extends-CF3RwP-h.js";import"./tslib.es6-CRos2fHm.js";import"./index-DXimoRZY.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-BjCnz-8e.js";import"./index-DJqTsaQd.js";import"./index-DrFu-skq.js";import"./Toggle-CsG9lN-c.js";import"./theme-D4oZEIDo.js";import"./generateUtilityClasses-BPeOZGYv.js";import"./FormControlLabel-DBnW5Tz0.js";import"./formControlState-D52vkf9K.js";import"./ButtonBase-XhX7nRZA.js";import"./useIsFocusVisible-B0UKn5st.js";import"./useControlled-im5M3O13.js";import"./getThemeProps-C7Gh6k4C.js";import"./useTheme-BNssCB1g.js";import"./StyledFormControl.styled-DtM0hwgT.js";import"./testIds-BYqMnK8Q.js";function i(t){const e={a:"a",code:"code",h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...r(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(n,{of:l}),`
+`,o.jsx(e.h1,{id:"toggle",children:"Toggle"}),`
+`,o.jsxs(e.p,{children:["The ",o.jsx(e.code,{children:"Toggle"})," component is built over a wrapper for ",o.jsx(e.a,{href:"https://mui.com/material-ui/react-switch/#main-content",rel:"nofollow",children:"MUI Switch"})," but with SimpleView brand colors."]}),`
+`,o.jsxs(e.ul,{children:[`
+`,o.jsx(e.li,{children:"Toggle Switch allow users to switch between two possible states. They are commonly used to turn a specific setting on or off"}),`
+`,o.jsx(e.li,{children:"Toggles should be used to turn on or off a preference, notification, or feature"}),`
+`,o.jsx(e.li,{children:"Should be used when an instant response is required/desired"}),`
+`]}),`
+`,o.jsx(e.h2,{id:"props",children:"Props"}),`
+`,o.jsx(e.p,{children:o.jsx(e.a,{href:"https://github.com/simpleviewinc/sv-mosaic/blob/develop/src/components/Toggle/Toggle.tsx",rel:"nofollow",children:"https://github.com/simpleviewinc/sv-mosaic/blob/develop/src/components/Toggle/Toggle.tsx"})}),`
+`,o.jsx(e.h2,{id:"toggle-1",children:"Toggle"}),`
+`,o.jsx(s,{of:m})]})}function R(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(i,{...t})}):i(t)}export{R as default};
---
+++ sb8/develop/assets/getOptions-Cji6KgKe.js
@@ -0,0 +1,1 @@
+import"./formUtils-D_L9MI_0.js";import{a as n}from"./advancedSelectionUtils-Dc6CTSWK.js";const s=async()=>{const t=n;return new Promise(o=>{setTimeout(()=>{o(t)},500)})};export{s as g};
