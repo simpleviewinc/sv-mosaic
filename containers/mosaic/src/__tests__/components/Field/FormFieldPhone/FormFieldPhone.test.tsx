e8c9dae34abb03aba7494f613830b1e238921333
George Batt <george.batt@simpleviewinc.com>
2024-08-27 17:40:50 +0100

    (PhoneField) Unused unit test import cleanup

--- containers/mosaic/src/__tests__/components/Field/FormFieldPhone/FormFieldPhone.test.tsx
+++ containers/mosaic/src/__tests__/components/Field/FormFieldPhone/FormFieldPhone.test.tsx
@@ -1,38 +1,10 @@
 import * as React from "react";
-import { render, cleanup, screen, fireEvent } from "@testing-library/react";
-import { useState } from "react";
+import { render, cleanup } from "@testing-library/react";
 
 // Components
 import FormFieldPhone from "@root/components/Field/FormFieldPhone";
 
 afterEach(cleanup);
-
-const { getByText, getByTitle } = screen;
-
-const FormFieldPhoneExample = () => {
-	const [value, setValue] = useState("No value");
-
-	const handleOnChange = async (value: string) => {
-		setValue(value);
-	};
-
-	return (
-		<>
-			<span>{value}</span>
-			<FormFieldPhone
-				fieldDef={{
-					name: "phoneSelectDropdown",
-					type: "phone",
-					label: "Label",
-					disabled: false,
-				}}
-				error="Error text"
-				onChange={handleOnChange}
-				value={value}
-			/>
-		</>
-	);
-};
 
 describe("FormFieldPhone country code prop", () => {
 	it("should display US phone number prefix when no country code is provided ", () => {
