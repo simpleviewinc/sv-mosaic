09ec13790b875d5058b8a2313520eca04b47d4c8
George Batt <george.batt@simpleviewinc.com>
2024-08-30 13:04:42 +0100

    (TextEditor) Cleanup and fix click away import

--- containers/mosaic/src/utils/dom/getOuterHeight.ts
+++
@@ -1,6 +0,0 @@
-export function getOuterHeight(el: HTMLElement) {
-	const styles = window.getComputedStyle(el);
-	const margin = parseInt(styles["marginTop"]) + parseFloat(styles["marginBottom"]);
-
-	return Math.ceil(el.offsetHeight + margin);
-}
