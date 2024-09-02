3ee46bfb3dee3b72e2a113dc25b56180fff9a2c0
George Batt <george.batt@simpleviewinc.com>
2024-09-02 16:31:02 +0100

    (TextEditor) Add support for custom image and link handlers

--- containers/mosaic/src/components/Field/FormFieldTextEditor/Toolbar/Controls/ImageControl.tsx
+++ containers/mosaic/src/components/Field/FormFieldTextEditor/Toolbar/Controls/ImageControl.tsx
@@ -4,19 +4,42 @@ import ImageIcon from "@mui/icons-material/Image";
 
 import type { ControlComponentProps } from "../../FormFieldTextEditorTypes";
 import { ControlButton } from ".";
+import { Node } from "@tiptap/pm/model";
 
 export function ImageControl({
 	editor,
 	setNodeForm,
+	inputSettings: { onImage },
 }: ControlComponentProps): ReactElement {
 	const onClick = () => {
 		const { view } = editor;
 		const { state: { selection: { from, to } } } = view;
 
+		// TypeScript thinks that selection doesn't have an
+		// associated node even though it does
+		const selectedNode = (editor.state.selection as unknown as { node: Node }).node;
+		const image = selectedNode?.type.name === "image" ? selectedNode : undefined;
+
+		const values = {
+			src: image?.attrs.src || "",
+			alt: image?.attrs.alt,
+		};
+
+		if (onImage) {
+			onImage({
+				...values,
+				updateImage: (params) => editor.chain().focus()
+					.setImage(params)
+					.run(),
+			});
+
+			return;
+		}
+
 		setNodeForm({
 			open: true,
 			anchorEl: { getBoundingClientRect: () => posToDOMRect(view, from, to) },
-			values: {},
+			values,
 			type: "image",
 		});
 	};
