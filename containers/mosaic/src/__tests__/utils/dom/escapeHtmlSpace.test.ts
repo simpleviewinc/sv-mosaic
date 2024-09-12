80dc6ec81e8c3e6c59311ef4f5a90c8847f0368f
George Batt <george.batt@simpleviewinc.com>
2024-09-12 09:57:35 +0100

    (TextEditor) Escape greater/less than HTML entities

--- containers/mosaic/src/__tests__/utils/dom/escapeHtmlSpace.test.ts
+++ containers/mosaic/src/__tests__/utils/dom/escapeHtmlSpace.test.ts
@@ -1,10 +1,10 @@
-import { escapeHtmlSpaces } from "@root/utils/dom/escapeHtmlSpaces";
+import { escapeHtml } from "@root/utils/dom/escapeHtml";
 
 describe(__filename, function () {
 	it("should convert multiple spaces into a single space followed by &nbsp; entities", async () => {
 		const html = "<p>   </p>";
 
-		expect(escapeHtmlSpaces(html)).toBe("<p> &nbsp;&nbsp;</p>");
+		expect(escapeHtml(html)).toBe("<p> &nbsp;&nbsp;</p>");
 	});
 
 	it("should convert multple whitespace characters into a single space followed by &nbsp; entities", async () => {
@@ -12,18 +12,18 @@ describe(__filename, function () {
 
  </p>`;
 
-		expect(escapeHtmlSpaces(html)).toBe("<p> &nbsp;&nbsp;</p>");
+		expect(escapeHtml(html)).toBe("<p> &nbsp;&nbsp;</p>");
 	});
 
 	it("should not convert single spaces", async () => {
 		const html = "<p>Foo Bar</p>";
 
-		expect(escapeHtmlSpaces(html)).toBe("<p>Foo Bar</p>");
+		expect(escapeHtml(html)).toBe("<p>Foo Bar</p>");
 	});
 
 	it("should not convert space-only content between tags", async () => {
 		const html = "<p><span>Foo</span>    <span>Bar</span></p>";
 
-		expect(escapeHtmlSpaces(html)).toBe("<p><span>Foo</span>    <span>Bar</span></p>");
+		expect(escapeHtml(html)).toBe("<p><span>Foo</span>    <span>Bar</span></p>");
 	});
 });
