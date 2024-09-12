b2099224e60102223bfa332075fb6dcc28cda045
Owen Allen <owenallenaz@gmail.com>
2024-09-12 14:40:25 +0000

    Updates

---
+++ sb8/qa/assets/FormFieldDropdown-CIf_wu0g.js
@@ -0,0 +1,51 @@
+import{j as t}from"./jsx-runtime-nGUw9YhZ.js";import{useMDXComponents as i}from"./index-3-_tf5dF.js";import{M as n}from"./index-BUiBcgO1.js";import{s}from"./FormFieldDropdown.stories-D1UCSXEy.js";import"./index-BP8_t0zE.js";import"./iframe-wafehBfP.js";import"../sb-preview/runtime.js";import"./index-BxmsGmlx.js";import"./extends-CF3RwP-h.js";import"./tslib.es6-CRos2fHm.js";import"./index-DXimoRZY.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-BjCnz-8e.js";import"./index-DJqTsaQd.js";import"./index-DrFu-skq.js";import"./storyUtils-BoiXCagy.js";import"./formUtils-RnxffM34.js";import"./FormFieldText.styled-B_lPWMo9.js";import"./theme-Cyujm90X.js";import"./Button-Cm1Qf39l.js";import"./generateUtilityClasses-CplxKB4o.js";import"./ButtonBase-Btj-nnLi.js";import"./useIsFocusVisible-De2ftVE9.js";import"./Popper-CA50KEz-.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useTheme-vMmOGwbU.js";import"./useToggle-lipGlewK.js";import"./useMosaicTranslation-CZCb37QS.js";import"./formControlState-D52vkf9K.js";import"./createSvgIcon-XnU1Ta54.js";import"./createSvgIcon-BwurTq3z.js";import"./useId-DLncWxdO.js";import"./useControlled-im5M3O13.js";import"./index-DBogRtpm.js";import"./CheckboxList-nF3GDUWX.js";import"./isSymbol-CEVJpixJ.js";import"./Checkbox-CJgaGoMX.js";import"./FormControlLabel-2HxiQW27.js";import"./getThemeProps-CBTZRUO9.js";import"./reactTools-D2O6dTsh.js";import"./debounce-C_xMouBr.js";import"./DataViewPrimaryFilter.styled-CN3zPhHZ.js";import"./ButtonRow-Dfei1Wea.js";import"./Spinner-fkz2uue8.js";import"./Chip-By8My2oo.js";import"./Typography-BXo9fTd2.js";import"./SubtitleText-BK7UxBzP.js";import"./sizes-C6oSOElx.js";import"./RadioButton-BChaCK1f.js";import"./StyledFormControl.styled-CNZVAVRt.js";import"./Toggle-SryGtWz_.js";import"./testIds-BmLWG2HQ.js";import"./ColorSelected-CMT4OGgY.js";import"./TitleWrapper-BnUyvD-V.js";import"./containerQuery-CxIxOiDA.js";import"./TitleText-SU3bZ5Lh.js";import"./MoreVert-CHB9Iy9y.js";import"./ChevronRight-Bc3jAIO-.js";import"./PageHeader-B-ax13VL.js";import"./MenuSelect-CGpLowyH.js";import"./formats-CDjt32hU.js";import"./Dialog-bxyBhLRk.js";import"./useMediaQuery-B5e8eNzK.js";import"./SideNav-BrsNz3mT.js";import"./Snackbar-Ba6pD4j8.js";import"./Close-40L3pRZX.js";import"./ClickAwayListener-DnjBLK-5.js";import"./Blank-D-yfChNi.js";import"./getOptions-DIH_SgCx.js";import"./advancedSelectionUtils-Dc6CTSWK.js";function o(e){const r={a:"a",code:"code",h1:"h1",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...i(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:s}),`
+`,t.jsx(r.h1,{id:"dropdown-single-selection",children:"Dropdown Single Selection"}),`
+`,t.jsxs(r.p,{children:["This field implements the ",t.jsx(r.a,{href:"#generic-field-props-fielddef",children:t.jsx(r.strong,{children:"FieldDef"})})," interface."]}),`
+`,t.jsxs(r.ul,{children:[`
+`,t.jsxs(r.li,{children:[t.jsx(r.code,{children:"Dropdown Single Selection"})," is a simple wrapper for ",t.jsx(r.a,{href:"https://mui.com/material-ui/react-autocomplete/#main-content",rel:"nofollow",children:"MUI Autocomplete"})," but with our brand colors."]}),`
+`,t.jsx(r.li,{children:t.jsx(r.a,{href:"/?path=/docs/formfields-formfielddropdown--playground",children:t.jsx(r.strong,{children:"Playground"})})}),`
+`,t.jsxs(r.li,{children:["Data: ",t.jsx(r.code,{children:"string"})," - String of the selected option."]}),`
+`,t.jsx(r.li,{children:"inputSettings:"}),`
+`]}),`
+`,t.jsxs(r.table,{children:[t.jsx(r.thead,{children:t.jsxs(r.tr,{children:[t.jsx(r.th,{children:"Name"}),t.jsx(r.th,{children:"Type"}),t.jsx(r.th,{children:"Description"})]})}),t.jsxs(r.tbody,{children:[t.jsxs(r.tr,{children:[t.jsx(r.td,{children:t.jsx(r.strong,{children:t.jsx(r.code,{children:"placeholder"})})}),t.jsx(r.td,{children:t.jsx(r.code,{children:"string"})}),t.jsx(r.td,{children:"Optional - Example text shown inside of the text field portion of the dropdown."})]}),t.jsxs(r.tr,{children:[t.jsx(r.td,{children:t.jsx(r.strong,{children:t.jsx(r.code,{children:"options"})})}),t.jsx(r.td,{children:t.jsx(r.code,{children:"MosaicLabelValue[]"})}),t.jsx(r.td,{children:"required - Array of options to be displayed on the options."})]}),t.jsxs(r.tr,{children:[t.jsx(r.td,{children:t.jsx(r.strong,{children:t.jsx(r.code,{children:"getOptions"})})}),t.jsx(r.td,{children:t.jsx(r.code,{children:"() => Promise<MosaicLabelValue[]>"})}),t.jsx(r.td,{children:"required - Function to get a set of options. This prop only applies when getting options from a DB."})]})]})]}),`
+`,t.jsx(r.h3,{id:"how-to-use-in-a-form",children:"How to use in a form?"}),`
+`,t.jsx(r.pre,{children:t.jsx(r.code,{className:"language-ts",children:`//Option recommended when getting options locally.\r
+const fields = useMemo(\r
+	() =>\r
+		[\r
+			//...other fields\r
+			{\r
+				//...all generic field props,\r
+				type: "dropdown",\r
+				inputSettings: {\r
+					options: [\r
+						{ label: "The Shawshank Redemption", value: "1994" },\r
+						{ label: "The Godfather", value: "1972" },\r
+						{ label: "The Godfather: Part II", value: "1974" },\r
+						{ label: "The Dark Knight", value: "2008" }\r
+					],\r
+					placeholder: "Placeholder",\r
+				}\r
+			},\r
+			//...other fields\r
+		],\r
+	[]\r
+);\r
+\r
+//Option recommended when getting options from a database.\r
+const fields = useMemo(\r
+	() =>\r
+		[\r
+			//...other fields\r
+			{\r
+				//...all generic field props,\r
+				type: "dropdown",\r
+				inputSettings: {\r
+					getOptions: myGetOptionsFn,\r
+				}\r
+			},\r
+			//...other fields\r
+		],\r
+	[]\r
+);
+`})})]})}function St(e={}){const{wrapper:r}={...i(),...e.components};return r?t.jsx(r,{...e,children:t.jsx(o,{...e})}):o(e)}export{St as default};
