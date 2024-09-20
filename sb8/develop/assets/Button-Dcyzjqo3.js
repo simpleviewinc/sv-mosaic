79c5e2c38a30681632093aed13a129395ebd0d74
Owen Allen <owenallenaz@gmail.com>
2024-09-20 20:50:18 +0000

    Updates

---
+++ sb8/develop/assets/Button-Dcyzjqo3.js
@@ -0,0 +1,17 @@
+import{j as o}from"./jsx-runtime-nGUw9YhZ.js";import{useMDXComponents as e}from"./index-3-_tf5dF.js";import{M as i,C as p}from"./index-XgD3WN55.js";import{s,P as m}from"./Button.stories-CINv07Ju.js";import"./index-BP8_t0zE.js";import"./iframe-DHbulq1N.js";import"../sb-preview/runtime.js";import"./index-BxmsGmlx.js";import"./extends-CF3RwP-h.js";import"./tslib.es6-CRos2fHm.js";import"./index-DXimoRZY.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-BjCnz-8e.js";import"./index-DJqTsaQd.js";import"./index-DrFu-skq.js";import"./Button-Cm1Qf39l.js";import"./theme-Cyujm90X.js";import"./generateUtilityClasses-CplxKB4o.js";import"./ButtonBase-Btj-nnLi.js";import"./useIsFocusVisible-De2ftVE9.js";import"./Popper-CA50KEz-.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useTheme-vMmOGwbU.js";import"./useToggle-lipGlewK.js";import"./Create-BKQrn80v.js";import"./styled-BsTcxDz6.js";import"./emotion-unitless.esm-sScrWPmR.js";import"./createSvgIcon-B4P8-XBj.js";import"./Delete-hZiYz-Ko.js";import"./Add-CvNngZ9b.js";import"./CloudDownload-BHXMIah9.js";import"./Help-C0qEC5jU.js";import"./ButtonRow-Dfei1Wea.js";import"./toggleOptions-1cXvXfFl.js";import"./listItemTextClasses-C4A0Wzjq.js";function r(n){const t={a:"a",code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...e(),...n.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{of:s}),`
+`,o.jsx(t.h1,{id:"button",children:"Button"}),`
+`,o.jsx(t.p,{children:"Displays a button."}),`
+`,o.jsx(t.h2,{id:"props",children:"Props"}),`
+`,o.jsx(t.p,{children:o.jsx(t.a,{href:"https://github.com/simpleviewinc/sv-mosaic/blob/develop/src/components/Button/ButtonTypes.tsx",rel:"nofollow",children:"https://github.com/simpleviewinc/sv-mosaic/blob/develop/src/components/Button/ButtonTypes.tsx"})}),`
+`,o.jsx(t.h2,{id:"popover",children:"Popover"}),`
+`,o.jsxs(t.p,{children:["When using ",o.jsx(t.code,{children:"popover"})," if you need to programmatically close it, you can do so by using ",o.jsx(t.code,{children:"onClose"})," provided by the React context API."]}),`
+`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-js",children:`import * as React from "react";
+import { useContext } from "react";
+import { Button, ButtonPopoverContext } from "@simpleview/sv-mosaic";
+
+function MyComponent(props) {
+	const buttonPopoverContext = useContext(ButtonPopoverContext);
+	// ...rest of component
+}
+`})}),`
+`,o.jsx(p,{of:m})]})}function K(n={}){const{wrapper:t}={...e(),...n.components};return t?o.jsx(t,{...n,children:o.jsx(r,{...n})}):r(n)}export{K as default};
---
+++ sb8/develop/assets/FormFieldText-CwgYE7YQ.js
@@ -0,0 +1,34 @@
+import{j as t}from"./jsx-runtime-nGUw9YhZ.js";import{useMDXComponents as n}from"./index-3-_tf5dF.js";import{M as o}from"./index-XgD3WN55.js";import{s}from"./FormFieldText.stories-0ZEsDe5J.js";import"./index-BP8_t0zE.js";import"./iframe-DHbulq1N.js";import"../sb-preview/runtime.js";import"./index-BxmsGmlx.js";import"./extends-CF3RwP-h.js";import"./tslib.es6-CRos2fHm.js";import"./index-DXimoRZY.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-BjCnz-8e.js";import"./index-DJqTsaQd.js";import"./index-DrFu-skq.js";import"./formUtils-BQUwAP7b.js";import"./FormFieldText.styled-B_lPWMo9.js";import"./theme-Cyujm90X.js";import"./Button-Cm1Qf39l.js";import"./generateUtilityClasses-CplxKB4o.js";import"./ButtonBase-Btj-nnLi.js";import"./useIsFocusVisible-De2ftVE9.js";import"./Popper-CA50KEz-.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useTheme-vMmOGwbU.js";import"./useToggle-lipGlewK.js";import"./useMosaicTranslation-CZCb37QS.js";import"./formControlState-D52vkf9K.js";import"./createSvgIcon-XnU1Ta54.js";import"./createSvgIcon-BwurTq3z.js";import"./useId-DLncWxdO.js";import"./useControlled-im5M3O13.js";import"./index-8Dqtjiso.js";import"./CheckboxList-GaM67kZx.js";import"./isSymbol-CEVJpixJ.js";import"./Checkbox-CJgaGoMX.js";import"./FormControlLabel-2HxiQW27.js";import"./getThemeProps-CBTZRUO9.js";import"./debounce-C_xMouBr.js";import"./DataViewPrimaryFilter.styled-CN3zPhHZ.js";import"./ButtonRow-Dfei1Wea.js";import"./Spinner-fkz2uue8.js";import"./Chip-By8My2oo.js";import"./Typography-BXo9fTd2.js";import"./SubtitleText-BK7UxBzP.js";import"./sizes-C6oSOElx.js";import"./RadioButton-BChaCK1f.js";import"./StyledFormControl.styled-CNZVAVRt.js";import"./Toggle-SryGtWz_.js";import"./testIds-BmLWG2HQ.js";import"./ColorSelected-B29oKSfy.js";import"./TitleWrapper-BnUyvD-V.js";import"./containerQuery-CxIxOiDA.js";import"./TitleText-SU3bZ5Lh.js";import"./MoreVert-CHB9Iy9y.js";import"./ChevronRight-Bc3jAIO-.js";import"./PageHeader-B-ax13VL.js";import"./MenuSelect-CGpLowyH.js";import"./formats-CDjt32hU.js";import"./Dialog-bxyBhLRk.js";import"./useMediaQuery-B5e8eNzK.js";import"./SideNav-BrsNz3mT.js";import"./Snackbar-Ba6pD4j8.js";import"./Close-40L3pRZX.js";import"./ClickAwayListener-DnjBLK-5.js";import"./Blank-D-yfChNi.js";import"./renderButtons-BmHfsf_z.js";import"./AccountCircle-D63mMaD_.js";import"./styled-BsTcxDz6.js";import"./emotion-unitless.esm-sScrWPmR.js";import"./createSvgIcon-B4P8-XBj.js";function i(r){const e={a:"a",code:"code",h1:"h1",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...n(),...r.components};return t.jsxs(t.Fragment,{children:[t.jsx(o,{of:s}),`
+`,t.jsx(e.h1,{id:"text",children:"Text"}),`
+`,t.jsxs(e.p,{children:["This field implements the ",t.jsx(e.a,{href:"#generic-field-props-fielddef",children:t.jsx(e.strong,{children:"FieldDef"})})," interface."]}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsx(e.li,{children:"Text input fields are used to enter text information, numbers, email addresses, or passwords."}),`
+`,t.jsx(e.li,{children:"A text field consists of a label and a line with variations in width."}),`
+`,t.jsx(e.li,{children:t.jsx(e.a,{href:"/?path=/story/formfields-formfieldtext--playground",children:t.jsx(e.strong,{children:"Playground"})})}),`
+`,t.jsxs(e.li,{children:["Data: ",t.jsx(e.code,{children:"string | number"})," - Value of the input typed."]}),`
+`,t.jsx(e.li,{children:"inputSettings:"}),`
+`]}),`
+`,t.jsxs(e.table,{children:[t.jsx(e.thead,{children:t.jsxs(e.tr,{children:[t.jsx(e.th,{children:"Name"}),t.jsx(e.th,{children:"Type"}),t.jsx(e.th,{children:"Description"})]})}),t.jsxs(e.tbody,{children:[t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.strong,{children:t.jsx(e.code,{children:"maxCharacters"})})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"number"})}),t.jsx(e.td,{children:"optional - Defines the limit of characters that are allow to type in the input element."})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.strong,{children:t.jsx(e.code,{children:"multiline"})})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"boolean"})}),t.jsx(e.td,{children:"optional - When is enabled the text field will expand its height."})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.strong,{children:t.jsx(e.code,{children:"placeholder"})})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"string"})}),t.jsx(e.td,{children:"optional - The hint displayed in the input before the user enters a value."})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.strong,{children:t.jsx(e.code,{children:"prefixElement"})})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"JSX.Element"})}),t.jsx(e.td,{children:"optional - Icon at the beginning of the text field."})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.strong,{children:t.jsx(e.code,{children:"type"})})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"string"})}),t.jsxs(e.td,{children:["optional - Type of the input element. It should be a ",t.jsx(e.a,{href:"https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#Form_%3Cinput%3E_types",rel:"nofollow",children:"valid HTML5 input type"}),"."]})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.strong,{children:t.jsx(e.code,{children:"maxRows"})})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"number"})}),t.jsx(e.td,{children:"optional - Maximum number of rows to display when multiline options is set to true."})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.strong,{children:t.jsx(e.code,{children:"minRows"})})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"number"})}),t.jsx(e.td,{children:"optional - Minimum number of rows to display when multiline option is set to true."})]})]})]}),`
+`,t.jsx(e.h3,{id:"how-to-use-in-a-form",children:"How to use in a form?"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-ts",children:`const fields: FieldDef[] = useMemo(\r
+	() =>\r
+		[\r
+			//...other fields\r
+			{\r
+				//...all generic field props,\r
+				type: "text",\r
+				size: "md",\r
+				inputSettings: {\r
+					prefixElement: <AccountCircle />,\r
+					maxCharacters: 20,\r
+					placeholder: "placeholder",\r
+					multiline: true,\r
+					maxRows: 4,\r
+					minRows: 3\r
+				},\r
+			},\r
+			//...other fields\r
+		],\r
+	[]\r
+);
+`})})]})}function Ct(r={}){const{wrapper:e}={...n(),...r.components};return e?t.jsx(e,{...r,children:t.jsx(i,{...r})}):i(r)}export{Ct as default};
