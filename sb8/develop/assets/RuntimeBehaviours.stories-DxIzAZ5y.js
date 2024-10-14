90addc3c15570946c1c29aac4275ac17d53d5f93
Owen Allen <owenallenaz@gmail.com>
2024-10-14 13:33:39 +0000

    Updates

---
+++ sb8/develop/assets/RuntimeBehaviours.stories-DxIzAZ5y.js
@@ -0,0 +1,88 @@
+import{j as t}from"./jsx-runtime-nGUw9YhZ.js";import{r as o}from"./index-BP8_t0zE.js";import{u as f,v as s,b as l,F as b}from"./formUtils-C6CCf70J.js";import"./Button-BrUhf9jf.js";import{r as y}from"./renderButtons-BmHfsf_z.js";import{O as T}from"./utils-BR2PSRFh.js";import"./FormFieldText.styled-CDQUwglR.js";import"./theme-DRtZtbPc.js";import"./useMosaicTranslation-CZCb37QS.js";import"./useIsFocusVisible-De2ftVE9.js";import"./generateUtilityClasses-CplxKB4o.js";import"./formControlState-D52vkf9K.js";import"./createSvgIcon-XnU1Ta54.js";import"./createSvgIcon-BwurTq3z.js";import"./createChainedFunction-BO_9K8Jh.js";import"./ownerWindow-DvT1GKkC.js";import"./useId-DLncWxdO.js";import"./useControlled-im5M3O13.js";import"./Popper-CA50KEz-.js";import"./useTheme-vMmOGwbU.js";import"./index-BxmsGmlx.js";import"./index-CiJN4qBb.js";import"./CheckboxList-9J3AJhZb.js";import"./isSymbol-CEVJpixJ.js";import"./Checkbox-Daz-CCp_.js";import"./FormControlLabel-2HxiQW27.js";import"./ButtonBase-Btj-nnLi.js";import"./getThemeProps-CBTZRUO9.js";import"./debounce-C_xMouBr.js";import"./DataViewPrimaryFilter.styled-CJaR1K_8.js";import"./ButtonRow-C8VNIQ4B.js";import"./useToggle-lipGlewK.js";import"./Spinner-Cxn5F_rN.js";import"./Chip-CuADowIf.js";import"./Typography-Br_RMLnS.js";import"./SubtitleText-Vdwjg1jD.js";import"./sizes-C6oSOElx.js";import"./RadioButton-D1cjnaj9.js";import"./StyledFormControl.styled-DR5TItxT.js";import"./Toggle-DqBdDGXG.js";import"./testIds-BmLWG2HQ.js";import"./ColorSelected-CUu1BnTo.js";import"./TitleWrapper-B_5wLb-y.js";import"./containerQuery-C0QVV4Kt.js";import"./TitleText-C9TlfF_O.js";import"./MoreVert-CHB9Iy9y.js";import"./ChevronRight-Bc3jAIO-.js";import"./PageHeader-B0SeyLtR.js";import"./formats-CDjt32hU.js";import"./Dialog-DYdX5JhR.js";import"./useMediaQuery-B5e8eNzK.js";import"./SideNav-biYpQHtM.js";import"./TooltipIcon-B3ORSSfg.js";import"./Snackbar-CeVWLZcc.js";import"./Close-40L3pRZX.js";import"./ClickAwayListener-DnjBLK-5.js";const Et={title:"Components/Form"},e=({showState:d})=>{const r=f(),{state:n,methods:{setFieldValue:i},handleSubmit:x}=r;o.useEffect(()=>(document.body.style.margin="0px",()=>{document.body.style.margin=T}),[]);const c=o.useMemo(()=>[{name:"text1",label:"Simple Text",type:"text",instructionText:"testing",validators:[s,l]},{name:"text2",label:"Text with validators and dynamic help",type:"text",helperText:n.data.text2,validators:[s,l]},{name:"text3",label:"Text that copies to the next input",type:"text"},{name:"text4",label:"Text that receives copy",type:"text"},{name:"text5",label:"Text that enables another field",type:"text"},{name:"text6",label:"Text that is initially disabled",type:"text",instructionText:'Type "ENABLE" into the previous field to enable this field',disabled:[({data:a})=>(a==null?void 0:a.text5)!=="ENABLE",!0,()=>!0],required:!0}],[n.data.text2]);o.useEffect(()=>{i({name:"text4",value:n.data.text3})},[i,n.data.text3]);const h=function(){i({name:"text1",value:"test@test.com"})},v=function(){i({name:"text2",value:"notanemail",validate:!0})};return t.jsxs(t.Fragment,{children:[d&&t.jsx("pre",{children:JSON.stringify(n,null,"  ")}),t.jsx("div",{style:{height:"100vh"},children:t.jsx(b,{...r,buttons:y(x),title:"Runtime behaviors",fields:c})}),t.jsxs("div",{children:[t.jsx("p",{children:"Here are some buttons that are not part of the form, but can change values in the form proving communication between in/out of the form. Notice that settext2 runs the validation after setting the value."}),t.jsx("button",{onClick:h,children:"Set Text1 Value"}),t.jsx("button",{onClick:v,children:"Set Text2 Value"})]})]})};e.args={showState:!1};e.argTypes={showState:{name:"Show state"}};var m,p,u;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`({
+  showState
+}: typeof RuntimeBehaviours.args): ReactElement => {
+  const controller = useForm();
+  const {
+    state,
+    methods: {
+      setFieldValue
+    },
+    handleSubmit
+  } = controller;
+  useEffect(() => {
+    document.body.style.margin = "0px";
+    return () => {
+      document.body.style.margin = ORIGINAL_BODY_MARGIN;
+    };
+  }, []);
+  const fields = useMemo((): FieldDef[] => [{
+    name: "text1",
+    label: "Simple Text",
+    type: "text",
+    instructionText: "testing",
+    validators: [validateEmail, validateSlow]
+  }, {
+    name: "text2",
+    label: "Text with validators and dynamic help",
+    type: "text",
+    helperText: state.data.text2,
+    validators: [validateEmail, validateSlow]
+  }, {
+    name: "text3",
+    label: "Text that copies to the next input",
+    type: "text"
+  }, {
+    name: "text4",
+    label: "Text that receives copy",
+    type: "text"
+  }, {
+    name: "text5",
+    label: "Text that enables another field",
+    type: "text"
+  }, {
+    name: "text6",
+    label: "Text that is initially disabled",
+    type: "text",
+    instructionText: "Type \\"ENABLE\\" into the previous field to enable this field",
+    disabled: [({
+      data
+    }) => data?.text5 !== "ENABLE", true, () => true],
+    required: true
+  }], [state.data.text2]);
+  useEffect(() => {
+    setFieldValue({
+      name: "text4",
+      value: state.data.text3
+    });
+  }, [setFieldValue, state.data.text3]);
+  const setText1Value = function () {
+    setFieldValue({
+      name: "text1",
+      value: "test@test.com"
+    });
+  };
+  const setText2Value = function () {
+    setFieldValue({
+      name: "text2",
+      value: "notanemail",
+      validate: true
+    });
+  };
+  return <>
+            {showState && <pre>{JSON.stringify(state, null, "  ")}</pre>}
+            <div style={{
+      height: "100vh"
+    }}>
+                <Form {...controller} buttons={renderButtons(handleSubmit)} title="Runtime behaviors" fields={fields} />
+            </div>
+            <div>
+                <p>
+                    Here are some buttons that are not part of the form, but can change
+                    values in the form proving communication between in/out of the form.
+                    Notice that settext2 runs the validation after setting the value.
+                </p>
+                <button onClick={setText1Value}>Set Text1 Value</button>
+                <button onClick={setText2Value}>Set Text2 Value</button>
+            </div>
+        </>;
+}`,...(u=(p=e.parameters)==null?void 0:p.docs)==null?void 0:u.source}}};const Vt=["RuntimeBehaviours"];export{e as RuntimeBehaviours,Vt as __namedExportsOrder,Et as default};
