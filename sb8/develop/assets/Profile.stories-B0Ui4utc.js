d9412a235e76bf65df34b362e7b36390913de7b5
Owen Allen <owenallenaz@gmail.com>
2024-10-15 15:38:21 +0000

    Updates

---
+++ sb8/develop/assets/Profile.stories-B0Ui4utc.js
@@ -0,0 +1,318 @@
+import{j as u}from"./jsx-runtime-nGUw9YhZ.js";import{r as a}from"./index-BP8_t0zE.js";import{d as A}from"./Delete-CyvBMJly.js";import{d as k}from"./Add-C9VFofpW.js";import{u as C,v as O,b as V,j as M,F as D}from"./formUtils-DHdTLqTn.js";import"./Button-Dv6p7qil.js";import{r as j}from"./renderButtons-BmHfsf_z.js";import{t as c,a as p}from"./toggleOptions-1cXvXfFl.js";import{g as R,a as G}from"./optionGetters-DXvNsMp-.js";import{O as L}from"./utils-BR2PSRFh.js";import{n as U}from"./index.browser-vcSNLBTf.js";import"./styled-DFznwKy7.js";import"./extends-CF3RwP-h.js";import"./emotion-unitless.esm-sScrWPmR.js";import"./createSvgIcon-mhwmaBA2.js";import"./FormFieldText.styled-DY-cBMyi.js";import"./theme-DRtZtbPc.js";import"./useMosaicTranslation-CZCb37QS.js";import"./useIsFocusVisible-De2ftVE9.js";import"./generateUtilityClasses-DAcHHM0W.js";import"./formControlState-D52vkf9K.js";import"./createSvgIcon-CcHL1II5.js";import"./createSvgIcon-CPgMIgId.js";import"./createChainedFunction-BO_9K8Jh.js";import"./ownerWindow-DvT1GKkC.js";import"./useId-DLncWxdO.js";import"./useControlled-im5M3O13.js";import"./Popper-CPbWUqAP.js";import"./useTheme-5hcycNQy.js";import"./index-BxmsGmlx.js";import"./index-BcS5vVNk.js";import"./CheckboxList-Bq1TaTYH.js";import"./isSymbol-CEVJpixJ.js";import"./Checkbox-aoYNk8Xu.js";import"./FormControlLabel-n1qioM9o.js";import"./ButtonBase-BZmvw3BH.js";import"./getThemeProps-Cs15h2IA.js";import"./debounce-C_xMouBr.js";import"./DataViewPrimaryFilter.styled-CylNlUs-.js";import"./ButtonRow-OnULoQP7.js";import"./useToggle-lipGlewK.js";import"./Spinner-DgupAfKo.js";import"./Chip-Da3idaCC.js";import"./Typography-Br_RMLnS.js";import"./SubtitleText-Vdwjg1jD.js";import"./sizes-C6oSOElx.js";import"./RadioButton-CO2cFro8.js";import"./StyledFormControl.styled-BHyCFBbY.js";import"./Toggle-PGODfCtA.js";import"./testIds-BmLWG2HQ.js";import"./ColorSelected-CEyZfcGg.js";import"./TitleWrapper-B_YZcXoR.js";import"./containerQuery-C0QVV4Kt.js";import"./TitleText-C9TlfF_O.js";import"./MoreVert-BbmWEP2K.js";import"./ChevronRight-CSz705Z0.js";import"./PageHeader-lzGsiXAs.js";import"./formats-CDjt32hU.js";import"./Dialog-DbUj91wm.js";import"./useMediaQuery-CguehUMS.js";import"./SideNav-biYpQHtM.js";import"./TooltipIcon-CO3K7ssQ.js";import"./Snackbar-DNEJC7rG.js";import"./Close-Dlpx7KpR.js";import"./ClickAwayListener-DnjBLK-5.js";const tt={title:"Components/Form"},E=[[["firstName"],["lastName"],["initials"]],[["dob"],[],[]],[["sex"],["gender"],["otherGender"]],[["profilePicture"]]];function Y(n,i){return Math.floor(Math.random()*(i-n+1)+n)}const B=n=>Array.from(Array(n+1),(i,m)=>m).filter(i=>n%i===0),s=({showState:n,height:i,section1Fields:m,showFirstName:h,showLastName:f,showInitials:b,skeleton:N})=>{const y=C(),{state:t,methods:{setFieldValue:o},handleSubmit:T}=y;a.useEffect(()=>(document.body.style.margin="0px",()=>{document.body.style.margin=L}),[]);const w=a.useMemo(()=>({columns:[{name:"id",label:"ID"},{name:"title",label:"Title"},{name:"description",label:"Description"}],primaryActions:[{name:"delete",color:"black",variant:"icon",mIcon:A,onClick:({data:e})=>{o({name:"favouriteNumbers",value:(r=[])=>r.filter(({id:l})=>l!==e.id),touched:!0})}}],data:t.data.formMatrix,limit:25,display:"list",activeColumns:["id","title","description"]}),[o,t.data.formMatrix]),g=a.useCallback(async({file:e,onChunkComplete:r,onUploadComplete:l})=>{for(let d=0;d<10;d++)await new Promise(P=>setTimeout(()=>P(r({percent:(d+1)*.1})),2*1e3/10));await l({id:U(),name:e.name,size:e.size,thumbnailUrl:["image/gif","image/jpeg","image/png"].includes(e.type)?URL.createObjectURL(e):"",fileUrl:URL.createObjectURL(e)})},[]),v=a.useCallback(async()=>{await new Promise(e=>setTimeout(()=>e(null),2*1e3))},[]),q=a.useMemo(()=>[{name:"firstName",label:"First Name",type:"text",required:!0,instructionText:"Please use the name that you were given at birth.",show:c[h]},{name:"lastName",label:"Last Name",type:"text",required:!0,instructionText:"Please use the family name that you had at birth.",show:c[f]},{name:"initials",label:"Initials",type:"text",required:!0,instructionText:"If you don't interact with this field, it'll be generated for you based on your first and last names.",show:c[b]},{name:"dob",label:"Date of Birth",type:"date",required:!0,instructionText:"Pick from the datepicker or enter a MM/DD/YYYY formatted date."},{name:"sex",label:"Sex",type:"radio",required:!0,instructionText:"This is the gender you were assigned at birth.",inputSettings:{options:[{value:"male",label:"Male"},{value:"female",label:"Female"}]}},{name:"gender",label:"Gender",type:"dropdown",required:!0,instructionText:"This is the gender that you identify as now.",inputSettings:{options:[{value:"male",label:"Male"},{value:"female",label:"Female"},{value:"other",label:"Other (Please specify)"},{value:"undisclosed",label:"Prefer not to say"}]}},{name:"otherGender",label:"Gender Specified",type:"text",required:!0,instructionText:"Please tell us what gender you identify as.",show:({data:e})=>e.gender&&e.gender.value==="other"},{name:"profilePicture",label:"Profile picture",type:"upload",instructionText:"A profile picture is not required, but it's nice to put a face to the name.",inputSettings:{onFileAdd:g,onFileDelete:v}},{name:"email",label:"Email Address",type:"text",required:!0,validators:[O,V]},{name:"phone",label:"Phone",type:"phone",required:!0,instructionText:"Your primary phone number. It can be a mobile or a home or work telephone."},{name:"secondaryPhone",label:"Secondary Phone",type:"phone",instructionText:"Good to have just in case we can't catch you on your primary phone."},{name:"addresses",label:"Addresses",type:"address",required:!0,instructionText:"Provide us with at least 1 physical address at which we can contact you.",inputSettings:{amountPhysical:3,getOptionsCountries:R,getOptionsStates:G,googleMapsApiKey:"AIzaSyArV4f-KFF86Zn9VWAu9wS4hHlG1TXxqac"}},{name:"favouriteColor",label:"Favourite Colour",type:"color"},{name:"favouriteNumbers",label:"What are your favourite numbers?",type:"matrix",required:!0,instructionText:"There must be at least 1 that you like. Pun intended.",inputSettings:{dataView:w,buttons:[{label:"Add",onClick:()=>{const e=Y(1,1e3),r=B(e);o({name:"favouriteNumbers",value:(l=[])=>[...l,{id:e,title:e,description:`I ❤️ ${e} because it has ${r.length} factors which are ${M(r.map(String))}`}],touched:!0})},color:"teal",variant:"text",mIcon:k}]}},{name:"hobbies",label:"What do you like?",type:"textEditor",required:!0,instructionText:"In a few words, describe to us your most distinguishing hobbies."},{name:"animalsOrVehicles",label:"Would you rather talk about animals or vehicles?",type:"radio",required:!0,inputSettings:{options:[{value:"animals",label:"Animals"},{value:"vehicles",label:"Vehicles"}]}},{name:"vehiclesRace",label:"If you had to choose to race around the nurburgring, what would it be?",type:"text",required:!0},{name:"vehiclesSleep",label:"If you had to choose a car to sleep in for a week, what would it be?",type:"text",required:!0},{name:"vehiclesTrip",label:"If you had to choose a car to road trip from east to west coast USA, what would it be?",type:"text",required:!0},{name:"animalsBattle",label:"If had to choose to ride an animal into battle, what would it be?",type:"text",required:!0},{name:"animalsCompanion",label:"If had to choose an animal to be your companion for life, what would it be?",type:"text",required:!0},{name:"animalsTalk",label:"If you could communicate with any animal, what would it be?",type:"text",required:!0}],[h,f,b,w,o,g,v]),I=a.useMemo(()=>[{title:"About You",description:"The official stuff",fields:m},{title:"Contact Details",description:"How would we get in touch with you otherwise?",fields:[[["email"],[],[]],[["phone"],["secondaryPhone"],[]],[["addresses"]]]},{title:"Preferences",description:"What you're into",fields:[[["favouriteNumbers"]],[["favouriteColor"]],[["hobbies"]],[["animalsOrVehicles"]]]},{title:"Vehicles",description:"Speed through these questions...",fields:[[["vehiclesRace"]],[["vehiclesSleep"]],[["vehiclesTrip"]]],show:({data:e})=>e.animalsOrVehicles&&e.animalsOrVehicles.value==="vehicles"},{title:"Animals",description:"Humans don't count.",fields:[[["animalsBattle"]],[["animalsCompanion"]],[["animalsTalk"]]],show:({data:e})=>e.animalsOrVehicles&&e.animalsOrVehicles.value==="animals"}],[m]);return a.useEffect(()=>{if(t.touched.initials)return;const e=[typeof t.data.firstName=="string"&&t.data.firstName.length?t.data.firstName[0]:"",typeof t.data.lastName=="string"&&t.data.lastName.length?t.data.lastName[0]:""].filter(Boolean).join("");o({name:"initials",value:e})},[t.data.firstName,t.data.lastName,t.touched.initials,o]),u.jsxs(u.Fragment,{children:[n&&u.jsx("pre",{children:JSON.stringify(t,null,"  ")}),u.jsx("div",{style:{height:i},children:u.jsx(D,{...y,buttons:j(T),title:"Profile",description:"Give us some information to understand a little more about you.",sections:I,fields:q,skeleton:N})})]})};s.args={showState:!1,height:"100vh",section1Fields:E,showFirstName:!0,showLastName:!0,showInitials:!0,skeleton:!1};s.argTypes={showState:{name:"Show state"},height:{name:"Height"},section1Fields:{name:"Section 1 Fields"},showFirstName:{name:"Show First Name Field",control:{type:"select"},options:p},showLastName:{name:"Show Last Name Field",control:{type:"select"},options:p},showInitials:{name:"Show Initials Field",control:{type:"select"},options:p},skeleton:{name:"Skeleton"}};var x,F,S;s.parameters={...s.parameters,docs:{...(x=s.parameters)==null?void 0:x.docs,source:{originalSource:`({
+  showState,
+  height,
+  section1Fields,
+  showFirstName,
+  showLastName,
+  showInitials,
+  skeleton
+}: typeof Profile.args): ReactElement => {
+  const controller = useForm();
+  const {
+    state,
+    methods: {
+      setFieldValue
+    },
+    handleSubmit
+  } = controller;
+  useEffect(() => {
+    document.body.style.margin = "0px";
+    return () => {
+      document.body.style.margin = ORIGINAL_BODY_MARGIN;
+    };
+  }, []);
+  const gridConfig: DataViewProps = useMemo(() => ({
+    columns: [{
+      name: "id",
+      label: "ID"
+    }, {
+      name: "title",
+      label: "Title"
+    }, {
+      name: "description",
+      label: "Description"
+    }],
+    primaryActions: [{
+      name: "delete",
+      color: "black",
+      variant: "icon",
+      mIcon: DeleteIcon,
+      onClick: ({
+        data
+      }) => {
+        setFieldValue({
+          name: "favouriteNumbers",
+          value: (numbers = []) => numbers.filter(({
+            id
+          }) => id !== data.id),
+          touched: true
+        });
+      }
+    }],
+    data: state.data.formMatrix,
+    limit: 25,
+    display: "list",
+    activeColumns: ["id", "title", "description"]
+  }), [setFieldValue, state.data.formMatrix]);
+  const onFileAdd: UploadFieldInputSettings["onFileAdd"] = useCallback(async ({
+    file,
+    onChunkComplete,
+    onUploadComplete
+  }) => {
+    for (let i = 0; i < 10; i++) {
+      await new Promise(resolve => setTimeout(() => resolve(onChunkComplete({
+        percent: (i + 1) * 0.1
+      })), 2 * 1000 / 10));
+    }
+    await onUploadComplete({
+      id: nanoid(),
+      name: file.name,
+      size: file.size,
+      thumbnailUrl: ["image/gif", "image/jpeg", "image/png"].includes(file.type) ? URL.createObjectURL(file) : "",
+      fileUrl: URL.createObjectURL(file)
+    });
+  }, []);
+  const onFileDelete = useCallback(async () => {
+    await new Promise(resolve => setTimeout(() => resolve(null), 2 * 1000));
+  }, []);
+  const fields = useMemo((): FieldDef[] => [{
+    name: "firstName",
+    label: "First Name",
+    type: "text",
+    required: true,
+    instructionText: "Please use the name that you were given at birth.",
+    show: toggleMap[showFirstName]
+  }, {
+    name: "lastName",
+    label: "Last Name",
+    type: "text",
+    required: true,
+    instructionText: "Please use the family name that you had at birth.",
+    show: toggleMap[showLastName]
+  }, {
+    name: "initials",
+    label: "Initials",
+    type: "text",
+    required: true,
+    instructionText: "If you don't interact with this field, it'll be generated for you based on your first and last names.",
+    show: toggleMap[showInitials]
+  }, {
+    name: "dob",
+    label: "Date of Birth",
+    type: "date",
+    required: true,
+    instructionText: "Pick from the datepicker or enter a MM/DD/YYYY formatted date."
+  }, {
+    name: "sex",
+    label: "Sex",
+    type: "radio",
+    required: true,
+    instructionText: "This is the gender you were assigned at birth.",
+    inputSettings: {
+      options: [{
+        value: "male",
+        label: "Male"
+      }, {
+        value: "female",
+        label: "Female"
+      }]
+    }
+  }, {
+    name: "gender",
+    label: "Gender",
+    type: "dropdown",
+    required: true,
+    instructionText: "This is the gender that you identify as now.",
+    inputSettings: {
+      options: [{
+        value: "male",
+        label: "Male"
+      }, {
+        value: "female",
+        label: "Female"
+      }, {
+        value: "other",
+        label: "Other (Please specify)"
+      }, {
+        value: "undisclosed",
+        label: "Prefer not to say"
+      }]
+    }
+  }, {
+    name: "otherGender",
+    label: "Gender Specified",
+    type: "text",
+    required: true,
+    instructionText: "Please tell us what gender you identify as.",
+    show: ({
+      data
+    }) => data.gender && data.gender.value === "other"
+  }, {
+    name: "profilePicture",
+    label: "Profile picture",
+    type: "upload",
+    instructionText: "A profile picture is not required, but it's nice to put a face to the name.",
+    inputSettings: {
+      onFileAdd,
+      onFileDelete
+    }
+  }, {
+    name: "email",
+    label: "Email Address",
+    type: "text",
+    required: true,
+    validators: [validateEmail, validateSlow]
+  }, {
+    name: "phone",
+    label: "Phone",
+    type: "phone",
+    required: true,
+    instructionText: "Your primary phone number. It can be a mobile or a home or work telephone."
+  }, {
+    name: "secondaryPhone",
+    label: "Secondary Phone",
+    type: "phone",
+    instructionText: "Good to have just in case we can't catch you on your primary phone."
+  }, {
+    name: "addresses",
+    label: "Addresses",
+    type: "address",
+    required: true,
+    instructionText: "Provide us with at least 1 physical address at which we can contact you.",
+    inputSettings: {
+      amountPhysical: 3,
+      getOptionsCountries,
+      getOptionsStates,
+      googleMapsApiKey: "AIzaSyArV4f-KFF86Zn9VWAu9wS4hHlG1TXxqac"
+    }
+  }, {
+    name: "favouriteColor",
+    label: "Favourite Colour",
+    type: "color"
+  }, {
+    name: "favouriteNumbers",
+    label: "What are your favourite numbers?",
+    type: "matrix",
+    required: true,
+    instructionText: "There must be at least 1 that you like. Pun intended.",
+    inputSettings: {
+      dataView: gridConfig,
+      buttons: [{
+        label: "Add",
+        onClick: () => {
+          const id = randomNumber(1, 1000);
+          const idFactors = factors(id);
+          setFieldValue({
+            name: "favouriteNumbers",
+            value: (numbers = []) => [...numbers, {
+              id,
+              title: id,
+              description: \`I ❤️ \${id} because it has \${idFactors.length} factors which are \${joinAnd(idFactors.map(String))}\`
+            }],
+            touched: true
+          });
+        },
+        color: "teal",
+        variant: "text",
+        mIcon: AddIcon
+      }]
+    }
+  }, {
+    name: "hobbies",
+    label: "What do you like?",
+    type: "textEditor",
+    required: true,
+    instructionText: "In a few words, describe to us your most distinguishing hobbies."
+  }, {
+    name: "animalsOrVehicles",
+    label: "Would you rather talk about animals or vehicles?",
+    type: "radio",
+    required: true,
+    inputSettings: {
+      options: [{
+        value: "animals",
+        label: "Animals"
+      }, {
+        value: "vehicles",
+        label: "Vehicles"
+      }]
+    }
+  }, {
+    name: "vehiclesRace",
+    label: "If you had to choose to race around the nurburgring, what would it be?",
+    type: "text",
+    required: true
+  }, {
+    name: "vehiclesSleep",
+    label: "If you had to choose a car to sleep in for a week, what would it be?",
+    type: "text",
+    required: true
+  }, {
+    name: "vehiclesTrip",
+    label: "If you had to choose a car to road trip from east to west coast USA, what would it be?",
+    type: "text",
+    required: true
+  }, {
+    name: "animalsBattle",
+    label: "If had to choose to ride an animal into battle, what would it be?",
+    type: "text",
+    required: true
+  }, {
+    name: "animalsCompanion",
+    label: "If had to choose an animal to be your companion for life, what would it be?",
+    type: "text",
+    required: true
+  }, {
+    name: "animalsTalk",
+    label: "If you could communicate with any animal, what would it be?",
+    type: "text",
+    required: true
+  }], [showFirstName, showLastName, showInitials, gridConfig, setFieldValue, onFileAdd, onFileDelete]);
+  const sections = useMemo(() => [{
+    title: "About You",
+    description: "The official stuff",
+    fields: section1Fields
+  }, {
+    title: "Contact Details",
+    description: "How would we get in touch with you otherwise?",
+    fields: [[["email"], [], []], [["phone"], ["secondaryPhone"], []], [["addresses"]]]
+  }, {
+    title: "Preferences",
+    description: "What you're into",
+    fields: [
+    // row 1
+    [["favouriteNumbers"]], [["favouriteColor"]], [["hobbies"]], [["animalsOrVehicles"]]]
+  }, {
+    title: "Vehicles",
+    description: "Speed through these questions...",
+    fields: [[["vehiclesRace"]], [["vehiclesSleep"]], [["vehiclesTrip"]]],
+    show: ({
+      data
+    }) => data.animalsOrVehicles && data.animalsOrVehicles.value === "vehicles"
+  }, {
+    title: "Animals",
+    description: "Humans don't count.",
+    fields: [[["animalsBattle"]], [["animalsCompanion"]], [["animalsTalk"]]],
+    show: ({
+      data
+    }) => data.animalsOrVehicles && data.animalsOrVehicles.value === "animals"
+  }], [section1Fields]);
+  useEffect(() => {
+    if (state.touched.initials) {
+      return;
+    }
+    const initials = [typeof state.data.firstName === "string" && state.data.firstName.length ? state.data.firstName[0] : "", typeof state.data.lastName === "string" && state.data.lastName.length ? state.data.lastName[0] : ""].filter(Boolean).join("");
+    setFieldValue({
+      name: "initials",
+      value: initials
+    });
+  }, [state.data.firstName, state.data.lastName, state.touched.initials, setFieldValue]);
+  return <>
+            {showState && <pre>{JSON.stringify(state, null, "  ")}</pre>}
+            <div style={{
+      height
+    }}>
+                <Form {...controller} buttons={renderButtons(handleSubmit)} title="Profile" description="Give us some information to understand a little more about you." sections={sections} fields={fields} skeleton={skeleton} />
+            </div>
+        </>;
+}`,...(S=(F=s.parameters)==null?void 0:F.docs)==null?void 0:S.source}}};const nt=["Profile"];export{s as Profile,nt as __namedExportsOrder,tt as default};
