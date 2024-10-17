11cf2f0a1f4386ebacae6f4f33c358be8d0a9cb9
George Batt <george.batt@simpleviewinc.com>
2024-10-17 16:05:55 +0100

    (chore) All working. Importing some other misc things.

--- containers/consumer-tests/index.ts
+++ containers/consumer-tests/index.ts
@@ -1,40 +1,50 @@
 /**
+ * Misc
+ */
+import type { MosaicLabelValue, MosaicObject, MosaicToggle, SvgIconComponent } from "@simpleview/sv-mosaic";
+
+
+/**
  * Components
  */
-import Button, { ButtonProps } from "@simpleview/sv-mosaic/components/Button";
-import Card, { CardProps } from "@simpleview/sv-mosaic/components/Card";
-import Checkbox, { CheckboxProps } from "@simpleview/sv-mosaic/components/Checkbox";
-import CheckboxList, { CheckboxListProps } from "@simpleview/sv-mosaic/components/CheckboxList";
-import Chip, { ChipsProps } from "@simpleview/sv-mosaic/components/Chip";
-import Content, { ContentProps } from "@simpleview/sv-mosaic/components/Content";
-import DataView, { DataViewProps } from "@simpleview/sv-mosaic/components/DataView";
-import DataViewFilterDate, { DataViewFilterDateProps } from "@simpleview/sv-mosaic/components/DataViewFilterDate";
-import DataViewFilterDropdown, { DataViewFilterDropdownProps } from "@simpleview/sv-mosaic/components/DataViewFilterDropdown";
-import DataViewFilterDropdownButtons, { DataViewFilterDropdownButtonsProps } from "@simpleview/sv-mosaic/components/DataViewFilterDropdownButtons";
-import DataViewFilterMultiselect, { DataViewFilterMultiselectProps } from "@simpleview/sv-mosaic/components/DataViewFilterMultiselect";
-import DataViewFilterNumber, { DataViewFilterNumberProps } from "@simpleview/sv-mosaic/components/DataViewFilterNumber";
-import DataViewFilterSingleSelect, { DataViewFilterSingleSelectProps } from "@simpleview/sv-mosaic/components/DataViewFilterSingleSelect";
-import DataViewFilterText, { DataViewFilterTextProps } from "@simpleview/sv-mosaic/components/DataViewFilterText";
-import DataViewPrimaryFilter, { DataViewPrimaryFilterProps } from "@simpleview/sv-mosaic/components/DataViewPrimaryFilter";
-import Dialog, { DialogProps } from "@simpleview/sv-mosaic/components/Dialog";
-import Drawer, { DrawerProps } from "@simpleview/sv-mosaic/components/Drawer";
-import DrawerContent, { DrawerContentProps } from "@simpleview/sv-mosaic/components/DrawerContent";
-import Drawers, { DrawersProps } from "@simpleview/sv-mosaic/components/Drawers";
-import Form, { FormProps } from "@simpleview/sv-mosaic/components/Form";
-import LeftNav, { LeftNavProps } from "@simpleview/sv-mosaic/components/LeftNav";
-import Menu, { MenuProps } from "@simpleview/sv-mosaic/components/Menu";
-import MenuBase, { MenuBaseProps } from "@simpleview/sv-mosaic/components/MenuBase";
-import MenuItem, { MenuItemProps } from "@simpleview/sv-mosaic/components/MenuItem";
+import Button, { type ButtonProps } from "@simpleview/sv-mosaic/components/Button";
+import Card, { type CardProps } from "@simpleview/sv-mosaic/components/Card";
+import Checkbox, { type CheckboxProps } from "@simpleview/sv-mosaic/components/Checkbox";
+import CheckboxList, { type CheckboxListProps } from "@simpleview/sv-mosaic/components/CheckboxList";
+import Chip, { type ChipsProps } from "@simpleview/sv-mosaic/components/Chip";
+import Content, { type ContentProps } from "@simpleview/sv-mosaic/components/Content";
+import DataView, { type DataViewProps } from "@simpleview/sv-mosaic/components/DataView";
+import type { DataViewAction, DataViewAdditionalAction, DataViewBulkAction, DataViewColumn } from "@simpleview/sv-mosaic/components/DataView"
+import DataViewFilterDate, { type DataViewFilterDateProps } from "@simpleview/sv-mosaic/components/DataViewFilterDate";
+import DataViewFilterDropdown, { type DataViewFilterDropdownProps } from "@simpleview/sv-mosaic/components/DataViewFilterDropdown";
+import DataViewFilterDropdownButtons, { type DataViewFilterDropdownButtonsProps } from "@simpleview/sv-mosaic/components/DataViewFilterDropdownButtons";
+import DataViewFilterMultiselect, { type DataViewFilterMultiselectProps } from "@simpleview/sv-mosaic/components/DataViewFilterMultiselect";
+import DataViewFilterNumber, { type DataViewFilterNumberProps } from "@simpleview/sv-mosaic/components/DataViewFilterNumber";
+import DataViewFilterSingleSelect, { type DataViewFilterSingleSelectProps } from "@simpleview/sv-mosaic/components/DataViewFilterSingleSelect";
+import DataViewFilterText, { type DataViewFilterTextProps } from "@simpleview/sv-mosaic/components/DataViewFilterText";
+import DataViewPrimaryFilter, { type DataViewPrimaryFilterProps } from "@simpleview/sv-mosaic/components/DataViewPrimaryFilter";
+import Dialog, { type DialogProps } from "@simpleview/sv-mosaic/components/Dialog";
+import Drawer, { type DrawerProps } from "@simpleview/sv-mosaic/components/Drawer";
+import DrawerContent, { type DrawerContentProps } from "@simpleview/sv-mosaic/components/DrawerContent";
+import Drawers, { type DrawersProps } from "@simpleview/sv-mosaic/components/Drawers";
+import Form, { type FormProps } from "@simpleview/sv-mosaic/components/Form";
+import type { FieldDef } from "@simpleview/sv-mosaic/components/Field";
+import type { Col, Row, NumberTableData } from "@simpleview/sv-mosaic/components/Field/FormFieldNumberTable";
+import LeftNav, { type LeftNavProps } from "@simpleview/sv-mosaic/components/LeftNav";
+import type { LeftNavItemDef } from "@simpleview/sv-mosaic/components/LeftNav";
+import Menu, { type MenuProps } from "@simpleview/sv-mosaic/components/Menu";
+import MenuBase, { type MenuBaseProps } from "@simpleview/sv-mosaic/components/MenuBase";
+import MenuItem, { type MenuItemProps } from "@simpleview/sv-mosaic/components/MenuItem";
 import MosaicContext from "@simpleview/sv-mosaic/components/MosaicContext";
-import PageHeader, { PageHeaderProps } from "@simpleview/sv-mosaic/components/PageHeader";
-import Popover, { PopoverProps } from "@simpleview/sv-mosaic/components/Popover";
-import RadioButton, { RadioButtonProps } from "@simpleview/sv-mosaic/components/RadioButton";
-import SideNav, { SideNavProps } from "@simpleview/sv-mosaic/components/SideNav";
-import Snackbar, { SnackbarProps } from "@simpleview/sv-mosaic/components/Snackbar";
-import Spinner, { SpinnerProps } from "@simpleview/sv-mosaic/components/Spinner";
-import Toggle, { ToggleProps } from "@simpleview/sv-mosaic/components/Toggle";
-import TopSummary, { TopSummaryProps } from "@simpleview/sv-mosaic/components/TopSummary";
-import Typography, { TypographyProps } from "@simpleview/sv-mosaic/components/Typography";
+import PageHeader, { type PageHeaderProps } from "@simpleview/sv-mosaic/components/PageHeader";
+import Popover, { type PopoverProps } from "@simpleview/sv-mosaic/components/Popover";
+import RadioButton, { type RadioButtonProps } from "@simpleview/sv-mosaic/components/RadioButton";
+import SideNav, { type SideNavProps } from "@simpleview/sv-mosaic/components/SideNav";
+import Snackbar, { type SnackbarProps } from "@simpleview/sv-mosaic/components/Snackbar";
+import Spinner, { type SpinnerProps } from "@simpleview/sv-mosaic/components/Spinner";
+import Toggle, { type ToggleProps } from "@simpleview/sv-mosaic/components/Toggle";
+import TopSummary, { type TopSummaryProps } from "@simpleview/sv-mosaic/components/TopSummary";
+import Typography, { type TypographyProps } from "@simpleview/sv-mosaic/components/Typography";
 
 /**
  * Theme
