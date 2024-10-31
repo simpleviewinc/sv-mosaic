849867cebe7563e09d85edec56febb517fa41d56
George Batt <george.batt@simpleviewinc.com>
2024-10-31 15:05:07 +0000

    (chore) Memo the result of the field config map to avoid creating the map every time it's needed.

--- containers/mosaic/src/utils/form/getFieldConfig.ts
+++ containers/mosaic/src/utils/form/getFieldConfig.ts
@@ -29,184 +29,202 @@ import FormFieldUpload from "@root/components/Field/FormFieldUpload/FormFieldUpl
 import FormFieldNumber from "@root/components/Field/FormFieldNumber/FormFieldNumber";
 import FormFieldNumberTable from "@root/components/Field/FormFieldNumberTable/FormFieldNumberTable";
 
-export function getFieldConfig(type: FieldDef["type"]): FieldConfig {
-	const fieldConfigMap: Partial<Record<Exclude<FieldDef["type"], FieldDefCustom["type"]>, FieldConfig>> = {
-		text: {
-			Component: FormFieldText,
-			validate: "onBlurAmend",
-			getResolvedValue: defaultResolver,
-		},
-		checkbox: {
-			Component: FormFieldCheckbox,
-			validate: "onChange",
-			getResolvedValue: defaultResolver,
-		},
-		chip: {
-			Component: FormFieldChips,
-			validate: "onChange",
-			getResolvedValue: defaultResolver,
-		},
-		dropdown: {
-			Component: FormFieldDropdown,
-			validate: "onChange",
-			getResolvedValue: defaultResolver,
-		},
-		phone: {
-			Component: FormFieldPhone,
-			validate: "onBlurAmend",
-			getResolvedValue: defaultResolver,
-		},
-		radio: {
-			Component: FormFieldRadio,
-			validate: "onChange",
-			getResolvedValue: defaultResolver,
-		},
-		toggle: {
-			Component: FormFieldToggle,
-			validate: "onChange",
-			getResolvedValue: defaultResolver,
-		},
-		color: {
-			Component: FormFieldColor,
-			validate: "onBlur",
-			getResolvedValue: defaultResolver,
-		},
-		date: {
-			Component: FormFieldDate,
-			validate: "onBlurChange",
-			getResolvedValue: (
-				value: DateData | Date | undefined,
-				fieldDef: FieldDefDate,
-			): {
-				internalValue: DateData | undefined;
-				value: Date | undefined;
-			} => {
-				if (value instanceof Date ) {
-					return {
-						internalValue: { date: value, validDate: true, time: value, validTime: true },
-						value,
-					};
-				} else {
-					const fixedTime = fieldDef?.inputSettings?.fixedTime || [0, 0, 0, 0];
+type FieldConfigMap = Partial<Record<Exclude<FieldDef["type"], FieldDefCustom["type"]>, FieldConfig>>;
 
-					if (!value) {
+function getFieldConfigMapMemo(): () => FieldConfigMap {
+	let cache: null | FieldConfigMap = null;
+
+	return () => {
+		if (cache) {
+			return cache;
+		}
+
+		cache = {
+			text: {
+				Component: FormFieldText,
+				validate: "onBlurAmend",
+				getResolvedValue: defaultResolver,
+			},
+			checkbox: {
+				Component: FormFieldCheckbox,
+				validate: "onChange",
+				getResolvedValue: defaultResolver,
+			},
+			chip: {
+				Component: FormFieldChips,
+				validate: "onChange",
+				getResolvedValue: defaultResolver,
+			},
+			dropdown: {
+				Component: FormFieldDropdown,
+				validate: "onChange",
+				getResolvedValue: defaultResolver,
+			},
+			phone: {
+				Component: FormFieldPhone,
+				validate: "onBlurAmend",
+				getResolvedValue: defaultResolver,
+			},
+			radio: {
+				Component: FormFieldRadio,
+				validate: "onChange",
+				getResolvedValue: defaultResolver,
+			},
+			toggle: {
+				Component: FormFieldToggle,
+				validate: "onChange",
+				getResolvedValue: defaultResolver,
+			},
+			color: {
+				Component: FormFieldColor,
+				validate: "onBlur",
+				getResolvedValue: defaultResolver,
+			},
+			date: {
+				Component: FormFieldDate,
+				validate: "onBlurChange",
+				getResolvedValue: (
+					value: DateData | Date | undefined,
+					fieldDef: FieldDefDate,
+				): {
+					internalValue: DateData | undefined;
+					value: Date | undefined;
+				} => {
+					if (value instanceof Date ) {
 						return {
-							internalValue: undefined,
-							value: undefined,
+							internalValue: { date: value, validDate: true, time: value, validTime: true },
+							value,
 						};
-					}
+					} else {
+						const fixedTime = fieldDef?.inputSettings?.fixedTime || [0, 0, 0, 0];
 
-					if (!value.validDate) {
-						return { internalValue: value, value: undefined };
-					}
+						if (!value) {
+							return {
+								internalValue: undefined,
+								value: undefined,
+							};
+						}
 
-					if (!fieldDef?.inputSettings?.showTime) {
-						return { internalValue: value, value: matchTime(value.date, fixedTime) };
-					}
+						if (!value.validDate) {
+							return { internalValue: value, value: undefined };
+						}
 
-					if (!value.validTime) {
-						return { internalValue: value, value: undefined };
-					}
+						if (!fieldDef?.inputSettings?.showTime) {
+							return { internalValue: value, value: matchTime(value.date, fixedTime) };
+						}
 
-					return { internalValue: value, value: matchTime(value.date, value.time) };
-				}
+						if (!value.validTime) {
+							return { internalValue: value, value: undefined };
+						}
+
+						return { internalValue: value, value: matchTime(value.date, value.time) };
+					}
+				},
 			},
-		},
-		time: {
-			Component: FormFieldTime,
-			validate: "onBlur",
-			getResolvedValue: (
-				value: TimeData | string | undefined,
-			): {
-				internalValue: TimeData | undefined;
-				value: string | undefined;
-			} => {
-				if (typeof value === "string") {
-					const date = new Date();
-					const [hrs, mins] = value.split(":");
+			time: {
+				Component: FormFieldTime,
+				validate: "onBlur",
+				getResolvedValue: (
+					value: TimeData | string | undefined,
+				): {
+					internalValue: TimeData | undefined;
+					value: string | undefined;
+				} => {
+					if (typeof value === "string") {
+						const date = new Date();
+						const [hrs, mins] = value.split(":");
 
-					date.setHours(Number(hrs));
-					date.setMinutes(Number(mins));
+						date.setHours(Number(hrs));
+						date.setMinutes(Number(mins));
 
-					return {
-						internalValue: { time: date, validTime: true },
-						value,
-					};
-				} else {
-					if (!value) {
 						return {
-							internalValue: undefined,
-							value: undefined,
+							internalValue: { time: date, validTime: true },
+							value,
 						};
+					} else {
+						if (!value) {
+							return {
+								internalValue: undefined,
+								value: undefined,
+							};
+						}
+
+						if (!value.validTime) {
+							return { internalValue: value, value: undefined };
+						}
+
+						const time = `${
+							String(value.time.getHours()).padStart(2, "0")
+						}:${
+							String(value.time.getMinutes()).padStart(2, "0")
+						}`;
+
+						return { internalValue: value, value: time };
 					}
-
-					if (!value.validTime) {
-						return { internalValue: value, value: undefined };
-					}
-
-					const time = `${
-						String(value.time.getHours()).padStart(2, "0")
-					}:${
-						String(value.time.getMinutes()).padStart(2, "0")
-					}`;
-
-					return { internalValue: value, value: time };
-				}
+				},
 			},
-		},
-		address: {
-			Component: FormFieldAddress,
-			validate: "onBlur",
-			getResolvedValue: defaultResolver,
-		},
-		textEditor: {
-			Component: FormFieldTextEditor,
-			validate: "onBlurAmend",
-			getResolvedValue: defaultResolver,
-		},
-		advancedSelection: {
-			Component: FormFieldAdvancedSelection,
-			validate: "onBlur",
-			getResolvedValue: defaultResolver,
-		},
-		mapCoordinates: {
-			Component: FormFieldMapCoordinates,
-			validate: "onBlur",
-			getResolvedValue: defaultResolver,
-		},
-		matrix: {
-			Component: FormFieldMatrix,
-			validate: "onChange",
-			getResolvedValue: defaultResolver,
-		},
-		upload: {
-			Component: FormFieldUpload,
-			validate: "onChange",
-			getResolvedValue: (
-				value: (UploadData | UploadDataPending)[] = [],
-			) => {
-				return {
-					value: cleanValue(value.filter((item) => !isPendingUploadData(item) && !item.isDeleting)),
-					internalValue: value || [],
-				};
+			address: {
+				Component: FormFieldAddress,
+				validate: "onBlur",
+				getResolvedValue: defaultResolver,
 			},
-		},
-		number: {
-			Component: FormFieldNumber,
-			validate: "onBlurAmend",
-			getResolvedValue: defaultResolver,
-		},
-		numberTable: {
-			Component: FormFieldNumberTable,
-			validate: "onChange",
-			getResolvedValue: defaultResolver,
-		},
-		raw: {
-			Component: FormFieldRaw,
-			validate: "onChange",
-			getResolvedValue: defaultResolver,
-		},
+			textEditor: {
+				Component: FormFieldTextEditor,
+				validate: "onBlurAmend",
+				getResolvedValue: defaultResolver,
+			},
+			advancedSelection: {
+				Component: FormFieldAdvancedSelection,
+				validate: "onBlur",
+				getResolvedValue: defaultResolver,
+			},
+			mapCoordinates: {
+				Component: FormFieldMapCoordinates,
+				validate: "onBlur",
+				getResolvedValue: defaultResolver,
+			},
+			matrix: {
+				Component: FormFieldMatrix,
+				validate: "onChange",
+				getResolvedValue: defaultResolver,
+			},
+			upload: {
+				Component: FormFieldUpload,
+				validate: "onChange",
+				getResolvedValue: (
+					value: (UploadData | UploadDataPending)[] = [],
+				) => {
+					return {
+						value: cleanValue(value.filter((item) => !isPendingUploadData(item) && !item.isDeleting)),
+						internalValue: value || [],
+					};
+				},
+			},
+			number: {
+				Component: FormFieldNumber,
+				validate: "onBlurAmend",
+				getResolvedValue: defaultResolver,
+			},
+			numberTable: {
+				Component: FormFieldNumberTable,
+				validate: "onChange",
+				getResolvedValue: defaultResolver,
+			},
+			raw: {
+				Component: FormFieldRaw,
+				validate: "onChange",
+				getResolvedValue: defaultResolver,
+			},
+		};
+
+		return cache;
 	};
+}
+
+const getFieldConfigMap = getFieldConfigMapMemo();
+
+export function getFieldConfig(type: FieldDef["type"]): FieldConfig {
+	const fieldConfigMap = getFieldConfigMap();
 
 	if (typeof type !== "string") {
 		return {
