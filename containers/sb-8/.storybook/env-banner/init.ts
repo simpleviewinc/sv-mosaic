d95e0edcdebbc0f53ed79d6ad0824bf3b06fe1d3
George Batt <george.batt@simpleviewinc.com>
2024-11-06 18:48:55 +0000

    (chore) Migrate the banner that indicates the current environment that was implemented in Storybook 5.

---
+++ containers/sb-8/.storybook/env-banner/init.ts
@@ -0,0 +1,34 @@
+import { version } from "../../package.json";
+import { environments } from "./environments";
+
+const state = {
+    added: false,
+};
+
+function escapeRegex(string) {
+    return string.replace(/[/\-\\^$*+?.()|[\]{}]/g, '\\$&');
+}
+
+export function init() {
+    if (state.added) {
+        return;
+    }
+
+    const { body } = document;
+    const banner = document.createElement("div");
+    banner.classList.add("env-banner");
+    body.appendChild(banner);
+
+    for(const { location, name } of environments) {
+        const regex = new RegExp(escapeRegex(location));
+        if (!regex.test(window.location.href)){
+            continue;
+        }
+
+        body.setAttribute("data-env", name);
+        banner.innerHTML = `${name} - Mosaic v${version}`
+
+    }
+
+    state.added = true;
+}
