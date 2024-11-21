c9a3a2d3cca47613f1ee18df276416ef5b85ccca
George Batt <george.batt@simpleviewinc.com>
2024-11-21 15:50:58 +0000

    (chore) Adds storybook eslint plugin to SB container.

--- containers/sb-8/eslint.config.mjs
+++ containers/sb-8/eslint.config.mjs
@@ -1,7 +1,9 @@
 import baseConfig from "@simpleview/sv-mosaic-eslint/base";
+import storybook from "eslint-plugin-storybook";
 
 export default [
 	...baseConfig,
+	...storybook.configs["flat/recommended"],
 	{
 		ignores: [
 			"storybook-static",
