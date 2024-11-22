7aa317c35ba3d9e3ed98927bd9ef4d6ed895a357
George Batt <george.batt@simpleviewinc.com>
2024-11-22 10:33:48 +0000

    (chore) Fix incorrect merge conflict

--- containers/sb-8/stories/components/Drawers/Drawers.stories.tsx
+++ containers/sb-8/stories/components/Drawers/Drawers.stories.tsx
@@ -4,9 +4,9 @@ import { useCallback, useMemo, useState } from "react";
 
 // Components
 import Drawers from "@root/components/Drawers";
-import AppContext from "@root/components/Drawers/example/AppContext";
-import type { AppState, DrawerDef } from "@root/components/Drawers/example/ExampleTypes";
-import Page from "@root/components/Drawers/example/Page";
+import AppContext from "./example/AppContext";
+import type { AppState, DrawerDef } from "./example/ExampleTypes";
+import Page from "./example/Page";
 
 export default {
 	title: "Components/Drawers",
