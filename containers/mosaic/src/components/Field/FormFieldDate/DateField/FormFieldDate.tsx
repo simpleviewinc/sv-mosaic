b0ccd996a508bad7f0317c4a964ac29b4d63de95
George Batt <george.batt@simpleviewinc.com>
2024-12-02 15:20:17 +0000

    (DateField) Utilises field groups to show time and date sub fields when the date field is used with showTime input setting.

--- containers/mosaic/src/components/Field/FormFieldDate/DateField/FormFieldDate.tsx
+++ containers/mosaic/src/components/Field/FormFieldDate/DateField/FormFieldDate.tsx
@@ -1,17 +1,16 @@
 import type { ReactElement } from "react";
 
-import React, { memo, useMemo, useRef } from "react";
+import React, { memo, useMemo } from "react";
 
 import type { MosaicFieldProps } from "@root/components/Field";
 import type { DateFieldInputSettings, DateData } from "./DateFieldTypes";
 
 import { matchTime, parseTime, textIsValidDate } from "@root/utils/date";
-import { DATE_FORMAT_FULL, DATE_FORMAT_FULL_PLACEHOLDER, TIME_FORMAT_FULL, TIME_FORMAT_FULL_PLACEHOLDER } from "@root/constants";
+import { DATE_FORMAT_FULL, DATE_FORMAT_FULL_PLACEHOLDER } from "@root/constants";
 import { INVALID_DATE, INVALID_TIME, TIME_REQUIRED } from "@root/components/Form/fieldErrors";
 import { useFieldErrors } from "@root/utils/hooks";
 import DatePicker from "../DatePicker";
-import TimePicker from "../../FormFieldTime/TimePicker";
-import { DateTimePickerWrapper, DateTimeInputRow } from "./DateField.styled";
+import { DateTimePickerWrapper } from "./DateField.styled";
 import { FormFieldDateSkeleton } from "./FormFieldDateSkeleton";
 
 const FormFieldDate = (props: MosaicFieldProps<"date", DateFieldInputSettings, DateData>): ReactElement => {
@@ -29,15 +28,12 @@ const FormFieldDate = (props: MosaicFieldProps<"date", DateFieldInputSettings, D
 	} = props;
 	const {
 		inputSettings: {
-			showTime,
 			minDate,
 			maxDate,
 			defaultTime,
 		} = {},
 	} = fieldDef;
 
-	const blurred = useRef<{ date: boolean; time: boolean }>({ date: false, time: false });
-	const usingDefaultTime = useRef(false);
 	const value = useMemo(() => providedValue || {
 		validDate: false,
 		validTime: false,
@@ -48,26 +44,15 @@ const FormFieldDate = (props: MosaicFieldProps<"date", DateFieldInputSettings, D
 		name: fieldDef.name,
 	});
 
-	const onBlurProxy = (type: "date" | "time") => async () => {
-		blurred.current[type] = true;
-
-		if (
-			blurred.current.date &&
-			(!showTime || blurred.current.time)
-		) {
-			onBlur();
-		}
-	};
-
 	const handleDateChange = async (date: Date, keyboardInputValue?: string) => {
 		const validKeyboardInput = textIsValidDate(keyboardInputValue, DATE_FORMAT_FULL);
 		const changeByDatepicker = date && !keyboardInputValue;
 
-		if (showTime && date && !value.validTime) {
-			addError(TIME_REQUIRED);
-		} else {
-			removeError(TIME_REQUIRED);
-		}
+		// if (showTime && date && !value.validTime) {
+		// 	addError(TIME_REQUIRED);
+		// } else {
+		// 	removeError(TIME_REQUIRED);
+		// }
 
 		if (
 			// The datepicker was used
@@ -90,7 +75,6 @@ const FormFieldDate = (props: MosaicFieldProps<"date", DateFieldInputSettings, D
 
 				result.time = time;
 				result.validTime = true;
-				usingDefaultTime.current = true;
 			}
 
 			onChange(result, { validate: true });
@@ -107,102 +91,75 @@ const FormFieldDate = (props: MosaicFieldProps<"date", DateFieldInputSettings, D
 				validDate: false,
 			};
 
-			if (usingDefaultTime.current) {
-				result.time = undefined;
-				result.validTime = false;
-			}
-
 			onChange(result, { validate: true });
 		}
 	};
 
-	const handleTimeChange = async (time: Date, keyboardInputValue?: string) => {
-		const isKeyboardEvent = keyboardInputValue !== undefined;
-		const validKeyboardInput = textIsValidDate(keyboardInputValue, TIME_FORMAT_FULL);
+	// const handleTimeChange = async (time: Date, keyboardInputValue?: string) => {
+	// 	const isKeyboardEvent = keyboardInputValue !== undefined;
+	// 	const validKeyboardInput = textIsValidDate(keyboardInputValue, TIME_FORMAT_FULL);
 
-		if (value.date && !time) {
-			removeError(INVALID_TIME);
-			addError(TIME_REQUIRED);
+	// 	if (value.date && !time) {
+	// 		removeError(INVALID_TIME);
+	// 		addError(TIME_REQUIRED);
 
-			onChange({
-				...value,
-				time,
-				validTime: false,
-			}, { validate: true });
-		} else if (isKeyboardEvent && !validKeyboardInput) {
-			// This handler was caused by keyboard input, but it's not a valid date
-			removeError(TIME_REQUIRED);
-			addError(INVALID_TIME);
+	// 		onChange({
+	// 			...value,
+	// 			time,
+	// 			validTime: false,
+	// 		}, { validate: true });
+	// 	} else if (isKeyboardEvent && !validKeyboardInput) {
+	// 		// This handler was caused by keyboard input, but it's not a valid date
+	// 		removeError(TIME_REQUIRED);
+	// 		addError(INVALID_TIME);
 
-			onChange({
-				...value,
-				time,
-				validTime: false,
-			}, { validate: true });
-		} else {
-			usingDefaultTime.current = false;
-			removeError([TIME_REQUIRED, INVALID_TIME]);
+	// 		onChange({
+	// 			...value,
+	// 			time,
+	// 			validTime: false,
+	// 		}, { validate: true });
+	// 	} else {
+	// 		usingDefaultTime.current = false;
+	// 		removeError([TIME_REQUIRED, INVALID_TIME]);
 
-			onChange({
-				...value,
-				time,
-				// An empty field is still an acceptable input as far as
-				// the time field goes, but it doesn't mean it's a valid time.
-				validTime: Boolean(time),
-			}, { validate: true });
-		}
-	};
+	// 		onChange({
+	// 			...value,
+	// 			time,
+	// 			// An empty field is still an acceptable input as far as
+	// 			// the time field goes, but it doesn't mean it's a valid time.
+	// 			validTime: Boolean(time),
+	// 		}, { validate: true });
+	// 	}
+	// };
 
 	if (skeleton) {
-		return <FormFieldDateSkeleton showTime={showTime} />;
+		return <FormFieldDateSkeleton />;
 	}
 
 	return (
-		<DateTimeInputRow $hasTimeField={showTime}>
-			<DateTimePickerWrapper>
-				<DatePicker
-					id={id}
-					error={error}
-					onChange={handleDateChange}
-					fieldDef={{
-						name: fieldDef?.name,
-						label: "",
-						type: "",
-						inputSettings: {
-							placeholder: DATE_FORMAT_FULL_PLACEHOLDER,
-							minDate,
-							maxDate,
-						},
-						required: fieldDef?.required,
-					}}
-					value={value?.date}
-					onBlur={onBlurProxy("date")}
-					disabled={disabled}
-					inputRef={inputRef}
+		<DateTimePickerWrapper>
+			<DatePicker
+				id={id}
+				error={error}
+				onChange={handleDateChange}
+				fieldDef={{
+					name: fieldDef?.name,
+					label: "",
+					type: "",
+					inputSettings: {
+						placeholder: DATE_FORMAT_FULL_PLACEHOLDER,
+						minDate,
+						maxDate,
+					},
+					required: fieldDef?.required,
+				}}
+				value={value?.date}
+				onBlur={onBlur}
+				disabled={disabled}
+				inputRef={inputRef}
 
-				/>
-			</DateTimePickerWrapper>
-			{showTime && (
-				<DateTimePickerWrapper>
-					<TimePicker
-						id={`${fieldDef?.name}-time`}
-						error={error}
-						onChange={handleTimeChange}
-						fieldDef={{
-							name: fieldDef?.name,
-							label: "",
-							type: "timePicker",
-							inputSettings: {
-								placeholder: TIME_FORMAT_FULL_PLACEHOLDER,
-							},
-						}}
-						value={value?.time}
-						onBlur={onBlurProxy("time")}
-						disabled={disabled}
-					/>
-				</DateTimePickerWrapper>
-			)}
-		</DateTimeInputRow>
+			/>
+		</DateTimePickerWrapper>
 	);
 };
 
--- containers/mosaic/src/components/Field/FormFieldTime/TimeField/TimeFieldTypes.tsx
+++ containers/mosaic/src/components/Field/FormFieldTime/TimeField/TimeFieldTypes.tsx
@@ -1,6 +1,15 @@
-import type { FieldDefBase } from "@root/components/Field";
+import type { DateFieldInputSettings, FieldDefBase } from "@root/components/Field";
 
-export type TimeFieldInputSettings = never;
+export interface TimeFieldInputSettings {
+	/**
+	 * The time to default to if the value for
+	 * this field is undefined. Can be a tuple like
+	 * `[hr, min, sec?, ms?]` or a 24hr string like `"HH:MM"`
+	 *
+	 * Only applicable if `showTime` is true.
+	 */
+	defaultTime?: DateFieldInputSettings["defaultTime"];
+}
 
 export interface TimeData {
 	time?: Date;
