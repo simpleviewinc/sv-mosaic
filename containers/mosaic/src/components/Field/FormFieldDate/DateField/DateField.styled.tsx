b967af092c2f97f2ba88f1f1747518a545556ff5
George Batt <george.batt@simpleviewinc.com>
2024-12-02 15:20:17 +0000

    (DateField) Utilises field groups to show time and date sub fields when the date field is used with showTime input setting.

--- containers/mosaic/src/components/Field/FormFieldDate/DateField/DateField.styled.tsx
+++ containers/mosaic/src/components/Field/FormFieldDate/DateField/DateField.styled.tsx
@@ -1,19 +1,6 @@
-import { containerQuery } from "@root/utils/css";
 import styled from "styled-components";
 
 export const DateTimePickerWrapper = styled.div`
 	display: flex;
 	flex-direction: column;
-`;
-
-export const DateTimeInputRow = styled.div<{ $hasTimeField?: boolean }>`
-	${({ $hasTimeField }) => $hasTimeField && `
-		display: grid;
-		grid-template-columns: repeat(1,minmax(0,1fr));
-		gap: 24px 12px;
-
-		${containerQuery("sm", "FORM_COL")} {
-			grid-template-columns: repeat(2,minmax(0,1fr));
-		}
-	`}
 `;
