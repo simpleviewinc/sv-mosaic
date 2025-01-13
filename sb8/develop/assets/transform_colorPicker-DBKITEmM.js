cfb6cc5ddee3be887529d662496851c879d10852
Owen Allen <owenallenaz@gmail.com>
2025-01-13 09:50:13 +0000

    Updates

---
+++ sb8/develop/assets/transform_colorPicker-DBKITEmM.js
@@ -0,0 +1,16 @@
+import{j as r}from"./jsx-runtime-nGUw9YhZ.js";import{useMDXComponents as m}from"./index-3-_tf5dF.js";import{M as p}from"./index-BbAFHYha.js";import{a as n}from"./column_transforms-CyX7y3_5.js";import{T as e}from"./TransformExample-CwCQUWbb.js";import"./index-BP8_t0zE.js";import"./iframe-CjjOpcna.js";import"../sb-preview/runtime.js";import"./index-BxmsGmlx.js";import"./extends-CF3RwP-h.js";import"./tslib.es6-CRos2fHm.js";import"./index-DXimoRZY.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-BjCnz-8e.js";import"./index-DJqTsaQd.js";import"./index-DrFu-skq.js";import"./CheckboxList-BTpJVlmR.js";import"./isSymbol-CEVJpixJ.js";import"./Checkbox-Q9UDhbdf.js";import"./theme-DRtZtbPc.js";import"./FormControlLabel-n1qioM9o.js";import"./generateUtilityClasses-DAcHHM0W.js";import"./formControlState-D52vkf9K.js";import"./ButtonBase-BZmvw3BH.js";import"./useIsFocusVisible-De2ftVE9.js";import"./useControlled-im5M3O13.js";import"./getThemeProps-Cs15h2IA.js";import"./useTheme-5hcycNQy.js";import"./createSvgIcon-CPgMIgId.js";import"./testIds-DZf96GK8.js";import"./ColorSelected-HjDSMxjB.js";import"./index-DRdTCjFK.js";import"./debounce-DO3dP4oK.js";import"./DataViewPrimaryFilter.styled-CylNlUs-.js";import"./createSvgIcon-CcHL1II5.js";import"./createChainedFunction-BO_9K8Jh.js";import"./ownerWindow-DvT1GKkC.js";import"./useId-DLncWxdO.js";import"./Chip-DoaLK3Y8.js";import"./useMosaicTranslation-CZCb37QS.js";import"./FormFieldText.styled-BhoqJBjr.js";import"./Button-Do5OxnZO.js";import"./Popper-CPbWUqAP.js";import"./useToggle-lipGlewK.js";import"./ButtonRow-DWtIszZP.js";import"./Spinner-DgupAfKo.js";import"./Typography-Br_RMLnS.js";import"./SubtitleText-Vdwjg1jD.js";import"./TitleWrapper-D2l-_w9e.js";import"./containerQuery-C0QVV4Kt.js";import"./TitleText-C9TlfF_O.js";import"./MoreVert-BbmWEP2K.js";import"./ChevronRight-CSz705Z0.js";import"./PageHeader-BRjQFO47.js";import"./Image-DW9YTXUC.js";import"./formats-sBod7wm8.js";function t(i){const o={code:"code",h1:"h1",h4:"h4",li:"li",p:"p",pre:"pre",ul:"ul",...m(),...i.components};return r.jsxs(r.Fragment,{children:[r.jsx(p,{title:"Utils/Transforms/ColorPicker"}),`
+`,r.jsx(o.h1,{id:"transform_colorpicker",children:"transform_colorPicker"}),`
+`,r.jsx(o.p,{children:"Converts a hexadecimal string to a readonly Color Picker."}),`
+`,r.jsx(o.h4,{id:"conversions",children:"Conversions"}),`
+`,r.jsxs(o.ul,{children:[`
+`,r.jsxs(o.li,{children:[r.jsx(o.code,{children:'"#a8001791"'})," => ",r.jsx(e,{cb:n(),data:"#a8001791"})]}),`
+`]}),`
+`,r.jsx(o.h4,{id:"args",children:"Args"}),`
+`,r.jsx(o.p,{children:"None"}),`
+`,r.jsx(o.h4,{id:"example",children:"Example"}),`
+`,r.jsx(o.pre,{children:r.jsx(o.code,{className:"language-js",children:`import { transform_colorPicker } from "@simpleview/sv-mosaic"
+...
+transforms : [
+    transform_colorPicker()
+]
+`})})]})}function ar(i={}){const{wrapper:o}={...m(),...i.components};return o?r.jsx(o,{...i,children:r.jsx(t,{...i})}):t(i)}export{ar as default};
