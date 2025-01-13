cdeddb56a8788db278d7b1971dbce3da38a4b6d1
George Batt <george.batt@simpleviewinc.com>
2025-01-13 09:07:33 +0000

    (chore) Avoid 2023 array features.

--- containers/mosaic/src/components/Form/useForm/utils/getFieldPaths.ts
+++ containers/mosaic/src/components/Form/useForm/utils/getFieldPaths.ts
@@ -2,8 +2,10 @@ import type { FieldObj } from "@root/components/Field";
 import type { FieldPath } from "../types";
 
 function getFieldPaths(fields: Record<string, FieldObj>, leading: FieldPath = []): FieldPath[] {
-	const entries = Object.entries(fields).toSorted(([, { order: a }], [, { order: b }]) => a - b);
+	const entries = Object.entries(fields);
 	const paths: FieldPath[] = [];
+
+	entries.sort(([, { order: a }], [, { order: b }]) => a - b);
 
 	for (const [, field] of entries) {
 		const fullPath = [...leading, field.name];
