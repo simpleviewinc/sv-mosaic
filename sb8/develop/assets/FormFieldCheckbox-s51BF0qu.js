631bc3171c970df03bedbbb861d3cc12670ffe6c
Owen Allen <owenallenaz@gmail.com>
2025-01-15 09:33:05 +0000

    Updates

---
+++ sb8/develop/assets/FormFieldCheckbox-s51BF0qu.js
@@ -0,0 +1,50 @@
+import{j as t}from"./jsx-runtime-nGUw9YhZ.js";import{useMDXComponents as e}from"./index-3-_tf5dF.js";import{M as n}from"./index-CGWOAm_e.js";import{s}from"./FormFieldCheckbox.stories-DVtzewRG.js";import"./index-BP8_t0zE.js";import"./iframe-pNl7__mS.js";import"../sb-preview/runtime.js";import"./index-BxmsGmlx.js";import"./extends-CF3RwP-h.js";import"./tslib.es6-CRos2fHm.js";import"./index-DXimoRZY.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-BjCnz-8e.js";import"./index-DJqTsaQd.js";import"./index-DrFu-skq.js";import"./Form-CV-BOiW8.js";import"./theme-DRtZtbPc.js";import"./SideNav-D66jd1x5.js";import"./containerQuery-C0QVV4Kt.js";import"./useToggle-lipGlewK.js";import"./CheckboxList-Dbk8JkiG.js";import"./isSymbol-CEVJpixJ.js";import"./Checkbox-CODv9_Wr.js";import"./FormControlLabel-f9znFBMA.js";import"./generateUtilityClasses-kMkq-zAT.js";import"./formControlState-DyMXFR7f.js";import"./ButtonBase-BP_P9WLq.js";import"./useIsFocusVisible-De2ftVE9.js";import"./useControlled-im5M3O13.js";import"./getThemeProps-BVsvmf2Q.js";import"./useTheme-Cud-ikZw.js";import"./createSvgIcon-BcZTMgP9.js";import"./testIds-DZf96GK8.js";import"./FormFieldText.styled-Cq8lzkgl.js";import"./Button-DuMlbF8s.js";import"./Popper-BWFpCfKc.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useMosaicTranslation-CZCb37QS.js";import"./createSvgIcon-Pzp5yWPs.js";import"./useId-DLncWxdO.js";import"./ButtonRow-CP4CrsnB.js";import"./Chip-CRqL1Gcl.js";import"./sizes-C6oSOElx.js";import"./DataViewPrimaryFilter.styled-auwRvG8E.js";import"./RadioButton-q7Nz9IFm.js";import"./StyledFormControl.styled-BDgwaEEo.js";import"./Toggle-aOIYV_sD.js";import"./index-BU7KlZI9.js";import"./debounce-DO3dP4oK.js";import"./Spinner-Dfqcw6pF.js";import"./Typography-Br_RMLnS.js";import"./SubtitleText-Vdwjg1jD.js";import"./ColorSelected-DyTCIv8L.js";import"./TitleWrapper-CYciWRcJ.js";import"./TitleText-C9TlfF_O.js";import"./MoreVert-gv79E1WA.js";import"./ChevronRight-bPXdPGpY.js";import"./PageHeader-DvUko4so.js";import"./formats-sBod7wm8.js";import"./Dialog-BI8WLJ8j.js";import"./useMediaQuery-Bupge7Mj.js";import"./Snackbar-BF1WUGPv.js";import"./Close-CC6bEs2P.js";import"./ClickAwayListener-DnjBLK-5.js";import"./TooltipIcon-Bg8I7hop.js";import"./FormFieldCheckboxUtils-C4lbz8gc.js";import"./renderButtons-BmHfsf_z.js";import"./options-CstHLxTf.js";function i(o){const r={a:"a",code:"code",h1:"h1",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...e(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:s}),`
+`,t.jsx(r.h1,{id:"checkbox",children:"Checkbox"}),`
+`,t.jsxs(r.p,{children:["This field implements the ",t.jsx(r.a,{href:"#generic-field-props-fielddef",children:t.jsx(r.strong,{children:"FieldDef"})})," interface."]}),`
+`,t.jsxs(r.ul,{children:[`
+`,t.jsx(r.li,{children:"A group of checkbox buttons that allows users to select multiple items from a list of possible options."}),`
+`,t.jsx(r.li,{children:t.jsx(r.a,{href:"/?path=/story/formfields-formfieldcheckbox--playground",children:t.jsx(r.strong,{children:"Playground"})})}),`
+`,t.jsxs(r.li,{children:["Data: ",t.jsx(r.code,{children:"array"})," of ",t.jsx(r.code,{children:"string"})," - Array of values of the selected options."]}),`
+`,t.jsx(r.li,{children:"inputSettings:"}),`
+`]}),`
+`,t.jsxs(r.table,{children:[t.jsx(r.thead,{children:t.jsxs(r.tr,{children:[t.jsx(r.th,{children:"Name"}),t.jsx(r.th,{children:"Type"}),t.jsx(r.th,{children:"Description"})]})}),t.jsxs(r.tbody,{children:[t.jsxs(r.tr,{children:[t.jsx(r.td,{children:t.jsx(r.strong,{children:t.jsx(r.code,{children:"options"})})}),t.jsx(r.td,{children:t.jsx(r.code,{children:"MosaicLabelValue[]"})}),t.jsx(r.td,{children:"required - Array of options to be rendered containing their corresponding label and value."})]}),t.jsxs(r.tr,{children:[t.jsx(r.td,{children:t.jsx(r.strong,{children:t.jsx(r.code,{children:"getOptions"})})}),t.jsx(r.td,{children:t.jsx(r.code,{children:"() => Promise<MosaicLabelValue[]>"})}),t.jsx(r.td,{children:"required - Function to get a set of options. This prop only applies when getting options from a DB."})]})]})]}),`
+`,t.jsx(r.h3,{id:"how-to-use-in-a-form",children:"How to use in a form?"}),`
+`,t.jsx(r.pre,{children:t.jsx(r.code,{className:"language-ts",children:`//Option recommended when getting options locally.\r
+const fields = useMemo(\r
+	() =>\r
+		[\r
+			//...other fields\r
+			{\r
+				//...all generic field props,\r
+				type: "checkbox",\r
+				inputSettings: {\r
+					options: [\r
+						{\r
+							label: "Option1",\r
+							value: "value1",\r
+						}\r
+					],\r
+				}\r
+			},\r
+			//...other fields\r
+		],\r
+	[]\r
+);\r
+\r
+//Option recommended when getting options from a database.\r
+const fields = useMemo(\r
+	() =>\r
+		[\r
+			//...other fields\r
+			{\r
+				//...all generic field props,\r
+				type: "checkbox",\r
+				inputSettings: {\r
+					getOptions: myGetOptionsFn,\r
+				}\r
+			},\r
+			//...other fields\r
+		],\r
+	[]\r
+);
+`})})]})}function wt(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{wt as default};
---
+++ sb8/develop/assets/transform_colorPicker-BiwRqio7.js
@@ -0,0 +1,16 @@
+import{j as r}from"./jsx-runtime-nGUw9YhZ.js";import{useMDXComponents as m}from"./index-3-_tf5dF.js";import{M as p}from"./index-CGWOAm_e.js";import{a as n}from"./column_transforms-CdVT4MBf.js";import{T as e}from"./TransformExample-CwCQUWbb.js";import"./index-BP8_t0zE.js";import"./iframe-pNl7__mS.js";import"../sb-preview/runtime.js";import"./index-BxmsGmlx.js";import"./extends-CF3RwP-h.js";import"./tslib.es6-CRos2fHm.js";import"./index-DXimoRZY.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-BjCnz-8e.js";import"./index-DJqTsaQd.js";import"./index-DrFu-skq.js";import"./CheckboxList-Dbk8JkiG.js";import"./isSymbol-CEVJpixJ.js";import"./Checkbox-CODv9_Wr.js";import"./theme-DRtZtbPc.js";import"./FormControlLabel-f9znFBMA.js";import"./generateUtilityClasses-kMkq-zAT.js";import"./formControlState-DyMXFR7f.js";import"./ButtonBase-BP_P9WLq.js";import"./useIsFocusVisible-De2ftVE9.js";import"./useControlled-im5M3O13.js";import"./getThemeProps-BVsvmf2Q.js";import"./useTheme-Cud-ikZw.js";import"./createSvgIcon-BcZTMgP9.js";import"./testIds-DZf96GK8.js";import"./ColorSelected-DyTCIv8L.js";import"./index-BU7KlZI9.js";import"./debounce-DO3dP4oK.js";import"./DataViewPrimaryFilter.styled-auwRvG8E.js";import"./createSvgIcon-Pzp5yWPs.js";import"./createChainedFunction-BO_9K8Jh.js";import"./ownerWindow-DvT1GKkC.js";import"./useId-DLncWxdO.js";import"./Chip-CRqL1Gcl.js";import"./useMosaicTranslation-CZCb37QS.js";import"./FormFieldText.styled-Cq8lzkgl.js";import"./Button-DuMlbF8s.js";import"./Popper-BWFpCfKc.js";import"./useToggle-lipGlewK.js";import"./ButtonRow-CP4CrsnB.js";import"./Spinner-Dfqcw6pF.js";import"./Typography-Br_RMLnS.js";import"./SubtitleText-Vdwjg1jD.js";import"./TitleWrapper-CYciWRcJ.js";import"./containerQuery-C0QVV4Kt.js";import"./TitleText-C9TlfF_O.js";import"./MoreVert-gv79E1WA.js";import"./ChevronRight-bPXdPGpY.js";import"./PageHeader-DvUko4so.js";import"./Image-DW9YTXUC.js";import"./formats-sBod7wm8.js";function t(i){const o={code:"code",h1:"h1",h4:"h4",li:"li",p:"p",pre:"pre",ul:"ul",...m(),...i.components};return r.jsxs(r.Fragment,{children:[r.jsx(p,{title:"Utils/Transforms/ColorPicker"}),`
+`,r.jsx(o.h1,{id:"transform_colorpicker",children:"transform_colorPicker"}),`
+`,r.jsx(o.p,{children:"Converts a hexadecimal string to a readonly Color Picker."}),`
+`,r.jsx(o.h4,{id:"conversions",children:"Conversions"}),`
+`,r.jsxs(o.ul,{children:[`
+`,r.jsxs(o.li,{children:[r.jsx(o.code,{children:'"#a8001791"'})," => ",r.jsx(e,{cb:n(),data:"#a8001791"})]}),`
+`]}),`
+`,r.jsx(o.h4,{id:"args",children:"Args"}),`
+`,r.jsx(o.p,{children:"None"}),`
+`,r.jsx(o.h4,{id:"example",children:"Example"}),`
+`,r.jsx(o.pre,{children:r.jsx(o.code,{className:"language-js",children:`import { transform_colorPicker } from "@simpleview/sv-mosaic"
+...
+transforms : [
+    transform_colorPicker()
+]
+`})})]})}function ar(i={}){const{wrapper:o}={...m(),...i.components};return o?r.jsx(o,{...i,children:r.jsx(t,{...i})}):t(i)}export{ar as default};
