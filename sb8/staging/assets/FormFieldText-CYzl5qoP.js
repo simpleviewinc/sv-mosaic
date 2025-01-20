d0bb0b25a68ce4aa203131ee2b9d5b3457e51e8c
Owen Allen <owenallenaz@gmail.com>
2025-01-20 10:11:27 +0000

    Updates

---
+++ sb8/staging/assets/FormFieldText-CYzl5qoP.js
@@ -0,0 +1,34 @@
+import{j as t}from"./jsx-runtime-nGUw9YhZ.js";import{useMDXComponents as n}from"./index-3-_tf5dF.js";import{M as o}from"./index-CGWOAm_e.js";import{s}from"./FormFieldText.stories-B-BETp3P.js";import"./index-BP8_t0zE.js";import"./iframe-pNl7__mS.js";import"../sb-preview/runtime.js";import"./index-BxmsGmlx.js";import"./extends-CF3RwP-h.js";import"./tslib.es6-CRos2fHm.js";import"./index-DXimoRZY.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-BjCnz-8e.js";import"./index-DJqTsaQd.js";import"./index-DrFu-skq.js";import"./Form-CV-BOiW8.js";import"./theme-DRtZtbPc.js";import"./SideNav-D66jd1x5.js";import"./containerQuery-C0QVV4Kt.js";import"./useToggle-lipGlewK.js";import"./CheckboxList-Dbk8JkiG.js";import"./isSymbol-CEVJpixJ.js";import"./Checkbox-CODv9_Wr.js";import"./FormControlLabel-f9znFBMA.js";import"./generateUtilityClasses-kMkq-zAT.js";import"./formControlState-DyMXFR7f.js";import"./ButtonBase-BP_P9WLq.js";import"./useIsFocusVisible-De2ftVE9.js";import"./useControlled-im5M3O13.js";import"./getThemeProps-BVsvmf2Q.js";import"./useTheme-Cud-ikZw.js";import"./createSvgIcon-BcZTMgP9.js";import"./testIds-DZf96GK8.js";import"./FormFieldText.styled-Cq8lzkgl.js";import"./Button-DuMlbF8s.js";import"./Popper-BWFpCfKc.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useMosaicTranslation-CZCb37QS.js";import"./createSvgIcon-Pzp5yWPs.js";import"./useId-DLncWxdO.js";import"./ButtonRow-CP4CrsnB.js";import"./Chip-CRqL1Gcl.js";import"./sizes-C6oSOElx.js";import"./DataViewPrimaryFilter.styled-auwRvG8E.js";import"./RadioButton-q7Nz9IFm.js";import"./StyledFormControl.styled-BDgwaEEo.js";import"./Toggle-aOIYV_sD.js";import"./index-BU7KlZI9.js";import"./debounce-DO3dP4oK.js";import"./Spinner-Dfqcw6pF.js";import"./Typography-Br_RMLnS.js";import"./SubtitleText-Vdwjg1jD.js";import"./ColorSelected-DyTCIv8L.js";import"./TitleWrapper-CYciWRcJ.js";import"./TitleText-C9TlfF_O.js";import"./MoreVert-gv79E1WA.js";import"./ChevronRight-bPXdPGpY.js";import"./PageHeader-DvUko4so.js";import"./formats-sBod7wm8.js";import"./Dialog-BI8WLJ8j.js";import"./useMediaQuery-Bupge7Mj.js";import"./Snackbar-BF1WUGPv.js";import"./Close-CC6bEs2P.js";import"./ClickAwayListener-DnjBLK-5.js";import"./TooltipIcon-Bg8I7hop.js";import"./renderButtons-BmHfsf_z.js";import"./AccountCircle-sY1JjAku.js";import"./styled-hJgfyZIG.js";import"./emotion-unitless.esm-sScrWPmR.js";import"./createSvgIcon-DE5yaevU.js";function i(r){const e={a:"a",code:"code",h1:"h1",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...n(),...r.components};return t.jsxs(t.Fragment,{children:[t.jsx(o,{of:s}),`
+`,t.jsx(e.h1,{id:"text",children:"Text"}),`
+`,t.jsxs(e.p,{children:["This field implements the ",t.jsx(e.a,{href:"#generic-field-props-fielddef",children:t.jsx(e.strong,{children:"FieldDef"})})," interface."]}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsx(e.li,{children:"Text input fields are used to enter text information, numbers, email addresses, or passwords."}),`
+`,t.jsx(e.li,{children:"A text field consists of a label and a line with variations in width."}),`
+`,t.jsx(e.li,{children:t.jsx(e.a,{href:"/?path=/story/formfields-formfieldtext--playground",children:t.jsx(e.strong,{children:"Playground"})})}),`
+`,t.jsxs(e.li,{children:["Data: ",t.jsx(e.code,{children:"string | number"})," - Value of the input typed."]}),`
+`,t.jsx(e.li,{children:"inputSettings:"}),`
+`]}),`
+`,t.jsxs(e.table,{children:[t.jsx(e.thead,{children:t.jsxs(e.tr,{children:[t.jsx(e.th,{children:"Name"}),t.jsx(e.th,{children:"Type"}),t.jsx(e.th,{children:"Description"})]})}),t.jsxs(e.tbody,{children:[t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.strong,{children:t.jsx(e.code,{children:"maxCharacters"})})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"number"})}),t.jsx(e.td,{children:"optional - Defines the limit of characters that are allow to type in the input element."})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.strong,{children:t.jsx(e.code,{children:"multiline"})})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"boolean"})}),t.jsx(e.td,{children:"optional - When is enabled the text field will expand its height."})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.strong,{children:t.jsx(e.code,{children:"placeholder"})})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"string"})}),t.jsx(e.td,{children:"optional - The hint displayed in the input before the user enters a value."})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.strong,{children:t.jsx(e.code,{children:"prefixElement"})})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"JSX.Element"})}),t.jsx(e.td,{children:"optional - Icon at the beginning of the text field."})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.strong,{children:t.jsx(e.code,{children:"type"})})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"string"})}),t.jsxs(e.td,{children:["optional - Type of the input element. It should be a ",t.jsx(e.a,{href:"https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#Form_%3Cinput%3E_types",rel:"nofollow",children:"valid HTML5 input type"}),"."]})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.strong,{children:t.jsx(e.code,{children:"maxRows"})})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"number"})}),t.jsx(e.td,{children:"optional - Maximum number of rows to display when multiline options is set to true."})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.strong,{children:t.jsx(e.code,{children:"minRows"})})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"number"})}),t.jsx(e.td,{children:"optional - Minimum number of rows to display when multiline option is set to true."})]})]})]}),`
+`,t.jsx(e.h3,{id:"how-to-use-in-a-form",children:"How to use in a form?"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-ts",children:`const fields: FieldDef[] = useMemo(\r
+	() =>\r
+		[\r
+			//...other fields\r
+			{\r
+				//...all generic field props,\r
+				type: "text",\r
+				size: "md",\r
+				inputSettings: {\r
+					prefixElement: <AccountCircle />,\r
+					maxCharacters: 20,\r
+					placeholder: "placeholder",\r
+					multiline: true,\r
+					maxRows: 4,\r
+					minRows: 3\r
+				},\r
+			},\r
+			//...other fields\r
+		],\r
+	[]\r
+);
+`})})]})}function Tt(r={}){const{wrapper:e}={...n(),...r.components};return e?t.jsx(e,{...r,children:t.jsx(i,{...r})}):i(r)}export{Tt as default};
---
+++ sb8/staging/assets/PerformanceWithSubmit.stories-YhcTQr7g.js
@@ -0,0 +1,36 @@
+import{j as r}from"./jsx-runtime-nGUw9YhZ.js";import{r as n}from"./index-BP8_t0zE.js";import{u as c,a as h,F as f}from"./Form-CV-BOiW8.js";import{r as x}from"./renderButtons-BmHfsf_z.js";import{O as b}from"./utils-BR2PSRFh.js";import"./theme-DRtZtbPc.js";import"./SideNav-D66jd1x5.js";import"./containerQuery-C0QVV4Kt.js";import"./useToggle-lipGlewK.js";import"./CheckboxList-Dbk8JkiG.js";import"./isSymbol-CEVJpixJ.js";import"./Checkbox-CODv9_Wr.js";import"./FormControlLabel-f9znFBMA.js";import"./generateUtilityClasses-kMkq-zAT.js";import"./formControlState-DyMXFR7f.js";import"./ButtonBase-BP_P9WLq.js";import"./useIsFocusVisible-De2ftVE9.js";import"./useControlled-im5M3O13.js";import"./getThemeProps-BVsvmf2Q.js";import"./useTheme-Cud-ikZw.js";import"./createSvgIcon-BcZTMgP9.js";import"./testIds-DZf96GK8.js";import"./FormFieldText.styled-Cq8lzkgl.js";import"./Button-DuMlbF8s.js";import"./Popper-BWFpCfKc.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./index-BxmsGmlx.js";import"./useMosaicTranslation-CZCb37QS.js";import"./createSvgIcon-Pzp5yWPs.js";import"./useId-DLncWxdO.js";import"./ButtonRow-CP4CrsnB.js";import"./Chip-CRqL1Gcl.js";import"./sizes-C6oSOElx.js";import"./DataViewPrimaryFilter.styled-auwRvG8E.js";import"./RadioButton-q7Nz9IFm.js";import"./StyledFormControl.styled-BDgwaEEo.js";import"./Toggle-aOIYV_sD.js";import"./index-BU7KlZI9.js";import"./debounce-DO3dP4oK.js";import"./Spinner-Dfqcw6pF.js";import"./Typography-Br_RMLnS.js";import"./SubtitleText-Vdwjg1jD.js";import"./ColorSelected-DyTCIv8L.js";import"./TitleWrapper-CYciWRcJ.js";import"./TitleText-C9TlfF_O.js";import"./MoreVert-gv79E1WA.js";import"./ChevronRight-bPXdPGpY.js";import"./PageHeader-DvUko4so.js";import"./formats-sBod7wm8.js";import"./Dialog-BI8WLJ8j.js";import"./useMediaQuery-Bupge7Mj.js";import"./Snackbar-BF1WUGPv.js";import"./Close-CC6bEs2P.js";import"./ClickAwayListener-DnjBLK-5.js";import"./TooltipIcon-Bg8I7hop.js";const gt={title:"Components/Form"},t=({showState:a})=>{const o=c(),{state:l,handleSubmit:d}=o;n.useEffect(()=>(document.body.style.margin="0px",()=>{document.body.style.margin=b}),[]);const u=n.useMemo(()=>{const i=[];for(let e=0;e<100;e++)i.push({name:`text${e}`,label:`Simple Text ${e}`,type:"text",instructionText:"testing",validators:[h]});return i},[]);return r.jsxs(r.Fragment,{children:[a&&r.jsx("pre",{children:JSON.stringify(l,null,"  ")}),r.jsx("div",{style:{height:"100vh"},children:r.jsx(f,{...o,buttons:x(d),fields:u,title:"Performance with submit"})})]})};t.args={showState:!1};t.argTypes={showState:{name:"Show state"}};var m,s,p;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`({
+  showState
+}: typeof PerformanceWithSubmit.args): ReactElement => {
+  const controller = useForm();
+  const {
+    state,
+    handleSubmit
+  } = controller;
+  useEffect(() => {
+    document.body.style.margin = "0px";
+    return () => {
+      document.body.style.margin = ORIGINAL_BODY_MARGIN;
+    };
+  }, []);
+  const fields = useMemo((): FieldDef[] => {
+    const hundredFields: FieldDef[] = [];
+    for (let i = 0; i < 100; i++) {
+      hundredFields.push({
+        name: \`text\${i}\`,
+        label: \`Simple Text \${i}\`,
+        type: "text",
+        instructionText: "testing",
+        validators: [required]
+      });
+    }
+    return hundredFields;
+  }, []);
+  return <>
+            {showState && <pre>{JSON.stringify(state, null, "  ")}</pre>}
+            <div style={{
+      height: "100vh"
+    }}>
+                <Form {...controller} buttons={renderButtons(handleSubmit)} fields={fields} title="Performance with submit" />
+            </div>
+        </>;
+}`,...(p=(s=t.parameters)==null?void 0:s.docs)==null?void 0:p.source}}};const Ft=["PerformanceWithSubmit"];export{t as PerformanceWithSubmit,Ft as __namedExportsOrder,gt as default};
