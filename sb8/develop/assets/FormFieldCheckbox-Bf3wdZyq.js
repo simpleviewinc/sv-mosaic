44770ff3eb61c0a7f70a663adcd627c6d2b39d45
Owen Allen <owenallenaz@gmail.com>
2025-02-04 15:11:27 +0000

    Updates

---
+++ sb8/develop/assets/FormFieldCheckbox-Bf3wdZyq.js
@@ -0,0 +1,50 @@
+import{j as t}from"./jsx-runtime-DiwAibxM.js";import{useMDXComponents as e}from"./index-B4U1yk18.js";import{M as n}from"./index-DDW-2-fV.js";import{s}from"./FormFieldCheckbox.stories-Ba2zpRmu.js";import"./index-Btj5Fd67.js";import"./iframe-ksgF3Z0w.js";import"../sb-preview/runtime.js";import"./index-T5NeFSal.js";import"./index-tXhrow7Y.js";import"./tslib.es6-CRos2fHm.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./inheritsLoose-Cqq1nRsk.js";import"./toString-CrHhZNKP.js";import"./index-CQ9F9_Tt.js";import"./index-ogSvIofg.js";import"./Form-Cu9MtH9A.js";import"./theme-C7C0QMlu.js";import"./SideNav-BXGvGLiL.js";import"./containerQuery-BqcK0eeN.js";import"./useToggle-B3mbTfSB.js";import"./ColorSelected-rVJC1bnf.js";import"./index-B6f3rorp.js";import"./debounce-B3RVa3dT.js";import"./DataViewPrimaryFilter.styled-BUfrUkss.js";import"./generateUtilityClasses-oWyMxXuq.js";import"./createSvgIcon-C7F5N1gl.js";import"./createSvgIcon-Bmixpj46.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useIsFocusVisible-C9uVWUyr.js";import"./useId-DwELyCdS.js";import"./ownerWindow-DvT1GKkC.js";import"./useControlled-D8KJxpyu.js";import"./Chip-BycvDKsV.js";import"./ButtonBase-Ci24pScM.js";import"./testIds-DZf96GK8.js";import"./useMosaicTranslation-L7Zdnfri.js";import"./FormFieldText.styled-BXg2h5EQ.js";import"./Button-BW21zc79.js";import"./Popper-lO7-sb5Y.js";import"./useTheme-CN1B93FI.js";import"./formControlState-Bl9Dd4wz.js";import"./ButtonRow-DaYhrvd1.js";import"./Spinner-C0FunRuE.js";import"./Typography-D0-akgTt.js";import"./SubtitleText-DUUVrgUU.js";import"./CheckboxList-CKLT5_IE.js";import"./Checkbox-faPr4U1Q.js";import"./FormControlLabel-DlxLljVV.js";import"./getThemeProps-DdDNKTGU.js";import"./TitleWrapper-Bkg-hsBc.js";import"./TitleText-CdOksVB0.js";import"./MoreVert-BH0BHXBp.js";import"./ChevronRight-Da8zFjWX.js";import"./PageHeader-CmlBcYYr.js";import"./sizes-C6oSOElx.js";import"./RadioButton-BFvQvs9V.js";import"./StyledFormControl.styled-D1xcYZGf.js";import"./Toggle-Lx-sDa2f.js";import"./formats-sBod7wm8.js";import"./Dialog-BFRUe6cu.js";import"./useMediaQuery-Yl5lg82S.js";import"./Snackbar-B25MKBii.js";import"./Close-CSY1ofSk.js";import"./ClickAwayListener-DweT8u9y.js";import"./TooltipIcon-C4tSz_X8.js";import"./options-DJM8a-gX.js";import"./renderButtons-BmHfsf_z.js";function i(o){const r={a:"a",code:"code",h1:"h1",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...e(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:s}),`
+`,t.jsx(r.h1,{id:"checkbox",children:"Checkbox"}),`
+`,t.jsxs(r.p,{children:["This field implements the ",t.jsx(r.a,{href:"#generic-field-props-fielddef",children:t.jsx(r.strong,{children:"FieldDef"})})," interface."]}),`
+`,t.jsxs(r.ul,{children:[`
+`,t.jsx(r.li,{children:"A group of checkbox buttons that allows users to select multiple items from a list of possible options."}),`
+`,t.jsx(r.li,{children:t.jsx(r.a,{href:"/?path=/story/formfields-formfieldcheckbox--playground",children:t.jsx(r.strong,{children:"Playground"})})}),`
+`,t.jsxs(r.li,{children:["Data: ",t.jsx(r.code,{children:"array"})," of ",t.jsx(r.code,{children:"string"})," - Array of values of the selected options."]}),`
+`,t.jsx(r.li,{children:"inputSettings:"}),`
+`]}),`
+`,t.jsxs(r.table,{children:[t.jsx(r.thead,{children:t.jsxs(r.tr,{children:[t.jsx(r.th,{children:"Name"}),t.jsx(r.th,{children:"Type"}),t.jsx(r.th,{children:"Description"})]})}),t.jsxs(r.tbody,{children:[t.jsxs(r.tr,{children:[t.jsx(r.td,{children:t.jsx(r.strong,{children:t.jsx(r.code,{children:"options"})})}),t.jsx(r.td,{children:t.jsx(r.code,{children:"MosaicLabelValue[]"})}),t.jsx(r.td,{children:"required - Array of options to be rendered containing their corresponding label and value."})]}),t.jsxs(r.tr,{children:[t.jsx(r.td,{children:t.jsx(r.strong,{children:t.jsx(r.code,{children:"getOptions"})})}),t.jsx(r.td,{children:t.jsx(r.code,{children:"() => Promise<MosaicLabelValue[]>"})}),t.jsx(r.td,{children:"required - Function to get a set of options. This prop only applies when getting options from a DB."})]})]})]}),`
+`,t.jsx(r.h3,{id:"how-to-use-in-a-form",children:"How to use in a form?"}),`
+`,t.jsx(r.pre,{children:t.jsx(r.code,{className:"language-ts",children:`//Option recommended when getting options locally.\r
+const fields = useMemo(\r
+	() =>\r
+		[\r
+			//...other fields\r
+			{\r
+				//...all generic field props,\r
+				type: "checkbox",\r
+				inputSettings: {\r
+					options: [\r
+						{\r
+							label: "Option1",\r
+							value: "value1",\r
+						}\r
+					],\r
+				}\r
+			},\r
+			//...other fields\r
+		],\r
+	[]\r
+);\r
+\r
+//Option recommended when getting options from a database.\r
+const fields = useMemo(\r
+	() =>\r
+		[\r
+			//...other fields\r
+			{\r
+				//...all generic field props,\r
+				type: "checkbox",\r
+				inputSettings: {\r
+					getOptions: myGetOptionsFn,\r
+				}\r
+			},\r
+			//...other fields\r
+		],\r
+	[]\r
+);
+`})})]})}function Mt(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{Mt as default};
---
+++ sb8/develop/assets/FormFieldNumberTable-B96FNCnH.js
@@ -0,0 +1,84 @@
+import{j as t}from"./jsx-runtime-DiwAibxM.js";import{useMDXComponents as i}from"./index-B4U1yk18.js";import{M as o}from"./index-DDW-2-fV.js";import{s as l}from"./FormFieldNumberTable.stories-30J-x1ev.js";import"./index-Btj5Fd67.js";import"./iframe-ksgF3Z0w.js";import"../sb-preview/runtime.js";import"./index-T5NeFSal.js";import"./index-tXhrow7Y.js";import"./tslib.es6-CRos2fHm.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./inheritsLoose-Cqq1nRsk.js";import"./toString-CrHhZNKP.js";import"./index-CQ9F9_Tt.js";import"./index-ogSvIofg.js";import"./renderButtons-BmHfsf_z.js";import"./Form-Cu9MtH9A.js";import"./theme-C7C0QMlu.js";import"./SideNav-BXGvGLiL.js";import"./containerQuery-BqcK0eeN.js";import"./useToggle-B3mbTfSB.js";import"./ColorSelected-rVJC1bnf.js";import"./index-B6f3rorp.js";import"./debounce-B3RVa3dT.js";import"./DataViewPrimaryFilter.styled-BUfrUkss.js";import"./generateUtilityClasses-oWyMxXuq.js";import"./createSvgIcon-C7F5N1gl.js";import"./createSvgIcon-Bmixpj46.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useIsFocusVisible-C9uVWUyr.js";import"./useId-DwELyCdS.js";import"./ownerWindow-DvT1GKkC.js";import"./useControlled-D8KJxpyu.js";import"./Chip-BycvDKsV.js";import"./ButtonBase-Ci24pScM.js";import"./testIds-DZf96GK8.js";import"./useMosaicTranslation-L7Zdnfri.js";import"./FormFieldText.styled-BXg2h5EQ.js";import"./Button-BW21zc79.js";import"./Popper-lO7-sb5Y.js";import"./useTheme-CN1B93FI.js";import"./formControlState-Bl9Dd4wz.js";import"./ButtonRow-DaYhrvd1.js";import"./Spinner-C0FunRuE.js";import"./Typography-D0-akgTt.js";import"./SubtitleText-DUUVrgUU.js";import"./CheckboxList-CKLT5_IE.js";import"./Checkbox-faPr4U1Q.js";import"./FormControlLabel-DlxLljVV.js";import"./getThemeProps-DdDNKTGU.js";import"./TitleWrapper-Bkg-hsBc.js";import"./TitleText-CdOksVB0.js";import"./MoreVert-BH0BHXBp.js";import"./ChevronRight-Da8zFjWX.js";import"./PageHeader-CmlBcYYr.js";import"./sizes-C6oSOElx.js";import"./RadioButton-BFvQvs9V.js";import"./StyledFormControl.styled-D1xcYZGf.js";import"./Toggle-Lx-sDa2f.js";import"./formats-sBod7wm8.js";import"./Dialog-BFRUe6cu.js";import"./useMediaQuery-Yl5lg82S.js";import"./Snackbar-B25MKBii.js";import"./Close-CSY1ofSk.js";import"./ClickAwayListener-DweT8u9y.js";import"./TooltipIcon-C4tSz_X8.js";function r(n){const e={a:"a",code:"code",h1:"h1",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...i(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(o,{of:l}),`
+`,t.jsx(e.h1,{id:"number-table",children:"Number Table"}),`
+`,t.jsxs(e.p,{children:["This field implements the ",t.jsx(e.a,{href:"#generic-field-props-fielddef",children:t.jsx(e.strong,{children:"FieldDef"})})," interface."]}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsx(e.li,{children:t.jsx(e.a,{href:"/?path=/story/formfields-formfieldnumbertable--playground",children:t.jsx(e.strong,{children:"Playground"})})}),`
+`,t.jsxs(e.li,{children:["Data: ",t.jsx(e.code,{children:"object"})," - Object that has the row name as key and nested another object with the column name as key and the value corresponds to text field value.",`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsxs(e.li,{children:[t.jsx(e.strong,{children:"rowName: string"})," - ",t.jsx(e.code,{children:"object"})," - Row name is used as key identifier.",`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsxs(e.li,{children:[t.jsx(e.strong,{children:"columnName: string"})," - ",t.jsx(e.code,{children:"string"})," - Value of each text field cell."]}),`
+`]}),`
+`]}),`
+`]}),`
+`]}),`
+`,t.jsx(e.li,{children:"inputSettings:"}),`
+`]}),`
+`,t.jsxs(e.table,{children:[t.jsx(e.thead,{children:t.jsxs(e.tr,{children:[t.jsx(e.th,{children:"Name"}),t.jsx(e.th,{children:"Type"}),t.jsx(e.th,{children:"Default"}),t.jsx(e.th,{children:"Description"})]})}),t.jsxs(e.tbody,{children:[t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.strong,{children:t.jsx(e.code,{children:"columns"})})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"{ name: string; title: string }[]"})}),t.jsx(e.td,{}),t.jsx(e.td,{children:"Columns definition."})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.strong,{children:t.jsx(e.code,{children:"columnTotalLabel"})})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"string"})}),t.jsx(e.td,{children:'"Total"'}),t.jsx(e.td,{children:'optional - Default: "Total". Label shown on the column that displays the totals.'})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.strong,{children:t.jsx(e.code,{children:"displaySumColumn"})})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"boolean"})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"true"})}),t.jsx(e.td,{children:"optional - Shows or hides the total sums of each column."})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.strong,{children:t.jsx(e.code,{children:"displaySumRow"})})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"boolean"})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"true"})}),t.jsx(e.td,{children:"optional - Shows or hides the total sums of each row."})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.strong,{children:t.jsx(e.code,{children:"numberFormatOptions"})})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"Intl.NumberFormatOptions"})}),t.jsx(e.td,{}),t.jsxs(e.td,{children:["optional - Configuration options for the formatter, see more at: ",t.jsx(e.a,{href:"https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat",rel:"nofollow",children:"https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat"})]})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.strong,{children:t.jsx(e.code,{children:"rows"})})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"{ name: string; title: string, subtitle?: string }[]"})}),t.jsx(e.td,{}),t.jsx(e.td,{children:"Rows definition."})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.strong,{children:t.jsx(e.code,{children:"rowTotalLabel"})})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"string"})}),t.jsx(e.td,{children:'"Total"'}),t.jsx(e.td,{children:"optional - Label shown on the total row."})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.strong,{children:t.jsx(e.code,{children:"topLeftLabel"})})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"string"})}),t.jsx(e.td,{}),t.jsx(e.td,{children:"optional - Label placed on the top left table corner i.e the first column."})]})]})]}),`
+`,t.jsx(e.h3,{id:"how-to-use-in-a-form",children:"How to use in a form?"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-ts",children:`const fields: FieldDef[] = useMemo(
+	(): FieldDef[] =>
+		[
+			//...other fields
+			{
+				//...all generic field props
+				type: "numberTable",
+				defaultValue: {
+					"2023_02_10": {
+						single: "12",
+						double: "13",
+						queen: "14",
+						king: "15",
+						suite: "16",
+						any: "17",
+					},
+					"2023_02_11": {
+						single: "18",
+						double: "19",
+						queen: "20",
+						king: "21",
+						suite: "22",
+						any: "23",
+					},
+					"2023_02_12": {
+						single: "12",
+						double: "13",
+						queen: "14",
+						king: "15",
+						suite: "16",
+						any: "20",
+					},
+					"2023_02_13": {
+						single: "1",
+						double: "1",
+						queen: "1",
+						king: "1",
+						suite: "1",
+						any: "1",
+					},
+				},
+				inputSettings: {
+					rowTotalLabel: "TOTAL",
+					columnTotalLabel: "No. Rooms",
+					topLeftLabel: "Day",
+					rows: [
+						{ name: "2023_02_10", title: "Shoulder Before" },
+						{ name: "2023_02_11", title: "Day 1", subtitle: "February 11" },
+						{ name: "2023_02_12", title: "Day 2" },
+						{ name: "2023_02_13", title: "Day 3" },
+					],
+					columns: [
+						{ name: "single", title: "Single" },
+						{ name: "double", title: "Double" },
+						{ name: "queen", title: "Queen" },
+						{ name: "king", title: "King" },
+						{ name: "suite", title: "Suite" },
+						{ name: "any", title: "Any" },
+					]
+				},
+			},
+			//...other fields
+		],
+	[]
+);
+`})})]})}function yt(n={}){const{wrapper:e}={...i(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(r,{...n})}):r(n)}export{yt as default};
