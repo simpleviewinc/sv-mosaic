a1c4410ac2c31e08eea559f26299482acc189bf6
George Batt <george.batt@simpleviewinc.com>
2025-02-05 15:07:48 +0000

    (chore) Adds local vscode env for automation testing.

--- containers/e2e-tests/utils/urls/environments.ts
+++ containers/e2e-tests/utils/urls/environments.ts
@@ -1,4 +1,5 @@
 const env = {
+	vscode: "http://localhost:10001/iframe.html",
 	local: "http://kube.simpleview.io:10000/iframe.html",
 	develop: "https://simpleviewinc.github.io/sv-mosaic/iframe.html",
 	docker: "http://sb-server:10001/iframe.html",
