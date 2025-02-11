476f02671cea66c91e43004969871917ba12fd9b
George Batt <george.batt@simpleviewinc.com>
2025-02-11 16:05:02 +0000

    (RawField) Improved unit tests and moves Storybook styled component.

--- containers/mosaic/src/components/Field/FormFieldRaw/FormFieldRaw.styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const RawValueWrapper = styled.div`
-    background-color: #aa1919;
-    border: 3px solid #570202;
-    color: white;
-    padding: 1rem 2rem;
-    border-radius: 99999em;
-`;
