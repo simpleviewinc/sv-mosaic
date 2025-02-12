75661f4b210176c4306d06958c3348ec7940fa35
Owen Allen <owenallenaz@gmail.com>
2025-02-12 14:51:57 +0000

    Updates

---
+++ sb8/develop/assets/CopyField.stories-CxTb6Ljn.js
@@ -0,0 +1,47 @@
+import{j as r}from"./jsx-runtime-DiwAibxM.js";import{r as o}from"./index-Btj5Fd67.js";import{u as f,F as h}from"./index-C0OFzvBJ.js";import{r as g}from"./renderButtons-BmHfsf_z.js";import"./theme-C7C0QMlu.js";import"./testIds-BTxDoLqd.js";import"./TitleWrapper-BAR2n_rD.js";import"./Button-CRbkpCI2.js";import"./generateUtilityClasses-oWyMxXuq.js";import"./ButtonBase-biKTBdl5.js";import"./useIsFocusVisible-DHjyF8IZ.js";import"./Popper-Cwezlgxk.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useTheme-R4ufcCXK.js";import"./index-T5NeFSal.js";import"./index-tXhrow7Y.js";import"./useToggle-B3mbTfSB.js";import"./containerQuery-BqcK0eeN.js";import"./createSvgIcon-CzNXaSwG.js";import"./createSvgIcon-Bmixpj46.js";import"./useId-DwELyCdS.js";import"./useControlled-D8KJxpyu.js";import"./Typography-D0-akgTt.js";import"./TitleText-CdOksVB0.js";import"./ButtonRow-B0KLUoh1.js";import"./index-DWVqnVXO.js";import"./debounce-B3RVa3dT.js";import"./DataViewPrimaryFilter.styled-Ckb-VyLy.js";import"./Chip-Dr-kVTvJ.js";import"./useMosaicTranslation-DPUDh126.js";import"./FormFieldText.styled-CYOPNvgp.js";import"./formControlState-Bl9Dd4wz.js";import"./Spinner-C0FunRuE.js";import"./SubtitleText-DUUVrgUU.js";import"./CheckboxList-DhxEMOfW.js";import"./Checkbox-BPpImdKi.js";import"./FormControlLabel-DHn1WcqZ.js";import"./getThemeProps-DdDNKTGU.js";import"./MoreVert-Djp96t97.js";import"./ChevronRight-CA1Fd4W_.js";import"./PageHeader-BGHmuIHO.js";import"./formats-CMvQHWsT.js";import"./sizes-C6oSOElx.js";import"./RadioButton-Ba6ulvNI.js";import"./StyledFormControl.styled-i13hAbZe.js";import"./Toggle-CQTabJa2.js";import"./Dialog-D-UDZ0Lf.js";import"./SideNav-BXGvGLiL.js";import"./Snackbar-DLaSlsnc.js";import"./Close-BLCogFul.js";import"./ClickAwayListener-DoPvkzLf.js";const gt={title:"Components/Form"},y=document.body.style.margin,t=({showState:p})=>{const n=f(),{state:e,methods:{setFieldValue:i},handleSubmit:d}=n;o.useEffect(()=>(document.body.style.margin="0px",()=>{document.body.style.margin=y}),[]);const u=o.useMemo(()=>[{name:"name",label:"Name",type:"text",required:!0},{name:"slug",label:"Slug",type:"text",required:!0,helperText:"The text for this field will be populated with a slugified version the name field, but only if this field hasn't been touched"}],[]);return o.useEffect(()=>{var s;if(!e.touched.slug){const c=(s=e.data.name)==null?void 0:s.trim().toLowerCase().replace(/ {1,}/g,"_").replace(/[^a-z_]/g,"");i({name:"slug",value:c})}},[i,e.data.name,e.touched]),r.jsxs(r.Fragment,{children:[p&&r.jsx("pre",{children:JSON.stringify(e,null,"  ")}),r.jsx("div",{style:{height:"100vh"},children:r.jsx(h,{...n,buttons:g(d),title:"Validators story",fields:u})})]})};t.args={showState:!1};t.argTypes={showState:{name:"Show state"}};var m,a,l;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`({
+  showState
+}: typeof CopyField.args): ReactElement => {
+  const controller = useForm();
+  const {
+    state,
+    methods: {
+      setFieldValue
+    },
+    handleSubmit
+  } = controller;
+  useEffect(() => {
+    document.body.style.margin = "0px";
+    return () => {
+      document.body.style.margin = ORIGINAL_BODY_MARGIN;
+    };
+  }, []);
+  const fields = useMemo((): FieldDef[] => [{
+    name: "name",
+    label: "Name",
+    type: "text",
+    required: true
+  }, {
+    name: "slug",
+    label: "Slug",
+    type: "text",
+    required: true,
+    helperText: "The text for this field will be populated with a slugified version the name field, but only if this field hasn't been touched"
+  }], []);
+  useEffect(() => {
+    if (!state.touched.slug) {
+      const transformedLabel = state.data.name?.trim().toLowerCase().replace(/ {1,}/g, "_").replace(/[^a-z_]/g, "");
+      setFieldValue({
+        name: "slug",
+        value: transformedLabel
+      });
+    }
+  }, [setFieldValue, state.data.name, state.touched]);
+  return <>
+            {showState && <pre>{JSON.stringify(state, null, "  ")}</pre>}
+            <div style={{
+      height: "100vh"
+    }}>
+                <Form {...controller} buttons={renderButtons(handleSubmit)} title="Validators story" fields={fields} />
+            </div>
+        </>;
+}`,...(l=(a=t.parameters)==null?void 0:a.docs)==null?void 0:l.source}}};const yt=["CopyField"];export{t as CopyField,yt as __namedExportsOrder,gt as default};
---
+++ sb8/develop/assets/FormFieldRadio.stories-TNyeyxgk.js
@@ -0,0 +1,44 @@
+import{j as e}from"./jsx-runtime-DiwAibxM.js";import{r as x}from"./index-Btj5Fd67.js";import{r as S}from"./renderButtons-BmHfsf_z.js";import{u as T,F as k}from"./index-C0OFzvBJ.js";import{o as i,g as O}from"./options-DJM8a-gX.js";const q={title:"FormFields/FormFieldRadio"},t=({label:n,required:o,skeleton:s,disabled:a,instructionText:l,helperText:p,prepop:d,prepopData:u,optionsType:c})=>{const m=T(),{state:j,handleSubmit:D}=m,R=x.useMemo(()=>[{name:"radio",label:n,type:"radio",required:o,disabled:a,inputSettings:{options:c==="Synchronous"?i:O},helperText:p,instructionText:l}],[n,o,a,l,p,c]),_=x.useMemo(()=>d?async()=>u:void 0,[d,u]);return e.jsxs(e.Fragment,{children:[e.jsx("pre",{children:JSON.stringify(j,null,"  ")}),e.jsx(k,{...m,buttons:S(D),title:"Radio Field",fields:R,skeleton:s,getFormValues:_})]})};t.args={label:"Label",disabled:!1,required:!1,skeleton:!1,instructionText:"Instruction text",helperText:"Helper text",prepop:!1,prepopData:{radio:{value:"option_1-cat_1",label:"Option 1"}},optionsType:"Synchronous"};t.argTypes={label:{name:"Label"},disabled:{name:"Disabled"},required:{name:"Required"},skeleton:{name:"Skeleton"},instructionText:{name:"Instruction Text"},helperText:{name:"Helper Text"},prepop:{name:"Prepopulate"},prepopData:{name:"Prepopulate Data",if:{arg:"prepop"}},optionsType:{name:"Options Type",options:["Synchronous","Asynchronous"],control:{type:"select"}}};const P=[{name:"radio",label:"Regular example",type:"radio",required:!1,disabled:!1,inputSettings:{options:i},helperText:"Helper text",instructionText:"Instruction text"},{name:"radio-disabled",label:"Disabled example",type:"radio",required:!1,disabled:!0,inputSettings:{options:i},helperText:"Helper text",instructionText:"Instruction text"},{name:"radio-db",label:"From data base example",type:"radio",required:!1,disabled:!1,inputSettings:{getOptions:O},helperText:"Helper text",instructionText:"Instruction text"}],r=()=>{const n=T(),{state:o,handleSubmit:s}=n;return e.jsxs(e.Fragment,{children:[e.jsx("pre",{children:JSON.stringify(o,null,"  ")}),e.jsx(k,{...n,buttons:S(s),title:"Form Title",description:"This is a description example",fields:P})]})};var b,f,y;t.parameters={...t.parameters,docs:{...(b=t.parameters)==null?void 0:b.docs,source:{originalSource:`({
+  label,
+  required,
+  skeleton,
+  disabled,
+  instructionText,
+  helperText,
+  prepop,
+  prepopData,
+  optionsType
+}: typeof Playground.args): ReactElement => {
+  const controller = useForm();
+  const {
+    state,
+    handleSubmit
+  } = controller;
+  const fields = useMemo((): FieldDef[] => [{
+    name: "radio",
+    label,
+    type: "radio",
+    required,
+    disabled,
+    inputSettings: {
+      options: optionsType === "Synchronous" ? optionsLibrary : getOptions
+    },
+    helperText,
+    instructionText
+  }] as FieldDef[], [label, required, disabled, instructionText, helperText, optionsType]);
+  const getFormValues = useMemo(() => prepop ? async () => prepopData : undefined, [prepop, prepopData]);
+  return <>
+            <pre>{JSON.stringify(state, null, "  ")}</pre>
+            <Form {...controller} buttons={renderButtons(handleSubmit)} title="Radio Field" fields={fields} skeleton={skeleton} getFormValues={getFormValues} />
+        </>;
+}`,...(y=(f=t.parameters)==null?void 0:f.docs)==null?void 0:y.source}}};var g,h,F;r.parameters={...r.parameters,docs:{...(g=r.parameters)==null?void 0:g.docs,source:{originalSource:`(): ReactElement => {
+  const controller = useForm();
+  const {
+    state,
+    handleSubmit
+  } = controller;
+  return <>
+            <pre>{JSON.stringify(state, null, "  ")}</pre>
+            <Form {...controller} buttons={renderButtons(handleSubmit)} title="Form Title" description="This is a description example" fields={kitchenSinkFields} />
+        </>;
+}`,...(F=(h=r.parameters)==null?void 0:h.docs)==null?void 0:F.source}}};const E=["Playground","KitchenSink"],N=Object.freeze(Object.defineProperty({__proto__:null,KitchenSink:r,Playground:t,__namedExportsOrder:E,default:q},Symbol.toStringTag,{value:"Module"}));export{N as s};
