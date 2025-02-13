9e2e4cdb37ed0738d3b31822f716ac93eeaabd4c
George Batt <george.batt@simpleviewinc.com>
2025-02-13 17:43:02 +0000

    (util) Updates merge function to ensure properties that are absent from second merge candidate are added to the first.

--- containers/mosaic/src/utils/object/merge.ts
+++ containers/mosaic/src/utils/object/merge.ts
@@ -11,20 +11,28 @@ interface MergeOptions<T, U> {
 function merge<T extends object, U extends object>(first: T, second: U, {
 	by = (first, second, key) => (key in second),
 }: MergeOptions<T, U> = {}): T & U {
-	return Object.fromEntries(Object.entries(first).map(([key, value]) => {
-		if (!by(first, second, key)) {
-			return [key, value];
-		}
+	const firstEntries = Object.entries(first);
+	const secondEntries = Object.entries(second);
 
-		if (isPlainObject(value) && isPlainObject(second[key])) {
-			return [key, merge(value, second[key])];
-		}
+	return {
+		...Object.fromEntries(firstEntries.map(([key, value]) => {
+			if (!by(first, second, key)) {
+				return [key, value];
+			}
 
-		return [
-			key,
-			second[key],
-		];
-	})) as T & U;
+			if (isPlainObject(value) && isPlainObject(second[key])) {
+				return [key, merge(value, second[key])];
+			}
+
+			return [
+				key,
+				second[key],
+			];
+		})),
+		...Object.fromEntries(secondEntries.filter(
+			([secondKey]) => !firstEntries.map(([firstKey]) => firstKey).includes(secondKey),
+		)),
+	} as T & U;
 }
 
 export default merge;
