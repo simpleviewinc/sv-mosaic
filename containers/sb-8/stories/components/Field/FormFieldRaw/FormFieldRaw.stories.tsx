81de5d70ae1d29a13943a6be89afad2522f51b99
George Batt <george.batt@simpleviewinc.com>
2025-02-13 18:30:11 +0000

    (chore) Moves field story args out to a common controls store.

--- containers/sb-8/stories/components/Field/FormFieldRaw/FormFieldRaw.stories.tsx
+++ containers/sb-8/stories/components/Field/FormFieldRaw/FormFieldRaw.stories.tsx
@@ -5,7 +5,7 @@ import { useMemo } from "react";
 // Components
 import Form, { useForm } from "@root/components/Form";
 import type { FieldDef } from "@root/components/Field";
-import { renderButtons } from "../../../../utils";
+import { commonFieldControls, renderButtons } from "../../../../utils";
 import { RawValueWrapper } from "@root/components/Field/FormFieldRaw/FormFieldRaw.styled";
 
 export default {
@@ -65,82 +65,10 @@ export const Playground = ({
 	);
 };
 
-const fields: FieldDef[] = [
-	{
-		name: "toggleDefault",
-		label: "Default example",
-		type: "toggle",
-		required: false,
-		disabled: false,
-		inputSettings: {
-			toggleLabel: "Toggle label",
-		},
-		helperText: "Helper text",
-		instructionText: "Instruction text",
-	},
-	{
-		name: "toggleDisabled",
-		label: "Disabled example",
-		type: "toggle",
-		required: false,
-		disabled: true,
-		inputSettings: {
-			toggleLabel: "Toggle label",
-		},
-		helperText: "Helper text",
-		instructionText: "Instruction text",
-	},
-	{
-		name: "toggleWithoutLabel",
-		label: "Toggle switch without label",
-		type: "toggle",
-		required : false,
-		disabled: false,
-		helperText: "Helper text",
-		instructionText: "Instruction text",
-	},
-];
-
 Playground.args = {
-	label: "Label",
-	disabled: false,
-	required: false,
-	instructionText: "Instruction text",
-	helperText: "Helper text",
+	...commonFieldControls.args,
 };
 
 Playground.argTypes = {
-	label: {
-		name: "Label",
-	},
-	disabled: {
-		name: "Disabled",
-	},
-	required: {
-		name: "Required",
-	},
-	instructionText: {
-		name: "Instruction Text",
-	},
-	helperText: {
-		name: "Helper Text",
-	},
-};
-
-export const KitchenSink = (): ReactElement => {
-	const controller = useForm();
-	const { state, handleSubmit } = controller;
-
-	return (
-		<>
-			<pre>{JSON.stringify(state, null, "  ")}</pre>
-			<Form
-				{...controller}
-				buttons={renderButtons(handleSubmit)}
-				title="Form Title"
-				description="This is a description example"
-				fields={fields}
-			/>
-		</>
-	);
+	...commonFieldControls.argTypes,
 };
