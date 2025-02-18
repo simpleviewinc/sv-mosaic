b6c981e54f92982b48358ff300639f1ea8208442
Owen Allen <owenallenaz@gmail.com>
2025-02-18 11:56:37 +0000

    Updates

---
+++ sb8/develop/assets/transform_dateFormat-IljoIEoK.js
@@ -0,0 +1,16 @@
+import{j as r}from"./jsx-runtime-DiwAibxM.js";import{useMDXComponents as i}from"./index-B4U1yk18.js";import{M as p}from"./index-C8tLePhk.js";import{b as n}from"./column_transforms-BS8t_7-7.js";import{T as e}from"./TransformExample-DKwiSzdP.js";import"./index-Btj5Fd67.js";import"./iframe-wkxF2MR5.js";import"../sb-preview/runtime.js";import"./index-T5NeFSal.js";import"./index-tXhrow7Y.js";import"./tslib.es6-CRos2fHm.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./inheritsLoose-Cqq1nRsk.js";import"./toString-CrHhZNKP.js";import"./index-CQ9F9_Tt.js";import"./index-ogSvIofg.js";import"./index-TRTj_9gV.js";import"./theme-C7C0QMlu.js";import"./testIds-fiKIunms.js";import"./TitleWrapper-BAR2n_rD.js";import"./Button-CRbkpCI2.js";import"./generateUtilityClasses-oWyMxXuq.js";import"./ButtonBase-biKTBdl5.js";import"./useIsFocusVisible-DHjyF8IZ.js";import"./Popper-Cwezlgxk.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useTheme-R4ufcCXK.js";import"./useToggle-B3mbTfSB.js";import"./containerQuery-BqcK0eeN.js";import"./createSvgIcon-CzNXaSwG.js";import"./createSvgIcon-Bmixpj46.js";import"./useId-DwELyCdS.js";import"./useControlled-D8KJxpyu.js";import"./Typography-D0-akgTt.js";import"./TitleText-CdOksVB0.js";import"./ButtonRow-BXJCCKLJ.js";import"./index-DvTtuJek.js";import"./debounce-B3RVa3dT.js";import"./DataViewPrimaryFilter.styled-Ckb-VyLy.js";import"./Chip-C_h72JNQ.js";import"./useMosaicTranslation-DPUDh126.js";import"./FormFieldText.styled-CWtnKjLU.js";import"./formControlState-Bl9Dd4wz.js";import"./Spinner-C0FunRuE.js";import"./SubtitleText-DUUVrgUU.js";import"./CheckboxList-8phOVoJn.js";import"./Checkbox-Dw77DCJG.js";import"./FormControlLabel-DHn1WcqZ.js";import"./getThemeProps-DdDNKTGU.js";import"./MoreVert-Djp96t97.js";import"./ChevronRight-CA1Fd4W_.js";import"./PageHeader-Dwi4ENWc.js";import"./formats-CMvQHWsT.js";import"./sizes-C6oSOElx.js";import"./RadioButton-Ba6ulvNI.js";import"./StyledFormControl.styled-i13hAbZe.js";import"./Toggle-DhOz43zT.js";import"./Dialog-D-UDZ0Lf.js";import"./SideNav-BXGvGLiL.js";import"./Snackbar-DLaSlsnc.js";import"./Close-BLCogFul.js";import"./ClickAwayListener-DoPvkzLf.js";import"./Image-CNIFLnmm.js";function m(o){const t={code:"code",h1:"h1",h4:"h4",li:"li",p:"p",pre:"pre",ul:"ul",...i(),...o.components};return r.jsxs(r.Fragment,{children:[r.jsx(p,{title:"Utils/Transforms/DateFormat"}),`
+`,r.jsx(t.h1,{id:"transform_dateformat",children:"transform_dateFormat"}),`
+`,r.jsx(t.p,{children:"Converts a Date object into a month / day / year string."}),`
+`,r.jsx(t.h4,{id:"conversions",children:"Conversions"}),`
+`,r.jsxs(t.ul,{children:[`
+`,r.jsxs(t.li,{children:[r.jsx(t.code,{children:'new Date("December 25, 2023")'})," => ",r.jsx(e,{cb:n(),data:new Date("December 25, 2023")})]}),`
+`]}),`
+`,r.jsx(t.h4,{id:"args",children:"Args"}),`
+`,r.jsx(t.p,{children:"None"}),`
+`,r.jsx(t.h4,{id:"example",children:"Example"}),`
+`,r.jsx(t.pre,{children:r.jsx(t.code,{className:"language-js",children:`import { transform_dateFormat } from "@simpleview/sv-mosaic"
+...
+transforms : [
+    transform_dateFormat()
+]
+`})})]})}function _r(o={}){const{wrapper:t}={...i(),...o.components};return t?r.jsx(t,{...o,children:r.jsx(m,{...o})}):m(o)}export{_r as default};
