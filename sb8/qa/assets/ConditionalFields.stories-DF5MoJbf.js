9143e542347d6ff60ff5a83078e245623cc8d4d0
Owen Allen <owenallenaz@gmail.com>
2025-02-24 10:48:39 +0000

    Updates

---
+++ sb8/qa/assets/ConditionalFields.stories-DF5MoJbf.js
@@ -0,0 +1,41 @@
+import{j as e}from"./jsx-runtime-DiwAibxM.js";import{r as i}from"./index-Btj5Fd67.js";import{u as c,F as y}from"./index-BtJjjTTO.js";import{r as h}from"./renderButtons-BmHfsf_z.js";import{O as f}from"./utils-BR2PSRFh.js";import"./theme-C7C0QMlu.js";import"./testIds-fiKIunms.js";import"./TitleWrapper-BAR2n_rD.js";import"./Button-CRbkpCI2.js";import"./generateUtilityClasses-oWyMxXuq.js";import"./ButtonBase-biKTBdl5.js";import"./useIsFocusVisible-DHjyF8IZ.js";import"./Popper-Cwezlgxk.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useTheme-R4ufcCXK.js";import"./index-T5NeFSal.js";import"./index-tXhrow7Y.js";import"./useToggle-B3mbTfSB.js";import"./containerQuery-BqcK0eeN.js";import"./createSvgIcon-CzNXaSwG.js";import"./createSvgIcon-Bmixpj46.js";import"./useId-DwELyCdS.js";import"./useControlled-D8KJxpyu.js";import"./Typography-D0-akgTt.js";import"./TitleText-CdOksVB0.js";import"./ButtonRow-BXJCCKLJ.js";import"./index-DvTtuJek.js";import"./debounce-B3RVa3dT.js";import"./DataViewPrimaryFilter.styled-Ckb-VyLy.js";import"./Chip-C_h72JNQ.js";import"./useMosaicTranslation-DPUDh126.js";import"./FormFieldText.styled-CWtnKjLU.js";import"./formControlState-Bl9Dd4wz.js";import"./Spinner-C0FunRuE.js";import"./SubtitleText-DUUVrgUU.js";import"./CheckboxList-8phOVoJn.js";import"./Checkbox-Dw77DCJG.js";import"./FormControlLabel-DHn1WcqZ.js";import"./getThemeProps-DdDNKTGU.js";import"./MoreVert-Djp96t97.js";import"./ChevronRight-CA1Fd4W_.js";import"./PageHeader-Dwi4ENWc.js";import"./formats-CMvQHWsT.js";import"./sizes-C6oSOElx.js";import"./RadioButton-Ba6ulvNI.js";import"./StyledFormControl.styled-i13hAbZe.js";import"./Toggle-DhOz43zT.js";import"./Dialog-D-UDZ0Lf.js";import"./SideNav-BXGvGLiL.js";import"./Snackbar-DLaSlsnc.js";import"./Close-BLCogFul.js";import"./ClickAwayListener-DoPvkzLf.js";const ft={title:"Components/Form"},t=({showState:d})=>{const o=c(),{state:l,handleSubmit:a}=o,n="SHOW";i.useEffect(()=>(document.body.style.margin="0px",()=>{document.body.style.margin=f}),[]);const u=i.useMemo(()=>[{name:"dependency",label:"Dependency",type:"text",instructionText:`Type "${n}" to show another field`,required:!0},{name:"dependent",label:"Dependent",type:"text",show:({data:r})=>(r==null?void 0:r.dependency)===n,required:!0}],[]);return e.jsxs(e.Fragment,{children:[d&&e.jsx("pre",{children:JSON.stringify(l,null,"  ")}),e.jsx("div",{style:{height:"100vh"},children:e.jsx(y,{...o,buttons:h(a),title:"Runtime behaviors",fields:u})})]})};t.args={showState:!1};t.argTypes={showState:{name:"Show state"}};var m,p,s;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`({
+  showState
+}: typeof ConditionalFields.args): ReactElement => {
+  const controller = useForm();
+  const {
+    state,
+    handleSubmit
+  } = controller;
+  const KEY_PHRASE = "SHOW";
+  useEffect(() => {
+    document.body.style.margin = "0px";
+    return () => {
+      document.body.style.margin = ORIGINAL_BODY_MARGIN;
+    };
+  }, []);
+  const fields = useMemo((): FieldDef[] => [{
+    name: "dependency",
+    label: "Dependency",
+    type: "text",
+    instructionText: \`Type "\${KEY_PHRASE}" to show another field\`,
+    required: true
+  }, {
+    name: "dependent",
+    label: "Dependent",
+    type: "text",
+    show: ({
+      data
+    }) => {
+      return data?.dependency === KEY_PHRASE;
+    },
+    required: true
+  }], []);
+  return <>
+            {showState && <pre>{JSON.stringify(state, null, "  ")}</pre>}
+            <div style={{
+      height: "100vh"
+    }}>
+                <Form {...controller} buttons={renderButtons(handleSubmit)} title="Runtime behaviors" fields={fields} />
+            </div>
+        </>;
+}`,...(s=(p=t.parameters)==null?void 0:p.docs)==null?void 0:s.source}}};const xt=["ConditionalFields"];export{t as ConditionalFields,xt as __namedExportsOrder,ft as default};
---
+++ sb8/qa/assets/FormFieldRaw.stories-I85YFVWu.js
@@ -0,0 +1,30 @@
+import{j as r}from"./jsx-runtime-DiwAibxM.js";import{r as F}from"./index-Btj5Fd67.js";import{u as f,F as g}from"./index-BtJjjTTO.js";import{r as b}from"./renderButtons-BmHfsf_z.js";import{c as u}from"./commonFieldControl-DXwRxrKb.js";import{s as x}from"./theme-C7C0QMlu.js";const w=x.div`
+    background-color: #aa1919;
+    border: 3px solid #570202;
+    color: white;
+    padding: 1rem 2rem;
+    border-radius: 99999em;
+`,y={title:"FormFields/FormFieldRaw"};function h(){return r.jsx(w,{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."})}async function j(){return{raw:r.jsx(h,{})}}const e=({label:o,required:t,disabled:n,instructionText:s,helperText:a})=>{const l=f(),{handleSubmit:c}=l,p=F.useMemo(()=>[{name:"raw",label:o,type:"raw",required:t,disabled:n,helperText:a,instructionText:s}],[t,n,o,a,s]);return r.jsx(r.Fragment,{children:r.jsx(g,{...l,buttons:b(c),title:"Raw Field",fields:p,getFormValues:j})})};e.args={...u.args};e.argTypes={...u.argTypes};var i,d,m;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`({
+  label,
+  required,
+  disabled,
+  instructionText,
+  helperText
+}: typeof Playground.args): ReactElement => {
+  const controller = useForm();
+  const {
+    handleSubmit
+  } = controller;
+  const fields: FieldDef[] = useMemo((): FieldDef[] => [{
+    name: "raw",
+    label,
+    type: "raw",
+    required,
+    disabled,
+    helperText,
+    instructionText
+  }], [required, disabled, label, helperText, instructionText]);
+  return <>
+            <Form {...controller} buttons={renderButtons(handleSubmit)} title="Raw Field" fields={fields} getFormValues={getFormValues} />
+        </>;
+}`,...(m=(d=e.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};const R=["Playground"],M=Object.freeze(Object.defineProperty({__proto__:null,Playground:e,__namedExportsOrder:R,default:y},Symbol.toStringTag,{value:"Module"}));export{M as s};
