5b868d8dd13624524ded0cdd3c27d087da08d15c
George Batt <george.batt@simpleviewinc.com>
2025-02-27 13:51:49 +0000

    (chore) Introduce and enforce ESLint rule that prevents imports from the root of MUI.

--- containers/mosaic/eslint.config.mjs
+++ containers/mosaic/eslint.config.mjs
@@ -3,6 +3,19 @@ import baseConfig from "@simpleview/sv-mosaic-eslint/base";
 export default [
 	...baseConfig,
 	{
+		rules: {
+			"no-restricted-imports": [
+				"error",
+				{
+					patterns: [{
+						regex: "@mui\\/material$",
+						message: "Do not import from the root of material UI. For example, instead of `import { InputAdornment } from \"@mui/material\"`, do `import InputAdornment from \"@mui/material/InputAdornment\"`",
+					}],
+				},
+			],
+		},
+	},
+	{
 		ignores: [
 			"dist",
 			"coverage",
