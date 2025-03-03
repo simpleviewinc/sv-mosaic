3c9885cdc433fb71695d1d61e0959f4330c6b327
Owen Allen <owenallenaz@gmail.com>
2025-03-03 10:08:04 +0000

    Updates

---
+++ sb8/staging/assets/FormFieldNumber.stories-axDNBxiP.js
@@ -0,0 +1,41 @@
+import{j as r}from"./jsx-runtime-DiwAibxM.js";import{r as P}from"./index-Btj5Fd67.js";import{u as N,F as T}from"./index-BtJjjTTO.js";import{r as j}from"./renderButtons-BmHfsf_z.js";import{c as b}from"./commonFieldControl-DXwRxrKb.js";import"./theme-C7C0QMlu.js";import"./testIds-fiKIunms.js";import"./TitleWrapper-BAR2n_rD.js";import"./Button-CRbkpCI2.js";import"./generateUtilityClasses-oWyMxXuq.js";import"./ButtonBase-biKTBdl5.js";import"./useIsFocusVisible-DHjyF8IZ.js";import"./Popper-Cwezlgxk.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useTheme-R4ufcCXK.js";import"./index-T5NeFSal.js";import"./index-tXhrow7Y.js";import"./useToggle-B3mbTfSB.js";import"./containerQuery-BqcK0eeN.js";import"./createSvgIcon-CzNXaSwG.js";import"./createSvgIcon-Bmixpj46.js";import"./useId-DwELyCdS.js";import"./useControlled-D8KJxpyu.js";import"./Typography-D0-akgTt.js";import"./TitleText-CdOksVB0.js";import"./ButtonRow-BXJCCKLJ.js";import"./index-DvTtuJek.js";import"./debounce-B3RVa3dT.js";import"./DataViewPrimaryFilter.styled-Ckb-VyLy.js";import"./Chip-C_h72JNQ.js";import"./useMosaicTranslation-DPUDh126.js";import"./FormFieldText.styled-CWtnKjLU.js";import"./formControlState-Bl9Dd4wz.js";import"./Spinner-C0FunRuE.js";import"./SubtitleText-DUUVrgUU.js";import"./CheckboxList-8phOVoJn.js";import"./Checkbox-Dw77DCJG.js";import"./FormControlLabel-DHn1WcqZ.js";import"./getThemeProps-DdDNKTGU.js";import"./MoreVert-Djp96t97.js";import"./ChevronRight-CA1Fd4W_.js";import"./PageHeader-Dwi4ENWc.js";import"./formats-CMvQHWsT.js";import"./sizes-C6oSOElx.js";import"./RadioButton-Ba6ulvNI.js";import"./StyledFormControl.styled-i13hAbZe.js";import"./Toggle-DhOz43zT.js";import"./Dialog-D-UDZ0Lf.js";import"./SideNav-BXGvGLiL.js";import"./Snackbar-DLaSlsnc.js";import"./Close-BLCogFul.js";import"./ClickAwayListener-DoPvkzLf.js";const je={title:"FormFields/FormFieldNumber"},e=({label:o,required:i,skeleton:F,disabled:n,instructionText:m,helperText:s,size:p,placeholder:t,decimalPlaces:l,prefix:a,suffix:d,sign:c})=>{const u=N(),{handleSubmit:h,state:y}=u,S=P.useMemo(()=>[{name:"numberfield",label:o,type:"number",required:i,disabled:n,size:p,inputSettings:{placeholder:t===""?void 0:t,decimalPlaces:l,prefix:a,suffix:d,sign:c},helperText:s,instructionText:m}],[o,i,n,p,t,s,m,l,a,d,c]);return r.jsxs(r.Fragment,{children:[r.jsx(T,{buttons:j(h),title:"Number Field",fields:S,...u,skeleton:F}),r.jsx("pre",{children:JSON.stringify(y,null,"  ")})]})};e.args={...b.args,size:"sm",placeholder:"placeholder",decimalPlaces:-1,prefix:"",suffix:"",sign:"any"};e.argTypes={...b.argTypes,size:{name:"Size",control:{type:"select"},options:["xs","sm","md","lg"]},placeholder:{name:"Placeholder"},decimalPlaces:{name:"Decimal Places"},prefix:{name:"Prefix"},suffix:{name:"Suffix"},sign:{name:"Sign",control:{type:"select"},options:["any","positive","negative"]}};var f,x,g;e.parameters={...e.parameters,docs:{...(f=e.parameters)==null?void 0:f.docs,source:{originalSource:`({
+  label,
+  required,
+  skeleton,
+  disabled,
+  instructionText,
+  helperText,
+  size,
+  placeholder,
+  decimalPlaces,
+  prefix,
+  suffix,
+  sign
+}: typeof Playground.args): ReactElement => {
+  const controller = useForm();
+  const {
+    handleSubmit,
+    state
+  } = controller;
+  const fields: FieldDef[] = useMemo((): FieldDef[] => [{
+    name: "numberfield",
+    label,
+    type: "number",
+    required,
+    disabled,
+    size,
+    inputSettings: {
+      placeholder: placeholder === "" ? undefined : placeholder,
+      decimalPlaces,
+      prefix,
+      suffix,
+      sign
+    },
+    helperText,
+    instructionText
+  }], [label, required, disabled, size, placeholder, helperText, instructionText, decimalPlaces, prefix, suffix, sign]);
+  return <>
+            <Form buttons={renderButtons(handleSubmit)} title="Number Field" fields={fields} {...controller} skeleton={skeleton} />
+            <pre>{JSON.stringify(state, null, "  ")}</pre>
+        </>;
+}`,...(g=(x=e.parameters)==null?void 0:x.docs)==null?void 0:g.source}}};const ze=["Playground"];export{e as Playground,ze as __namedExportsOrder,je as default};
---
+++ sb8/staging/assets/FormFieldPhone.stories-BRGER6sQ.js
@@ -0,0 +1,75 @@
+import{j as e}from"./jsx-runtime-DiwAibxM.js";import{r as x}from"./index-Btj5Fd67.js";import{u as F,F as g}from"./index-BtJjjTTO.js";import{r as T}from"./renderButtons-BmHfsf_z.js";import{c as S}from"./commonFieldControl-DXwRxrKb.js";const E={title:"FormFields/FormFieldPhone"},t=({label:n,required:o,skeleton:l,disabled:s,instructionText:a,helperText:i,autoFormat:u,country:d})=>{const c=F(),{state:j,handleSubmit:q}=c,P=x.useMemo(()=>[{name:"phone",label:n,type:"phone",required:o,disabled:s,inputSettings:{autoFormat:u,country:d},helperText:i,instructionText:a}],[s,o,u,d,n,i,a]);return e.jsxs(e.Fragment,{children:[e.jsx("pre",{children:JSON.stringify(j,null,"  ")}),e.jsx(g,{...c,buttons:T(q),title:"Phone Field",fields:P,skeleton:l})]})};t.args={...S.args,autoFormat:!0,country:""};t.argTypes={...S.argTypes,autoFormat:{name:"Autoformat"},country:{name:"Country"}};const r=()=>{const n=F(),{state:o,handleSubmit:l}=n,s=x.useMemo(()=>[{name:"phone",label:"Regular example",type:"phone",required:!1,disabled:!1,helperText:"Helper text",instructionText:'Default contry code is "us"'},{name:"countryCode",label:"With a country code provided",type:"phone",required:!1,disabled:!1,inputSettings:{country:"ar"},helperText:"Helper text",instructionText:'The country code of "ar" was provided'},{name:"autoformatEnabled",label:"Autoformat enabled",type:"phone",required:!1,disabled:!1,inputSettings:{autoFormat:!0,country:"us"},helperText:"Helper text",instructionText:"Type a phone number to see the format"}],[]);return e.jsxs(e.Fragment,{children:[e.jsx("pre",{children:JSON.stringify(o,null,"  ")}),e.jsx(g,{...n,buttons:T(l),title:"Form Title",description:"Form description",fields:s})]})};var p,m,f;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`({
+  label,
+  required,
+  skeleton,
+  disabled,
+  instructionText,
+  helperText,
+  autoFormat,
+  country
+}: typeof Playground.args): ReactElement => {
+  const controller = useForm();
+  const {
+    state,
+    handleSubmit
+  } = controller;
+  const fields = useMemo((): FieldDef[] => [{
+    name: "phone",
+    label,
+    type: "phone",
+    required,
+    disabled,
+    inputSettings: {
+      autoFormat,
+      country
+    },
+    helperText,
+    instructionText
+  }], [disabled, required, autoFormat, country, label, helperText, instructionText]);
+  return <>
+            <pre>{JSON.stringify(state, null, "  ")}</pre>
+            <Form {...controller} buttons={renderButtons(handleSubmit)} title="Phone Field" fields={fields} skeleton={skeleton} />
+        </>;
+}`,...(f=(m=t.parameters)==null?void 0:m.docs)==null?void 0:f.source}}};var h,y,b;r.parameters={...r.parameters,docs:{...(h=r.parameters)==null?void 0:h.docs,source:{originalSource:`(): ReactElement => {
+  const controller = useForm();
+  const {
+    state,
+    handleSubmit
+  } = controller;
+  const fields: FieldDef[] = useMemo(() => [{
+    name: "phone",
+    label: "Regular example",
+    type: "phone",
+    required: false,
+    disabled: false,
+    helperText: "Helper text",
+    instructionText: 'Default contry code is "us"'
+  }, {
+    name: "countryCode",
+    label: "With a country code provided",
+    type: "phone",
+    required: false,
+    disabled: false,
+    inputSettings: {
+      country: "ar"
+    },
+    helperText: "Helper text",
+    instructionText: 'The country code of "ar" was provided'
+  }, {
+    name: "autoformatEnabled",
+    label: "Autoformat enabled",
+    type: "phone",
+    required: false,
+    disabled: false,
+    inputSettings: {
+      autoFormat: true,
+      country: "us"
+    },
+    helperText: "Helper text",
+    instructionText: "Type a phone number to see the format"
+  }], []);
+  return <>
+            <pre>{JSON.stringify(state, null, "  ")}</pre>
+            <Form {...controller} buttons={renderButtons(handleSubmit)} title="Form Title" description="Form description" fields={fields} />
+        </>;
+}`,...(b=(y=r.parameters)==null?void 0:y.docs)==null?void 0:b.source}}};const O=["Playground","KitchenSink"],R=Object.freeze(Object.defineProperty({__proto__:null,KitchenSink:r,Playground:t,__namedExportsOrder:O,default:E},Symbol.toStringTag,{value:"Module"}));export{R as s};
