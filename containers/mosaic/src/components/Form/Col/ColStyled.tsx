d6f3409ca5d7e86c51ac4c246764f505cae01bbf
George Batt <george.batt@simpleviewinc.com>
2025-03-06 13:49:22 +0000

    WIP

--- containers/mosaic/src/components/Form/Col/ColStyled.tsx
+++ containers/mosaic/src/components/Form/Col/ColStyled.tsx
@@ -1,13 +1,16 @@
 import styled from "styled-components";
-import type { TransientProps } from "@root/types";
 import { CONTAINERS } from "@root/theme/theme";
-import type { ColPropsTypes } from "./ColTypes";
 
-export const StyledCol = styled.div<TransientProps<ColPropsTypes, "colsInRow">>`
+export const StyledCol = styled.div<{ $gridColumn?: string }>`
 	display: flex;
 	flex-direction: column;
 	position: relative;
 	gap: 24px;
+
+	${({ $gridColumn }) => `
+		grid-column: ${$gridColumn};
+	`}
+
 
 	container-type: inline-size;
 	container-name: ${CONTAINERS.FORM_COL};
