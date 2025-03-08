9d8a5e3f3597a3b1fa1e913de36ee21e205dfd0f
George Batt <george.batt@simpleviewinc.com>
2025-03-08 01:43:36 +0000

    (ChipList) Factor out chip list into a consumable component. Amend show more/less styling.

---
+++ containers/mosaic/src/__tests__/components/ChipList/ChipList.test.tsx
@@ -0,0 +1,63 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import React, { act } from "react";
+
+import type { ChipListProps } from "@root/components/ChipList";
+
+import ChipList from "@root/components/ChipList";
+import { mockOptions } from "@root/mock";
+import testIds from "@root/utils/testIds";
+
+async function setup(props: Partial<ChipListProps> = {}) {
+	const renderResult = await act(() => render(
+		<ChipList
+			chips={mockOptions}
+			{...props}
+		/>,
+	));
+
+	return {
+		...renderResult,
+		user: userEvent.setup(),
+	};
+}
+
+describe(__dirname, () => {
+	it("should render chips with title", async () => {
+		await setup();
+
+		expect(screen.getByText("Dog", { exact: true })).toBeInTheDocument();
+		expect(screen.getByText("Cat", { exact: true })).toBeInTheDocument();
+		expect(screen.getByText("Horse", { exact: true })).toBeInTheDocument();
+	});
+
+	it("should not throw an error when chips are undefined", async () => {
+		expect(() => setup({
+			chips: undefined,
+		})).not.toThrow();
+	});
+
+	it("should fire on delete handler with reduced chips when delete icon is clicked", async () => {
+		const onDeleteMock = vi.fn();
+		const { user } = await setup({
+			onDelete: onDeleteMock,
+			chips: mockOptions.slice(0, 3),
+		});
+
+		const deleteIcon = screen.getAllByTestId(testIds.CHIP_DELETE_ICON);
+		expect(deleteIcon).toHaveLength(3);
+		const [firstDeleteIcon] = deleteIcon;
+		await user.click(firstDeleteIcon);
+		expect(onDeleteMock).toBeCalledWith(mockOptions.slice(1, 3));
+	});
+
+	it("should display all chips when show more button is clicked", async () => {
+		const { user } = await setup();
+
+		expect(screen.getAllByTestId(testIds.CHIP)).toHaveLength(8);
+		const showMore = screen.getByRole("button", { name: "Show More" });
+		expect(showMore).toBeInTheDocument();
+		await user.click(showMore);
+		expect(screen.getAllByTestId(testIds.CHIP)).toHaveLength(30);
+	});
+});
