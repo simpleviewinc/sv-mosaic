409ec67ef4b6b17ff300065468fd778b300e0c12
George Batt <george.batt@simpleviewinc.com>
2025-03-10 16:31:30 +0000

    (chore) Allow linter test to run for longer.

--- containers/mosaic/src/__tests__/quality/linter.test.ts
+++ containers/mosaic/src/__tests__/quality/linter.test.ts
@@ -2,7 +2,7 @@ import path from "path";
 import { execSync } from "child_process";
 
 describe(__filename, function() {
-	it("Run linter to check code style", { timeout: 30_000 }, async () => {
+	it("Run linter to check code style", { timeout: 60_000 }, async () => {
 		execSync("yarn lint", {
 			stdio : "inherit",
 			cwd: path.resolve(__dirname, "../.."),
