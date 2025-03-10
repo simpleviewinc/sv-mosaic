75e584681012fdf389f1a5b17c55a4fce6f8638b
George Batt <george.batt@simpleviewinc.com>
2025-03-10 11:14:34 +0000

    (ChipList) Fixes inconsistent styling.

--- containers/mosaic/src/components/Field/FormFieldAdvancedSelection/AdvancedSelection.styled.tsx
+++ containers/mosaic/src/components/Field/FormFieldAdvancedSelection/AdvancedSelection.styled.tsx
@@ -1,10 +1,15 @@
+import Button from "@root/components/Button";
 import styled from "styled-components";
 
 export const StyledSkeletonWrapper = styled.div`
-  display: flex;
-  gap: 16px;
+	display: flex;
+	gap: 16px;
 
-  & .MuiSkeleton-root {
-    border-radius: 99999em;
-  }
+	& .MuiSkeleton-root {
+		border-radius: 99999em;
+	}
+`;
+
+export const StyledButton = styled(Button)`
+	margin-bottom: 8px;
 `;
