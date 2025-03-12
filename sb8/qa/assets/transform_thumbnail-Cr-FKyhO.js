bf5efd2ce8a996bf119676fbe7b1549c91b96003
Owen Allen <owenallenaz@gmail.com>
2025-03-12 16:07:48 +0000

    Updates

---
+++ sb8/qa/assets/transform_thumbnail-Cr-FKyhO.js
@@ -0,0 +1,19 @@
+import{j as i}from"./jsx-runtime-DiwAibxM.js";import{useMDXComponents as m}from"./index-B4U1yk18.js";import{M as n}from"./index-Drn8lfyg.js";import{c as p}from"./column_transforms-B_zNuoZX.js";import{T as e}from"./TransformExample-DKwiSzdP.js";import"./index-Btj5Fd67.js";import"./iframe-3KNTEZDI.js";import"../sb-preview/runtime.js";import"./index-T5NeFSal.js";import"./index-tXhrow7Y.js";import"./tslib.es6-CRos2fHm.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./inheritsLoose-Cqq1nRsk.js";import"./toString-CrHhZNKP.js";import"./index-CQ9F9_Tt.js";import"./index-ogSvIofg.js";import"./Form-BIgjRCm6.js";import"./theme-D38K4W-Z.js";import"./SideNav-D2hLlXvZ.js";import"./containerQuery-CksQJzsL.js";import"./useToggle-B3mbTfSB.js";import"./Button-BzJSDP1S.js";import"./generateUtilityClasses-B5i8Rs7p.js";import"./ButtonBase-B2xCaBev.js";import"./useIsFocusVisible-DHjyF8IZ.js";import"./Popper-BV6MzckQ.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useTheme-DELdgGyu.js";import"./index-Dk1V5coX.js";import"./debounce-IFS8tfQQ.js";import"./DataViewPrimaryFilter.styled-CTVdcnW9.js";import"./createSvgIcon-CAs7t9wT.js";import"./createSvgIcon-CM-As40z.js";import"./useId-DwELyCdS.js";import"./useControlled-D8KJxpyu.js";import"./Chip-ulzUmfU4.js";import"./testIds-C2FMCy5K.js";import"./useMosaicTranslation-DPUDh126.js";import"./DataViewFilterDropdown-CqTisVOW.js";import"./formControlState-BAf21Shc.js";import"./ButtonRow-iQTdUMH8.js";import"./Spinner-C_uMqEwM.js";import"./Typography-iUA5kZEF.js";import"./SubtitleText-BrufIBLI.js";import"./CheckboxList-BKgo-y8p.js";import"./Checkbox-1rJEaydD.js";import"./FormControlLabel-2hVWRnF1.js";import"./getThemeProps-Fw47730D.js";import"./sizes-C6oSOElx.js";import"./RadioButton-LFlv3WCh.js";import"./StyledFormControl.styled-CNCCtQwC.js";import"./Toggle-H5i5NPQc.js";import"./formats-sBod7wm8.js";import"./Dialog-BYWfLX43.js";import"./ChevronRight-CNo34ydh.js";import"./TitleWrapper-OgvLyBY4.js";import"./TitleText-b_QXd7Yf.js";import"./Snackbar-CrdURRDn.js";import"./Close-gDfoKZ0c.js";import"./ClickAwayListener-DoPvkzLf.js";import"./MoreVert-B1D8IEMN.js";import"./PageHeader-C-k19XWk.js";import"./Content.styled-CvEeminH.js";import"./Image-CNIFLnmm.js";function o(t){const r={code:"code",h1:"h1",h4:"h4",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...m(),...t.components};return i.jsxs(i.Fragment,{children:[i.jsx(n,{title:"Utils/Transforms/Thumbnail"}),`
+`,i.jsx(r.h1,{id:"transform_thumbnail",children:"transform_thumbnail"}),`
+`,i.jsxs(r.p,{children:["Converts an image URL to an ",i.jsx(r.code,{children:"<img />"})," element with a given height and width."]}),`
+`,i.jsx(r.h4,{id:"conversions",children:"Conversions"}),`
+`,i.jsxs(r.ul,{children:[`
+`,i.jsxs(r.li,{children:[i.jsx(r.code,{children:'"https://myImgUrl.com"'})," => ",i.jsx(e,{cb:p({width:150,height:150}),data:"https://res.cloudinary.com/simpleview/image/upload/v1542821844/clients/grandrapids/_OD_0354_c78fbb66-c75a-4804-9430-9af38ed8e9d5.jpg"})]}),`
+`]}),`
+`,i.jsx(r.h4,{id:"args",children:"Args"}),`
+`,i.jsxs(r.ul,{children:[`
+`,i.jsxs(r.li,{children:[i.jsx(r.strong,{children:"width"})," - width of the image in pixels."]}),`
+`,i.jsxs(r.li,{children:[i.jsx(r.strong,{children:"height"})," - height of the image in pixels."]}),`
+`]}),`
+`,i.jsx(r.h4,{id:"example",children:"Example"}),`
+`,i.jsx(r.pre,{children:i.jsx(r.code,{className:"language-js",children:`import { transform_thumbnail } from "@simpleview/sv-mosaic"
+...
+transforms : [
+    transform_thumbnail()
+]
+`})})]})}function bi(t={}){const{wrapper:r}={...m(),...t.components};return r?i.jsx(r,{...t,children:i.jsx(o,{...t})}):o(t)}export{bi as default};
