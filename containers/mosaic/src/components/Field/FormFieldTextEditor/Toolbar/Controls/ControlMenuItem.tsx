2bfb6a8c7574dc5d91f1f2b11f952d3c855dfbaa
George Batt <george.batt@simpleviewinc.com>
2025-03-20 11:19:54 +0000

    (TextEditor) Accessibility improvements to ensure control menus can be navigated using the keyboard.

--- containers/mosaic/src/components/Field/FormFieldTextEditor/Toolbar/Controls/ControlMenuItem.tsx
+++ containers/mosaic/src/components/Field/FormFieldTextEditor/Toolbar/Controls/ControlMenuItem.tsx
@@ -8,8 +8,9 @@ import type { ControlWithProps, TextEditorInputSettings } from "../../FormFieldT
 import { MenuItemLabel, MenuItemShortcut, StyledMenuItem } from "../../FormFieldTextEditor.styled";
 import testIds from "@root/utils/testIds";
 import { Shortcut } from "../Shortcut";
+import type { MenuItemProps } from "@mui/material/MenuItem";
 
-type ControlMenuItemProps = ControlWithProps & {
+type ControlMenuItemProps = ControlWithProps & MenuItemProps & {
 	editor: Editor;
 	inputSettings: TextEditorInputSettings;
 	onClose: () => void;
@@ -24,6 +25,9 @@ export function ControlMenuItem({
 	editor,
 	onClose,
 	inputSettings,
+	value: _value,
+	show: _show,
+	...props
 }: ControlMenuItemProps): ReactElement {
 	const onClick = (event: MouseEvent<HTMLButtonElement>) => {
 		cmd({ editor, inputSettings, event });
@@ -31,22 +35,41 @@ export function ControlMenuItem({
 	};
 
 	return (
-		<li>
-			<StyledMenuItem
-				onClick={onClick}
-				$active={editor.isActive(name)}
-				data-testid={`${testIds.TEXT_EDITOR_CONTROL}:${name}`}
-			>
-				<MenuItemLabel>
-					{Icon && <Icon />}
-					{label}
-				</MenuItemLabel>
-				{shortcut && (
-					<MenuItemShortcut>
-						<Shortcut shortcut={shortcut} />
-					</MenuItemShortcut>
-				)}
-			</StyledMenuItem>
-		</li>
+		<StyledMenuItem
+			{...props}
+			onClick={onClick}
+			$active={editor.isActive(name)}
+			data-testid={`${testIds.TEXT_EDITOR_CONTROL}:${name}`}
+		>
+			<MenuItemLabel>
+				{Icon && <Icon />}
+				{label}
+			</MenuItemLabel>
+			{shortcut && (
+				<MenuItemShortcut>
+					<Shortcut shortcut={shortcut} />
+				</MenuItemShortcut>
+			)}
+		</StyledMenuItem>
 	);
+
+	// return (
+	// 	<li>
+	// 		<StyledMenuItem
+	// 			onClick={onClick}
+	// 			$active={editor.isActive(name)}
+	// 			data-testid={`${testIds.TEXT_EDITOR_CONTROL}:${name}`}
+	// 		>
+	// 			<MenuItemLabel>
+	// 				{Icon && <Icon />}
+	// 				{label}
+	// 			</MenuItemLabel>
+	// 			{shortcut && (
+	// 				<MenuItemShortcut>
+	// 					<Shortcut shortcut={shortcut} />
+	// 				</MenuItemShortcut>
+	// 			)}
+	// 		</StyledMenuItem>
+	// 	</li>
+	// );
 }
