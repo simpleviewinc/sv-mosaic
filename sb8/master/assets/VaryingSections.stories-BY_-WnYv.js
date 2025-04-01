2e9c198d4cc6ab7fe4d3814f93ab9a21d76edbf2
Owen Allen <owenallenaz@gmail.com>
2025-04-01 09:19:57 +0000

    Updates

---
+++ sb8/master/assets/VaryingSections.stories-BY_-WnYv.js
@@ -0,0 +1,71 @@
+import{j as t}from"./jsx-runtime-DiwAibxM.js";import{r as o}from"./index-Btj5Fd67.js";import{u as _,F as u}from"./Form-B7P8EMV6.js";import{r as S}from"./renderButtons-BmHfsf_z.js";import"./theme-D38K4W-Z.js";import"./SideNav-D2hLlXvZ.js";import"./containerQuery-CksQJzsL.js";import"./useToggle-B3mbTfSB.js";import"./Button-DxOHnzJo.js";import"./generateUtilityClasses-B5i8Rs7p.js";import"./ButtonBase-B2xCaBev.js";import"./useIsFocusVisible-DHjyF8IZ.js";import"./Popper-BV6MzckQ.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useTheme-DELdgGyu.js";import"./index-T5NeFSal.js";import"./index-tXhrow7Y.js";import"./index-CrPr68dg.js";import"./debounce-IFS8tfQQ.js";import"./DataViewPrimaryFilter.styled-CTVdcnW9.js";import"./createSvgIcon-CAs7t9wT.js";import"./createSvgIcon-CM-As40z.js";import"./useId-DwELyCdS.js";import"./useControlled-D8KJxpyu.js";import"./Chip-BLORHG_J.js";import"./testIds-Cxqhkw8S.js";import"./useMosaicTranslation-DPUDh126.js";import"./DataViewFilterDropdown-M-CReHkM.js";import"./formControlState-BAf21Shc.js";import"./ButtonRow-KlomlsFy.js";import"./Spinner-C_uMqEwM.js";import"./Typography-iUA5kZEF.js";import"./SubtitleText-BrufIBLI.js";import"./CheckboxList-Dua-Ccba.js";import"./Checkbox-CMTKNKa4.js";import"./FormControlLabel-2hVWRnF1.js";import"./getThemeProps-Fw47730D.js";import"./sizes-C6oSOElx.js";import"./RadioButton-LFlv3WCh.js";import"./StyledFormControl.styled-CNCCtQwC.js";import"./Toggle-_4jhqXCQ.js";import"./formats-sBod7wm8.js";import"./Dialog-9INDoSZN.js";import"./ChevronRight-CNo34ydh.js";import"./TitleWrapper-kkXpxrCn.js";import"./TitleText-b_QXd7Yf.js";import"./Snackbar-DkvzNMib.js";import"./Close-gDfoKZ0c.js";import"./ClickAwayListener-DoPvkzLf.js";import"./MoreVert-B1D8IEMN.js";import"./PageHeader-BStOH7U_.js";const Se={title:"Components/Form"},y=document.body.style.margin,i=({showState:d})=>{const n=_(),{state:p,handleSubmit:c}=n;o.useEffect(()=>(document.body.style.margin="0px",()=>{document.body.style.margin=y}),[]);const f=o.useMemo(()=>[...Array(80).fill(null).map((a,e)=>({name:`field_${e}`,label:`Field ${e}`,type:"text"}))],[]),m=o.useMemo(()=>[{title:"Section 1",description:"Description for section 1",collapsed:!0,fields:[[["field_1"]],[["field_2"]]]},{title:"Section 2",description:"Description for section 2",collapsed:!1,fields:[[["field_3"]]]},{title:"Section 3",description:"Description for section 3",collapsed:!1,fields:[[["field_4"]],[["field_5"]],[["field_6"]],[["field_7"]],[["field_8"]]]},...Array(10).fill(null).map((a,e)=>({title:`Section ${e+4}`,description:`Description for section ${e+4}`,collapsed:!0,fields:[[[`field_${e*5+9}`]],[[`field_${e*5+10}`]],[[`field_${e*5+11}`]],[[`field_${e*5+12}`]],[[`field_${e*5+13}`]]]})),{title:"Section 14",description:"Description for section 14",collapsed:!1,fields:[[["field_59"]],[["field_60"]],[["field_61"]],[["field_62"]],[["field_63"]]]},{title:"Section 15",description:"Description for section 15",collapsed:!1,fields:[[["field_64"]]]}],[]);return t.jsxs(t.Fragment,{children:[d&&t.jsx("pre",{children:JSON.stringify(p,null,"  ")}),t.jsx("div",{style:{height:"100vh"},children:t.jsx(u,{...n,buttons:S(c),title:"Varying Sections",fields:f,sections:m})})]})};i.args={showState:!1};i.argTypes={showState:{name:"Show state"}};var r,s,l;i.parameters={...i.parameters,docs:{...(r=i.parameters)==null?void 0:r.docs,source:{originalSource:`({
+  showState
+}: typeof VaryingSections.args): ReactElement => {
+  const controller = useForm();
+  const {
+    state,
+    handleSubmit
+  } = controller;
+  useEffect(() => {
+    document.body.style.margin = "0px";
+    return () => {
+      document.body.style.margin = ORIGINAL_BODY_MARGIN;
+    };
+  }, []);
+  const fields = useMemo((): FieldDef[] => [...Array(80).fill(null).map<FieldDef>((_, i) => ({
+    name: \`field_\${i}\`,
+    label: \`Field \${i}\`,
+    type: "text"
+  }))], []);
+  const sections = useMemo(() => [{
+    title: "Section 1",
+    description: "Description for section 1",
+    collapsed: true,
+    fields: [
+    // row 1
+    [["field_1"]], [["field_2"]]]
+  }, {
+    title: "Section 2",
+    description: "Description for section 2",
+    collapsed: false,
+    fields: [
+    // row 1
+    [["field_3"]]]
+  }, {
+    title: "Section 3",
+    description: "Description for section 3",
+    collapsed: false,
+    fields: [
+    // row 1
+    [["field_4"]], [["field_5"]], [["field_6"]], [["field_7"]], [["field_8"]]]
+  }, ...Array(10).fill(null).map((_, i) => ({
+    title: \`Section \${i + 4}\`,
+    description: \`Description for section \${i + 4}\`,
+    collapsed: true,
+    fields: [
+    // row 1
+    [[\`field_\${i * 5 + 9}\`]], [[\`field_\${i * 5 + 10}\`]], [[\`field_\${i * 5 + 11}\`]], [[\`field_\${i * 5 + 12}\`]], [[\`field_\${i * 5 + 13}\`]]]
+  })), {
+    title: "Section 14",
+    description: "Description for section 14",
+    collapsed: false,
+    fields: [
+    // row 1
+    [["field_59"]], [["field_60"]], [["field_61"]], [["field_62"]], [["field_63"]]]
+  }, {
+    title: "Section 15",
+    description: "Description for section 15",
+    collapsed: false,
+    fields: [
+    // row 1
+    [["field_64"]]]
+  }], []);
+  return <>
+            {showState && <pre>{JSON.stringify(state, null, "  ")}</pre>}
+            <div style={{
+      height: "100vh"
+    }}>
+                <Form {...controller} buttons={renderButtons(handleSubmit)} title="Varying Sections" fields={fields} sections={sections} />
+            </div>
+        </>;
+}`,...(l=(s=i.parameters)==null?void 0:s.docs)==null?void 0:l.source}}};const ye=["VaryingSections"];export{i as VaryingSections,ye as __namedExportsOrder,Se as default};
