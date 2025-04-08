639eb8158983715ce4fe5135508e35b35f33d8a2
Owen Allen <owenallenaz@gmail.com>
2025-04-08 16:44:00 +0000

    Updates

---
+++ sb8/develop/assets/Menu-wU3ZEJIw.js
@@ -0,0 +1,14 @@
+import{j as o}from"./jsx-runtime-EKYJJIwR.js";import{useMDXComponents as r}from"./index-BwDDMXeo.js";import{M as s,C as t}from"./index-DdVwmMdD.js";import{s as m,P as c,W as p}from"./Menu.stories-C0s7x5Gd.js";import"./index-D0AnReJb.js";import"./iframe-wjggGZT_.js";import"./index-Bqzh3Cp6.js";import"./index-SI8xS9YR.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./Create-C3twQYnV.js";import"./styled-D_JiKFWz.js";import"./createSvgIcon-DgkyijZl.js";import"./Delete-BQJpjAkE.js";import"./CloudDownload-ku40vlQl.js";import"./Button-DuPZ_syw.js";import"./theme-BSoCkN4E.js";import"./generateUtilityClasses-DR3ZN2W1.js";import"./ButtonBase-D151CXhN.js";import"./useIsFocusVisible-CvEUtp4E.js";import"./Popper-CwglX3X7.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useTheme-D2wr39e0.js";import"./useToggle-RmkKdcfw.js";import"./Typography-CID4xQqz.js";function e(i){const n={a:"a",code:"code",h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...r(),...i.components};return o.jsxs(o.Fragment,{children:[o.jsx(s,{of:m}),`
+`,o.jsx(n.h1,{id:"menu",children:"Menu"}),`
+`,o.jsxs(n.p,{children:[o.jsx(n.code,{children:"Menu"})," is a component that allows you to invoke a dropdown of MenuItems. Each menuItem can have it's own unique ",o.jsx(n.code,{children:"onClick"})," handler."]}),`
+`,o.jsx(n.h2,{id:"props",children:"Props"}),`
+`,o.jsxs(n.ul,{children:[`
+`,o.jsx(n.li,{children:"anchorEl - HTMLElement"}),`
+`,o.jsx(n.li,{children:"open - boolean"}),`
+`,o.jsx(n.li,{children:"onClose - function"}),`
+`,o.jsxs(n.li,{children:["items - Array of ",o.jsx(n.a,{href:"https://github.com/simpleviewinc/sv-mosaic/blob/develop/containers/mosaic/src/components/MenuItem/MenuItemTypes.ts",rel:"nofollow",children:"MenuItemProps"})]}),`
+`]}),`
+`,o.jsx(n.h2,{id:"basic",children:"Basic"}),`
+`,o.jsx(t,{of:c}),`
+`,o.jsx(n.h2,{id:"with-icons-and-colors",children:"With Icons and Colors"}),`
+`,o.jsx(t,{of:p})]})}function A(i={}){const{wrapper:n}={...r(),...i.components};return n?o.jsx(n,{...i,children:o.jsx(e,{...i})}):e(i)}export{A as default};
