94cea23ecbe8b4e4d39704eaf41a4eb8be237a0b
George Batt <george.batt@simpleviewinc.com>
2025-04-15 09:04:15 +0100

    (DataView) Adds a button to column drawer items that can be used to easily remove items.

--- containers/mosaic/src/components/DataView/DataViewColumDrawer/DataViewColumnDrawer.styled.ts
+++ containers/mosaic/src/components/DataView/DataViewColumDrawer/DataViewColumnDrawer.styled.ts
@@ -2,38 +2,38 @@ import styled from "styled-components";
 import theme from "@root/theme";
 
 export const ColumnTitle = styled.h2`
-  color: ${theme.newColors.almostBlack["100"]};
-  font-size: 20px;
-  font-weight: ${theme.fontWeight.normal};
-  line-height: 24px;
-  margin: 0px;
+	color: ${theme.newColors.almostBlack["100"]};
+	font-size: 20px;
+	font-weight: ${theme.fontWeight.normal};
+	line-height: 24px;
+	margin: 0px;
 `;
 
 export const StyledWrapper = styled.div`
-  display: flex;
-  margin-top: 30px;
+	display: flex;
+	margin-top: 30px;
 
-  & > .left {
-    border-right: 2px solid ${theme.newColors.grey2["100"]};
-    flex: 1;
-    margin: 0 32px 0 40px;
-    width: 265px;
-  }
+	& > .left {
+		border-right: 2px solid ${theme.newColors.grey2["100"]};
+		flex: 1;
+		margin: 0 32px 0 40px;
+		width: 265px;
+	}
 
-  & > .left .listItem {
-    margin: 14px 0px 0px -10px;
-    background: white;
-  }
+	& > .left .listItem {
+		margin: 14px 0px 0px -10px;
+		background: white;
+	}
 
-  & > .right {
-    flex: 1;
-    margin: 0 44px 0 32px;
-    width: 280px;
-  }
+	& > .right {
+		flex: 1;
+		margin: 0 44px 0 32px;
+		width: 280px;
+	}
 
-  & > .right .droppable-columns {
-	margin-top: 4px;
-}
+	& > .right .droppable-columns {
+		margin-top: 4px;
+	}
 `;
 
 export const ColumnItem = styled.div<{ $isDragging?: boolean }>`
@@ -47,18 +47,20 @@ export const ColumnItem = styled.div<{ $isDragging?: boolean }>`
 	`}
 
 	cursor: grab;
-  	align-items: center;
+	align-items: center;
 	border-bottom: 1px solid ${theme.newColors.grey2["100"]};
-  	display: flex;
-  	padding: 20px 0 20px 0;
+	display: flex;
+	padding: 12px 0;
 
-	span {
-		margin-left: 16px;
-		color: ${theme.newColors.almostBlack["100"]};
-		font-weight: ${theme.fontWeight.normal};
-	}
 
 	.MuiSvgIcon-root {
 		color: ${theme.newColors.grey3["100"]};
 	}
+`;
+
+export const ColumnItemLabel = styled.div`
+	flex-grow: 1;
+	margin-left: 16px;
+	color: ${theme.newColors.almostBlack["100"]};
+	font-weight: ${theme.fontWeight.normal};
 `;
