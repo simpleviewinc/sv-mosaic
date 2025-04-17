e990e85a8cd92fb9932645e104bb63f0adaefbaf
Owen Allen <owenallenaz@gmail.com>
2025-04-17 08:38:15 +0000

    Updates

---
+++ sb8/qa/assets/PerformanceWithSubmit.stories-DlBBSECb.js
@@ -0,0 +1,36 @@
+import{j as r}from"./jsx-runtime-EKYJJIwR.js";import{r as n}from"./index-DDfD_3TG.js";import{u as c,r as h,F as f}from"./Form-rskk-Nac.js";import{r as x}from"./renderButtons-BmHfsf_z.js";import{O as b}from"./utils-BR2PSRFh.js";import"./theme-DWrYR5Gu.js";import"./SideNav-CvsHqpPT.js";import"./containerQuery-XietOkEy.js";import"./useToggle-BK_3QGB6.js";import"./Button-_6SRCSsN.js";import"./generateUtilityClasses-Dav9K5Jg.js";import"./ButtonBase-B3sVjXKq.js";import"./useIsFocusVisible-DxTeNLkh.js";import"./Popper-BFIFukJ-.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useTheme-BjWv6DhY.js";import"./index-DCMmMN8S.js";import"./index-ywIwhVjx.js";import"./index-BpD74PYt.js";import"./debounce-G1u4Gy2U.js";import"./DataViewPrimaryFilter.styled-DOEMDM8r.js";import"./createSvgIcon-ryN2wBPb.js";import"./createSvgIcon-Bc8E_7Df.js";import"./useId-C6ulm9BR.js";import"./useControlled-0MmPqhal.js";import"./Chip-TPBsYk2z.js";import"./testIds-Dso8kRSM.js";import"./useMosaicTranslation-CCZzHAOs.js";import"./DataViewFilterDropdown-D8zsAVgS.js";import"./formControlState-D0eomaiA.js";import"./ButtonRow-5AdSMJlC.js";import"./Spinner-D0BoAIGc.js";import"./Typography-DhgnvJ03.js";import"./SubtitleText-DyATjJCY.js";import"./CheckboxList-DKZDPSzE.js";import"./Checkbox-CL37ymQB.js";import"./FormControlLabel-D-u7WmCt.js";import"./getThemeProps-DXoot0qR.js";import"./sizes-C6oSOElx.js";import"./RadioButton-D-T1QIB0.js";import"./StyledFormControl.styled-pfNyCqn0.js";import"./Toggle-BjiP2rWg.js";import"./formats-sBod7wm8.js";import"./Dialog-CyXhuKHl.js";import"./ChevronRight-BWmUBWCy.js";import"./TitleWrapper-CEGR9Ons.js";import"./TitleText-B4ASHioH.js";import"./Snackbar-Bslrzt6A.js";import"./Close-BnT1QDqQ.js";import"./ClickAwayListener-MSE9i65R.js";import"./MoreVert-cizeWCO8.js";import"./PageHeader-SBBckNNB.js";const bt={title:"Components/Form"},t=({showState:a})=>{const o=c(),{state:l,handleSubmit:d}=o;n.useEffect(()=>(document.body.style.margin="0px",()=>{document.body.style.margin=b}),[]);const u=n.useMemo(()=>{const i=[];for(let e=0;e<100;e++)i.push({name:`text${e}`,label:`Simple Text ${e}`,type:"text",instructionText:"testing",validators:[h]});return i},[]);return r.jsxs(r.Fragment,{children:[a&&r.jsx("pre",{children:JSON.stringify(l,null,"  ")}),r.jsx("div",{style:{height:"100vh"},children:r.jsx(f,{...o,buttons:x(d),fields:u,title:"Performance with submit"})})]})};t.args={showState:!1};t.argTypes={showState:{name:"Show state"}};var m,s,p;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`({
+  showState
+}: typeof PerformanceWithSubmit.args): ReactElement => {
+  const controller = useForm();
+  const {
+    state,
+    handleSubmit
+  } = controller;
+  useEffect(() => {
+    document.body.style.margin = "0px";
+    return () => {
+      document.body.style.margin = ORIGINAL_BODY_MARGIN;
+    };
+  }, []);
+  const fields = useMemo((): FieldDef[] => {
+    const hundredFields: FieldDef[] = [];
+    for (let i = 0; i < 100; i++) {
+      hundredFields.push({
+        name: \`text\${i}\`,
+        label: \`Simple Text \${i}\`,
+        type: "text",
+        instructionText: "testing",
+        validators: [required]
+      });
+    }
+    return hundredFields;
+  }, []);
+  return <>
+            {showState && <pre>{JSON.stringify(state, null, "  ")}</pre>}
+            <div style={{
+      height: "100vh"
+    }}>
+                <Form {...controller} buttons={renderButtons(handleSubmit)} fields={fields} title="Performance with submit" />
+            </div>
+        </>;
+}`,...(p=(s=t.parameters)==null?void 0:s.docs)==null?void 0:p.source}}};const St=["PerformanceWithSubmit"];export{t as PerformanceWithSubmit,St as __namedExportsOrder,bt as default};
