848732e9c871a0786742c4d92fb472885f4198c9
Owen Allen <owenallenaz@gmail.com>
2025-04-22 09:10:08 +0000

    Updates

---
+++ sb8/develop/assets/FormFieldRaw-BR0GN_8t.js
@@ -0,0 +1,34 @@
+import{j as t}from"./jsx-runtime-EKYJJIwR.js";import{useMDXComponents as e}from"./index-yrER70vX.js";import{M as n}from"./index-UpxAD4c2.js";import{s as p}from"./FormFieldRaw.stories-DDFtMlDr.js";import"./index-DDfD_3TG.js";import"./iframe-DoUpv1TX.js";import"../sb-preview/runtime.js";import"./index-DCMmMN8S.js";import"./index-ywIwhVjx.js";import"./tslib.es6-CvpyDmub.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-CUnS8Soe.js";import"./index-wdBq5kwF.js";import"./index-DrFu-skq.js";import"./styled-components.browser.esm-DXsh1Zr7.js";import"./emotion-is-prop-valid.cjs.default-BrZTFlsY.js";import"./Form-947MsihE.js";import"./theme-DWrYR5Gu.js";import"./SideNav-CvsHqpPT.js";import"./containerQuery-XietOkEy.js";import"./useToggle-BK_3QGB6.js";import"./Button-_6SRCSsN.js";import"./generateUtilityClasses-Dav9K5Jg.js";import"./ButtonBase-B3sVjXKq.js";import"./useIsFocusVisible-DxTeNLkh.js";import"./Popper-BFIFukJ-.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useTheme-BjWv6DhY.js";import"./index-BpD74PYt.js";import"./debounce-G1u4Gy2U.js";import"./DataViewPrimaryFilter.styled-DOEMDM8r.js";import"./createSvgIcon-ryN2wBPb.js";import"./createSvgIcon-Bc8E_7Df.js";import"./useId-C6ulm9BR.js";import"./useControlled-0MmPqhal.js";import"./Chip-TPBsYk2z.js";import"./testIds-Dso8kRSM.js";import"./useMosaicTranslation-CCZzHAOs.js";import"./DataViewFilterDropdown-D8zsAVgS.js";import"./formControlState-D0eomaiA.js";import"./ButtonRow-5AdSMJlC.js";import"./Spinner-D0BoAIGc.js";import"./Typography-DhgnvJ03.js";import"./SubtitleText-DyATjJCY.js";import"./CheckboxList-DKZDPSzE.js";import"./Checkbox-CL37ymQB.js";import"./FormControlLabel-D-u7WmCt.js";import"./getThemeProps-DXoot0qR.js";import"./sizes-C6oSOElx.js";import"./RadioButton-D-T1QIB0.js";import"./StyledFormControl.styled-pfNyCqn0.js";import"./Toggle-BjiP2rWg.js";import"./formats-sBod7wm8.js";import"./Dialog-CyXhuKHl.js";import"./ChevronRight-BWmUBWCy.js";import"./TitleWrapper-CEGR9Ons.js";import"./TitleText-B4ASHioH.js";import"./Snackbar-Bslrzt6A.js";import"./Close-BnT1QDqQ.js";import"./ClickAwayListener-MSE9i65R.js";import"./MoreVert-cizeWCO8.js";import"./PageHeader-SBBckNNB.js";import"./renderButtons-BmHfsf_z.js";import"./commonFieldControl-LQDYMh-C.js";function o(i){const r={a:"a",code:"code",h1:"h1",h3:"h3",p:"p",pre:"pre",strong:"strong",...e(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:p}),`
+`,t.jsx(r.h1,{id:"raw",children:"Raw"}),`
+`,t.jsxs(r.p,{children:["This field implements the ",t.jsx(r.a,{href:"#generic-field-props-fielddef",children:t.jsx(r.strong,{children:"FieldDef"})})," interface."]}),`
+`,t.jsxs(r.p,{children:["This type of field will render the corresponding ",t.jsx(r.code,{children:"value"})," as-is. It can be used to render anything that is a valid ",t.jsx(r.code,{children:"ReactNode"})," while maintaining the normal field display behaviour, like the label and field hint. It has no ",t.jsx(r.code,{children:"inputSettings"}),"."]}),`
+`,t.jsx(r.h3,{id:"how-to-use-in-a-form",children:"How to use in a form?"}),`
+`,t.jsx(r.pre,{children:t.jsx(r.code,{className:"language-ts",children:`\r
+function RawValue() {\r
+	return (\r
+		<RawValueWrapper>\r
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit.\r
+		</RawValueWrapper>\r
+	)\r
+}\r
+\r
+async function getFormValues() {\r
+	return {\r
+		raw: <RawValue />\r
+	}\r
+}\r
+\r
+const fields = useMemo(\r
+	() =>\r
+		[\r
+			//...other fields\r
+			{\r
+				//...all generic field props,\r
+				type: "raw",\r
+			},\r
+			//...other fields\r
+		],\r
+	[]\r
+);\r
+
+`})})]})}function Mt(i={}){const{wrapper:r}={...e(),...i.components};return r?t.jsx(r,{...i,children:t.jsx(o,{...i})}):o(i)}export{Mt as default};
---
+++ sb8/develop/assets/Toggle-C1W7T2vn.js
@@ -0,0 +1,12 @@
+import{j as o}from"./jsx-runtime-EKYJJIwR.js";import{useMDXComponents as r}from"./index-yrER70vX.js";import{M as n,C as s}from"./index-UpxAD4c2.js";import{s as l,P as m}from"./Toggle.stories-BxpE_vd9.js";import"./index-DDfD_3TG.js";import"./iframe-DoUpv1TX.js";import"../sb-preview/runtime.js";import"./index-DCMmMN8S.js";import"./index-ywIwhVjx.js";import"./tslib.es6-CvpyDmub.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-CUnS8Soe.js";import"./index-wdBq5kwF.js";import"./index-DrFu-skq.js";import"./Toggle-BjiP2rWg.js";import"./theme-DWrYR5Gu.js";import"./generateUtilityClasses-Dav9K5Jg.js";import"./FormControlLabel-D-u7WmCt.js";import"./formControlState-D0eomaiA.js";import"./useControlled-0MmPqhal.js";import"./ButtonBase-B3sVjXKq.js";import"./useIsFocusVisible-DxTeNLkh.js";import"./getThemeProps-DXoot0qR.js";import"./useTheme-BjWv6DhY.js";import"./StyledFormControl.styled-pfNyCqn0.js";import"./testIds-Dso8kRSM.js";function i(t){const e={a:"a",code:"code",h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...r(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(n,{of:l}),`
+`,o.jsx(e.h1,{id:"toggle",children:"Toggle"}),`
+`,o.jsxs(e.p,{children:["The ",o.jsx(e.code,{children:"Toggle"})," component is built over a wrapper for ",o.jsx(e.a,{href:"https://mui.com/material-ui/react-switch/#main-content",rel:"nofollow",children:"MUI Switch"})," but with SimpleView brand colors."]}),`
+`,o.jsxs(e.ul,{children:[`
+`,o.jsx(e.li,{children:"Toggle Switch allow users to switch between two possible states. They are commonly used to turn a specific setting on or off"}),`
+`,o.jsx(e.li,{children:"Toggles should be used to turn on or off a preference, notification, or feature"}),`
+`,o.jsx(e.li,{children:"Should be used when an instant response is required/desired"}),`
+`]}),`
+`,o.jsx(e.h2,{id:"props",children:"Props"}),`
+`,o.jsx(e.p,{children:o.jsx(e.a,{href:"https://github.com/simpleviewinc/sv-mosaic/blob/develop/containers/mosaic/src/components/Toggle/Toggle.tsx",rel:"nofollow",children:"https://github.com/simpleviewinc/sv-mosaic/blob/develop/containers/mosaic/src/components/Toggle/Toggle.tsx"})}),`
+`,o.jsx(e.h2,{id:"toggle-1",children:"Toggle"}),`
+`,o.jsx(s,{of:m})]})}function U(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(i,{...t})}):i(t)}export{U as default};
