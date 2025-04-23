d6322c7e3d57554fa3729df944fec0a189a8dea3
Owen Allen <owenallenaz@gmail.com>
2025-04-23 10:18:37 +0000

    Updates

---
+++ sb8/develop/assets/TimeField-D3ejxOfD.js
@@ -0,0 +1,23 @@
+import{j as t}from"./jsx-runtime-EKYJJIwR.js";import{useMDXComponents as e}from"./index-yrER70vX.js";import{M as m}from"./index-C9a7q3BQ.js";import{s as p}from"./TimeField.stories-BhLGq5iT.js";import"./index-DDfD_3TG.js";import"./iframe-v5zmNBDu.js";import"../sb-preview/runtime.js";import"./index-DCMmMN8S.js";import"./index-ywIwhVjx.js";import"./tslib.es6-CvpyDmub.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-CUnS8Soe.js";import"./index-wdBq5kwF.js";import"./index-DrFu-skq.js";import"./Form-GzCS7kAd.js";import"./theme-DWrYR5Gu.js";import"./SideNav-CvsHqpPT.js";import"./containerQuery-XietOkEy.js";import"./useToggle-BK_3QGB6.js";import"./Button-_6SRCSsN.js";import"./generateUtilityClasses-Dav9K5Jg.js";import"./ButtonBase-B3sVjXKq.js";import"./useIsFocusVisible-DxTeNLkh.js";import"./Popper-BFIFukJ-.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useTheme-BjWv6DhY.js";import"./index-BpD74PYt.js";import"./debounce-G1u4Gy2U.js";import"./DataViewPrimaryFilter.styled-DOEMDM8r.js";import"./createSvgIcon-ryN2wBPb.js";import"./createSvgIcon-Bc8E_7Df.js";import"./useId-C6ulm9BR.js";import"./useControlled-0MmPqhal.js";import"./Chip-TPBsYk2z.js";import"./testIds-Dso8kRSM.js";import"./useMosaicTranslation-CCZzHAOs.js";import"./DataViewFilterDropdown-D8zsAVgS.js";import"./formControlState-D0eomaiA.js";import"./ButtonRow-5AdSMJlC.js";import"./Spinner-D0BoAIGc.js";import"./Typography-DhgnvJ03.js";import"./SubtitleText-DyATjJCY.js";import"./CheckboxList-DKZDPSzE.js";import"./Checkbox-CL37ymQB.js";import"./FormControlLabel-D-u7WmCt.js";import"./getThemeProps-DXoot0qR.js";import"./sizes-C6oSOElx.js";import"./RadioButton-D-T1QIB0.js";import"./StyledFormControl.styled-pfNyCqn0.js";import"./Toggle-BjiP2rWg.js";import"./formats-sBod7wm8.js";import"./Dialog-CyXhuKHl.js";import"./ChevronRight-BWmUBWCy.js";import"./TitleWrapper-CEGR9Ons.js";import"./TitleText-B4ASHioH.js";import"./Snackbar-Bslrzt6A.js";import"./Close-BnT1QDqQ.js";import"./ClickAwayListener-MSE9i65R.js";import"./MoreVert-cizeWCO8.js";import"./PageHeader-SBBckNNB.js";import"./renderButtons-BmHfsf_z.js";import"./commonFieldControl-LQDYMh-C.js";import"./parseDateControl-CsM_YeqS.js";function o(i){const r={a:"a",code:"code",h1:"h1",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...e(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{of:p}),`
+`,t.jsx(r.h1,{id:"time",children:"Time"}),`
+`,t.jsxs(r.p,{children:["This field implements the ",t.jsx(r.a,{href:"#generic-field-props-fielddef",children:t.jsx(r.strong,{children:"FieldDef"})})," interface."]}),`
+`,t.jsxs(r.ul,{children:[`
+`,t.jsx(r.li,{children:"It allows the user to view and pick a time from a calendar widget or manually type the time in the text field."}),`
+`,t.jsx(r.li,{children:t.jsx(r.a,{href:"/?path=/story/formfields-formfieldtimefield--playground",children:t.jsx(r.strong,{children:"Playground"})})}),`
+`,t.jsxs(r.li,{children:["Data: ",t.jsx(r.code,{children:"string"})," - Time in ",t.jsx(r.code,{children:"HH:MM"})," 24 hour format."]}),`
+`,t.jsx(r.li,{children:"This field accepts no inputSettings"}),`
+`]}),`
+`,t.jsx(r.h3,{id:"how-to-use-in-a-form",children:"How to use in a form?"}),`
+`,t.jsx(r.pre,{children:t.jsx(r.code,{className:"language-ts",children:`const fields = useMemo(\r
+	() =>\r
+		[\r
+			//...other fields\r
+			{\r
+				//...all generic field props,\r
+				type: "time",\r
+			},\r
+			//...other fields\r
+		],\r
+	[]\r
+);
+`})})]})}function Mt(i={}){const{wrapper:r}={...e(),...i.components};return r?t.jsx(r,{...i,children:t.jsx(o,{...i})}):o(i)}export{Mt as default};
---
+++ sb8/develop/assets/transform_join-Dyuk6vgH.js
@@ -0,0 +1,17 @@
+import{j as n}from"./jsx-runtime-EKYJJIwR.js";import{useMDXComponents as i}from"./index-yrER70vX.js";import{M as s}from"./index-C9a7q3BQ.js";import"./index-DDfD_3TG.js";import"./iframe-v5zmNBDu.js";import"../sb-preview/runtime.js";import"./index-DCMmMN8S.js";import"./index-ywIwhVjx.js";import"./tslib.es6-CvpyDmub.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-CUnS8Soe.js";import"./index-wdBq5kwF.js";import"./index-DrFu-skq.js";function e(o){const r={code:"code",h1:"h1",h4:"h4",li:"li",p:"p",pre:"pre",ul:"ul",...i(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{title:"Utils/Transforms/Join"}),`
+`,n.jsx(r.h1,{id:"transform_join",children:"transform_join"}),`
+`,n.jsx(r.p,{children:"Converts array of values into comma separated string."}),`
+`,n.jsx(r.h4,{id:"conversions",children:"Conversions"}),`
+`,n.jsxs(r.ul,{children:[`
+`,n.jsxs(r.li,{children:[n.jsx(r.code,{children:'["foo", "bar", "baz"]'})," => ",n.jsx(r.code,{children:'"Foo, bar, baz"'})]}),`
+`,n.jsxs(r.li,{children:[n.jsx(r.code,{children:"[]"})," => ",n.jsx(r.code,{children:'""'})]}),`
+`]}),`
+`,n.jsx(r.h4,{id:"args",children:"Args"}),`
+`,n.jsx(r.p,{children:"None"}),`
+`,n.jsx(r.h4,{id:"example",children:"Example"}),`
+`,n.jsx(r.pre,{children:n.jsx(r.code,{className:"language-js",children:`import { transform_join } from "@simpleview/sv-mosaic"\r
+...\r
+transforms : [\r
+    transform_join()\r
+]
+`})})]})}function _(o={}){const{wrapper:r}={...i(),...o.components};return r?n.jsx(r,{...o,children:n.jsx(e,{...o})}):e(o)}export{_ as default};
