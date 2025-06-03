5783e9f44bf9438bcbb23124e1d8c325b0ea568a
Owen Allen <owenallenaz@gmail.com>
2025-06-03 16:45:36 +0000

    Updates

---
+++ sb8/qa/assets/QuickSubmitForm.stories-D0bWnRnn.js
@@ -0,0 +1,41 @@
+import{j as i}from"./jsx-runtime-EKYJJIwR.js";import{r as o}from"./index-D0AnReJb.js";import{u as d,F as b}from"./index-ChNyYHZm.js";import{O as f}from"./dom-BR2PSRFh.js";import"./styled-components.browser.esm-B06rF3P8.js";import"./sizes-HwDk6ODv.js";import"./testIds-Dso8kRSM.js";import"./TitleWrapper-DeOGO1IS.js";import"./Button-mgz2aTYm.js";import"./DefaultPropsProvider-BwNoCFE_.js";import"./generateUtilityClasses-42_at76R.js";import"./ButtonBase-EThHB291.js";import"./useIsFocusVisible-CvEUtp4E.js";import"./index-CtiN8CS7.js";import"./Popper-BpP7Dmyd.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useTheme-Dt0_M2VT.js";import"./useThemeWithoutDefault-ML7LN7QY.js";import"./index-Bqzh3Cp6.js";import"./index-SI8xS9YR.js";import"./useToggle-CTbnrs6g.js";import"./containerQuery-BgNGRgoI.js";import"./createSvgIcon-Bdc0w2F6.js";import"./createSvgIcon-CmcFkBiI.js";import"./useId-BgzzDjMB.js";import"./useControlled-DuXZYv4M.js";import"./Typography-CaKiXxBh.js";import"./DisplayText-p-dXd17y.js";import"./ButtonRow-gAFOvWL6.js";import"./index-CWM90NNz.js";import"./debounce-D-BUC-g1.js";import"./DataViewPrimaryFilter.styled-B6PsGsAS.js";import"./Chip-BiF3n3mk.js";import"./useMosaicTranslation-CoWLI8L7.js";import"./FormFieldText.styled-CpwAbK4m.js";import"./formControlState-DKt-4xzo.js";import"./Spinner-BIub0DbD.js";import"./Text-SN-dsdu2.js";import"./CheckboxList-BXiGiY-c.js";import"./Checkbox-CABnih5k.js";import"./FormControlLabel-FaPuCwUU.js";import"./MoreVert-BxoNaVuW.js";import"./ChevronRight-C-yhpJTc.js";import"./PageHeader-DYkch7zz.js";import"./formats-CMvQHWsT.js";import"./RadioButton-peGC_BMl.js";import"./StyledFormControl.styled-CW3JaFWW.js";import"./Toggle-IqG9sXDk.js";import"./Dialog-kc3-0tkB.js";import"./ThemeProvider-CyphNCQQ.js";import"./ChipList-D5NUhJyC.js";import"./SideNav-CRhfVc2i.js";import"./Snackbar-DXnnQTTm.js";import"./Close-CGtmtFuS.js";import"./ClickAwayListener-Dkkr5f_-.js";const gt={title:"Components/Form"},y=[{title:"Other",fields:[[["age"]]]},{title:"Name",fields:[[["lastName"]],[["firstName"]]]}],t=({showSections:s})=>{const e=d(),{handleSubmit:a}=e;o.useEffect(()=>(document.body.style.margin="0px",()=>{document.body.style.margin=f}),[]);const p=o.useMemo(()=>[{name:"firstName",label:"First Name",type:"text"},{name:"lastName",label:"Last Name",type:"text"},{name:"age",label:"Age",type:"text"}],[]),l=o.useMemo(()=>[{label:"Save",color:"yellow",variant:"contained",type:"submit"}],[]),u=a(c=>{alert("Form submitted with the following data: "+JSON.stringify(c,null," "))});return i.jsx("div",{style:{height:"100vh"},children:i.jsx(b,{...e,buttons:l,title:"Quick Submit",fields:p,sections:s?y:void 0,onSubmit:u,autoFocus:!0})})};t.args={showSections:!1};t.argTypes={showSections:{name:"Show Sections"}};var r,m,n;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`({
+  showSections
+}: typeof QuickSubmit.args): ReactElement => {
+  const controller = useForm();
+  const {
+    handleSubmit
+  } = controller;
+  useEffect(() => {
+    document.body.style.margin = "0px";
+    return () => {
+      document.body.style.margin = ORIGINAL_BODY_MARGIN;
+    };
+  }, []);
+  const fields = useMemo((): FieldDef[] => [{
+    name: "firstName",
+    label: "First Name",
+    type: "text"
+  }, {
+    name: "lastName",
+    label: "Last Name",
+    type: "text"
+  }, {
+    name: "age",
+    label: "Age",
+    type: "text"
+  }], []);
+  const buttons = useMemo<ButtonProps[]>(() => [{
+    label: "Save",
+    color: "yellow",
+    variant: "contained",
+    type: "submit"
+  }], []);
+  const onSubmit = handleSubmit(data => {
+    alert("Form submitted with the following data: " + JSON.stringify(data, null, " "));
+  });
+  return <div style={{
+    height: "100vh"
+  }}>
+            <Form {...controller} buttons={buttons} title="Quick Submit" fields={fields} sections={showSections ? sections : undefined} onSubmit={onSubmit} autoFocus />
+        </div>;
+}`,...(n=(m=t.parameters)==null?void 0:m.docs)==null?void 0:n.source}}};const Nt=["QuickSubmit"];export{t as QuickSubmit,Nt as __namedExportsOrder,gt as default};
---
+++ sb8/qa/assets/Toggle-DmvIMZYv.js
@@ -0,0 +1,12 @@
+import{j as o}from"./jsx-runtime-EKYJJIwR.js";import{useMDXComponents as r}from"./index-BwDDMXeo.js";import{M as n,C as s}from"./index-BmZiBRjj.js";import{s as m,P as p}from"./Toggle.stories-D3HSCkui.js";import"./index-D0AnReJb.js";import"./iframe-DikCakFS.js";import"../sb-preview/runtime.js";import"./index-Bqzh3Cp6.js";import"./index-SI8xS9YR.js";import"./tslib.es6-CvpyDmub.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-DoexKoos.js";import"./_baseForOwn-YgwlavHx.js";import"./index-QVqIEGA1.js";import"./_arrayIncludesWith-XJRwRZaz.js";import"./index-DrFu-skq.js";import"./Toggle-IqG9sXDk.js";import"./styled-components.browser.esm-B06rF3P8.js";import"./sizes-HwDk6ODv.js";import"./DefaultPropsProvider-BwNoCFE_.js";import"./generateUtilityClasses-42_at76R.js";import"./FormControlLabel-FaPuCwUU.js";import"./formControlState-DKt-4xzo.js";import"./useControlled-DuXZYv4M.js";import"./ButtonBase-EThHB291.js";import"./useIsFocusVisible-CvEUtp4E.js";import"./useTheme-Dt0_M2VT.js";import"./useThemeWithoutDefault-ML7LN7QY.js";import"./StyledFormControl.styled-CW3JaFWW.js";import"./testIds-Dso8kRSM.js";function i(t){const e={a:"a",code:"code",h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...r(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(n,{of:m}),`
+`,o.jsx(e.h1,{id:"toggle",children:"Toggle"}),`
+`,o.jsxs(e.p,{children:["The ",o.jsx(e.code,{children:"Toggle"})," component is built over a wrapper for ",o.jsx(e.a,{href:"https://mui.com/material-ui/react-switch/#main-content",rel:"nofollow",children:"MUI Switch"})," but with SimpleView brand colors."]}),`
+`,o.jsxs(e.ul,{children:[`
+`,o.jsx(e.li,{children:"Toggle Switch allow users to switch between two possible states. They are commonly used to turn a specific setting on or off"}),`
+`,o.jsx(e.li,{children:"Toggles should be used to turn on or off a preference, notification, or feature"}),`
+`,o.jsx(e.li,{children:"Should be used when an instant response is required/desired"}),`
+`]}),`
+`,o.jsx(e.h2,{id:"props",children:"Props"}),`
+`,o.jsx(e.p,{children:o.jsx(e.a,{href:"https://github.com/simpleviewinc/sv-mosaic/blob/develop/containers/mosaic/src/components/Toggle/Toggle.tsx",rel:"nofollow",children:"https://github.com/simpleviewinc/sv-mosaic/blob/develop/containers/mosaic/src/components/Toggle/Toggle.tsx"})}),`
+`,o.jsx(e.h2,{id:"toggle-1",children:"Toggle"}),`
+`,o.jsx(s,{of:p})]})}function A(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(i,{...t})}):i(t)}export{A as default};
