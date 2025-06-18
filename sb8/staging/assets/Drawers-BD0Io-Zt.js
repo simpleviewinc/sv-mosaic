578fa29c5534e2fd76ab11bdaafeac694e6436bb
Owen Allen <owenallenaz@gmail.com>
2025-06-18 11:10:11 +0000

    Updates

---
+++ sb8/staging/assets/Drawers-BD0Io-Zt.js
@@ -0,0 +1,109 @@
+import{j as t}from"./jsx-runtime-EKYJJIwR.js";import{useMDXComponents as o}from"./index-BwDDMXeo.js";import{M as a,C as i}from"./index-CgGh8EVu.js";import{s,P as p}from"./Drawers.stories-CteGk-sf.js";import"./index-D0AnReJb.js";import"./iframe-CkBUjnQP.js";import"../sb-preview/runtime.js";import"./index-Bqzh3Cp6.js";import"./index-SI8xS9YR.js";import"./tslib.es6-CvpyDmub.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-DoexKoos.js";import"./_baseForOwn-YgwlavHx.js";import"./index-QVqIEGA1.js";import"./_arrayIncludesWith-XJRwRZaz.js";import"./index-DrFu-skq.js";import"./Drawers-CuiQUxnf.js";import"./styled-components.browser.esm-B06rF3P8.js";import"./sizes-HwDk6ODv.js";import"./testIds-Dso8kRSM.js";import"./ChevronRight-C-yhpJTc.js";import"./DefaultPropsProvider-BwNoCFE_.js";import"./createSvgIcon-Bdc0w2F6.js";import"./createSvgIcon-CmcFkBiI.js";import"./generateUtilityClasses-42_at76R.js";import"./createChainedFunction-BO_9K8Jh.js";import"./ownerWindow-DvT1GKkC.js";import"./useIsFocusVisible-CvEUtp4E.js";import"./useId-BgzzDjMB.js";import"./useControlled-DuXZYv4M.js";import"./index-CtiN8CS7.js";import"./Popper-BpP7Dmyd.js";import"./useTheme-Dt0_M2VT.js";import"./useThemeWithoutDefault-ML7LN7QY.js";import"./index-Cc5HBuF_.js";import"./TitleWrapper-ePYozolN.js";import"./Button-mgz2aTYm.js";import"./ButtonBase-EThHB291.js";import"./useToggle-CTbnrs6g.js";import"./containerQuery-BgNGRgoI.js";import"./Typography-DKFSDQw0.js";import"./DisplayText-CNJUHUbG.js";import"./ButtonRow-gAFOvWL6.js";import"./index-3jzVDf5u.js";import"./debounce-D-BUC-g1.js";import"./ExpandMore-DyHVniBt.js";import"./Chip-BiF3n3mk.js";import"./useMosaicTranslation-CoWLI8L7.js";import"./FormFieldText.styled-CpwAbK4m.js";import"./formControlState-DKt-4xzo.js";import"./Spinner-BIub0DbD.js";import"./Text-9y4gMspU.js";import"./CheckboxList-BXiGiY-c.js";import"./Checkbox-CABnih5k.js";import"./FormControlLabel-FaPuCwUU.js";import"./DataViewPrimaryFilter.styled-8yAJNZFq.js";import"./MoreVert-BxoNaVuW.js";import"./PageHeader-u9qr2_b1.js";import"./formats-CMvQHWsT.js";import"./ChipList-BHKNfYFT.js";import"./RadioButton-peGC_BMl.js";import"./StyledFormControl.styled-CW3JaFWW.js";import"./Toggle-IqG9sXDk.js";import"./Dialog-BFp4KNLR.js";import"./ThemeProvider-CyphNCQQ.js";import"./SideNav-CRhfVc2i.js";import"./Snackbar-DXnnQTTm.js";import"./Close-CGtmtFuS.js";import"./ClickAwayListener-Dkkr5f_-.js";function r(n){const e={a:"a",code:"code",h1:"h1",h2:"h2",li:"li",p:"p",pre:"pre",ul:"ul",...o(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{of:s}),`
+`,t.jsx(e.h1,{id:"drawers",children:"Drawers"}),`
+`,t.jsxs(e.p,{children:["The ",t.jsx(e.code,{children:"Drawers"})," component is wrapper that generates several ",t.jsx(e.a,{href:"https://mui.com/material-ui/react-drawer/#main-content",rel:"nofollow",children:"Material-UI Drawers"})," but with SimpleView brand colors."]}),`
+`,t.jsx(e.p,{children:`The component does not provide a mechanism for navigating between drawers, it only helps stacking one on top of another.
+All the navgitation mechanism should be defined on the parent app creating the Drawers component.`}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsx(e.li,{children:"While animating, the UI is locked to prevent race conditions with opening/closing new drawers."}),`
+`,t.jsx(e.li,{children:"Can open infinite amount of stacked drawers."}),`
+`,t.jsx(e.li,{children:"When a drawer is opened, the opener passes callbacks to it to execute when save() is clicked."}),`
+`,t.jsx(e.li,{children:"Animations custom built via keyframes rather than using material-ui drawer defaults to handle stacking cleanly."}),`
+`,t.jsx(e.li,{children:"App passes to Drawers a JSON Def for each drawer and passes a render prop. This separates the concerns of animating and displaying the drawer (Mosaic) from rendering the content of the drawer (App)."}),`
+`]}),`
+`,t.jsx(e.h2,{id:"props",children:"Props"}),`
+`,t.jsx(e.p,{children:t.jsx(e.a,{href:"https://github.com/simpleviewinc/sv-mosaic/blob/develop/containers/mosaic/src/components/Drawers/DrawersTypes.ts",rel:"nofollow",children:"https://github.com/simpleviewinc/sv-mosaic/blob/develop/containers/mosaic/src/components/Drawers/DrawersTypes.ts"})}),`
+`,t.jsx(e.h2,{id:"usage",children:"Usage"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-ts",children:`const App = (props) => {
+	const [state, setState] = useState<AppState>({
+		content: {},
+		drawers: [],
+	});
+
+	const addDrawer = useCallback((drawerDef: DrawerDef) => {
+		setState((state) => ({
+			...state,
+			drawers: [...state.drawers, drawerDef],
+		}));
+	}, []);
+
+	const removeDrawer = useCallback(() => {
+		setState((state) => ({
+			...state,
+			drawers: [...state.drawers.slice(0, -1)],
+		}));
+	}, []);
+
+	const appContext = useMemo(
+		() => ({
+			addDrawer,
+			removeDrawer,
+		}),
+		[addDrawer, removeDrawer]
+	);
+
+	return (
+		<AppContext.Provider value={appContext}>
+			<div className="App">
+				<h1>Drawer Demo</h1>
+				<p>From Form: {JSON.stringify(state.content)}</p>
+				<button
+					onClick={() =>
+						addDrawer({
+							config: {
+								type: "form",
+								title: "New Form",
+								fields: [
+									{
+										name: "foo",
+										label: "Foo",
+										type: "text",
+									},
+									{
+										name: "bar",
+										label: "Bar",
+										type: "text",
+									},
+									{
+										name: "baz",
+										label: "Baz",
+										type: "text",
+									},
+									{
+										name: "from_parent",
+										label: "From Parent",
+										type: "text",
+									},
+								],
+							},
+							callbacks: {
+								save: (data) => {
+									setState((state) => ({
+										...state,
+										content: data,
+									}));
+
+									removeDrawer();
+								},
+							},
+						})
+					}
+				>
+					Add Form
+				</button>
+
+				<Drawers drawers={state.drawers}>
+					{(drawerDef) => {
+						return (
+							<Page
+								config={drawerDef.config}
+								callbacks={drawerDef.callbacks ?? {}}
+							/>
+						);
+					}}
+				</Drawers>
+			</div>
+		</AppContext.Provider>
+	);
+}
+`})}),`
+`,t.jsx(i,{of:p})]})}function At(n={}){const{wrapper:e}={...o(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(r,{...n})}):r(n)}export{At as default};
---
+++ sb8/staging/assets/FormFieldTextEditor-Sljhz1Jq.js
@@ -0,0 +1,72 @@
+import{j as t}from"./jsx-runtime-EKYJJIwR.js";import{useMDXComponents as n}from"./index-BwDDMXeo.js";import{M as r}from"./index-CgGh8EVu.js";import{s}from"./FormFieldTextEditor.stories-D39i3wJ_.js";import"./index-D0AnReJb.js";import"./iframe-CkBUjnQP.js";import"../sb-preview/runtime.js";import"./index-Bqzh3Cp6.js";import"./index-SI8xS9YR.js";import"./tslib.es6-CvpyDmub.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-DoexKoos.js";import"./_baseForOwn-YgwlavHx.js";import"./index-QVqIEGA1.js";import"./_arrayIncludesWith-XJRwRZaz.js";import"./index-DrFu-skq.js";import"./index-Cc5HBuF_.js";import"./styled-components.browser.esm-B06rF3P8.js";import"./sizes-HwDk6ODv.js";import"./testIds-Dso8kRSM.js";import"./TitleWrapper-ePYozolN.js";import"./Button-mgz2aTYm.js";import"./DefaultPropsProvider-BwNoCFE_.js";import"./generateUtilityClasses-42_at76R.js";import"./ButtonBase-EThHB291.js";import"./useIsFocusVisible-CvEUtp4E.js";import"./index-CtiN8CS7.js";import"./Popper-BpP7Dmyd.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useTheme-Dt0_M2VT.js";import"./useThemeWithoutDefault-ML7LN7QY.js";import"./useToggle-CTbnrs6g.js";import"./containerQuery-BgNGRgoI.js";import"./createSvgIcon-Bdc0w2F6.js";import"./createSvgIcon-CmcFkBiI.js";import"./useId-BgzzDjMB.js";import"./useControlled-DuXZYv4M.js";import"./Typography-DKFSDQw0.js";import"./DisplayText-CNJUHUbG.js";import"./ButtonRow-gAFOvWL6.js";import"./index-3jzVDf5u.js";import"./debounce-D-BUC-g1.js";import"./ExpandMore-DyHVniBt.js";import"./Chip-BiF3n3mk.js";import"./useMosaicTranslation-CoWLI8L7.js";import"./FormFieldText.styled-CpwAbK4m.js";import"./formControlState-DKt-4xzo.js";import"./Spinner-BIub0DbD.js";import"./Text-9y4gMspU.js";import"./CheckboxList-BXiGiY-c.js";import"./Checkbox-CABnih5k.js";import"./FormControlLabel-FaPuCwUU.js";import"./DataViewPrimaryFilter.styled-8yAJNZFq.js";import"./MoreVert-BxoNaVuW.js";import"./ChevronRight-C-yhpJTc.js";import"./PageHeader-u9qr2_b1.js";import"./formats-CMvQHWsT.js";import"./ChipList-BHKNfYFT.js";import"./RadioButton-peGC_BMl.js";import"./StyledFormControl.styled-CW3JaFWW.js";import"./Toggle-IqG9sXDk.js";import"./Dialog-BFp4KNLR.js";import"./ThemeProvider-CyphNCQQ.js";import"./SideNav-CRhfVc2i.js";import"./Snackbar-DXnnQTTm.js";import"./Close-CGtmtFuS.js";import"./ClickAwayListener-Dkkr5f_-.js";import"./renderButtons-BmHfsf_z.js";import"./commonFieldControl-LQDYMh-C.js";import"./styled-components.browser.esm-njFAddlw.js";import"./emotion-is-prop-valid.cjs.default-BrZTFlsY.js";import"./styled-BWOE7yIf.js";import"./createSvgIcon-CFLdSkGK.js";import"./Grid-BbIy_5I8.js";import"./useThemeWithoutDefault-B0y5BOgo.js";import"./Typography-DFZHALTz.js";import"./TridentIcon-DK3OMhMM.js";function i(o){const e={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...n(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:s,name:"Docs - Tiptap"}),`
+`,t.jsx(e.h2,{id:"text-editor---tiptap",children:"Text Editor - Tiptap"}),`
+`,t.jsxs(e.p,{children:["The integration of this field into the form system is not complete. It is being included alongside the existing Jodit editor to ensure correct alignment between the two fields before replacing it completely. To use it, import the ",t.jsx(e.code,{children:"FormFieldTextEditorTipTapFieldType"})," and pass that as the field type:"]}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-ts",children:`import { FormFieldTextEditorTipTapFieldType } from "#mosaic/components";
+
+const fields: FieldDef = [
+	{
+		name: "My Text Editor",
+		type: FormFieldTextEditorTipTapFieldType,
+		// ...
+	}
+]
+
+`})}),`
+`,t.jsxs(e.p,{children:["This field implements the ",t.jsx(e.a,{href:"#generic-field-props-fielddef",children:t.jsx(e.strong,{children:"FieldDef"})})," interface."]}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsx(e.li,{children:"The text editor is a text area with added capabilities for use in various publishers, allowing the users to format their input in a text area."}),`
+`,t.jsxs(e.li,{children:["This is an implementation using the ",t.jsx(e.a,{href:"https://www.npmjs.com/package/@tiptap/react",rel:"nofollow",children:"Tiptap"})," package."]}),`
+`,t.jsx(e.li,{children:t.jsx(e.a,{href:"/story/formfields-formfieldtexteditor--playground",children:t.jsx(e.strong,{children:"Playground"})})}),`
+`,t.jsxs(e.li,{children:["Data: ",t.jsx(e.code,{children:"string"})," - HTML-based value of the input typed."]}),`
+`,t.jsx(e.li,{children:"inputSettings:"}),`
+`]}),`
+`,t.jsxs(e.table,{children:[t.jsx(e.thead,{children:t.jsxs(e.tr,{children:[t.jsx(e.th,{children:"Name"}),t.jsx(e.th,{children:"Type"}),t.jsx(e.th,{children:"Description"})]})}),t.jsxs(e.tbody,{children:[t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.code,{children:"controls"})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"ControlsConfig"})}),t.jsxs(e.td,{children:["optional - Defines the control configuration. See ",t.jsx(e.a,{href:"#controls",children:"controls"})," below for more information."]})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.code,{children:"extensions"})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"Extensions"})}),t.jsxs(e.td,{children:["optional - The extensions to be loaded by the underlying Tiptap library. This property will override default extensions defined by Mosaic. Those default extensions are listed ",t.jsx(e.a,{href:"https://github.com/simpleview/sv-mosaic/tree/develop/containers/mosaic/src/components/Field/FormFieldTextEditor/Extensions/defaultExtensions.ts",rel:"nofollow",children:"here"}),". See the Tiptap ",t.jsx(e.a,{href:"https://tiptap.dev/docs/editor/extensions/custom-extensions",rel:"nofollow",children:"documentation"})," for more information on creating extensions."]})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.code,{children:"onImage"})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"(params: TextEditorOnLinkParams) => void"})}),t.jsxs(e.td,{children:["A function to be invoked when the image control is clicked. It will recieve a single parameter which is an object containing ",t.jsx(e.code,{children:"updateImage"}),", ",t.jsx(e.code,{children:"alt"})," and ",t.jsx(e.code,{children:"src"}),". ",t.jsx(e.code,{children:"updateImage"})," should be subsequently called to add/update the image. ",t.jsx(e.code,{children:"alt"})," and ",t.jsx(e.code,{children:"src"})," will correspond to the current image if applicable."]})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.code,{children:"onLink"})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"(params: TextEditorOnLinkParams) => void"})}),t.jsxs(e.td,{children:["A function to be invoked when the link control is clicked. It will recieve a single parameter which is an object containing ",t.jsx(e.code,{children:"updateLink"}),", ",t.jsx(e.code,{children:"href"})," and ",t.jsx(e.code,{children:"newTab"}),". ",t.jsx(e.code,{children:"updateLink"})," should be subsequently called to add/update the link. ",t.jsx(e.code,{children:"href"})," and ",t.jsx(e.code,{children:"newTab"})," will correspond to the current image if applicable."]})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.code,{children:"maxCharacters"})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"number"})}),t.jsx(e.td,{children:"optional - Defines the limit of characters that are allow to type in the input element."})]})]})]}),`
+`,t.jsx(e.h3,{id:"controls",children:"Controls"}),`
+`,t.jsxs(e.p,{children:["The ",t.jsx(e.code,{children:"controls"})," input settings accepts a number of ways to define the controls that appear in the text editor's toolbar. The property is an array of groups, each group is an array of controls. Each item of the group can optionally be a third nested array which will produce a menu button which, when clicked, will reveal the controls listed within. By default, the controls configuration is as follows:"]}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-ts",children:`const defaultControls: ControlsConfig = [
+	// Control group containing a single control
+	["headings"],
+	// Control group containing 2 controls and a menu which contains 5 more controls.
+	["bold", "italic", ["underline", "strike", "superscript", "subscript", "clear"]],
+	["bulletList", "orderedList"],
+	["alignLeft", "alignCenter", ["alignRight", "alignJustify"]],
+	["link", ["image", "codeBlock", "blockquote"]],
+	["undo", "redo"],
+];
+`})}),`
+`,t.jsxs(e.p,{children:["The control identifiers shown in the example above map to a control definition object, but you can also pass that definition object instead of the identifying string for more granular control. This example uses the ",t.jsx(e.code,{children:"controlUndo"})," and ",t.jsx(e.code,{children:"controlRedo"})," object available from Mosaic with a slight modification:"]}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-ts",children:`import { controlUndo, controlRedo } from "@sv-mosaic/mosaic/components/Field/FormFieldTextEditor";
+
+const controls: ControlsConfig = [
+	// Other controls...
+	[
+		{
+			...controlUndo,
+			label: "Roll it back",
+		},
+		{
+			...controlRedo,
+			label: "Restore it",
+		}
+	],
+];
+`})}),`
+`,t.jsx(e.p,{children:"But you could also define the entire control object to implement your own functionality:"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-ts",children:`import { controlUndo, controlRedo } from "@simpleview/mosaic/components/Field/FormFieldTextEditor";
+
+const controls: ControlsConfig = [
+	// Other controls...
+	[
+		{
+			name: "selectAll",
+			label: "Select All",
+			cmd: ({ editor }) => editor.chain().selectAll().run(),
+			Icon: SelectAllIcon,
+			shortcut: {
+				mac: "Cmd+A",
+				pc: "Ctrl+A",
+			},
+		}
+	],
+];
+`})})]})}function Rt(o={}){const{wrapper:e}={...n(),...o.components};return e?t.jsx(e,{...o,children:t.jsx(i,{...o})}):i(o)}export{Rt as default};
