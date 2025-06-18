2ac4941893a8a4a585fde6277285aae72eca1281
Owen Allen <owenallenaz@gmail.com>
2025-06-18 11:03:00 +0000

    Updates

---
+++ sb8/qa/assets/QuickSubmitForm.stories-BMyeFQ7U.js
@@ -0,0 +1,41 @@
+import{j as i}from"./jsx-runtime-EKYJJIwR.js";import{r as o}from"./index-D0AnReJb.js";import{u as d,F as b}from"./index-Cc5HBuF_.js";import{O as f}from"./dom-BR2PSRFh.js";import"./styled-components.browser.esm-B06rF3P8.js";import"./sizes-HwDk6ODv.js";import"./testIds-Dso8kRSM.js";import"./TitleWrapper-ePYozolN.js";import"./Button-mgz2aTYm.js";import"./DefaultPropsProvider-BwNoCFE_.js";import"./generateUtilityClasses-42_at76R.js";import"./ButtonBase-EThHB291.js";import"./useIsFocusVisible-CvEUtp4E.js";import"./index-CtiN8CS7.js";import"./Popper-BpP7Dmyd.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useTheme-Dt0_M2VT.js";import"./useThemeWithoutDefault-ML7LN7QY.js";import"./index-Bqzh3Cp6.js";import"./index-SI8xS9YR.js";import"./useToggle-CTbnrs6g.js";import"./containerQuery-BgNGRgoI.js";import"./createSvgIcon-Bdc0w2F6.js";import"./createSvgIcon-CmcFkBiI.js";import"./useId-BgzzDjMB.js";import"./useControlled-DuXZYv4M.js";import"./Typography-DKFSDQw0.js";import"./DisplayText-CNJUHUbG.js";import"./ButtonRow-gAFOvWL6.js";import"./index-3jzVDf5u.js";import"./debounce-D-BUC-g1.js";import"./ExpandMore-DyHVniBt.js";import"./Chip-BiF3n3mk.js";import"./useMosaicTranslation-CoWLI8L7.js";import"./FormFieldText.styled-CpwAbK4m.js";import"./formControlState-DKt-4xzo.js";import"./Spinner-BIub0DbD.js";import"./Text-9y4gMspU.js";import"./CheckboxList-BXiGiY-c.js";import"./Checkbox-CABnih5k.js";import"./FormControlLabel-FaPuCwUU.js";import"./DataViewPrimaryFilter.styled-8yAJNZFq.js";import"./MoreVert-BxoNaVuW.js";import"./ChevronRight-C-yhpJTc.js";import"./PageHeader-u9qr2_b1.js";import"./formats-CMvQHWsT.js";import"./ChipList-BHKNfYFT.js";import"./RadioButton-peGC_BMl.js";import"./StyledFormControl.styled-CW3JaFWW.js";import"./Toggle-IqG9sXDk.js";import"./Dialog-BFp4KNLR.js";import"./ThemeProvider-CyphNCQQ.js";import"./SideNav-CRhfVc2i.js";import"./Snackbar-DXnnQTTm.js";import"./Close-CGtmtFuS.js";import"./ClickAwayListener-Dkkr5f_-.js";const Nt={title:"Components/Form"},y=[{title:"Other",fields:[[["age"]]]},{title:"Name",fields:[[["lastName"]],[["firstName"]]]}],t=({showSections:s})=>{const e=d(),{handleSubmit:a}=e;o.useEffect(()=>(document.body.style.margin="0px",()=>{document.body.style.margin=f}),[]);const p=o.useMemo(()=>[{name:"firstName",label:"First Name",type:"text"},{name:"lastName",label:"Last Name",type:"text"},{name:"age",label:"Age",type:"text"}],[]),l=o.useMemo(()=>[{label:"Save",color:"yellow",variant:"contained",type:"submit"}],[]),u=a(c=>{alert("Form submitted with the following data: "+JSON.stringify(c,null," "))});return i.jsx("div",{style:{height:"100vh"},children:i.jsx(b,{...e,buttons:l,title:"Quick Submit",fields:p,sections:s?y:void 0,onSubmit:u,autoFocus:!0})})};t.args={showSections:!1};t.argTypes={showSections:{name:"Show Sections"}};var r,m,n;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`({
+  showSections
+}: typeof QuickSubmit.args): ReactElement => {
+  const controller = useForm();
+  const {
+    handleSubmit
+  } = controller;
+  useEffect(() => {
+    document.body.style.margin = "0px";
+    return () => {
+      document.body.style.margin = ORIGINAL_BODY_MARGIN;
+    };
+  }, []);
+  const fields = useMemo((): FieldDef[] => [{
+    name: "firstName",
+    label: "First Name",
+    type: "text"
+  }, {
+    name: "lastName",
+    label: "Last Name",
+    type: "text"
+  }, {
+    name: "age",
+    label: "Age",
+    type: "text"
+  }], []);
+  const buttons = useMemo<ButtonProps[]>(() => [{
+    label: "Save",
+    color: "yellow",
+    variant: "contained",
+    type: "submit"
+  }], []);
+  const onSubmit = handleSubmit(data => {
+    alert("Form submitted with the following data: " + JSON.stringify(data, null, " "));
+  });
+  return <div style={{
+    height: "100vh"
+  }}>
+            <Form {...controller} buttons={buttons} title="Quick Submit" fields={fields} sections={showSections ? sections : undefined} onSubmit={onSubmit} autoFocus />
+        </div>;
+}`,...(n=(m=t.parameters)==null?void 0:m.docs)==null?void 0:n.source}}};const xt=["QuickSubmit"];export{t as QuickSubmit,xt as __namedExportsOrder,Nt as default};
