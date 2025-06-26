77b7292fd5940840bf046ec5320566efc2143b5f
Owen Allen <owenallenaz@gmail.com>
2025-06-26 12:00:19 +0000

    Updates

---
+++ sb8/develop/assets/Drawers-DAdw7rRF.js
@@ -0,0 +1,109 @@
+import{j as t}from"./jsx-runtime-EKYJJIwR.js";import{useMDXComponents as o}from"./index-BwDDMXeo.js";import{M as a,C as i}from"./index-VfVtSX7o.js";import{s,P as p}from"./Drawers.stories-BnqDPZ19.js";import"./index-D0AnReJb.js";import"./iframe-C-KbtVbr.js";import"../sb-preview/runtime.js";import"./index-Bqzh3Cp6.js";import"./index-SI8xS9YR.js";import"./tslib.es6-CvpyDmub.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-DoexKoos.js";import"./_baseForOwn-YgwlavHx.js";import"./index-QVqIEGA1.js";import"./_arrayIncludesWith-XJRwRZaz.js";import"./index-DrFu-skq.js";import"./Drawers-C2K8RuPy.js";import"./styled-components.browser.esm-aVcmUYMZ.js";import"./sizes-CTdyEcea.js";import"./testIds-Dso8kRSM.js";import"./ChevronRight-BMdJevFR.js";import"./identifier-C21A9frq.js";import"./createSvgIcon-DLboviGw.js";import"./createSvgIcon-YkkRvR3V.js";import"./generateUtilityClasses-iHFtG1jj.js";import"./createChainedFunction-BO_9K8Jh.js";import"./ownerWindow-DvT1GKkC.js";import"./useIsFocusVisible-CvEUtp4E.js";import"./useId-BgzzDjMB.js";import"./useControlled-DuXZYv4M.js";import"./index-Csnu-DvG.js";import"./Popper-B4A_VUge.js";import"./useTheme-4eCcP-Dc.js";import"./useThemeWithoutDefault-BHc79qG-.js";import"./index-BzvhE4oF.js";import"./TitleWrapper-D4ztpYuS.js";import"./Button-Cu24UfKA.js";import"./ButtonBase-CLHtCICo.js";import"./useToggle-CTbnrs6g.js";import"./containerQuery-D9vD66Ez.js";import"./Typography-DM2nLVeQ.js";import"./DisplayText-BRdY6ERT.js";import"./ButtonRow-Cj7WA1zR.js";import"./index-DDIZOs0d.js";import"./debounce-D-BUC-g1.js";import"./ExpandMore-CEbXN-sG.js";import"./Chip-BxvGyYQV.js";import"./useMosaicTranslation-CoWLI8L7.js";import"./FormFieldText.styled-CW8xvHG-.js";import"./formControlState-FGREjXRn.js";import"./Spinner-C5cgjXi2.js";import"./Text-Bw6wJuVZ.js";import"./CheckboxList-B0rkeBY0.js";import"./Checkbox-ig6Jea2e.js";import"./FormControlLabel-sQC-fLMo.js";import"./DataViewPrimaryFilter.styled-DJMCb03L.js";import"./MoreVert-DAsd8IpG.js";import"./PageHeader-ClUPNuza.js";import"./formats-CMvQHWsT.js";import"./ChipList-63E8Z3sb.js";import"./RadioButton-Bcxn9PJS.js";import"./StyledFormControl.styled-BQMYotuj.js";import"./Toggle-CrhCMNND.js";import"./Dialog-CCRRhmOO.js";import"./ThemeProvider-DKfWeLFc.js";import"./SideNav-AzQy47h5.js";import"./Snackbar-DyF0qJal.js";import"./Close-CheCpke4.js";import"./ClickAwayListener-Dkkr5f_-.js";function r(n){const e={a:"a",code:"code",h1:"h1",h2:"h2",li:"li",p:"p",pre:"pre",ul:"ul",...o(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{of:s}),`
+`,t.jsx(e.h1,{id:"drawers",children:"Drawers"}),`
+`,t.jsxs(e.p,{children:["The ",t.jsx(e.code,{children:"Drawers"})," component is wrapper that generates several ",t.jsx(e.a,{href:"https://mui.com/material-ui/react-drawer/#main-content",rel:"nofollow",children:"Material-UI Drawers"})," but with SimpleView brand colors."]}),`
+`,t.jsx(e.p,{children:`The component does not provide a mechanism for navigating between drawers, it only helps stacking one on top of another.
+All the navgitation mechanism should be defined on the parent app creating the Drawers component.`}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsx(e.li,{children:"While animating, the UI is locked to prevent race conditions with opening/closing new drawers."}),`
+`,t.jsx(e.li,{children:"Can open infinite amount of stacked drawers."}),`
+`,t.jsx(e.li,{children:"When a drawer is opened, the opener passes callbacks to it to execute when save() is clicked."}),`
+`,t.jsx(e.li,{children:"Animations custom built via keyframes rather than using material-ui drawer defaults to handle stacking cleanly."}),`
+`,t.jsx(e.li,{children:"App passes to Drawers a JSON Def for each drawer and passes a render prop. This separates the concerns of animating and displaying the drawer (Mosaic) from rendering the content of the drawer (App)."}),`
+`]}),`
+`,t.jsx(e.h2,{id:"props",children:"Props"}),`
+`,t.jsx(e.p,{children:t.jsx(e.a,{href:"https://github.com/simpleviewinc/sv-mosaic/blob/develop/containers/mosaic/src/components/Drawers/DrawersTypes.ts",rel:"nofollow",children:"https://github.com/simpleviewinc/sv-mosaic/blob/develop/containers/mosaic/src/components/Drawers/DrawersTypes.ts"})}),`
+`,t.jsx(e.h2,{id:"usage",children:"Usage"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-ts",children:`const App = (props) => {
+	const [state, setState] = useState<AppState>({
+		content: {},
+		drawers: [],
+	});
+
+	const addDrawer = useCallback((drawerDef: DrawerDef) => {
+		setState((state) => ({
+			...state,
+			drawers: [...state.drawers, drawerDef],
+		}));
+	}, []);
+
+	const removeDrawer = useCallback(() => {
+		setState((state) => ({
+			...state,
+			drawers: [...state.drawers.slice(0, -1)],
+		}));
+	}, []);
+
+	const appContext = useMemo(
+		() => ({
+			addDrawer,
+			removeDrawer,
+		}),
+		[addDrawer, removeDrawer]
+	);
+
+	return (
+		<AppContext.Provider value={appContext}>
+			<div className="App">
+				<h1>Drawer Demo</h1>
+				<p>From Form: {JSON.stringify(state.content)}</p>
+				<button
+					onClick={() =>
+						addDrawer({
+							config: {
+								type: "form",
+								title: "New Form",
+								fields: [
+									{
+										name: "foo",
+										label: "Foo",
+										type: "text",
+									},
+									{
+										name: "bar",
+										label: "Bar",
+										type: "text",
+									},
+									{
+										name: "baz",
+										label: "Baz",
+										type: "text",
+									},
+									{
+										name: "from_parent",
+										label: "From Parent",
+										type: "text",
+									},
+								],
+							},
+							callbacks: {
+								save: (data) => {
+									setState((state) => ({
+										...state,
+										content: data,
+									}));
+
+									removeDrawer();
+								},
+							},
+						})
+					}
+				>
+					Add Form
+				</button>
+
+				<Drawers drawers={state.drawers}>
+					{(drawerDef) => {
+						return (
+							<Page
+								config={drawerDef.config}
+								callbacks={drawerDef.callbacks ?? {}}
+							/>
+						);
+					}}
+				</Drawers>
+			</div>
+		</AppContext.Provider>
+	);
+}
+`})}),`
+`,t.jsx(i,{of:p})]})}function At(n={}){const{wrapper:e}={...o(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(r,{...n})}):r(n)}export{At as default};
--- sb8/develop/assets/useTheme-Dt0_M2VT.js
+++
@@ -1,1 +0,0 @@
-import{u as m}from"./useThemeWithoutDefault-ML7LN7QY.js";import{c as t}from"./DefaultPropsProvider-BwNoCFE_.js";const r=t();function u(e=r){return m(e)}export{u};
