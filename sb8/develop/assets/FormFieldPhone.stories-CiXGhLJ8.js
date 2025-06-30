495db2ad9343c4a814c041abf0fc3d3385dd79be
Owen Allen <owenallenaz@gmail.com>
2025-06-30 10:45:39 +0000

    Updates

---
+++ sb8/develop/assets/FormFieldPhone.stories-CiXGhLJ8.js
@@ -0,0 +1,84 @@
+import{j as e}from"./jsx-runtime-EKYJJIwR.js";import{r as T}from"./index-D0AnReJb.js";import{u as S,F as j}from"./index-DCsI_z-L.js";import{r as v}from"./renderButtons-BmHfsf_z.js";import{c as q}from"./commonFieldControl-LQDYMh-C.js";const _={title:"FormFields/FormFieldPhone"},n=({label:t,hideLabel:o,required:s,prepop:l,prepopData:P,disabled:a,instructionText:i,forceInstructionTooltip:d,helperText:u,size:p,country:c,international:m})=>{const f=S({data:l?P:{}}),{state:D,handleSubmit:E}=f,O=T.useMemo(()=>[{name:"phone",label:t,hideLabel:o,type:"phone",required:s,disabled:a,inputSettings:{country:c,international:m},helperText:u,size:p,instructionText:i,forceInstructionTooltip:d}],[t,o,s,a,c,u,p,i,d,m]);return e.jsxs(e.Fragment,{children:[e.jsx("pre",{children:JSON.stringify(D,null,"  ")}),e.jsx(j,{...f,buttons:v(E),title:"Phone Field",fields:O})]})};n.args={...q.args({prepopData:{phone:"+15205302271"}}),size:void 0,country:"",international:void 0};n.argTypes={...q.argTypes,size:{name:"Size",control:{type:"select"},options:{"Undefined (default: sm)":void 0,XSmall:"xs",Small:"sm",Medium:"md",Large:"lg",Full:"full"}},country:{name:"Initial Country"},international:{name:"International",type:{control:"select"},options:{"Undefined (default: false)":void 0,true:!0,false:!1}}};const r=()=>{const t=S(),{state:o,handleSubmit:s}=t,l=T.useMemo(()=>[{name:"phone",label:"Regular example",type:"phone",required:!1,disabled:!1,helperText:"Helper text",instructionText:'Default contry code is "us"'},{name:"countryCode",label:"With a country code provided",type:"phone",required:!1,disabled:!1,inputSettings:{country:"ar"},helperText:"Helper text",instructionText:'The country code of "ar" was provided'},{name:"autoformatEnabled",label:"Autoformat enabled",type:"phone",required:!1,disabled:!1,inputSettings:{autoFormat:!0,country:"us"},helperText:"Helper text",instructionText:"Type a phone number to see the format"}],[]);return e.jsxs(e.Fragment,{children:[e.jsx("pre",{children:JSON.stringify(o,null,"  ")}),e.jsx(j,{...t,buttons:v(s),title:"Form Title",description:"Form description",fields:l})]})};var h,y,b;n.parameters={...n.parameters,docs:{...(h=n.parameters)==null?void 0:h.docs,source:{originalSource:`({
+  label,
+  hideLabel,
+  required,
+  prepop,
+  prepopData,
+  disabled,
+  instructionText,
+  forceInstructionTooltip,
+  helperText,
+  size,
+  country,
+  international
+}: typeof Playground.args): ReactElement => {
+  const controller = useForm({
+    data: prepop ? prepopData : {}
+  });
+  const {
+    state,
+    handleSubmit
+  } = controller;
+  const fields = useMemo((): FieldDef[] => [{
+    name: "phone",
+    label,
+    hideLabel,
+    type: "phone",
+    required,
+    disabled,
+    inputSettings: {
+      country,
+      international
+    },
+    helperText,
+    size,
+    instructionText,
+    forceInstructionTooltip
+  }], [label, hideLabel, required, disabled, country, helperText, size, instructionText, forceInstructionTooltip, international]);
+  return <>
+            <pre>{JSON.stringify(state, null, "  ")}</pre>
+            <Form {...controller} buttons={renderButtons(handleSubmit)} title="Phone Field" fields={fields} />
+        </>;
+}`,...(b=(y=n.parameters)==null?void 0:y.docs)==null?void 0:b.source}}};var x,g,F;r.parameters={...r.parameters,docs:{...(x=r.parameters)==null?void 0:x.docs,source:{originalSource:`(): ReactElement => {
+  const controller = useForm();
+  const {
+    state,
+    handleSubmit
+  } = controller;
+  const fields: FieldDef[] = useMemo(() => [{
+    name: "phone",
+    label: "Regular example",
+    type: "phone",
+    required: false,
+    disabled: false,
+    helperText: "Helper text",
+    instructionText: 'Default contry code is "us"'
+  }, {
+    name: "countryCode",
+    label: "With a country code provided",
+    type: "phone",
+    required: false,
+    disabled: false,
+    inputSettings: {
+      country: "ar"
+    },
+    helperText: "Helper text",
+    instructionText: 'The country code of "ar" was provided'
+  }, {
+    name: "autoformatEnabled",
+    label: "Autoformat enabled",
+    type: "phone",
+    required: false,
+    disabled: false,
+    inputSettings: {
+      autoFormat: true,
+      country: "us"
+    },
+    helperText: "Helper text",
+    instructionText: "Type a phone number to see the format"
+  }], []);
+  return <>
+            <pre>{JSON.stringify(state, null, "  ")}</pre>
+            <Form {...controller} buttons={renderButtons(handleSubmit)} title="Form Title" description="Form description" fields={fields} />
+        </>;
+}`,...(F=(g=r.parameters)==null?void 0:g.docs)==null?void 0:F.source}}};const z=["Playground","KitchenSink"],J=Object.freeze(Object.defineProperty({__proto__:null,KitchenSink:r,Playground:n,__namedExportsOrder:z,default:_},Symbol.toStringTag,{value:"Module"}));export{J as s};
---
+++ sb8/develop/assets/QuickSubmitForm.stories-Cx4iDubj.js
@@ -0,0 +1,41 @@
+import{j as i}from"./jsx-runtime-EKYJJIwR.js";import{r as o}from"./index-D0AnReJb.js";import{u as d,F as b}from"./index-DCsI_z-L.js";import{O as f}from"./dom-BR2PSRFh.js";import"./styled-components.browser.esm-aVcmUYMZ.js";import"./sizes-CTdyEcea.js";import"./testIds-BKutUmNQ.js";import"./TitleWrapper-D4ztpYuS.js";import"./Button-Cu24UfKA.js";import"./identifier-C21A9frq.js";import"./generateUtilityClasses-iHFtG1jj.js";import"./ButtonBase-CLHtCICo.js";import"./useIsFocusVisible-CvEUtp4E.js";import"./index-Csnu-DvG.js";import"./Popper-B4A_VUge.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useTheme-4eCcP-Dc.js";import"./useThemeWithoutDefault-BHc79qG-.js";import"./index-Bqzh3Cp6.js";import"./index-SI8xS9YR.js";import"./useToggle-CTbnrs6g.js";import"./containerQuery-D9vD66Ez.js";import"./createSvgIcon-DLboviGw.js";import"./createSvgIcon-YkkRvR3V.js";import"./useId-BgzzDjMB.js";import"./useControlled-DuXZYv4M.js";import"./Typography-DM2nLVeQ.js";import"./DisplayText-BRdY6ERT.js";import"./ButtonRow-DEKalGfm.js";import"./toNumber-ANF_7kha.js";import"./useMosaicTranslation-CoWLI8L7.js";import"./DataViewFilterDropdownButtons-B-NSlGS8.js";import"./index-BvK5pSCI.js";import"./ExpandMore-CEbXN-sG.js";import"./Chip-BMBxB4HT.js";import"./PickerPanel-CJUaTFnG.js";import"./PageHeader-CEZufbNa.js";import"./Text-Bw6wJuVZ.js";import"./FormFieldText.styled-DTQIdddZ.js";import"./formControlState-FGREjXRn.js";import"./Spinner-DrhQOeHK.js";import"./CheckboxList-6G7BlDZu.js";import"./Checkbox-DqndGKzg.js";import"./FormControlLabel-sQC-fLMo.js";import"./DataViewPrimaryFilter.styled-DJMCb03L.js";import"./MoreVert-DAsd8IpG.js";import"./formats-CMvQHWsT.js";import"./ChipList-CJvhQDiV.js";import"./RadioButton-Bcxn9PJS.js";import"./StyledFormControl.styled-BQMYotuj.js";import"./Toggle-DDpxCdm7.js";import"./Dialog-CCRRhmOO.js";import"./ThemeProvider-DKfWeLFc.js";import"./SideNav-AzQy47h5.js";import"./Snackbar-DyF0qJal.js";import"./Close-CheCpke4.js";import"./ClickAwayListener-Dkkr5f_-.js";const xt={title:"Components/Form"},y=[{title:"Other",fields:[[["age"]]]},{title:"Name",fields:[[["lastName"]],[["firstName"]]]}],t=({showSections:s})=>{const e=d(),{handleSubmit:a}=e;o.useEffect(()=>(document.body.style.margin="0px",()=>{document.body.style.margin=f}),[]);const p=o.useMemo(()=>[{name:"firstName",label:"First Name",type:"text"},{name:"lastName",label:"Last Name",type:"text"},{name:"age",label:"Age",type:"text"}],[]),l=o.useMemo(()=>[{label:"Save",color:"yellow",variant:"contained",type:"submit"}],[]),u=a(c=>{alert("Form submitted with the following data: "+JSON.stringify(c,null," "))});return i.jsx("div",{style:{height:"100vh"},children:i.jsx(b,{...e,buttons:l,title:"Quick Submit",fields:p,sections:s?y:void 0,onSubmit:u,autoFocus:!0})})};t.args={showSections:!1};t.argTypes={showSections:{name:"Show Sections"}};var r,m,n;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`({
+  showSections
+}: typeof QuickSubmit.args): ReactElement => {
+  const controller = useForm();
+  const {
+    handleSubmit
+  } = controller;
+  useEffect(() => {
+    document.body.style.margin = "0px";
+    return () => {
+      document.body.style.margin = ORIGINAL_BODY_MARGIN;
+    };
+  }, []);
+  const fields = useMemo((): FieldDef[] => [{
+    name: "firstName",
+    label: "First Name",
+    type: "text"
+  }, {
+    name: "lastName",
+    label: "Last Name",
+    type: "text"
+  }, {
+    name: "age",
+    label: "Age",
+    type: "text"
+  }], []);
+  const buttons = useMemo<ButtonProps[]>(() => [{
+    label: "Save",
+    color: "yellow",
+    variant: "contained",
+    type: "submit"
+  }], []);
+  const onSubmit = handleSubmit(data => {
+    alert("Form submitted with the following data: " + JSON.stringify(data, null, " "));
+  });
+  return <div style={{
+    height: "100vh"
+  }}>
+            <Form {...controller} buttons={buttons} title="Quick Submit" fields={fields} sections={showSections ? sections : undefined} onSubmit={onSubmit} autoFocus />
+        </div>;
+}`,...(n=(m=t.parameters)==null?void 0:m.docs)==null?void 0:n.source}}};const Ft=["QuickSubmit"];export{t as QuickSubmit,Ft as __namedExportsOrder,xt as default};
