c23f3f9f7e320e1059e408d29f08718a81867587
Owen Allen <owenallenaz@gmail.com>
2025-06-30 11:16:34 +0000

    Updates

---
+++ sb8/develop/assets/transform_join-CBRdlS5A.js
@@ -0,0 +1,17 @@
+import{j as r}from"./jsx-runtime-EKYJJIwR.js";import{useMDXComponents as e}from"./index-BwDDMXeo.js";import{M as s}from"./index-Bsj1ziNY.js";import"./index-D0AnReJb.js";import"./iframe-j56FUPmj.js";import"../sb-preview/runtime.js";import"./index-Bqzh3Cp6.js";import"./index-SI8xS9YR.js";import"./tslib.es6-CvpyDmub.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-DoexKoos.js";import"./_baseForOwn-YgwlavHx.js";import"./index-QVqIEGA1.js";import"./_arrayIncludesWith-XJRwRZaz.js";import"./index-DrFu-skq.js";function i(o){const n={code:"code",h1:"h1",h4:"h4",li:"li",p:"p",pre:"pre",ul:"ul",...e(),...o.components};return r.jsxs(r.Fragment,{children:[r.jsx(s,{title:"Utils/Transforms/Join"}),`
+`,r.jsx(n.h1,{id:"transform_join",children:"transform_join"}),`
+`,r.jsx(n.p,{children:"Converts array of values into comma separated string."}),`
+`,r.jsx(n.h4,{id:"conversions",children:"Conversions"}),`
+`,r.jsxs(n.ul,{children:[`
+`,r.jsxs(n.li,{children:[r.jsx(n.code,{children:'["foo", "bar", "baz"]'})," => ",r.jsx(n.code,{children:'"Foo, bar, baz"'})]}),`
+`,r.jsxs(n.li,{children:[r.jsx(n.code,{children:"[]"})," => ",r.jsx(n.code,{children:'""'})]}),`
+`]}),`
+`,r.jsx(n.h4,{id:"args",children:"Args"}),`
+`,r.jsx(n.p,{children:"None"}),`
+`,r.jsx(n.h4,{id:"example",children:"Example"}),`
+`,r.jsx(n.pre,{children:r.jsx(n.code,{className:"language-js",children:`import { transform_join } from "#mosaic"\r
+...\r
+transforms : [\r
+    transform_join()\r
+]
+`})})]})}function v(o={}){const{wrapper:n}={...e(),...o.components};return n?r.jsx(n,{...o,children:r.jsx(i,{...o})}):i(o)}export{v as default};
