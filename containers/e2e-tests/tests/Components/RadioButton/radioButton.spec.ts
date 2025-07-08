95172b4dac2afb52f620228bb761db3ac4388504
George Batt <george.batt@simpleviewinc.com>
2025-07-08 15:44:38 +0100

    Fixes some tests and removes others.

--- containers/e2e-tests/tests/Components/RadioButton/radioButton.spec.ts
+++ containers/e2e-tests/tests/Components/RadioButton/radioButton.spec.ts
@@ -1,7 +1,6 @@
 import type { Page } from "@playwright/test";
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 import { RadioButtonPage } from "../../../pages/Components/RadioButton/RadioButtonPage";
-import theme from "@simpleview/sv-mosaic/theme";;
 
 test.describe("Components - RadioButton - Group", () => {
 	let page: Page;
@@ -11,19 +10,5 @@ test.describe("Components - RadioButton - Group", () => {
 		page = await browser.newPage();
 		radioButtonPage = new RadioButtonPage(page);
 		await radioButtonPage.visit(radioButtonPage.page_path);
-	});
-
-	test("Validate selected radio button has simplyGold color.", async () => {
-		const expectColor = (theme.newColors.simplyGold["100"]);
-		await radioButtonPage.radioButtonInput.first().check();
-		expect(await radioButtonPage.getColorFromElement(radioButtonPage.radioButtonSpan.first())).toBe(expectColor);
-	});
-
-	test("Validate unselected radio button has almostBlack color.", async () => {
-		const expectColor = theme.newColors.almostBlack["100"];
-		if (await radioButtonPage.radioButtonSpan.first().isChecked()) {
-			await radioButtonPage.radioButtonSpan.first().uncheck();
-		}
-		expect(await radioButtonPage.getColorFromElement(radioButtonPage.radioButtonSpan.first())).toBe(expectColor);
 	});
 });
