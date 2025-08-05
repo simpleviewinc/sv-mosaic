ce56390a9351ff5199fe14747ee694c5256ce3d8
George Batt <george.batt@simpleviewinc.com>
2025-08-05 16:27:21 +0100

    (Button) Adds an icon library from which to choose when viewing the button playground. Employs a larger button icon size where the label is absent. Uses standard MUI icon sizes.

---
+++ containers/sb-8/stories/components/Button/icons.ts
@@ -0,0 +1,95 @@
+import type SvgIcon from "@mui/material/SvgIcon";
+
+import Add from "@mui/icons-material/Add";
+import Archive from "@mui/icons-material/Archive";
+import Build from "@mui/icons-material/Build";
+import Business from "@mui/icons-material/Business";
+import Campaign from "@mui/icons-material/Campaign";
+import Chat from "@mui/icons-material/Chat";
+import Clear from "@mui/icons-material/Clear";
+import Close from "@mui/icons-material/Close";
+import Contacts from "@mui/icons-material/Contacts";
+import ContentCopy from "@mui/icons-material/ContentCopy";
+import Delete from "@mui/icons-material/Delete";
+import DeleteForever from "@mui/icons-material/DeleteForever";
+import Download from "@mui/icons-material/Download";
+import DynamicForm from "@mui/icons-material/DynamicForm";
+import Done from "@mui/icons-material/Done";
+import Edit from "@mui/icons-material/Edit";
+import Event from "@mui/icons-material/Event";
+import Forum from "@mui/icons-material/Forum";
+import Group from "@mui/icons-material/Group";
+import Help from "@mui/icons-material/Help";
+import Home from "@mui/icons-material/Home";
+import InsertDriveFile from "@mui/icons-material/InsertDriveFile";
+import List from "@mui/icons-material/List";
+import LocalOffer from "@mui/icons-material/LocalOffer";
+import Mail from "@mui/icons-material/Mail";
+import ManageAccounts from "@mui/icons-material/ManageAccounts";
+import MeetingRoom from "@mui/icons-material/MeetingRoom";
+import Note from "@mui/icons-material/StickyNote2";
+import People from "@mui/icons-material/People";
+import PendingActions from "@mui/icons-material/PendingActions";
+import Person from "@mui/icons-material/Person";
+import Print from "@mui/icons-material/Print";
+import Public from "@mui/icons-material/Public";
+import RecentActors from "@mui/icons-material/RecentActors";
+import Send from "@mui/icons-material/Send";
+import Settings from "@mui/icons-material/Settings";
+import SettingsApplications from "@mui/icons-material/SettingsApplications";
+import SettingsBackupRestore from "@mui/icons-material/SettingsBackupRestore";
+import SmartButton from "@mui/icons-material/SmartButton";
+import Sync from "@mui/icons-material/Sync";
+import TaskAlt from "@mui/icons-material/TaskAlt";
+import NewReleases from "@mui/icons-material/NewReleases";
+import Notifications from "@mui/icons-material/Notifications";
+import Visibility from "@mui/icons-material/Visibility";
+
+const icons: Record<string, typeof SvgIcon> = {
+	Add,
+	Archive,
+	Build,
+	Business,
+	Campaign,
+	Chat,
+	Clear,
+	Close,
+	Contacts,
+	ContentCopy,
+	Delete,
+	DeleteForever,
+	Done,
+	Download,
+	DynamicForm,
+	Edit,
+	Event,
+	Forum,
+	Group,
+	Help,
+	Home,
+	InsertDriveFile,
+	List,
+	LocalOffer,
+	Mail,
+	ManageAccounts,
+	MeetingRoom,
+	Note,
+	People,
+	PendingActions,
+	Person,
+	Print,
+	Public,
+	RecentActors,
+	Send,
+	Settings,
+	SettingsApplications,
+	SettingsBackupRestore,
+	SmartButton,
+	Sync,
+	TaskAlt,
+	NewReleases,
+	Notifications,
+	Visibility,
+};
+
+export default icons;
