ec744bda2e7bb2ac74fa30704300c9360758067e
Owen Allen <owenallenaz@gmail.com>
2025-08-06 08:35:21 +0000

    Updates

---
+++ sb8/develop/assets/Address-BnaJqepr.js
@@ -0,0 +1,56 @@
+import{j as e}from"./jsx-runtime-EKYJJIwR.js";import{useMDXComponents as n}from"./index-BwDDMXeo.js";import{M as s}from"./index-rQXuHiXv.js";import{s as o}from"./Address.stories-D6Mc3qHq.js";import"./index-D0AnReJb.js";import"./iframe-DqPE_PUQ.js";import"../sb-preview/runtime.js";import"./index-Bqzh3Cp6.js";import"./index-SI8xS9YR.js";import"./tslib.es6-CvpyDmub.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-DoexKoos.js";import"./_baseForOwn-YgwlavHx.js";import"./index-QVqIEGA1.js";import"./_arrayIncludesWith-XJRwRZaz.js";import"./index-DrFu-skq.js";import"./renderButtons-DP0bGf4C.js";import"./commonFieldControl-LQDYMh-C.js";import"./index-XS4bmHCM.js";import"./styled-components.browser.esm-C5LJ_neh.js";import"./sizes-_ON_TzAH.js";import"./ExpandMore-Dl7LJMh2.js";import"./identifier-Bktyv6O_.js";import"./createSvgIcon-D5aI0OD-.js";import"./createSvgIcon-C735tyhT.js";import"./generateUtilityClasses-D4jmUuEZ.js";import"./createChainedFunction-BO_9K8Jh.js";import"./ownerWindow-DvT1GKkC.js";import"./useIsFocusVisible-o8lB1h8w.js";import"./useId-BgzzDjMB.js";import"./useControlled-DuXZYv4M.js";import"./useMosaicTranslation-di9nduwf.js";import"./testIds-DLbO0bd9.js";import"./TitleWrapper-BbWupsUF.js";import"./Button-BOlAo_yO.js";import"./ButtonBase-CyOgbtoj.js";import"./index-R4Jqw3Tw.js";import"./Popover-DMfUH9d_.js";import"./Popper-D9JYON0d.js";import"./useTheme-DTBsETmh.js";import"./useThemeWithoutDefault-CsQox1c5.js";import"./useToggle-CTbnrs6g.js";import"./containerQuery-Byjr_VO4.js";import"./Typography-DBsSg8HG.js";import"./DisplayText-Ip6DAA4t.js";import"./ButtonRow-Dil46hZf.js";import"./toNumber-CqnDHfZM.js";import"./DataViewFilterDropdownButtons-Bd_5anyu.js";import"./StyledPopperPaper-BxcX73gl.js";import"./index-DYyO2Iln.js";import"./Chip-D4exI_Oq.js";import"./DataViewPrimaryFilter.styled-ChQqXjcm.js";import"./Badge.styled-qjrvmt6O.js";import"./Text-ChOjGkiI.js";import"./PickerPanel-tAAYph5M.js";import"./PageHeader-t6Ly6_Ob.js";import"./FormFieldText.styled-DouhbL_d.js";import"./formControlState-BI4toe_X.js";import"./Spinner-m5_IcvNq.js";import"./CheckboxList-B7SbK3Ha.js";import"./Checkbox-SBjhRN_S.js";import"./StyledOptionFormControl-Cwl0Q2GB.js";import"./MoreVert-DKRvdU6X.js";import"./formats-CMvQHWsT.js";import"./Snackbar-at35UkfL.js";import"./Close-DkzKxLx5.js";import"./ClickAwayListener-Cp0VGVh1.js";import"./ChipList-zisjpXtz.js";import"./RadioButton-DJt--7LN.js";import"./Toggle-BsX429Os.js";import"./Dialog-BQccgbOq.js";import"./ThemeProvider-DAuzgAUw.js";import"./SideNav-BYUaKA9k.js";import"./options-DEOzrsxm.js";function i(r){const t={a:"a",code:"code",h1:"h1",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...n(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:o}),`
+`,e.jsx(t.h1,{id:"address",children:"Address"}),`
+`,e.jsxs(t.p,{children:["This field implements the ",e.jsx(t.a,{href:"#generic-field-props-fielddef",children:e.jsx(t.strong,{children:"FieldDef"})})," interface."]}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:["Countries, states and cities information retrieved from: ",e.jsx(t.a,{href:"https://github.com/dr5hn/countries-states-cities-database",rel:"nofollow",children:"Countries States Cities Database"})]}),`
+`,e.jsx(t.li,{children:'Features a draw with an embedded "child" form which can be used to create or edit addresses.'}),`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"/?path=/story/formfields-formfieldaddress--playground",children:e.jsx(t.strong,{children:"Playground"})})}),`
+`,e.jsxs(t.li,{children:["Data: ",e.jsx(t.code,{children:"object"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[e.jsx(t.strong,{children:"address1"})," - ",e.jsx(t.code,{children:"string"})," required - Main address."]}),`
+`,e.jsxs(t.li,{children:[e.jsx(t.strong,{children:"address2"})," - ",e.jsx(t.code,{children:"string"})," optional - Additional address."]}),`
+`,e.jsxs(t.li,{children:[e.jsx(t.strong,{children:"address3"})," - ",e.jsx(t.code,{children:"string"})," optional - Additional address."]}),`
+`,e.jsxs(t.li,{children:[e.jsx(t.strong,{children:"city"})," - ",e.jsx(t.code,{children:"string"})," required - Name of the city."]}),`
+`,e.jsxs(t.li,{children:[e.jsx(t.strong,{children:"country"})," - ",e.jsx(t.code,{children:"MosaicLabelValue"})," required - Object containing the label and value of the country."]}),`
+`,e.jsxs(t.li,{children:[e.jsx(t.strong,{children:"postalCode"})," - ",e.jsx(t.code,{children:"string"})," required - Postal code of the address."]}),`
+`,e.jsxs(t.li,{children:[e.jsx(t.strong,{children:"state"})," - ",e.jsx(t.code,{children:"MosaicLabelValue"})," required - Object containing the label and value of the state."]}),`
+`,e.jsxs(t.li,{children:[e.jsx(t.strong,{children:"types"})," - ",e.jsx(t.code,{children:"array"})," of ",e.jsx(t.code,{children:"string"})," required - Could be one of the following: Physical, Billing, Shipping."]}),`
+`]}),`
+`]}),`
+`,e.jsx(t.li,{children:"inputSettings:"}),`
+`]}),`
+`,e.jsxs(t.table,{children:[e.jsx(t.thead,{children:e.jsxs(t.tr,{children:[e.jsx(t.th,{children:"Name"}),e.jsx(t.th,{children:"Type"}),e.jsx(t.th,{children:"Description"})]})}),e.jsxs(t.tbody,{children:[e.jsxs(t.tr,{children:[e.jsx(t.td,{children:e.jsx(t.strong,{children:e.jsx(t.code,{children:"amountPerType"})})}),e.jsx(t.td,{children:e.jsx(t.code,{children:"number"})}),e.jsx(t.td,{children:"optional - When defined, limits the amount of address types. For example: If a dev adds “amountPerType: 2” then users will be able to add 2 physical, 2 billing, and 2 shipping addresses. Defaults to 1 if no other amount properties are defined and 0 otherwise."})]}),e.jsxs(t.tr,{children:[e.jsx(t.td,{children:e.jsx(t.strong,{children:e.jsx(t.code,{children:"amountShipping"})})}),e.jsx(t.td,{children:e.jsx(t.code,{children:"number"})}),e.jsx(t.td,{children:'optional - When defined, limits the amount of address type of "shipping". Takes precedence over amountPerType.'})]}),e.jsxs(t.tr,{children:[e.jsx(t.td,{children:e.jsx(t.strong,{children:e.jsx(t.code,{children:"amountPhysical"})})}),e.jsx(t.td,{children:e.jsx(t.code,{children:"number"})}),e.jsx(t.td,{children:'optional - When defined, limits the amount of address type of "physical". Takes precedence over amountPerType.'})]}),e.jsxs(t.tr,{children:[e.jsx(t.td,{children:e.jsx(t.strong,{children:e.jsx(t.code,{children:"amountBilling"})})}),e.jsx(t.td,{children:e.jsx(t.code,{children:"number"})}),e.jsx(t.td,{children:'optional - When defined, limits the amount of address type of "billing". Takes precedence over amountPerType.'})]}),e.jsxs(t.tr,{children:[e.jsx(t.td,{children:e.jsx(t.strong,{children:e.jsx(t.code,{children:"getOptionsCountries"})})}),e.jsx(t.td,{children:e.jsx(t.code,{children:"(): Promise<MosaicLabelValue[]>"})}),e.jsx(t.td,{children:"Required - Returns a list of mapped countries in the shape of label-value array."})]}),e.jsxs(t.tr,{children:[e.jsx(t.td,{children:e.jsx(t.strong,{children:e.jsx(t.code,{children:"getOptionsStates"})})}),e.jsx(t.td,{children:e.jsx(t.code,{children:"(country: string): Promise<MosaicLabelValue[]>"})}),e.jsx(t.td,{children:"Given a country value it returns the list of states of the given country in the shape of label-value array."})]}),e.jsxs(t.tr,{children:[e.jsx(t.td,{children:e.jsx(t.strong,{children:e.jsx(t.code,{children:"googleMapsApiKey"})})}),e.jsx(t.td,{children:e.jsx(t.code,{children:"string"})}),e.jsx(t.td,{children:"Required - Google Maps API key needed to access Place Autocomplete Google web service that returns place predictions in response to an HTTP request."})]})]})]}),`
+`,e.jsx(t.p,{children:"Note that if the amount rules mean that only 1 type of address is allowed, the type field will not appear in the address drawer."}),`
+`,e.jsx(t.h3,{id:"how-to-use-in-a-form",children:"How to use in a form?"}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-ts",children:`const getOptionsCountries = async () => {\r
+	const countriesArr: MosaicLabelValue[] = await countriesMicroService();\r
+	return countriesArr;\r
+}\r
+\r
+const getOptionsStates = async (country) => {\r
+	const statesArr: MosaicLabelValue[] = await statesMicroService(country);\r
+	return statesArr;\r
+}\r
+\r
+const fields = useMemo(\r
+	() =>\r
+		[\r
+			//...other fields\r
+			{\r
+				//...all generic field props,\r
+				type: "address",\r
+				inputSettings: {\r
+					amountPerType: 1,\r
+					amountShipping: 0,\r
+					amountPhysical: 2,\r
+					amountBilling: 3,\r
+					getOptionsCountries,\r
+					getOptionsStates,\r
+					googleMapsApiKey: "ABCDEFG1234567890"\r
+				},\r
+			},\r
+			//...other fields\r
+		],\r
+	[]\r
+);
+`})})]})}function qe(r={}){const{wrapper:t}={...n(),...r.components};return t?e.jsx(t,{...r,children:e.jsx(i,{...r})}):i(r)}export{qe as default};
---
+++ sb8/develop/assets/TimeField-Cs8aZz_w.js
@@ -0,0 +1,23 @@
+import{j as t}from"./jsx-runtime-EKYJJIwR.js";import{useMDXComponents as m}from"./index-BwDDMXeo.js";import{M as e}from"./index-rQXuHiXv.js";import{s as p}from"./TimeField.stories-Yrd-cmoR.js";import"./index-D0AnReJb.js";import"./iframe-DqPE_PUQ.js";import"../sb-preview/runtime.js";import"./index-Bqzh3Cp6.js";import"./index-SI8xS9YR.js";import"./tslib.es6-CvpyDmub.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-DoexKoos.js";import"./_baseForOwn-YgwlavHx.js";import"./index-QVqIEGA1.js";import"./_arrayIncludesWith-XJRwRZaz.js";import"./index-DrFu-skq.js";import"./index-XS4bmHCM.js";import"./styled-components.browser.esm-C5LJ_neh.js";import"./sizes-_ON_TzAH.js";import"./ExpandMore-Dl7LJMh2.js";import"./identifier-Bktyv6O_.js";import"./createSvgIcon-D5aI0OD-.js";import"./createSvgIcon-C735tyhT.js";import"./generateUtilityClasses-D4jmUuEZ.js";import"./createChainedFunction-BO_9K8Jh.js";import"./ownerWindow-DvT1GKkC.js";import"./useIsFocusVisible-o8lB1h8w.js";import"./useId-BgzzDjMB.js";import"./useControlled-DuXZYv4M.js";import"./useMosaicTranslation-di9nduwf.js";import"./testIds-DLbO0bd9.js";import"./TitleWrapper-BbWupsUF.js";import"./Button-BOlAo_yO.js";import"./ButtonBase-CyOgbtoj.js";import"./index-R4Jqw3Tw.js";import"./Popover-DMfUH9d_.js";import"./Popper-D9JYON0d.js";import"./useTheme-DTBsETmh.js";import"./useThemeWithoutDefault-CsQox1c5.js";import"./useToggle-CTbnrs6g.js";import"./containerQuery-Byjr_VO4.js";import"./Typography-DBsSg8HG.js";import"./DisplayText-Ip6DAA4t.js";import"./ButtonRow-Dil46hZf.js";import"./toNumber-CqnDHfZM.js";import"./DataViewFilterDropdownButtons-Bd_5anyu.js";import"./StyledPopperPaper-BxcX73gl.js";import"./index-DYyO2Iln.js";import"./Chip-D4exI_Oq.js";import"./DataViewPrimaryFilter.styled-ChQqXjcm.js";import"./Badge.styled-qjrvmt6O.js";import"./Text-ChOjGkiI.js";import"./PickerPanel-tAAYph5M.js";import"./PageHeader-t6Ly6_Ob.js";import"./FormFieldText.styled-DouhbL_d.js";import"./formControlState-BI4toe_X.js";import"./Spinner-m5_IcvNq.js";import"./CheckboxList-B7SbK3Ha.js";import"./Checkbox-SBjhRN_S.js";import"./StyledOptionFormControl-Cwl0Q2GB.js";import"./MoreVert-DKRvdU6X.js";import"./formats-CMvQHWsT.js";import"./Snackbar-at35UkfL.js";import"./Close-DkzKxLx5.js";import"./ClickAwayListener-Cp0VGVh1.js";import"./ChipList-zisjpXtz.js";import"./RadioButton-DJt--7LN.js";import"./Toggle-BsX429Os.js";import"./Dialog-BQccgbOq.js";import"./ThemeProvider-DAuzgAUw.js";import"./SideNav-BYUaKA9k.js";import"./renderButtons-DP0bGf4C.js";import"./commonFieldControl-LQDYMh-C.js";import"./parseDateControl-BljN0TWJ.js";function o(i){const r={a:"a",code:"code",h1:"h1",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...m(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:p}),`
+`,t.jsx(r.h1,{id:"time",children:"Time"}),`
+`,t.jsxs(r.p,{children:["This field implements the ",t.jsx(r.a,{href:"#generic-field-props-fielddef",children:t.jsx(r.strong,{children:"FieldDef"})})," interface."]}),`
+`,t.jsxs(r.ul,{children:[`
+`,t.jsx(r.li,{children:"It allows the user to view and pick a time from a calendar widget or manually type the time in the text field."}),`
+`,t.jsx(r.li,{children:t.jsx(r.a,{href:"/?path=/story/formfields-formfieldtimefield--playground",children:t.jsx(r.strong,{children:"Playground"})})}),`
+`,t.jsxs(r.li,{children:["Data: ",t.jsx(r.code,{children:"string"})," - Time in ",t.jsx(r.code,{children:"HH:MM"})," 24 hour format."]}),`
+`,t.jsx(r.li,{children:"This field accepts no inputSettings"}),`
+`]}),`
+`,t.jsx(r.h3,{id:"how-to-use-in-a-form",children:"How to use in a form?"}),`
+`,t.jsx(r.pre,{children:t.jsx(r.code,{className:"language-ts",children:`const fields = useMemo(\r
+	() =>\r
+		[\r
+			//...other fields\r
+			{\r
+				//...all generic field props,\r
+				type: "time",\r
+			},\r
+			//...other fields\r
+		],\r
+	[]\r
+);
+`})})]})}function kt(i={}){const{wrapper:r}={...m(),...i.components};return r?t.jsx(r,{...i,children:t.jsx(o,{...i})}):o(i)}export{kt as default};
