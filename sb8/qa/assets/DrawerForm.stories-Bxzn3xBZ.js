26a744338e6d64038e6c97e9474d4f519b4b559a
Owen Allen <owenallenaz@gmail.com>
2025-08-08 09:28:15 +0000

    Updates

---
+++ sb8/qa/assets/DrawerForm.stories-Bxzn3xBZ.js
@@ -0,0 +1,331 @@
+import{j as e}from"./jsx-runtime-EKYJJIwR.js";import{r as u}from"./index-D0AnReJb.js";import{u as F,v as T,D as P,F as k}from"./index-BeCxSKiY.js";import{B as A}from"./Button-CN4kYzMe.js";import{S as o,t as r,B as t}from"./sizes-DkIXWPpc.js";import{u as D}from"./uniqueId-B70Z0sZW.js";import{a as H,b as M}from"./options-DEOzrsxm.js";import{s as N}from"./styled-components.browser.esm-njFAddlw.js";import{A as z}from"./AddCircleOutline-Dbx-uNcC.js";import"./styled-components.browser.esm-8X0uN6v9.js";import"./ExpandMore-JUNNgEdW.js";import"./identifier-DOqdnZ6L.js";import"./createSvgIcon-B2sN9389.js";import"./createSvgIcon-C3PpPknf.js";import"./generateUtilityClasses-BL8vvUJw.js";import"./createChainedFunction-BO_9K8Jh.js";import"./ownerWindow-DvT1GKkC.js";import"./useIsFocusVisible-o8lB1h8w.js";import"./useId-BgzzDjMB.js";import"./useControlled-DuXZYv4M.js";import"./useMosaicTranslation-di9nduwf.js";import"./testIds-DLbO0bd9.js";import"./TitleWrapper-CQxWIXzH.js";import"./containerQuery-CYj0-5X2.js";import"./Typography-CZQzWHyE.js";import"./DisplayText-DLjAy6kH.js";import"./ButtonRow-C5wOwzPe.js";import"./useToggle-CTbnrs6g.js";import"./toNumber-DuWhFW-p.js";import"./index-CYsLg4TE.js";import"./Popper-DKTKzQfx.js";import"./useTheme-CJVJa3gz.js";import"./useThemeWithoutDefault-BQF-0HLi.js";import"./index-Bqzh3Cp6.js";import"./index-SI8xS9YR.js";import"./DataViewFilterDropdownButtons-DRwFtKQ6.js";import"./StyledPopperPaper-DKpxPU4i.js";import"./Popover-BG0BPeg-.js";import"./index-CtsnE9bo.js";import"./Chip-CpsUtANy.js";import"./ButtonBase-Dg-QHo0d.js";import"./DataViewPrimaryFilter.styled-hTjiiCYc.js";import"./Badge-D14GCFVi.js";import"./Text-BjwqtcAz.js";import"./PickerPanel-D4_DzPYT.js";import"./PageHeader-Rtx_tFBF.js";import"./FormFieldText.styled-CCsJeQGV.js";import"./formControlState-BI4toe_X.js";import"./Spinner-BATOmOoB.js";import"./CheckboxList-CIADAAOD.js";import"./Checkbox-DnVUUwfp.js";import"./StyledOptionFormControl-CmOcJT1x.js";import"./MoreVert-BXVssJ4w.js";import"./formats-CMvQHWsT.js";import"./Snackbar-Bv8HGMID.js";import"./Close-BKfiyhHV.js";import"./ClickAwayListener-Cp0VGVh1.js";import"./ChipList-TQ6-UhAA.js";import"./RadioButton-1BL3SEux.js";import"./Toggle-BYU1iYU_.js";import"./Dialog-Bg1_LB1W.js";import"./ThemeProvider-CFABLTDy.js";import"./SideNav-CFjdJbjv.js";import"./toString-DoexKoos.js";import"./tslib.es6-CvpyDmub.js";import"./emotion-is-prop-valid.cjs.default-BrZTFlsY.js";import"./styled-Cb9FMILg.js";import"./extends-CF3RwP-h.js";import"./createSvgIcon-DtczVsqP.js";const Ze={title:"Components/Form"},n={type:"text",instructionText:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras venenatis arcu sed rutrum maximus.",required:!0,inputSettings:{maxCharacters:20}},m={type:"address",required:!0,inputSettings:{getOptionsCountries:M,getOptionsStates:H,googleMapsApiKey:"AIzaSyArV4f-KFF86Zn9VWAu9wS4hHlG1TXxqac"}},a={type:"mapCoordinates",instructionText:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras venenatis arcu sed rutrum maximus.",inputSettings:{googleMapsApiKey:"AIzaSyArV4f-KFF86Zn9VWAu9wS4hHlG1TXxqac"}},c=N.h2`
+    font-size: 26px;
+    font-weight: ${r.weight.regular};
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    color: ${r.colors.simplyGray};
+    margin: 8px -12px 0;
+    display: flex;
+    align-items: center;
+
+    &::after{
+        content: " ";
+        flex-grow: 1;
+        border-top: 1px solid ${r.newColors.grey2[100]};
+        margin-left: 16px;
+        margin-top: 4px;
+    }
+`,W=[{title:"Text field sizes",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras venenatis arcu sed rutrum maximus.",fields:[[["text-xs"]],[["text-sm"]],[["text-md"]],[["text-lg"]]]},{title:"Fields with grid layout",fields:[[["firstName"],["lastName"],["dob"]],[["profilePicture"],["biography"],[]],[["homeAddress"],["workAddress"],["altAddress"]],[["petsHeading"]],[["favouritePet","firstPet","desiredPet"]],[["destinationsHeading"]],[["whereToGo"]],[["availableStart"],["availableEnd"]],[["novaScotia"],["eiffelTower"],["lochNessMonster"]]]}],v={default:`${parseInt(t.sm,10)-160}px`,sm:t.sm,md:t.md,lg:t.lg,xl:t.xl,"2xl":t["2xl"]},i=({showSections:x,drawWidth:w})=>{const d=F({data:{petsHeading:e.jsx(c,{children:"Pets"}),destinationsHeading:e.jsx(c,{children:"Destinations"}),novaScotia:{lat:44.64933472911243,lng:-63.615047475871876},eiffelTower:{lat:48.858348895100555,lng:2.294492026111051},lochNessMonster:{lat:57.27050873488408,lng:-4.493444954407284}}}),{handleSubmit:f}=d,S=u.useMemo(()=>[{...n,name:"text-xs",label:"Extra small text",size:o.xs},{...n,name:"text-sm",label:"Small text",size:o.sm},{...n,name:"text-md",label:"Medium text",size:o.md},{...n,name:"text-lg",label:"Email address",size:o.lg,validators:[T]},{...n,name:"firstName",label:"First Name"},{...n,name:"lastName",label:"Last Name"},{type:"date",name:"dob",label:"Date of Birth"},{name:"profilePicture",label:"Profile Picture",type:"upload",inputSettings:{onFileAdd:async({onUploadComplete:s})=>{s({name:"Test",fileUrl:"https://www.google.com",id:D(),size:1098})},onFileDelete:async()=>{}},required:!0},{...n,name:"biography",label:"Biography",size:o.lg,inputSettings:{multiline:!0,minRows:8}},{name:"phone",label:"Phone",type:"phone"},{name:"petsHeading",type:"raw"},{label:"Favourite Pet",name:"favouritePet",required:!0,type:"dropdown",inputSettings:{options:[{value:"dog",label:"Dog"},{value:"cat",label:"Cat"},{value:"rabbit",label:"Rabbit"}]}},{label:"First Pet",name:"firstPet",required:!0,type:"dropdown",inputSettings:{options:[{value:"dog",label:"Dog"},{value:"cat",label:"Cat"},{value:"rabbit",label:"Rabbit"}]}},{label:"Desired Pet",name:"desiredPet",required:!0,type:"chip",inputSettings:{options:[{value:"kangaroo",label:"Kangaroo"},{value:"cow",label:"Cow"},{value:"duckbillPlatypus",label:"Duckbill Platypus"},{value:"raccoon",label:"Raccoon"},{value:"hog",label:"Hog"},{value:"madagascarHissingCockroach",label:"Madagascar Hissing Cockroach"},{value:"pronghorn",label:"Pronghorn"},{value:"alligator",label:"Alligator"}]}},{...m,name:"homeAddress",label:"Home address"},{...m,name:"workAddress",label:"Work address"},{...m,name:"altAddress",label:"Alternative address"},{name:"mostVisitedLocations",label:"Most visited locations",type:"advancedSelection"},{name:"booksRead",label:"Books read",type:"advancedSelection",inputSettings:{options:[{value:"the",label:"The Great Gatsby"},{value:"cow",label:"To Kill a Mockingbird"},{value:"duckbillPlatypus",label:"Pride and Prejudice"},{value:"raccoon",label:"The Book Thief"},{value:"hog",label:"Lord of the Flies"},{value:"madagascarHissingCockroach",label:"The Catcher in the Rye"}]}},{name:"destinationsHeading",type:"raw"},{name:"whereToGo",label:"Where do you want to go?",type:"matrix",instructionText:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras venenatis arcu sed rutrum maximus.",inputSettings:{dataView:{columns:[{name:"name",label:"Name"}],data:[{id:1,name:"Australia"},{id:2,name:"Japan"}]},buttons:[{label:"Add",onClick:()=>null,intent:"info",variant:"text",mIcon:z}]}},{name:"availableStart",label:"When are you available?",type:"date",instructionText:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras venenatis arcu sed rutrum maximus.",inputSettings:{showTime:!0}},{name:"availableEnd",label:" ",type:"date",instructionText:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras venenatis arcu sed rutrum maximus.",inputSettings:{showTime:!0}},{...a,name:"eiffelTower",label:"Where is the Eiffel tower?",inputSettings:{...a.inputSettings,initialCenter:{lat:57.27050873488408,lng:-4.493444954407284}}},{...a,name:"lochNessMonster",label:"Where's the Loch Ness monster?",inputSettings:{...a.inputSettings,initialCenter:{lat:48.858348895100555,lng:2.294492026111051}}},{...a,name:"novaScotia",label:"Where is Nova Scotia?",inputSettings:{...a.inputSettings,initialCenter:{lat:48.858348895100555,lng:2.294492026111051}}}],[]),[y,l]=u.useState(!1),p=()=>l(!1),C=[{label:"Save",onClick:f(s=>{l(!1),alert("Form submitted with the following data: "+JSON.stringify(s,null," "))}),intent:"primary",variant:"contained"}];return e.jsxs(e.Fragment,{children:[e.jsx(P,{open:y,onClose:p,PaperProps:{sx:{width:"90%"}},children:e.jsx("div",{style:{width:w,height:"100vh"},children:e.jsx(k,{...d,buttons:C,title:"Drawer form example",fields:S,onBack:p,sections:x?W:void 0})})}),e.jsx("div",{style:{margin:20},children:e.jsx(A,{onClick:()=>l(!0),label:"Open Drawer",intent:"primary",variant:"contained"})})]})};i.args={showSections:!1,drawWidth:v.default};i.argTypes={showSections:{name:"Show Sections"},drawWidth:{name:"Draw Width",control:{type:"select"},options:v}};var b,g,h;i.parameters={...i.parameters,docs:{...(b=i.parameters)==null?void 0:b.docs,source:{originalSource:`({
+  showSections,
+  drawWidth
+}: typeof DrawerForm.args): ReactElement => {
+  const controller = useForm({
+    data: {
+      petsHeading: <Heading>Pets</Heading>,
+      destinationsHeading: <Heading>Destinations</Heading>,
+      novaScotia: {
+        lat: 44.64933472911243,
+        lng: -63.615047475871876
+      },
+      eiffelTower: {
+        lat: 48.858348895100555,
+        lng: 2.294492026111051
+      },
+      lochNessMonster: {
+        lat: 57.27050873488408,
+        lng: -4.493444954407284
+      }
+    }
+  });
+  const {
+    handleSubmit
+  } = controller;
+  const fields = useMemo<FieldDef[]>(() => [{
+    ...baseTextField,
+    name: "text-xs",
+    label: "Extra small text",
+    size: Sizes.xs
+  }, {
+    ...baseTextField,
+    name: "text-sm",
+    label: "Small text",
+    size: Sizes.sm
+  }, {
+    ...baseTextField,
+    name: "text-md",
+    label: "Medium text",
+    size: Sizes.md
+  }, {
+    ...baseTextField,
+    name: "text-lg",
+    label: "Email address",
+    size: Sizes.lg,
+    validators: [validateEmail]
+  }, {
+    ...baseTextField,
+    name: "firstName",
+    label: "First Name"
+  }, {
+    ...baseTextField,
+    name: "lastName",
+    label: "Last Name"
+  }, {
+    type: "date",
+    name: "dob",
+    label: "Date of Birth"
+  }, {
+    name: "profilePicture",
+    label: "Profile Picture",
+    type: "upload",
+    inputSettings: {
+      onFileAdd: async ({
+        onUploadComplete
+      }) => {
+        onUploadComplete({
+          name: "Test",
+          fileUrl: "https://www.google.com",
+          id: uniqueId(),
+          size: 1098
+        });
+      },
+      onFileDelete: async () => {
+        // This is fine
+      }
+    },
+    required: true
+  }, {
+    ...baseTextField,
+    name: "biography",
+    label: "Biography",
+    size: Sizes.lg,
+    inputSettings: {
+      multiline: true,
+      minRows: 8
+    }
+  }, {
+    name: "phone",
+    label: "Phone",
+    type: "phone"
+  }, {
+    name: "petsHeading",
+    type: "raw"
+  }, {
+    label: "Favourite Pet",
+    name: "favouritePet",
+    required: true,
+    type: "dropdown",
+    inputSettings: {
+      options: [{
+        value: "dog",
+        label: "Dog"
+      }, {
+        value: "cat",
+        label: "Cat"
+      }, {
+        value: "rabbit",
+        label: "Rabbit"
+      }]
+    }
+  }, {
+    label: "First Pet",
+    name: "firstPet",
+    required: true,
+    type: "dropdown",
+    inputSettings: {
+      options: [{
+        value: "dog",
+        label: "Dog"
+      }, {
+        value: "cat",
+        label: "Cat"
+      }, {
+        value: "rabbit",
+        label: "Rabbit"
+      }]
+    }
+  }, {
+    label: "Desired Pet",
+    name: "desiredPet",
+    required: true,
+    type: "chip",
+    inputSettings: {
+      options: [{
+        value: "kangaroo",
+        label: "Kangaroo"
+      }, {
+        value: "cow",
+        label: "Cow"
+      }, {
+        value: "duckbillPlatypus",
+        label: "Duckbill Platypus"
+      }, {
+        value: "raccoon",
+        label: "Raccoon"
+      }, {
+        value: "hog",
+        label: "Hog"
+      }, {
+        value: "madagascarHissingCockroach",
+        label: "Madagascar Hissing Cockroach"
+      }, {
+        value: "pronghorn",
+        label: "Pronghorn"
+      }, {
+        value: "alligator",
+        label: "Alligator"
+      }]
+    }
+  }, {
+    ...baseAddressField,
+    name: "homeAddress",
+    label: "Home address"
+  }, {
+    ...baseAddressField,
+    name: "workAddress",
+    label: "Work address"
+  }, {
+    ...baseAddressField,
+    name: "altAddress",
+    label: "Alternative address"
+  }, {
+    name: "mostVisitedLocations",
+    label: "Most visited locations",
+    type: "advancedSelection"
+  }, {
+    name: "booksRead",
+    label: "Books read",
+    type: "advancedSelection",
+    inputSettings: {
+      options: [{
+        value: "the",
+        label: "The Great Gatsby"
+      }, {
+        value: "cow",
+        label: "To Kill a Mockingbird"
+      }, {
+        value: "duckbillPlatypus",
+        label: "Pride and Prejudice"
+      }, {
+        value: "raccoon",
+        label: "The Book Thief"
+      }, {
+        value: "hog",
+        label: "Lord of the Flies"
+      }, {
+        value: "madagascarHissingCockroach",
+        label: "The Catcher in the Rye"
+      }]
+    }
+  }, {
+    name: "destinationsHeading",
+    type: "raw"
+  } as any, {
+    name: "whereToGo",
+    label: "Where do you want to go?",
+    type: "matrix",
+    instructionText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras venenatis arcu sed rutrum maximus.",
+    inputSettings: {
+      dataView: {
+        columns: [{
+          name: "name",
+          label: "Name"
+        }],
+        data: [{
+          id: 1,
+          name: "Australia"
+        }, {
+          id: 2,
+          name: "Japan"
+        }]
+      },
+      buttons: [{
+        label: "Add",
+        onClick: () => null,
+        intent: "info",
+        variant: "text",
+        mIcon: AddCircleOutline
+      }]
+    }
+  }, {
+    name: "availableStart",
+    label: "When are you available?",
+    type: "date",
+    instructionText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras venenatis arcu sed rutrum maximus.",
+    inputSettings: {
+      showTime: true
+    }
+  }, {
+    name: "availableEnd",
+    label: " ",
+    type: "date",
+    instructionText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras venenatis arcu sed rutrum maximus.",
+    inputSettings: {
+      showTime: true
+    }
+  }, {
+    ...baseMapCoordinatesField,
+    name: "eiffelTower",
+    label: "Where is the Eiffel tower?",
+    inputSettings: {
+      ...baseMapCoordinatesField.inputSettings,
+      initialCenter: {
+        lat: 57.27050873488408,
+        lng: -4.493444954407284
+      }
+    }
+  }, {
+    ...baseMapCoordinatesField,
+    name: "lochNessMonster",
+    label: "Where's the Loch Ness monster?",
+    inputSettings: {
+      ...baseMapCoordinatesField.inputSettings,
+      initialCenter: {
+        lat: 48.858348895100555,
+        lng: 2.294492026111051
+      }
+    }
+  }, {
+    ...baseMapCoordinatesField,
+    name: "novaScotia",
+    label: "Where is Nova Scotia?",
+    inputSettings: {
+      ...baseMapCoordinatesField.inputSettings,
+      initialCenter: {
+        lat: 48.858348895100555,
+        lng: 2.294492026111051
+      }
+    }
+  }], []);
+  const [open, setOpen] = useState(false);
+  const onCancel = () => setOpen(false);
+  const onDrawerSubmit = handleSubmit(data => {
+    setOpen(false);
+    alert("Form submitted with the following data: " + JSON.stringify(data, null, " "));
+  });
+  const buttons: ButtonProps[] = [{
+    label: "Save",
+    onClick: onDrawerSubmit,
+    intent: "primary",
+    variant: "contained"
+  }];
+  return <>
+            <Drawer open={open} onClose={onCancel} PaperProps={{
+      sx: {
+        width: "90%"
+      }
+    }}>
+                <div style={{
+        width: drawWidth,
+        height: "100vh"
+      }}>
+                    <Form {...controller} buttons={buttons} title="Drawer form example" fields={fields} onBack={onCancel} sections={showSections ? sections : undefined} />
+                </div>
+            </Drawer>
+            <div style={{
+      margin: 20
+    }}>
+                <Button onClick={() => setOpen(true)} label="Open Drawer" intent="primary" variant="contained" />
+            </div>
+        </>;
+}`,...(h=(g=i.parameters)==null?void 0:g.docs)==null?void 0:h.source}}};const Qe=["DrawerForm"];export{i as DrawerForm,Qe as __namedExportsOrder,Ze as default};
