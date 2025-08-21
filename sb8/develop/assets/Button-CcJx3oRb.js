89e7fca5b1f06a0df1c8d9e70fcbc0e47b9b0192
Owen Allen <owenallenaz@gmail.com>
2025-08-21 13:38:56 +0000

    Updates

---
+++ sb8/develop/assets/Button-CcJx3oRb.js
@@ -0,0 +1,17 @@
+import{j as o}from"./jsx-runtime-EKYJJIwR.js";import{useMDXComponents as n}from"./index-BwDDMXeo.js";import{M as p,C as e}from"./index-vRJdbGSw.js";import{s as m,P as s}from"./Button.stories-CU05BGp1.js";import"./index-D0AnReJb.js";import"./iframe-Dl4xZWxJ.js";import"../sb-preview/runtime.js";import"./index-Bqzh3Cp6.js";import"./index-SI8xS9YR.js";import"./tslib.es6-CvpyDmub.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-DoexKoos.js";import"./_baseForOwn-YgwlavHx.js";import"./index-QVqIEGA1.js";import"./_arrayIncludesWith-XJRwRZaz.js";import"./index-DrFu-skq.js";import"./Button-CyFYeSR6.js";import"./styled-components.browser.esm-8X0uN6v9.js";import"./sizes-DkIXWPpc.js";import"./identifier-DOqdnZ6L.js";import"./generateUtilityClasses-BL8vvUJw.js";import"./ButtonBase-O4ZQd6U4.js";import"./index-CYsLg4TE.js";import"./Popper-CeYJ7yNz.js";import"./useTheme-CJVJa3gz.js";import"./useThemeWithoutDefault-BQF-0HLi.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useToggle-CTbnrs6g.js";import"./Create-CiSRifbU.js";import"./styled-Cb9FMILg.js";import"./emotion-is-prop-valid.cjs.default-BrZTFlsY.js";import"./createSvgIcon-DtczVsqP.js";import"./Add-BJ5QuHLi.js";import"./TaskAlt-RXyojKCJ.js";import"./CloudDownload-D9naCuWK.js";import"./toggleOptions-1cXvXfFl.js";import"./useStretchedWidthStory-CuY1g7oB.js";import"./Typography-CZQzWHyE.js";import"./DisplayText-DLjAy6kH.js";import"./styled-components.browser.esm-njFAddlw.js";import"./BodyText-Jkm8d11N.js";import"./Contacts-BEiOy0Fv.js";import"./Delete-uzHJ4pI6.js";import"./Edit-Bk4AvtQF.js";import"./Help-Cv3bsFBc.js";import"./Home-BptBjTMc.js";import"./Public-DZ4IpRPG.js";import"./Visibility-BAjH-T2s.js";function i(r){const t={a:"a",code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...n(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(p,{of:m}),`
+`,o.jsx(t.h1,{id:"button",children:"Button"}),`
+`,o.jsx(t.p,{children:"Displays a button."}),`
+`,o.jsx(t.h2,{id:"props",children:"Props"}),`
+`,o.jsx(t.p,{children:o.jsx(t.a,{href:"https://github.com/simpleviewinc/sv-mosaic/blob/develop/containers/mosaic/src/components/Button/ButtonTypes.tsx",rel:"nofollow",children:"https://github.com/simpleviewinc/sv-mosaic/blob/develop/containers/mosaic/src/components/Button/ButtonTypes.tsx"})}),`
+`,o.jsx(t.h2,{id:"popover",children:"Popover"}),`
+`,o.jsxs(t.p,{children:["When using ",o.jsx(t.code,{children:"popover"})," if you need to programmatically close it, you can do so by using ",o.jsx(t.code,{children:"onClose"})," provided by the React context API."]}),`
+`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-js",children:`import * as React from "react";
+import { useContext } from "react";
+import { Button, ButtonPopoverContext } from "#mosaic";
+
+function MyComponent(props) {
+	const buttonPopoverContext = useContext(ButtonPopoverContext);
+	// ...rest of component
+}
+`})}),`
+`,o.jsx(e,{of:s})]})}function eo(r={}){const{wrapper:t}={...n(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(i,{...r})}):i(r)}export{eo as default};
