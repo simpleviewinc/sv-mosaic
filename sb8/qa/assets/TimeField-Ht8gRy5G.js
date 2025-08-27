c1929fb94eaf1c2856d7baaefe570e864daae0dd
Owen Allen <owenallenaz@gmail.com>
2025-08-27 09:07:49 +0000

    Updates

---
+++ sb8/qa/assets/TimeField-Ht8gRy5G.js
@@ -0,0 +1,23 @@
+import{j as t}from"./jsx-runtime-EKYJJIwR.js";import{useMDXComponents as m}from"./index-BwDDMXeo.js";import{M as e}from"./index-vr38IWFQ.js";import{s as p}from"./TimeField.stories-CxVIu1qd.js";import"./index-D0AnReJb.js";import"./iframe-CIKHt0aS.js";import"../sb-preview/runtime.js";import"./index-Bqzh3Cp6.js";import"./index-SI8xS9YR.js";import"./tslib.es6-CvpyDmub.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-DoexKoos.js";import"./_baseForOwn-YgwlavHx.js";import"./index-QVqIEGA1.js";import"./_arrayIncludesWith-XJRwRZaz.js";import"./index-DrFu-skq.js";import"./index-BjjNLCMz.js";import"./styled-components.browser.esm-8X0uN6v9.js";import"./sizes-DkIXWPpc.js";import"./ExpandMore-JUNNgEdW.js";import"./identifier-DOqdnZ6L.js";import"./createSvgIcon-B2sN9389.js";import"./createSvgIcon-C3PpPknf.js";import"./generateUtilityClasses-BL8vvUJw.js";import"./createChainedFunction-BO_9K8Jh.js";import"./ownerWindow-DvT1GKkC.js";import"./useIsFocusVisible-o8lB1h8w.js";import"./useId-BgzzDjMB.js";import"./useControlled-DuXZYv4M.js";import"./useMosaicTranslation-di9nduwf.js";import"./testIds-DLbO0bd9.js";import"./TitleWrapper-CQxWIXzH.js";import"./Button-CN4kYzMe.js";import"./ButtonBase-Dg-QHo0d.js";import"./index-CYsLg4TE.js";import"./Popover-BG0BPeg-.js";import"./Popper-DKTKzQfx.js";import"./useTheme-CJVJa3gz.js";import"./useThemeWithoutDefault-BQF-0HLi.js";import"./useToggle-CTbnrs6g.js";import"./containerQuery-CYj0-5X2.js";import"./Typography-CZQzWHyE.js";import"./DisplayText-DLjAy6kH.js";import"./ButtonRow-C5wOwzPe.js";import"./toNumber-DuWhFW-p.js";import"./DataViewFilterDropdownButtons-DRwFtKQ6.js";import"./StyledPopperPaper-DKpxPU4i.js";import"./index-CtsnE9bo.js";import"./Chip-CpsUtANy.js";import"./DataViewPrimaryFilter.styled-hTjiiCYc.js";import"./Badge-D14GCFVi.js";import"./Text-BjwqtcAz.js";import"./PickerPanel-D4_DzPYT.js";import"./PageHeader-Rtx_tFBF.js";import"./FormFieldText.styled-CCsJeQGV.js";import"./formControlState-BI4toe_X.js";import"./Spinner-BATOmOoB.js";import"./CheckboxList-CIADAAOD.js";import"./Checkbox-DnVUUwfp.js";import"./StyledOptionFormControl-CmOcJT1x.js";import"./MoreVert-BXVssJ4w.js";import"./formats-CMvQHWsT.js";import"./Snackbar-Bv8HGMID.js";import"./Close-BKfiyhHV.js";import"./ClickAwayListener-Cp0VGVh1.js";import"./ChipList-TQ6-UhAA.js";import"./RadioButton-1BL3SEux.js";import"./Toggle-BYU1iYU_.js";import"./Dialog-Bg1_LB1W.js";import"./ThemeProvider-CFABLTDy.js";import"./SideNav-CFjdJbjv.js";import"./renderButtons-DP0bGf4C.js";import"./commonFieldControl-LQDYMh-C.js";import"./parseDateControl-DtolAPSn.js";function o(i){const r={a:"a",code:"code",h1:"h1",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...m(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:p}),`
+`,t.jsx(r.h1,{id:"time",children:"Time"}),`
+`,t.jsxs(r.p,{children:["This field implements the ",t.jsx(r.a,{href:"#generic-field-props-fielddef",children:t.jsx(r.strong,{children:"FieldDef"})})," interface."]}),`
+`,t.jsxs(r.ul,{children:[`
+`,t.jsx(r.li,{children:"It allows the user to view and pick a time from a calendar widget or manually type the time in the text field."}),`
+`,t.jsx(r.li,{children:t.jsx(r.a,{href:"/?path=/story/formfields-formfieldtimefield--playground",children:t.jsx(r.strong,{children:"Playground"})})}),`
+`,t.jsxs(r.li,{children:["Data: ",t.jsx(r.code,{children:"string"})," - Time in ",t.jsx(r.code,{children:"HH:MM"})," 24 hour format."]}),`
+`,t.jsx(r.li,{children:"This field accepts no inputSettings"}),`
+`]}),`
+`,t.jsx(r.h3,{id:"how-to-use-in-a-form",children:"How to use in a form?"}),`
+`,t.jsx(r.pre,{children:t.jsx(r.code,{className:"language-ts",children:`const fields = useMemo(\r
+	() =>\r
+		[\r
+			//...other fields\r
+			{\r
+				//...all generic field props,\r
+				type: "time",\r
+			},\r
+			//...other fields\r
+		],\r
+	[]\r
+);
+`})})]})}function kt(i={}){const{wrapper:r}={...m(),...i.components};return r?t.jsx(r,{...i,children:t.jsx(o,{...i})}):o(i)}export{kt as default};
