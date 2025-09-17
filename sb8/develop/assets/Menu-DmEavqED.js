3d2e47b5da608f3b7a8919db6ee271afaca433d8
Owen Allen <owenallenaz@gmail.com>
2025-09-17 10:37:29 +0000

    Updates

---
+++ sb8/develop/assets/Menu-DmEavqED.js
@@ -0,0 +1,14 @@
+import{j as o}from"./jsx-runtime-EKYJJIwR.js";import{useMDXComponents as e}from"./index-BwDDMXeo.js";import{M as s,C as t}from"./index-BJnGodqB.js";import{s as m,P as p,W as c}from"./Menu.stories-xiMFI_rC.js";import"./index-D0AnReJb.js";import"./iframe-B4Rh5W6Q.js";import"../sb-preview/runtime.js";import"./index-Bqzh3Cp6.js";import"./index-SI8xS9YR.js";import"./tslib.es6-CvpyDmub.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-DoexKoos.js";import"./_baseForOwn-YgwlavHx.js";import"./index-QVqIEGA1.js";import"./_arrayIncludesWith-XJRwRZaz.js";import"./index-DrFu-skq.js";import"./Create-CiSRifbU.js";import"./styled-Cb9FMILg.js";import"./emotion-is-prop-valid.cjs.default-BrZTFlsY.js";import"./createSvgIcon-DtczVsqP.js";import"./Delete-uzHJ4pI6.js";import"./CloudDownload-D9naCuWK.js";import"./Button-Dlaq5vsb.js";import"./styled-components.browser.esm-8X0uN6v9.js";import"./sizes-DkIXWPpc.js";import"./identifier-DOqdnZ6L.js";import"./generateUtilityClasses-BL8vvUJw.js";import"./ButtonBase-O4ZQd6U4.js";import"./index-CYsLg4TE.js";import"./Popper-CeYJ7yNz.js";import"./useTheme-CJVJa3gz.js";import"./useThemeWithoutDefault-BQF-0HLi.js";import"./ownerWindow-DvT1GKkC.js";import"./createChainedFunction-BO_9K8Jh.js";import"./Typography-CZQzWHyE.js";import"./DisplayText-DLjAy6kH.js";function r(n){const i={a:"a",code:"code",h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...e(),...n.components};return o.jsxs(o.Fragment,{children:[o.jsx(s,{of:m}),`
+`,o.jsx(i.h1,{id:"menu",children:"Menu"}),`
+`,o.jsxs(i.p,{children:[o.jsx(i.code,{children:"Menu"})," is a component that allows you to invoke a dropdown of MenuItems. Each menuItem can have it's own unique ",o.jsx(i.code,{children:"onClick"})," handler."]}),`
+`,o.jsx(i.h2,{id:"props",children:"Props"}),`
+`,o.jsxs(i.ul,{children:[`
+`,o.jsx(i.li,{children:"anchorEl - HTMLElement"}),`
+`,o.jsx(i.li,{children:"open - boolean"}),`
+`,o.jsx(i.li,{children:"onClose - function"}),`
+`,o.jsxs(i.li,{children:["items - Array of ",o.jsx(i.a,{href:"https://github.com/simpleviewinc/sv-mosaic/blob/develop/containers/mosaic/src/components/MenuItem/MenuItemTypes.ts",rel:"nofollow",children:"MenuItemProps"})]}),`
+`]}),`
+`,o.jsx(i.h2,{id:"basic",children:"Basic"}),`
+`,o.jsx(t,{of:p}),`
+`,o.jsx(i.h2,{id:"with-icons-and-colors",children:"With Icons and Colors"}),`
+`,o.jsx(t,{of:c})]})}function S(n={}){const{wrapper:i}={...e(),...n.components};return i?o.jsx(i,{...n,children:o.jsx(r,{...n})}):r(n)}export{S as default};
