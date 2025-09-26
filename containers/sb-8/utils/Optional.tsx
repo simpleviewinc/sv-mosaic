ac95300022d0491fdca4732f83c3d34aa0a38430
George Batt <george.batt@simpleviewinc.com>
2025-09-26 16:10:21 +0100

    (Content) Documentation updates and use Roboto for doc font face.

---
+++ containers/sb-8/utils/Optional.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import theme from "@simpleview/sv-mosaic/theme";
+
+export function Optional() {
+	return (
+		<span style={{ color: theme.color.gray[600] }}>Optional</span>
+	);
+}
