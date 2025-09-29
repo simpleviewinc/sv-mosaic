99097d98e2b847e99f240fae11c60fdd79df340a
George Batt <george.batt@simpleviewinc.com>
2025-09-29 16:24:43 +0100

    (Content) Automation testing for column breakpoints.

--- containers/e2e-tests/tests/Components/Content/Content.spec.ts
+++ containers/e2e-tests/tests/Components/Content/Content.spec.ts
@@ -1,8 +1,8 @@
 import type { Page } from "@playwright/test";
-import { test, expect } from "@playwright/test";
+import { test, expect } from "fixtures";
 import { ContentPage } from "../../../pages/Components/Content/ContentPage";
 import theme from "@simpleview/sv-mosaic/theme";
-import { cardKnobs as knob, pageHeaderKnobs } from "../../../utils/data/knobs";
+import { contentKnobs, pageHeaderKnobs } from "../../../utils/data/knobs";
 import { ensureRgbCss } from "utils/helpers/color";
 
 test.describe("Components - Content - Playground", () => {
@@ -32,5 +32,83 @@ test.describe("Components - Content - Playground", () => {
 	test("Validate that when the buttons knob is undefined, the additional buttons are not displayed.", async () => {
 		await contentPage.visit(contentPage.page_path, [pageHeaderKnobs.knobButtons + "undefined"]);
 		await expect(contentPage.editButton).not.toBeVisible();
+	});
+
+	test("should display fields in a single column at all container sizes", async () => {
+		await contentPage.visit(contentPage.page_path, contentKnobs.knobSingleField);
+		await page.setViewportSize({ width: 380, height: 800 });
+		const row = page.getByTestId("mos:Content:row");
+		await expect(row).toBeVisible();
+		await expect(row).toHaveGridColumns(1);
+		await page.setViewportSize({ width: 900, height: 800 });
+		await expect(row).toHaveGridColumns(1);
+	});
+
+	test("should display fields as columns at the default (medium) breakpoint", async () => {
+		await contentPage.visit(contentPage.page_path, contentKnobs.knobTwoColumns);
+
+		const container = page.getByTestId("mos:Content:fieldList");
+		const viewportGutter = await contentPage.getElementViewportGutter(container, 380);
+
+		const row = page.getByTestId("mos:Content:row");
+		await expect(row).toBeVisible();
+		await expect(row).toHaveGridColumns(1);
+		await page.setViewportSize({ width: 767 + viewportGutter, height: 800 });
+		await expect(row).toHaveGridColumns(1);
+		await page.setViewportSize({ width: 768 + viewportGutter, height: 800 });
+		await expect(row).toHaveGridColumns(2);
+	});
+
+	test("should display fields as columns at a custom (large) breakpoint", async () => {
+		await contentPage.visit(contentPage.page_path, [contentKnobs.knobProvideColumnsLarge, ...contentKnobs.knobTwoColumns]);
+
+		const container = page.getByTestId("mos:Content:fieldList");
+		const viewportGutter = await contentPage.getElementViewportGutter(container, 380);
+
+		const row = page.getByTestId("mos:Content:row");
+		await expect(row).toBeVisible();
+		await expect(row).toHaveGridColumns(1);
+		await page.setViewportSize({ width: 1023 + viewportGutter, height: 800 });
+		await expect(row).toHaveGridColumns(1);
+		await page.setViewportSize({ width: 1024 + viewportGutter, height: 800 });
+		await expect(row).toHaveGridColumns(2);
+	});
+
+	test.only("should display fields as columns with custom breakpoint rules", async () => {
+		await contentPage.visit(contentPage.page_path, [contentKnobs.knobProvideColumnsCustom, ...contentKnobs.knobFourColumns]);
+
+		const container = page.getByTestId("mos:Content:fieldList");
+		const viewportGutter = await contentPage.getElementViewportGutter(container, 380);
+
+		const row = page.getByTestId("mos:Content:row");
+		await expect(row).toBeVisible();
+		await expect(row).toHaveGridColumns(1);
+		await page.setViewportSize({ width: 767 + viewportGutter, height: 800 });
+		await expect(row).toHaveGridColumns(1);
+		await page.setViewportSize({ width: 768 + viewportGutter, height: 800 });
+		await expect(row).toHaveGridColumns(2);
+		await page.setViewportSize({ width: 1023 + viewportGutter, height: 800 });
+		await expect(row).toHaveGridColumns(2);
+		await page.setViewportSize({ width: 1024 + viewportGutter, height: 800 });
+		await expect(row).toHaveGridColumns(4);
+	});
+
+	test.only("should display individual row fields as columns with custom breakpoint rules", async () => {
+		await contentPage.visit(contentPage.page_path, [contentKnobs.knobProvideColumnsCustom, ...contentKnobs.knobFourColumns]);
+
+		const container = page.getByTestId("mos:Content:fieldList");
+		const viewportGutter = await contentPage.getElementViewportGutter(container, 380);
+
+		const row = page.getByTestId("mos:Content:row");
+		await expect(row).toBeVisible();
+		await expect(row).toHaveGridColumns(1);
+		await page.setViewportSize({ width: 767 + viewportGutter, height: 800 });
+		await expect(row).toHaveGridColumns(1);
+		await page.setViewportSize({ width: 768 + viewportGutter, height: 800 });
+		await expect(row).toHaveGridColumns(2);
+		await page.setViewportSize({ width: 1023 + viewportGutter, height: 800 });
+		await expect(row).toHaveGridColumns(2);
+		await page.setViewportSize({ width: 1024 + viewportGutter, height: 800 });
+		await expect(row).toHaveGridColumns(4);
 	});
 });
