bca8d6640c7ae6873d9fea4cdd36483e0e724cce
George Batt <george.batt@simpleviewinc.com>
2025-10-07 12:27:40 +0100

    (AddressField) Align address cards with Apex design system.

--- containers/mosaic/src/components/Field/FormFieldAddress/AddressCard/AddressCard.styled.tsx
+++ containers/mosaic/src/components/Field/FormFieldAddress/AddressCard/AddressCard.styled.tsx
@@ -1,33 +1,26 @@
+import { CardWrapper } from "@root/components/Card/Card.styled";
 import styled from "styled-components";
 
-// Utils
-import theme from "@root/theme";
-import ButtonRow from "@root/components/ButtonRow";
-
-export const StyledAddressCard = styled.div`
-  display: flex;
-  flex-direction: column;
-  color: ${theme.newColors.almostBlack["100"]};
-  background-color: ${theme.newColors.grey1["100"]};
-  height: 204px;
-  padding: 24px 16px 16px 24px;
+export const StyledCardWrapper = styled(CardWrapper)`
   width: 300px;
+`;
 
-  & span {
-    margin: 0;
-    overflow: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-	line-height: 20px;
+export const ScreenReaderOnly = styled.span`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  border: 0;
+`;
+
+export const AddressLine = styled.div`
+  margin-bottom: 4px;
+
+  &:last-child {
+    margin-bottom: 0;
   }
-`;
-
-export const AddressTitle = styled.span`
-  font-weight: ${theme.weight.medium};
-  margin-bottom: 12px !important;
-`;
-
-export const StyledButtonRow = styled(ButtonRow)`
-  align-self: flex-end;
-  margin-top: auto;
 `;
--- containers/mosaic/src/components/Field/FormFieldAddress/AddressCard/AddressCard.tsx
+++ containers/mosaic/src/components/Field/FormFieldAddress/AddressCard/AddressCard.tsx
@@ -1,57 +1,109 @@
-import * as React from "react";
 import type { ReactElement } from "react";
+
+import * as React from "react";
 import { memo } from "react";
 
-// Components
-import Button from "@root/components/Button";
-
-// Styles
-import {
-	AddressTitle,
-	StyledAddressCard,
-	StyledButtonRow,
-} from "./AddressCard.styled";
-
-// Types
 import type { AddressCardProps } from "../AddressTypes";
+import { ScreenReaderOnly, AddressLine, StyledCardWrapper } from "./AddressCard.styled";
 import { joinAnd } from "@root/utils/string";
+import { CardBottom, CardContent } from "@root/components/Card/Card.styled";
+import { EMPTY_ARRAY } from "@root/constants/stable";
+import ButtonRow from "@root/components/ButtonRow";
 
-const AddressCard = (props: AddressCardProps): ReactElement => {
-	const { address, onEdit, onRemoveAddress, disabled } = props;
+const AddressCard = ({ address, onEdit, onRemoveAddress, disabled }: AddressCardProps): ReactElement => {
+	const {
+		address1,
+		address2,
+		address3,
+		city,
+		country,
+		postalCode,
+		state,
+		types = EMPTY_ARRAY,
+	} = address;
 
-	const typesLabels = (address.types || []).map(type => type.label);
+	const typesLabels = types.map(type => type.label);
 
 	return (
-		<StyledAddressCard data-testid="address-card-test">
-			<AddressTitle>
-				{`${joinAnd(typesLabels)} Address`}
-			</AddressTitle>
-			<span aria-label="Address 1">{address?.address1}</span>
-			{address?.address2 && <span aria-label="Address 2">{address?.address2}</span>}
-			{address?.address3 && <span aria-label="Address 3">{address?.address3}</span>}
-			<span aria-label="City, State and Postal Code">
-				{`${address?.city}, ${address?.state?.label ? address.state.label : ""} ${address?.postalCode}`}
-			</span>
-			<span aria-label="Country">{address?.country?.label}</span>
-			<StyledButtonRow separator>
-				<Button
-					label="Edit"
-					intent="info"
-					variant="text"
-					size="small"
-					disabled={disabled}
-					onClick={() => onEdit(address)}
+		<StyledCardWrapper data-testid="address-card-test">
+			<CardContent $paddingBottom $compact>
+				<AddressLine>
+					<ScreenReaderOnly>Address types: </ScreenReaderOnly>
+					<strong>{`${joinAnd(typesLabels)} Address`}</strong>
+				</AddressLine>
+				<div role="group" aria-label="Mailing address">
+					{address1 && (
+						<AddressLine>
+							<ScreenReaderOnly>Street address: </ScreenReaderOnly>
+							{address1}
+						</AddressLine>
+					)}
+					{address2 && (
+						<AddressLine>
+							<ScreenReaderOnly>Address line 2: </ScreenReaderOnly>
+							{address2}
+						</AddressLine>
+					)}
+					{address3 && (
+						<AddressLine>
+							<ScreenReaderOnly>Address line 3: </ScreenReaderOnly>
+							{address3}
+						</AddressLine>
+					)}
+					{(city || state || postalCode) && (
+						<AddressLine>
+							{city && (
+								<>
+									<ScreenReaderOnly>City: </ScreenReaderOnly>
+									{city}
+									{(state || postalCode) && ", "}
+								</>
+							)}
+							{state && (
+								<>
+									<ScreenReaderOnly>State: </ScreenReaderOnly>
+									{state.label}
+									{postalCode && " "}
+								</>
+							)}
+							{postalCode && (
+								<>
+									<ScreenReaderOnly>Postal code: </ScreenReaderOnly>
+									{postalCode}
+								</>
+							)}
+						</AddressLine>
+					)}
+					{country && (
+						<AddressLine>
+							<ScreenReaderOnly>Country: </ScreenReaderOnly>
+							{country.label}
+						</AddressLine>
+					)}
+				</div>
+			</CardContent>
+			<CardBottom $compact>
+				<ButtonRow buttons={[
+					{
+						label: "Edit",
+						intent: "info",
+						variant: "text",
+						size: "small",
+						disabled: disabled,
+						onClick: () => onEdit(address),
+					},
+					{
+						intent: "danger",
+						variant: "text",
+						size: "small",
+						disabled: disabled,
+						label: "Remove",
+						onClick: () => onRemoveAddress(address),
+					},
+				]}
 				/>
-				<Button
-					intent="danger"
-					variant="text"
-					size="small"
-					disabled={disabled}
-					label="Remove"
-					onClick={() => onRemoveAddress(address)}
-				/>
-			</StyledButtonRow>
-		</StyledAddressCard>
+			</CardBottom>
+		</StyledCardWrapper>
 	);
 };
 
