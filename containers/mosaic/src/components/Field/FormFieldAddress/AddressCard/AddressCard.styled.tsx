d36c663cd808e1c5f27b2ef0cc5a4cece730ea84
George Batt <george.batt@simpleviewinc.com>
2025-10-07 12:54:45 +0100

    (AddressField) Add address card line labels.

--- containers/mosaic/src/components/Field/FormFieldAddress/AddressCard/AddressCard.styled.tsx
+++ containers/mosaic/src/components/Field/FormFieldAddress/AddressCard/AddressCard.styled.tsx
@@ -1,26 +1,15 @@
 import { CardWrapper } from "@root/components/Card/Card.styled";
+import theme from "@root/theme";
 import styled from "styled-components";
 
 export const StyledCardWrapper = styled(CardWrapper)`
-  width: 300px;
-`;
-
-export const ScreenReaderOnly = styled.span`
-  position: absolute;
-  width: 1px;
-  height: 1px;
-  padding: 0;
-  margin: -1px;
-  overflow: hidden;
-  clip: rect(0, 0, 0, 0);
-  white-space: nowrap;
-  border: 0;
+	width: 300px;
 `;
 
 export const AddressLine = styled.div`
-  margin-bottom: 4px;
+	margin-bottom: ${theme.spacing(1)};
 
-  &:last-child {
-    margin-bottom: 0;
-  }
+	&:last-child {
+		margin-bottom: 0;
+	}
 `;
