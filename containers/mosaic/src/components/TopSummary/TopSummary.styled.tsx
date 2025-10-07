67345c99d68f822de31ef2adcc97c4e1ab83c4ff
George Batt <george.batt@simpleviewinc.com>
2025-10-07 12:34:24 +0100

    Minor border color changes.

--- containers/mosaic/src/components/TopSummary/TopSummary.styled.tsx
+++ containers/mosaic/src/components/TopSummary/TopSummary.styled.tsx
@@ -2,75 +2,74 @@ import styled from "styled-components";
 import theme from "../../theme";
 
 export const StyledTopSummary = styled.div`
-  background: white;
-  border-bottom: 2px solid ${theme.newColors.grey2["100"]};
-  padding: 24px 24px 16px 24px;
-  display: flex;
-  gap: 24px;
-  align-items: center;
+	border-bottom: 1px solid ${theme.color.gray[300]};
+	padding: ${theme.spacing(5, 6)};
+	display: flex;
+	gap: ${theme.spacing(5)};
+	align-items: center;
 
-  .img-rounded {
-    width: 71px;
-    height: 71px;
-    border: 2px solid ${theme.newColors.grey2["100"]};
-    border-radius: 50%;
-    opacity: 1;
-  }
+	.img-rounded {
+		width: 71px;
+		height: 71px;
+		border: 2px solid ${theme.newColors.grey2["100"]};
+		border-radius: 50%;
+		opacity: 1;
+	}
 `;
 
 export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-  width: 100%;
-  flex-wrap: wrap;
+	display: flex;
+	flex-direction: column;
+	gap: 16px;
+	width: 100%;
+	flex-wrap: wrap;
 `;
 
 export const ContainerTitle = styled.div`
-  display: flex;
-  align-items: center;
-  flex-wrap: wrap;
+	display: flex;
+	align-items: center;
+	flex-wrap: wrap;
 
-  & h1 {
-    max-width: 650px;
-  }
+	& h1 {
+		max-width: 650px;
+	}
 
-  & .checked,
-  .unchecked {
-    margin-left: 6px;
-  }
+	& .checked,
+	.unchecked {
+		margin-left: 6px;
+	}
 `;
 
 export const Row = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-  flex-wrap: wrap;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	width: 100%;
+	flex-wrap: wrap;
 `;
 
 export const ContainerItems = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
+	display: flex;
+	flex-wrap: wrap;
+	align-items: center;
 
-  div:last-child {
-    border: none;
-  }
+	div:last-child {
+    	border: none;
+	}
 `;
 
 export const Item = styled.div`
-  & p,
-  & h1,
-  & h2,
-  & h3,
-  & h4,
-  & h5,
-  & h6 {
-    margin: 0px;
-    max-width: 280px;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-  }
+	& p,
+	& h1,
+	& h2,
+	& h3,
+	& h4,
+	& h5,
+	& h6 {
+		margin: 0px;
+		max-width: 280px;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+	}
 `;
