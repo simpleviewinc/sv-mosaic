3722135599e7ed4674bb34d3fb842fef4a2376eb
Owen Allen <owenallenaz@gmail.com>
2025-10-14 08:16:32 +0000

    Updates

---
+++ sb8/master/assets/ConditionalFields.stories-UsArwhz4.js
@@ -0,0 +1,41 @@
+import{j as e}from"./jsx-runtime-EKYJJIwR.js";import{r as i}from"./index-D0AnReJb.js";import{u as c,F as y}from"./index-B3jGg_If.js";import{r as h}from"./renderButtons-DP0bGf4C.js";import{O as f}from"./dom-BR2PSRFh.js";import"./ExpandMore-Ca3pe8z0.js";import"./identifier-DOqdnZ6L.js";import"./sizes-DkIXWPpc.js";import"./createSvgIcon-DDyBApXB.js";import"./createSvgIcon-C3PpPknf.js";import"./generateUtilityClasses-BL8vvUJw.js";import"./styled-components.browser.esm-8X0uN6v9.js";import"./createChainedFunction-BO_9K8Jh.js";import"./ownerWindow-DvT1GKkC.js";import"./ButtonBase-O4ZQd6U4.js";import"./useId-BgzzDjMB.js";import"./useControlled-DuXZYv4M.js";import"./MosaicContext-Y61KIvFP.js";import"./FormFieldText.styled-DQ22Tenv.js";import"./formControlState-BI4toe_X.js";import"./Popper-CeYJ7yNz.js";import"./useTheme-CJVJa3gz.js";import"./useThemeWithoutDefault-BQF-0HLi.js";import"./index-Bqzh3Cp6.js";import"./index-SI8xS9YR.js";import"./Button-Dlaq5vsb.js";import"./index-CYsLg4TE.js";import"./testIds-B6Pox1zA.js";import"./TitleWrapper-D8cEbksV.js";import"./containerQuery-CYj0-5X2.js";import"./ChevronLeft-Bjv82Anv.js";import"./Typography-CZQzWHyE.js";import"./DisplayText-DLjAy6kH.js";import"./ButtonRow-DsoxFHT_.js";import"./DataViewFilterDropdownButtons-bgrEEUrv.js";import"./StyledPopperPaper-BODJ-ail.js";import"./index-Dmgma1UG.js";import"./Chip-16cSQUDZ.js";import"./DataViewPrimaryFilter.styled-Ej2pGgIE.js";import"./Badge-D14GCFVi.js";import"./Text-BjwqtcAz.js";import"./PickerPanel-BJ1qYTpx.js";import"./PageHeader-DTKSoW5K.js";import"./Spinner-BATOmOoB.js";import"./CheckboxList-DFjk1mlV.js";import"./Checkbox-Cg_T_WRa.js";import"./StyledOptionFormControl-Br1jmfxi.js";import"./MoreVert-BAJenQd7.js";import"./stable-BFc8HSVh.js";import"./throws-DLZ75Q5e.js";import"./formats-CMvQHWsT.js";import"./Snackbar-YB8AgySY.js";import"./Close-DizofMnI.js";import"./ClickAwayListener-Cp7Ry44a.js";import"./ChipList-Dk1_tgce.js";import"./RadioButton-DplP-WlB.js";import"./Toggle-BnqnqUHF.js";import"./Dialog-CJUzIf8Q.js";import"./ThemeProvider-CFABLTDy.js";import"./SideNav-tVV9ac9c.js";const Rt={title:"Components/Form"},t=({showState:d})=>{const o=c(),{state:l,handleSubmit:a}=o,n="SHOW";i.useEffect(()=>(document.body.style.margin="0px",()=>{document.body.style.margin=f}),[]);const u=i.useMemo(()=>[{name:"dependency",label:"Dependency",type:"text",instructionText:`Type "${n}" to show another field`,required:!0},{name:"dependent",label:"Dependent",type:"text",show:({data:r})=>(r==null?void 0:r.dependency)===n,required:!0}],[]);return e.jsxs(e.Fragment,{children:[d&&e.jsx("pre",{children:JSON.stringify(l,null,"  ")}),e.jsx("div",{style:{height:"100vh"},children:e.jsx(y,{...o,buttons:h(a),title:"Runtime behaviors",fields:u})})]})};t.args={showState:!1};t.argTypes={showState:{name:"Show state"}};var m,p,s;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`({
+  showState
+}: typeof ConditionalFields.args): ReactElement => {
+  const controller = useForm();
+  const {
+    state,
+    handleSubmit
+  } = controller;
+  const KEY_PHRASE = "SHOW";
+  useEffect(() => {
+    document.body.style.margin = "0px";
+    return () => {
+      document.body.style.margin = ORIGINAL_BODY_MARGIN;
+    };
+  }, []);
+  const fields = useMemo((): FieldDef[] => [{
+    name: "dependency",
+    label: "Dependency",
+    type: "text",
+    instructionText: \`Type "\${KEY_PHRASE}" to show another field\`,
+    required: true
+  }, {
+    name: "dependent",
+    label: "Dependent",
+    type: "text",
+    show: ({
+      data
+    }) => {
+      return data?.dependency === KEY_PHRASE;
+    },
+    required: true
+  }], []);
+  return <>
+            {showState && <pre>{JSON.stringify(state, null, "  ")}</pre>}
+            <div style={{
+      height: "100vh"
+    }}>
+                <Form {...controller} buttons={renderButtons(handleSubmit)} title="Runtime behaviors" fields={fields} />
+            </div>
+        </>;
+}`,...(s=(p=t.parameters)==null?void 0:p.docs)==null?void 0:s.source}}};const _t=["ConditionalFields"];export{t as ConditionalFields,_t as __namedExportsOrder,Rt as default};
---
+++ sb8/master/assets/FormFieldMatrix-Dfn3GVWO.js
@@ -0,0 +1,116 @@
+import{j as t}from"./jsx-runtime-EKYJJIwR.js";import{useMDXComponents as o}from"./index-BwDDMXeo.js";import{M as r}from"./index-ErwLhiuT.js";import{s as a}from"./FormFieldMatrix.stories-DivWGNKZ.js";import"./index-D0AnReJb.js";import"./iframe-Dgo-5i4d.js";import"../sb-preview/runtime.js";import"./index-Bqzh3Cp6.js";import"./index-SI8xS9YR.js";import"./tslib.es6-CvpyDmub.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-DoexKoos.js";import"./_baseForOwn-YgwlavHx.js";import"./index-QVqIEGA1.js";import"./_arrayIncludesWith-XJRwRZaz.js";import"./index-DrFu-skq.js";import"./index-B3jGg_If.js";import"./ExpandMore-Ca3pe8z0.js";import"./identifier-DOqdnZ6L.js";import"./sizes-DkIXWPpc.js";import"./createSvgIcon-DDyBApXB.js";import"./createSvgIcon-C3PpPknf.js";import"./generateUtilityClasses-BL8vvUJw.js";import"./styled-components.browser.esm-8X0uN6v9.js";import"./createChainedFunction-BO_9K8Jh.js";import"./ownerWindow-DvT1GKkC.js";import"./ButtonBase-O4ZQd6U4.js";import"./useId-BgzzDjMB.js";import"./useControlled-DuXZYv4M.js";import"./MosaicContext-Y61KIvFP.js";import"./FormFieldText.styled-DQ22Tenv.js";import"./formControlState-BI4toe_X.js";import"./Popper-CeYJ7yNz.js";import"./useTheme-CJVJa3gz.js";import"./useThemeWithoutDefault-BQF-0HLi.js";import"./Button-Dlaq5vsb.js";import"./index-CYsLg4TE.js";import"./testIds-B6Pox1zA.js";import"./TitleWrapper-D8cEbksV.js";import"./containerQuery-CYj0-5X2.js";import"./ChevronLeft-Bjv82Anv.js";import"./Typography-CZQzWHyE.js";import"./DisplayText-DLjAy6kH.js";import"./ButtonRow-DsoxFHT_.js";import"./DataViewFilterDropdownButtons-bgrEEUrv.js";import"./StyledPopperPaper-BODJ-ail.js";import"./index-Dmgma1UG.js";import"./Chip-16cSQUDZ.js";import"./DataViewPrimaryFilter.styled-Ej2pGgIE.js";import"./Badge-D14GCFVi.js";import"./Text-BjwqtcAz.js";import"./PickerPanel-BJ1qYTpx.js";import"./PageHeader-DTKSoW5K.js";import"./Spinner-BATOmOoB.js";import"./CheckboxList-DFjk1mlV.js";import"./Checkbox-Cg_T_WRa.js";import"./StyledOptionFormControl-Br1jmfxi.js";import"./MoreVert-BAJenQd7.js";import"./stable-BFc8HSVh.js";import"./throws-DLZ75Q5e.js";import"./formats-CMvQHWsT.js";import"./Snackbar-YB8AgySY.js";import"./Close-DizofMnI.js";import"./ClickAwayListener-Cp7Ry44a.js";import"./ChipList-Dk1_tgce.js";import"./RadioButton-DplP-WlB.js";import"./Toggle-BnqnqUHF.js";import"./Dialog-CJUzIf8Q.js";import"./ThemeProvider-CFABLTDy.js";import"./SideNav-tVV9ac9c.js";import"./Add-BJ5QuHLi.js";import"./styled-Cb9FMILg.js";import"./emotion-is-prop-valid.cjs.default-BrZTFlsY.js";import"./createSvgIcon-DtczVsqP.js";import"./Create-CiSRifbU.js";import"./Delete-uzHJ4pI6.js";import"./Drawers-ykleyFLh.js";import"./renderButtons-DP0bGf4C.js";import"./commonFieldControl-LQDYMh-C.js";import"./useMosaicSettings-D4y8mIb9.js";function e(i){const n={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...o(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:a}),`
+`,t.jsx(n.h2,{id:"matrix",children:"Matrix"}),`
+`,t.jsxs(n.p,{children:["This field implements the ",t.jsx(n.a,{href:"#generic-field-props-fielddef",children:t.jsx(n.strong,{children:"FieldDef"})})," interface."]}),`
+`,t.jsxs(n.p,{children:[t.jsx(n.code,{children:"Matrix"}),` This component is used to dynamically create content in a data view. Some examples may be creating rows using a form fields or displaying
+selected rows from another Data View.`]}),`
+`,t.jsxs(n.ul,{children:[`
+`,t.jsxs(n.li,{children:["Data: ",t.jsx(n.a,{href:"#mosaicobject-type",children:t.jsx(n.code,{children:"MosaicObject[]"})})," - Array of objects containing all data required to popoulate the list"]}),`
+`,t.jsx(n.li,{children:"inputSettings:"}),`
+`]}),`
+`,t.jsxs(n.table,{children:[t.jsx(n.thead,{children:t.jsxs(n.tr,{children:[t.jsx(n.th,{children:"Name"}),t.jsx(n.th,{children:"Type"}),t.jsx(n.th,{children:"Description"})]})}),t.jsxs(n.tbody,{children:[t.jsxs(n.tr,{children:[t.jsx(n.td,{children:t.jsx(n.strong,{children:t.jsx(n.code,{children:"dataView"})})}),t.jsx(n.td,{children:t.jsx(n.code,{children:"DataViewProps"})}),t.jsxs(n.td,{children:["Data view configurations (see ",t.jsx(n.a,{href:"https://simpleviewinc.github.io/sv-mosaic/master/?path=/docs/components-dataview-readme--page#props",rel:"nofollow",children:"DataView props"}),")."]})]}),t.jsxs(n.tr,{children:[t.jsx(n.td,{children:t.jsx(n.strong,{children:t.jsx(n.code,{children:"buttons"})})}),t.jsx(n.td,{children:t.jsx(n.code,{children:"ButtonAttrs[]"})}),t.jsxs(n.td,{children:["List of buttons that will be display above the data view (see ",t.jsx(n.a,{href:"#buttonattrs-type",children:"ButtonAttrs type"}),")."]})]})]})]}),`
+`,t.jsx(n.h3,{id:"how-to-use-in-a-form",children:"How to use in a form?"}),`
+`,t.jsx(n.pre,{children:t.jsx(n.code,{className:"language-ts",children:`import formActions from "#mosaic/formActions";
+
+const { dispatch } = useForm();
+
+const listColumns = [
+	{
+		name: "id",
+		label: "ID"
+	},
+	{
+		name: "description",
+		label: "Description"
+	},
+	{
+		name: "title",
+		label: "Title",
+	},
+];
+
+const gridConfig: DataViewProps = {
+	columns: listColumns,
+	primaryActions: [
+		{
+			name: "edit",
+			color: "black",
+			variant: "icon",
+			mIcon: CreateIcon,
+			onClick: function ({ data }) {
+				alert(\`EDIT \${data.id}\`);
+			}
+		},
+		{
+			name: "delete",
+			color: "black",
+			variant: "icon",
+			mIcon: DeleteIcon,
+			onClick: function ({ data }) {
+				alert(\`EDIT \${data.id}\`);
+			}
+		}
+	],
+	sticky: true,
+	data: state.data.formMatrix,
+	limit: 25,
+	onReorder: async (newRows) => {
+		const rows = newRows.map(row => state.data.formMatrix.find(element => element.id === row));
+
+		await dispatch(
+			formActions.setFieldValue({
+				name: "formMatrix",
+				value: rows,
+				touched: true
+			})
+		);
+	},
+	display: "list",
+	activeColumns: ["id", "description", "title"],
+};
+
+const addRow = async () => {
+	const mappedData = rawData.slice(1, 4).map((data) => {
+		// convert the date columns to dates, since they are ISOStrings in the file
+		return {
+			...data,
+			created: data.created ? new Date(data.created) : undefined,
+			updated: data.updated ? new Date(data.updated) : undefined
+		}
+	});
+
+	await dispatch(
+		formActions.setFieldValue({
+			name: "formMatrix",
+			value: mappedData,
+			touched: true
+		})
+	);
+};
+
+
+const fields = useMemo(
+	() =>
+		[
+			//...other fields
+			{
+				//...all generic field props,
+				name: "formMatrix",
+				type: "matrix",
+				inputSettings: {
+						dataView: gridConfig,
+						buttons: [
+							{
+								label: "Add",
+								onClick: addRow,
+								color: "teal",
+								variant: "text",
+								mIcon: AddIcon
+							},
+						] as ButtonProps[]
+					},
+			},
+			//...other fields
+		],
+	[gridConfig]
+);
+`})})]})}function Et(i={}){const{wrapper:n}={...o(),...i.components};return n?t.jsx(n,{...i,children:t.jsx(e,{...i})}):e(i)}export{Et as default};
