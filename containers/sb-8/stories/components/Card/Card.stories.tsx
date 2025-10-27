5193afd1462c3cc0a91da3bb37bd0ab861af6727
George Batt <george.batt@simpleviewinc.com>
2025-10-27 11:47:34 +0000

    (Card) Renders placeholder text or customisable React node when there are no items to display.

--- containers/sb-8/stories/components/Card/Card.stories.tsx
+++ containers/sb-8/stories/components/Card/Card.stories.tsx
@@ -4,13 +4,14 @@ import theme from "#mosaic/theme";
 import type { ButtonProps } from "#mosaic/components/Button";
 
 // Components
-import Card from "#mosaic/components/Card";
+import Card, { CardNoItems } from "#mosaic/components/Card";
 import AddIcon from "@mui/icons-material/Add";
 import ContactsIcon from "@mui/icons-material/Contacts";
 import CreateIcon from "@mui/icons-material/Create";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { Text } from "@simpleview/sv-mosaic/components/Typography";
 import { Column } from "@simpleview/sv-mosaic/components/common";
+import Button from "@simpleview/sv-mosaic/components/Button";
 
 export default {
 	title: "Components/Card",
@@ -39,8 +40,10 @@ export const Playground = ({
 	quantityOfTopActions,
 	quantityOfBottomActions,
 	count,
-	showZeroCount,
+	showCount,
 	collapsed,
+	noItemDisplay,
+	noItemDisplayCustom,
 }: typeof Playground.args): ReactElement => {
 	const topActions: ButtonProps[] = [
 		{
@@ -90,14 +93,31 @@ export const Playground = ({
 	return (
 		<div style={{ maxWidth: "100%", width: 400 }}>
 			<Card
-				content={content}
+				content={content.slice(0, count)}
 				title="Section Title"
 				titleIcon={showTitleIcon && ContactsIcon}
 				topActions={topActions.slice(0, quantityOfTopActions)}
 				bottomActions={bottomActions.slice(0, quantityOfBottomActions)}
-				count={count !== undefined && count >= 0 ? count : undefined}
-				showZeroCount={showZeroCount}
+				count={showCount !== "No" ? count : undefined}
+				showZeroCount={showCount !== "Yes, if greater than 0"}
 				collapsed={collapsed}
+				noItems={noItemDisplay === "Default" ? (
+					undefined
+				) : noItemDisplay === "Custom Text" ? (
+					noItemDisplayCustom
+				) : (
+					<CardNoItems>
+						You have no items.
+						{" "}
+						<Button
+							label="Where are my items?"
+							size="inherit"
+							variant="text"
+							intent="info"
+							onClick={() => alert("Create one clicked")}
+						/>
+					</CardNoItems>
+				)}
 			/>
 		</div>
 	);
@@ -108,8 +128,10 @@ Playground.args = {
 	quantityOfTopActions: 1,
 	quantityOfBottomActions: 1,
 	count: 0,
-	showZeroCount: false,
+	showCount: "No",
 	collapsed: false,
+	noItemDisplay: "Default",
+	noItemDisplayCustom: "These aren't the droids you're looking for.",
 };
 
 Playground.argTypes = {
@@ -117,22 +139,32 @@ Playground.argTypes = {
 		name: "Show Title Icon",
 	},
 	quantityOfTopActions: {
-		options: [0, 1, 2, 3],
-		control: { type: "select" },
 		name: "Top Actions",
+		control: { type: "number", min: 0, max: 3 },
 	},
 	quantityOfBottomActions: {
-		options: [0, 1, 2, 3],
-		control: { type: "select" },
 		name: "Bottom Actions",
+		control: { type: "number", min: 0, max: 3 },
 	},
 	count: {
 		name: "Count",
+		control: { type: "number", min: 0, max: 3 },
 	},
-	showZeroCount: {
-		name: "Display Zero Count",
+	showCount: {
+		name: "Show Count",
+		options: ["No", "Yes", "Yes, if greater than 0"],
+		control: { type: "select" },
 	},
 	collapsed: {
 		name: "Collapsed",
+	},
+	noItemDisplay: {
+		name: "No Item Display",
+		options: ["Default", "Custom Text", "React Node"],
+		control: { type: "select" },
+	},
+	noItemDisplayCustom: {
+		name: "Custom No Item Display",
+		if: { arg: "noItemDisplay", eq: "Custom Text" },
 	},
 };
