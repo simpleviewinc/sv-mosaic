f7a6f07bf1729c448ffc9686934129a2ca19309e
Owen Allen <owenallenaz@gmail.com>
2025-11-10 10:07:00 +0000

    Updates

---
+++ sb8/staging/assets/PickerPanel.stories-BtTkh0UJ.js
@@ -0,0 +1,20 @@
+import{j as t}from"./jsx-runtime-EKYJJIwR.js";import{r as v}from"./index-ePW8zFKp.js";import{P as k,a as h,b as P}from"./PickerPanel-BzSJ-e0U.js";import{m as S}from"./options-DEOzrsxm.js";import{C as b}from"./Chip-0jtXmB96.js";import{C}from"./StyledPopperPaper-D2kQT34B.js";import"./identifier-vWKndVvV.js";import"./sizes-1ZdlMWGC.js";import"./createSvgIcon-DOp_2lHr.js";import"./createSvgIcon-JtMyjPpt.js";import"./generateUtilityClasses-CUUJXLh7.js";import"./styled-components.browser.esm-CUKKF2J4.js";import"./ButtonBase-YTDCQqeW.js";import"./useIsFocusVisible-BxLBCNny.js";import"./ownerWindow-DvT1GKkC.js";import"./PageHeader-PoyfzcQN.js";import"./TitleWrapper-sA64N9Qg.js";import"./Button-BUa6dDgD.js";import"./index-Ca1iOyRL.js";import"./Popper-Bt--aTPm.js";import"./useTheme-DTRH4aGG.js";import"./useThemeWithoutDefault-Dj6yTNm1.js";import"./index-DnRBG-o-.js";import"./index-CKydZLen.js";import"./containerQuery-NPnQgBq-.js";import"./ChevronLeft-Bppxtp-L.js";import"./Typography-Bs-xuYxw.js";import"./DisplayText-DlqFHcnF.js";import"./ButtonRow-CKtT1B9L.js";import"./testIds-B6Pox1zA.js";import"./Text-cPxBJcyq.js";import"./FormFieldText.styled-CQLmxjGY.js";import"./MosaicContext-CjWw6F81.js";import"./formControlState-kJY1j3p4.js";import"./Spinner-Bi6mQivf.js";import"./CheckboxList-C0PviF2_.js";import"./Checkbox-DHQwFOa3.js";import"./SwitchBase-BYEDHepN.js";import"./StyledOptionFormControl-Ca00866Q.js";const ae={title:"Components/PickerPanel"},o=({title:a,optionsSubtitle:l,activeSubtitle:p})=>{const[c,s]=v.useState([]),m=e=>{s(u=>u.filter(d=>d.value!==e.value))};return t.jsx("div",{style:{height:"80vh"},children:t.jsx(k,{title:a,optionsSubtitle:l,options:S,onSave:e=>alert(`Save checked: ${JSON.stringify(e)}`),onCancel:()=>alert("Cancel"),activePanel:t.jsxs(h,{children:[p&&t.jsx(P,{children:"Checked Options"}),t.jsx(C,{$align:"start",$gap:[3],children:c.map(e=>t.jsx(b,{label:e.label,onDelete:()=>m(e)},e.value))})]}),onLoadMore:()=>alert("Load more options"),onCreateNew:e=>alert(`Create new option for "${e}"`)})})};o.args={title:"Picker panel",optionsSubtitle:"Available Options",activeSubtitle:"Checked Options"};o.argTypes={title:{name:"Title"},optionsSubtitle:{name:"Options Subtitle"},activeSubtitle:{name:"Active Options Subtitle"}};var i,r,n;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`({
+  title,
+  optionsSubtitle,
+  activeSubtitle
+}: typeof Playground.args): ReactElement => {
+  const [checked, setChecked] = React.useState<MosaicLabelValue[]>([]);
+  const onOptionRemove = (option: MosaicLabelValue) => {
+    setChecked(checked => checked.filter(item => item.value !== option.value));
+  };
+  return <div style={{
+    height: "80vh"
+  }}>
+            <PickerPanel title={title} optionsSubtitle={optionsSubtitle} options={mockOptions} onSave={checked => alert(\`Save checked: \${JSON.stringify(checked)}\`)} onCancel={() => alert("Cancel")} activePanel={<PickerPanelActive>
+                        {activeSubtitle && <PickerPanelSubtitle>Checked Options</PickerPanelSubtitle>}
+                        <Column $align="start" $gap={[3]}>
+                            {checked.map(option => <Chip key={option.value} label={option.label} onDelete={() => onOptionRemove(option)} />)}
+                        </Column>
+                    </PickerPanelActive>} onLoadMore={() => alert("Load more options")} onCreateNew={value => alert(\`Create new option for "\${value}"\`)} />
+        </div>;
+}`,...(n=(r=o.parameters)==null?void 0:r.docs)==null?void 0:n.source}}};const le=["Playground"];export{o as Playground,le as __namedExportsOrder,ae as default};
