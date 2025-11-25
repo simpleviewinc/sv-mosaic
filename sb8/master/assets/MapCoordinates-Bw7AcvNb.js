46f482559567badffffa8e1c32ae96f1881abc35
Owen Allen <owenallenaz@gmail.com>
2025-11-25 10:53:53 +0000

    Updates

---
+++ sb8/master/assets/MapCoordinates-Bw7AcvNb.js
@@ -0,0 +1,45 @@
+import{j as t}from"./jsx-runtime-EKYJJIwR.js";import{useMDXComponents as n}from"./index-DhUyNoj3.js";import{M as o}from"./index-DBcemkIk.js";import{s}from"./MapCoordinates.stories-CpZziMR4.js";import"./index-ePW8zFKp.js";import"./iframe-CTpWM_UQ.js";import"../sb-preview/runtime.js";import"./index-DnRBG-o-.js";import"./index-CKydZLen.js";import"./tslib.es6-CvpyDmub.js";import"./index-Cu4lwwaE.js";import"./extends-CF3RwP-h.js";import"./inheritsLoose-CMy1E8oj.js";import"./toString-CZ_faPKH.js";import"./_baseForOwn-CCkfl5oU.js";import"./index-CGmM_oJM.js";import"./_arrayIncludesWith-XJRwRZaz.js";import"./index-DrFu-skq.js";import"./renderButtons-DP0bGf4C.js";import"./commonFieldControl-LQDYMh-C.js";import"./index-BdxBBtd6.js";import"./ExpandMore-DTgjfsqJ.js";import"./identifier-vWKndVvV.js";import"./sizes-1ZdlMWGC.js";import"./createSvgIcon-DOp_2lHr.js";import"./createSvgIcon-JtMyjPpt.js";import"./generateUtilityClasses-CUUJXLh7.js";import"./styled-components.browser.esm-CUKKF2J4.js";import"./ButtonBase-YTDCQqeW.js";import"./useIsFocusVisible-BxLBCNny.js";import"./ownerWindow-DvT1GKkC.js";import"./MosaicContext-CjWw6F81.js";import"./FormFieldText.styled-CQLmxjGY.js";import"./formControlState-kJY1j3p4.js";import"./Popper-Bt--aTPm.js";import"./useTheme-DTRH4aGG.js";import"./useThemeWithoutDefault-Dj6yTNm1.js";import"./Button-BUa6dDgD.js";import"./index-Ca1iOyRL.js";import"./testIds-B6Pox1zA.js";import"./TitleWrapper-sA64N9Qg.js";import"./containerQuery-NPnQgBq-.js";import"./ChevronLeft-Bppxtp-L.js";import"./Typography-Bs-xuYxw.js";import"./DisplayText-DlqFHcnF.js";import"./ButtonRow-CKtT1B9L.js";import"./DataViewFilterDropdownButtons-_2SqC-T2.js";import"./StyledPopperPaper-D2kQT34B.js";import"./index-3HUVGtNK.js";import"./Chip-0jtXmB96.js";import"./DataViewPrimaryFilter.styled-CJT5o7qD.js";import"./Badge-BWKQiQcj.js";import"./Text-cPxBJcyq.js";import"./PickerPanel-BzSJ-e0U.js";import"./PageHeader-PoyfzcQN.js";import"./Spinner-Bi6mQivf.js";import"./CheckboxList-C0PviF2_.js";import"./Checkbox-DHQwFOa3.js";import"./SwitchBase-BYEDHepN.js";import"./StyledOptionFormControl-Ca00866Q.js";import"./MoreVert-BnBEmsSM.js";import"./stable-BFc8HSVh.js";import"./scrollbarStyles-CVTIwy1f.js";import"./Snackbar-83ELDc2S.js";import"./Close-Da-iw-_d.js";import"./ChipList-Bh3gHNbK.js";import"./RadioButton-C4xgaHsm.js";import"./Toggle-Bi6du1gm.js";import"./Dialog-Bnn_xCj4.js";import"./ThemeProvider-B65WN4y2.js";import"./SideNav-DjAa19Sh.js";import"./CardHeading-vMKjysYd.js";function i(e){const r={a:"a",code:"code",h1:"h1",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...n(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(o,{of:s}),`
+`,t.jsx(r.h1,{id:"map-coordinates",children:"Map Coordinates"}),`
+`,t.jsxs(r.p,{children:["This field implements the ",t.jsx(r.a,{href:"#generic-field-props-fielddef",children:t.jsx(r.strong,{children:"FieldDef"})})," interface."]}),`
+`,t.jsxs(r.ul,{children:[`
+`,t.jsxs(r.li,{children:[t.jsx(r.code,{children:"Map Coordinates"})," This component is helpful when the user needs browse coordinates in a map."]}),`
+`,t.jsx(r.li,{children:t.jsx(r.a,{href:"/?path=/docs/formfields-formfieldmapcoordinates--playground",children:t.jsx(r.strong,{children:"Playground"})})}),`
+`,t.jsxs(r.li,{children:["Data: ",t.jsx(r.code,{children:"object"})," - Object of the value arguments",`
+`,t.jsxs(r.ul,{children:[`
+`,t.jsxs(r.li,{children:[t.jsx(r.strong,{children:"lat"})," - ",t.jsx(r.code,{children:"number"})," - Latitude"]}),`
+`,t.jsxs(r.li,{children:[t.jsx(r.strong,{children:"lng"})," - ",t.jsx(r.code,{children:"number"})," - longitude"]}),`
+`]}),`
+`]}),`
+`,t.jsx(r.li,{children:"inputSettings:"}),`
+`]}),`
+`,t.jsxs(r.table,{children:[t.jsx(r.thead,{children:t.jsxs(r.tr,{children:[t.jsx(r.th,{children:"Name"}),t.jsx(r.th,{children:"Type"}),t.jsx(r.th,{children:"Description"})]})}),t.jsxs(r.tbody,{children:[t.jsxs(r.tr,{children:[t.jsx(r.td,{children:t.jsx(r.strong,{children:t.jsx(r.code,{children:"address"})})}),t.jsx(r.td,{children:t.jsx(r.code,{children:"object"})}),t.jsx(r.td,{children:"Optional - Address object used to set lat and lng values when using the autocoordinates feature (please see Address section)."})]}),t.jsxs(r.tr,{children:[t.jsx(r.td,{children:t.jsx(r.strong,{children:t.jsx(r.code,{children:"googleMapsApiKey"})})}),t.jsx(r.td,{children:t.jsx(r.code,{children:"string"})}),t.jsx(r.td,{children:"Required - Google Maps API key needed to consume the Maps JavaScript API and Places API."})]}),t.jsxs(r.tr,{children:[t.jsx(r.td,{children:t.jsx(r.strong,{children:t.jsx(r.code,{children:"zoom"})})}),t.jsx(r.td,{children:t.jsx(r.code,{children:"number"})}),t.jsx(r.td,{children:"Optional - Zoom applied to the map. If is not defined the map will be all the way zoomed out."})]}),t.jsxs(r.tr,{children:[t.jsx(r.td,{children:t.jsx(r.strong,{children:t.jsx(r.code,{children:"initialCenter"})})}),t.jsx(r.td,{children:t.jsx(r.code,{children:"MapPosition"})}),t.jsx(r.td,{children:"Optional - Latitude and longitude object. If is not defined the map will be positioned at 0,0."})]})]})]}),`
+`,t.jsx(r.h3,{id:"how-to-use-in-a-form",children:"How to use in a form?"}),`
+`,t.jsx(r.pre,{children:t.jsx(r.code,{className:"language-ts",children:`const fields = useMemo(\r
+	() =>\r
+		[\r
+			//...other fields\r
+			{\r
+				//...all generic field props,\r
+				type: "mapCoordinates",\r
+				inputSettings: {\r
+					apiKey: "QWErttyuyi-QWDQWFWEeqwfcQWEwefcwfqwew",\r
+					initialCenter: {\r
+						lat: -3.766925461296721\r
+						lng: -40.54497265625\r
+					},\r
+					address: {\r
+						id: 1,\r
+						address1: "8950 N Oracle Rd",\r
+						city: "Oro Valley",\r
+						postalCode: "85704",\r
+						country: "US",\r
+						state: "AZ",\r
+						types: ["physical", "billing"]\r
+					}\r
+				}\r
+			},\r
+			//...other fields\r
+		],\r
+	[]\r
+);
+`})})]})}function Dt(e={}){const{wrapper:r}={...n(),...e.components};return r?t.jsx(r,{...e,children:t.jsx(i,{...e})}):i(e)}export{Dt as default};
---
+++ sb8/master/assets/SetFormValues.stories-DpHLrRSp.js
@@ -0,0 +1,30 @@
+import{j as l}from"./jsx-runtime-EKYJJIwR.js";import{r as e}from"./index-ePW8zFKp.js";import{u,F as d}from"./index-BdxBBtd6.js";import{r as f}from"./renderButtons-DP0bGf4C.js";import"./ExpandMore-DTgjfsqJ.js";import"./identifier-vWKndVvV.js";import"./sizes-1ZdlMWGC.js";import"./createSvgIcon-DOp_2lHr.js";import"./createSvgIcon-JtMyjPpt.js";import"./generateUtilityClasses-CUUJXLh7.js";import"./styled-components.browser.esm-CUKKF2J4.js";import"./ButtonBase-YTDCQqeW.js";import"./useIsFocusVisible-BxLBCNny.js";import"./ownerWindow-DvT1GKkC.js";import"./MosaicContext-CjWw6F81.js";import"./FormFieldText.styled-CQLmxjGY.js";import"./formControlState-kJY1j3p4.js";import"./Popper-Bt--aTPm.js";import"./useTheme-DTRH4aGG.js";import"./useThemeWithoutDefault-Dj6yTNm1.js";import"./index-DnRBG-o-.js";import"./index-CKydZLen.js";import"./Button-BUa6dDgD.js";import"./index-Ca1iOyRL.js";import"./testIds-B6Pox1zA.js";import"./TitleWrapper-sA64N9Qg.js";import"./containerQuery-NPnQgBq-.js";import"./ChevronLeft-Bppxtp-L.js";import"./Typography-Bs-xuYxw.js";import"./DisplayText-DlqFHcnF.js";import"./ButtonRow-CKtT1B9L.js";import"./DataViewFilterDropdownButtons-_2SqC-T2.js";import"./StyledPopperPaper-D2kQT34B.js";import"./index-3HUVGtNK.js";import"./Chip-0jtXmB96.js";import"./DataViewPrimaryFilter.styled-CJT5o7qD.js";import"./Badge-BWKQiQcj.js";import"./Text-cPxBJcyq.js";import"./PickerPanel-BzSJ-e0U.js";import"./PageHeader-PoyfzcQN.js";import"./Spinner-Bi6mQivf.js";import"./CheckboxList-C0PviF2_.js";import"./Checkbox-DHQwFOa3.js";import"./SwitchBase-BYEDHepN.js";import"./StyledOptionFormControl-Ca00866Q.js";import"./MoreVert-BnBEmsSM.js";import"./stable-BFc8HSVh.js";import"./scrollbarStyles-CVTIwy1f.js";import"./Snackbar-83ELDc2S.js";import"./Close-Da-iw-_d.js";import"./ChipList-Bh3gHNbK.js";import"./RadioButton-C4xgaHsm.js";import"./Toggle-Bi6du1gm.js";import"./Dialog-Bnn_xCj4.js";import"./ThemeProvider-B65WN4y2.js";import"./SideNav-DjAa19Sh.js";import"./CardHeading-vMKjysYd.js";const yt={title:"Components/Form"},t=()=>{const o=u({disabled:!0,skeleton:!0}),{methods:{setFormValues:r},handleSubmit:n}=o,p=e.useMemo(()=>[{name:"firstName",label:"First Name",type:"text"}],[]);return e.useEffect(()=>{(async()=>(await new Promise(a=>setTimeout(()=>a(null),2e3)),r({values:{firstName:"Fluffy"},skeleton:!1,disabled:!1})))()},[r]),l.jsx(d,{...o,buttons:f(n),title:"Profile",description:"Give us some information to understand a little more about you.",fields:p})};t.args={};t.argTypes={};var m,i,s;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`(): ReactElement => {
+  const controller = useForm({
+    disabled: true,
+    skeleton: true
+  });
+  const {
+    methods: {
+      setFormValues
+    },
+    handleSubmit
+  } = controller;
+  const fields = useMemo((): FieldDef[] => [{
+    name: "firstName",
+    label: "First Name",
+    type: "text"
+  }], []);
+  useEffect(() => {
+    (async () => {
+      await new Promise(resolve => setTimeout(() => resolve(null), 2000));
+      setFormValues({
+        values: {
+          firstName: "Fluffy"
+        },
+        skeleton: false,
+        disabled: false
+      });
+    })();
+  }, [setFormValues]);
+  return <Form {...controller} buttons={renderButtons(handleSubmit)} title="Profile" description="Give us some information to understand a little more about you." fields={fields} />;
+}`,...(s=(i=t.parameters)==null?void 0:i.docs)==null?void 0:s.source}}};const xt=["SetFormValues"];export{t as SetFormValues,xt as __namedExportsOrder,yt as default};
